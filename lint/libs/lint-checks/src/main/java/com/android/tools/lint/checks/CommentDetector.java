804a252ca4d3d2b50f98a4db0c5a4dbf3afe0c2a
Tor Norbye <tnorbye@google.com>
2016-04-15 16:56:27 -0700

    207168: StopShip doesn't work in XML
    Also deletes unused method getCompileTarget (it's redundant;
    use getBuildTarget instead.)
    
    Change-Id: I2a69bfdfa5328942bfe123ade7ccb647d2623bfa

--- lint/libs/lint-checks/src/main/java/com/android/tools/lint/checks/CommentDetector.java
+++ lint/libs/lint-checks/src/main/java/com/android/tools/lint/checks/CommentDetector.java
@@ -19,31 +19,36 @@ package com.android.tools.lint.checks;
 import com.android.annotations.NonNull;
 import com.android.annotations.Nullable;
 import com.android.tools.lint.detector.api.Category;
+import com.android.tools.lint.detector.api.Context;
 import com.android.tools.lint.detector.api.Detector;
 import com.android.tools.lint.detector.api.Detector.JavaPsiScanner;
 import com.android.tools.lint.detector.api.Implementation;
 import com.android.tools.lint.detector.api.Issue;
 import com.android.tools.lint.detector.api.JavaContext;
 import com.android.tools.lint.detector.api.Location;
+import com.android.tools.lint.detector.api.ResourceXmlDetector;
 import com.android.tools.lint.detector.api.Scope;
 import com.android.tools.lint.detector.api.Severity;
-import com.intellij.psi.JavaElementVisitor;
-import com.intellij.psi.PsiComment;
+import com.android.tools.lint.detector.api.XmlContext;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiLiteralExpression;
 
-import java.util.Collections;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 import java.util.List;
 
 /**
  * Looks for issues in Java comments
  */
-public class CommentDetector extends Detector implements JavaPsiScanner {
+public class CommentDetector extends ResourceXmlDetector implements JavaPsiScanner {
     private static final String STOPSHIP_COMMENT = "STOPSHIP"; //$NON-NLS-1$
 
     private static final Implementation IMPLEMENTATION = new Implementation(
             CommentDetector.class,
-            Scope.JAVA_FILE_SCOPE);
+            Scope.JAVA_AND_RESOURCE_FILES,
+            Scope.JAVA_FILE_SCOPE,
+            Scope.RESOURCE_FILE_SCOPE);
 
     /** Looks for hidden code */
     public static final Issue EASTER_EGG = Issue.create(
@@ -74,10 +79,6 @@ public class CommentDetector extends Detector implements JavaPsiScanner {
 
     private static final String ESCAPE_STRING = "\\u002a\\u002f"; //$NON-NLS-1$
 
-    /** The current AST only passes comment nodes for Javadoc so I need to do manual token scanning
-         instead */
-    private static final boolean USE_AST = false;
-
 
     /** Constructs a new {@link CommentDetector} check */
     public CommentDetector() {
@@ -85,91 +86,100 @@ public class CommentDetector extends Detector implements JavaPsiScanner {
 
     @Override
     public List<Class<? extends PsiElement>> getApplicablePsiTypes() {
-        if (USE_AST) {
-            return Collections.<Class<? extends PsiElement>>singletonList(
-                    PsiLiteralExpression.class);
-        } else {
-            return null;
+        return null;
+    }
+
+    @Override
+    public void afterCheckFile(@NonNull Context context) {
+        if (context instanceof JavaContext) {
+            checkJava((JavaContext) context);
+        }
+    }
+
+    private static void checkJava(@NonNull JavaContext context) {
+        // Lombok does not generate comment nodes for block and line comments, only for
+        // javadoc comments!
+        String source = context.getContents();
+        if (source == null) {
+            return;
+        }
+        // Process the Java source such that we pass tokens to it
+
+        for (int i = 0, n = source.length() - 1; i < n; i++) {
+            char c = source.charAt(i);
+            if (c == '\\') {
+                i += 1;
+            } else if (c == '/') {
+                char next = source.charAt(i + 1);
+                if (next == '/') {
+                    // Line comment
+                    int start = i + 2;
+                    int end = source.indexOf('\n', start);
+                    if (end == -1) {
+                        end = n;
+                    }
+                    checkComment(context, null, null, source, 0, start, end);
+                } else if (next == '*') {
+                    // Block comment
+                    int start = i + 2;
+                    int end = source.indexOf("*/", start);
+                    if (end == -1) {
+                        end = n;
+                    }
+                    checkComment(context, null, null, source, 0, start, end);
+                }
+            }
         }
     }
 
     @Override
-    public JavaElementVisitor createPsiVisitor(@NonNull JavaContext context) {
-        // Lombok does not generate comment nodes for block and line comments, only for
-        // javadoc comments!
-        if (USE_AST) {
-            return new CommentChecker(context);
-        } else {
-            String source = context.getContents();
-            if (source == null) {
-                return null;
-            }
-            // Process the Java source such that we pass tokens to it
-
-            for (int i = 0, n = source.length() - 1; i < n; i++) {
-                char c = source.charAt(i);
-                if (c == '\\') {
-                    i += 1;
-                } else if (c == '/') {
-                    char next = source.charAt(i + 1);
-                    if (next == '/') {
-                        // Line comment
-                        int start = i + 2;
-                        int end = source.indexOf('\n', start);
-                        if (end == -1) {
-                            end = n;
-                        }
-                        checkComment(context, null, source, 0, start, end);
-                    } else if (next == '*') {
-                        // Block comment
-                        int start = i + 2;
-                        int end = source.indexOf("*/", start);
-                        if (end == -1) {
-                            end = n;
-                        }
-                        checkComment(context, null, source, 0, start, end);
-                    }
-                }
-            }
-            return null;
-        }
+    public void visitDocument(@NonNull XmlContext context, @NonNull Document document) {
+        checkXml(context, document);
     }
 
-    private static class CommentChecker extends JavaElementVisitor {
-        private final JavaContext mContext;
-
-        public CommentChecker(JavaContext context) {
-            mContext = context;
+    private static void checkXml(@NonNull XmlContext context, Node node) {
+        if (node.getNodeType() == Node.COMMENT_NODE) {
+            String source = node.getNodeValue();
+            checkComment(null, context, node, source, 0, 0, source.length());
         }
 
-        @Override
-        public void visitComment(PsiComment comment) {
-            String contents = comment.getText();
-            checkComment(mContext, comment, contents, comment.getTextRange().getStartOffset(), 0,
-                    contents.length());
+        NodeList children = node.getChildNodes();
+        for (int i = 0, n = children.getLength(); i < n; i++) {
+            checkXml(context, children.item(i));
         }
     }
 
     private static void checkComment(
-            @NonNull JavaContext context,
-            @Nullable PsiComment node,
+            @Nullable JavaContext javaContext,
+            @Nullable XmlContext xmlContext,
+            @Nullable Node xmlNode,
             @NonNull String source,
             int offset,
             int start,
             int end) {
+        assert javaContext != null || xmlContext != null;
         char prev = 0;
         char c;
+        Context context = javaContext != null ? javaContext : xmlContext;
         for (int i = start; i < end - 2; i++, prev = c) {
             c = source.charAt(i);
             if (prev == '\\') {
                 if (c == 'u' || c == 'U') {
                     if (source.regionMatches(true, i - 1, ESCAPE_STRING,
                             0, ESCAPE_STRING.length())) {
-                        Location location = Location.create(context.file, source,
-                                offset + i - 1, offset + i - 1 + ESCAPE_STRING.length());
-                        context.report(EASTER_EGG, node, location,
+                        String message =
                                 "Code might be hidden here; found unicode escape sequence " +
-                                "which is interpreted as comment end, compiled code follows");
+                                "which is interpreted as comment end, compiled code follows";
+                        if (javaContext != null) {
+                            Location location = Location.create(context.file, source,
+                                    offset + i - 1, offset + i - 1 + ESCAPE_STRING.length());
+                            javaContext.report(EASTER_EGG, (PsiElement) null, location, message);
+                        } else {
+                            assert xmlNode != null;
+                            Location location = xmlContext.getLocation(xmlNode, i,
+                                    i + ESCAPE_STRING.length());
+                            xmlContext.report(EASTER_EGG, xmlNode, location, message);
+                        }
                     }
                 } else {
                     i++;
@@ -177,11 +187,19 @@ public class CommentDetector extends Detector implements JavaPsiScanner {
             } else if (prev == 'S' && c == 'T' &&
                     source.regionMatches(i - 1, STOPSHIP_COMMENT, 0, STOPSHIP_COMMENT.length())) {
                 // TODO: Only flag this issue in release mode??
-                Location location = Location.create(context.file, source,
-                        offset + i - 1, offset + i - 1 + STOPSHIP_COMMENT.length());
-                context.report(STOP_SHIP, node, location,
+                String message =
                         "`STOPSHIP` comment found; points to code which must be fixed prior " +
-                        "to release");
+                        "to release";
+                if (javaContext != null) {
+                    Location location = Location.create(context.file, source,
+                            offset + i - 1, offset + i - 1 + STOPSHIP_COMMENT.length());
+                    javaContext.report(STOP_SHIP, (PsiElement) null, location, message);
+                } else {
+                    assert xmlNode != null;
+                    Location location = xmlContext.getLocation(xmlNode, i,
+                            i + STOPSHIP_COMMENT.length());
+                    xmlContext.report(STOP_SHIP, xmlNode, location, message);
+                }
             }
         }
     }
