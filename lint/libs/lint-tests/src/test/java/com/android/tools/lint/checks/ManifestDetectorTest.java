3d25da9eef9c601a5a383e34710c155a319bcfe1
Nagesh Susarla <nageshs@google.com>
2016-04-18 12:07:07 -0700

    Remove usage of deprecated methods in ManifestDetector
    Change-Id: Ie5ea30f9e3e3496e4079aa9af7e38be604a50e3b

--- lint/libs/lint-tests/src/test/java/com/android/tools/lint/checks/ManifestDetectorTest.java
+++ lint/libs/lint-tests/src/test/java/com/android/tools/lint/checks/ManifestDetectorTest.java
@@ -1263,6 +1263,8 @@ public class ManifestDetectorTest extends AbstractCheckTest {
                             when(dependencies.getLibraries())
                                     .thenReturn(Collections.singleton(library));
                             AndroidArtifact artifact = mock(AndroidArtifact.class);
+                            when(artifact.getCompileDependencies()).thenReturn(dependencies);
+                            //noinspection deprecation
                             when(artifact.getDependencies()).thenReturn(dependencies);
                             Variant mockVariant = mock(Variant.class);
                             when(mockVariant.getMainArtifact()).thenReturn(artifact);
