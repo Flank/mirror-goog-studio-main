a41ff6fcc59f7006ba721be3699b813df6282fb8
Chris Warrington <cmw@google.com>
2016-06-22 15:34:58 +0100

    Convert basic tests to java.
    Change-Id: I81529d06e328f6ee2ee4f9cb7501bef6228d6302

--- build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/application/BasicTest.java
+++ build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/application/BasicTest.java
@@ -14,80 +14,84 @@
  * limitations under the License.
  */
 
-package com.android.build.gradle.integration.application
-import com.android.build.gradle.integration.common.category.DeviceTests
-import com.android.build.gradle.integration.common.category.SmokeTests
-import com.android.build.gradle.integration.common.fixture.Adb
-import com.android.build.gradle.integration.common.fixture.GradleTestProject
-import com.android.build.gradle.integration.common.utils.ModelHelper
-import com.android.builder.model.AndroidArtifact
-import com.android.builder.model.AndroidProject
-import com.android.builder.model.JavaCompileOptions
-import com.android.builder.model.Variant
-import groovy.transform.CompileStatic
-import org.gradle.api.JavaVersion
-import org.junit.AfterClass
-import org.junit.BeforeClass
-import org.junit.ClassRule
-import org.junit.Rule
-import org.junit.Test
-import org.junit.experimental.categories.Category
+package com.android.build.gradle.integration.application;
 
-import static com.google.common.truth.Truth.assertThat
-import static org.junit.Assert.assertEquals
-import static org.junit.Assert.assertFalse
-import static org.junit.Assert.assertNotNull
-import static org.junit.Assert.assertNull
-import static org.junit.Assert.assertTrue
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import com.android.build.gradle.integration.common.category.DeviceTests;
+import com.android.build.gradle.integration.common.category.SmokeTests;
+import com.android.build.gradle.integration.common.fixture.Adb;
+import com.android.build.gradle.integration.common.fixture.GradleTestProject;
+import com.android.build.gradle.integration.common.utils.ModelHelper;
+import com.android.builder.model.AndroidArtifact;
+import com.android.builder.model.AndroidProject;
+import com.android.builder.model.JavaCompileOptions;
+import com.android.builder.model.Variant;
+
+import org.gradle.api.JavaVersion;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import java.io.IOException;
+
 /**
  * Assemble tests for basic.
  */
 @Category(SmokeTests.class)
-@CompileStatic
-class BasicTest {
+public class BasicTest {
     @ClassRule
-    static public GradleTestProject project = GradleTestProject.builder()
+    public static GradleTestProject project = GradleTestProject.builder()
             .fromTestProject("basic")
             .withoutNdk()
-            .create()
+            .create();
 
     @Rule
     public Adb adb = new Adb();
 
-    static public AndroidProject model
+    public static AndroidProject model;
 
     @BeforeClass
-    static void setUp() {
-        model = project.executeAndReturnModel("clean", "assembleDebug")
+    public static void getModel() {
+        model = project.executeAndReturnModel("clean", "assembleDebug");
     }
 
     @AfterClass
-    static void cleanUp() {
-        project = null
-        model = null
+    public static void cleanUp() {
+        project = null;
+        model = null;
     }
 
     @Test
-    void report() {
-        project.execute("androidDependencies")
+    public void report() {
+        project.execute("androidDependencies");
     }
 
     @Test
-    void basicModel() {
-        assertFalse("Library Project", model.isLibrary())
-        assertEquals("Compile Target", "android-23", model.getCompileTarget())
-        assertFalse("Non empty bootclasspath", model.getBootClasspath().isEmpty())
+    public void basicModel() {
+        assertFalse("Library Project", model.isLibrary());
+        assertEquals("Compile Target", "android-23", model.getCompileTarget());
+        assertFalse("Non empty bootclasspath", model.getBootClasspath().isEmpty());
 
-        assertNotNull("aaptOptions not null", model.getAaptOptions())
-        assertEquals("aaptOptions noCompress", 1, model.getAaptOptions().getNoCompress().size())
-        assertTrue("aaptOptions noCompress", model.getAaptOptions().getNoCompress().contains("txt"))
+        assertNotNull("aaptOptions not null", model.getAaptOptions());
+        assertEquals("aaptOptions noCompress", 1, model.getAaptOptions().getNoCompress().size());
+        assertTrue("aaptOptions noCompress",
+                model.getAaptOptions().getNoCompress().contains("txt"));
         assertEquals(
                 "aaptOptions ignoreAssetsPattern",
                 "!.svn:!.git:!.ds_store:!*.scc:.*:<dir>_*:!CVS:!thumbs.db:!picasa.ini:!*~",
-                model.getAaptOptions().getIgnoreAssets())
+                model.getAaptOptions().getIgnoreAssets());
         assertFalse(
                 "aaptOptions getFailOnMissingConfigEntry",
-                model.getAaptOptions().getFailOnMissingConfigEntry())
+                model.getAaptOptions().getFailOnMissingConfigEntry());
 
         // Since source and target compatibility are not explicitly set in the build.gradle,
         // the default value depends on the JDK used.
@@ -98,56 +102,56 @@ class BasicTest {
             expected = JavaVersion.VERSION_1_6;
         }
 
-        JavaCompileOptions javaCompileOptions = model.getJavaCompileOptions()
+        JavaCompileOptions javaCompileOptions = model.getJavaCompileOptions();
         assertEquals(
                 expected.toString(),
-                javaCompileOptions.getSourceCompatibility())
+                javaCompileOptions.getSourceCompatibility());
         assertEquals(
                 expected.toString(),
-                javaCompileOptions.getTargetCompatibility())
-        assertEquals("UTF-8", javaCompileOptions.getEncoding())
+                javaCompileOptions.getTargetCompatibility());
+        assertEquals("UTF-8", javaCompileOptions.getEncoding());
     }
 
     @Test
     public void sourceProvidersModel() {
-        ModelHelper.testDefaultSourceSets(model, project.getTestDir())
+        ModelHelper.testDefaultSourceSets(model, project.getTestDir());
 
         // test the source provider for the artifacts
         for (Variant variant : model.getVariants()) {
-            AndroidArtifact artifact = variant.getMainArtifact()
-            assertNull(artifact.getVariantSourceProvider())
-            assertNull(artifact.getMultiFlavorSourceProvider())
+            AndroidArtifact artifact = variant.getMainArtifact();
+            assertNull(artifact.getVariantSourceProvider());
+            assertNull(artifact.getMultiFlavorSourceProvider());
         }
     }
 
     @Test
-    void "check debug and release output have different names"() {
-        ModelHelper.compareDebugAndReleaseOutput(model)
+    public void checkDebugAndReleaseOutputHaveDifferentNames() {
+        ModelHelper.compareDebugAndReleaseOutput(model);
     }
 
     @Test
-    void "we don't fail on LICENSE.txt when packaging dependencies"() {
-        project.execute("assembleAndroidTest")
+    public void weDontFailOnLicenceDotTxtWhenPackagingDependencies() {
+        project.execute("assembleAndroidTest");
     }
 
     @Test
-    void generationInModel() {
-        AndroidProject model = project.model().getSingle()
+    public void generationInModel() {
+        AndroidProject model = project.model().getSingle();
         assertThat(model.getPluginGeneration())
                 .named("Plugin Generation")
-                .isEqualTo(AndroidProject.GENERATION_ORIGINAL)
+                .isEqualTo(AndroidProject.GENERATION_ORIGINAL);
     }
 
     @Test
     @Category(DeviceTests.class)
-    void install() {
-        adb.exclusiveAccess()
-        project.execute("installDebug", "uninstallAll")
+    public void install() throws IOException {
+        adb.exclusiveAccess();
+        project.execute("installDebug", "uninstallAll");
     }
 
     @Test
     @Category(DeviceTests.class)
-    void connectedCheck() {
-        project.executeConnectedCheck()
+    public void connectedCheck() {
+        project.executeConnectedCheck();
     }
 }
