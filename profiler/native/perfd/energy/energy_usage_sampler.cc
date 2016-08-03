e3045291c3a16d7ac8aae38816f186b3a7065138
genli <genli@google.com>
2016-08-02 17:58:04 -0700

    Add cell and wifi energy components to perfd sampling.
    Change-Id: I6ca7a5e1e70806bf52f53179b6fe64d5c5e8d2af

--- profiler/native/perfd/energy/energy_usage_sampler.cc
+++ profiler/native/perfd/energy/energy_usage_sampler.cc
@@ -21,6 +21,9 @@
 
 #include "utils/tokenizer.h"
 
+using std::istringstream;
+using std::string;
+
 // TODO required work:
 // - Report actual power usage instead of component time count (power_profiles)
 // - Add rest of the required stats to the sample
@@ -30,17 +33,28 @@ static constexpr const char* kDumpsysBatterystatsCommand =
 static constexpr const int kLineBufferSize = 1024;
 
 // TODO remove this when std::stoi is available.
-static const int32_t temp_stoi(const std::string& s) {
-  std::istringstream stream(s);
+// Tries to convert given string to it's represented int value.
+// Returns 0 if it fails.
+static const int32_t temp_stoi(const string& s) {
+  istringstream stream(s);
   int32_t i;
-  stream >> i;
+  if (!(stream >> i)) {  // If conversion fails.
+    i = 0;
+  }
   return i;
 }
+
+// Gets the next token and try to convert it to int before returning it.
+// Returns 0 if there is no next token or next token is not an int.
+static const int32_t getNextInteger(profiler::Tokenizer* tokenizer) {
+  string token;
+  tokenizer->GetNextToken(&token);
+  return temp_stoi(token);
+}
+
 }  // namespace
 
 namespace profiler {
-
-using std::string;
 
 const bool EnergyUsageSampler::VerifyRequiredHeading(
     Tokenizer* tokenizer, const int32_t required_uid) {
@@ -52,8 +66,7 @@ const bool EnergyUsageSampler::VerifyRequiredHeading(
 }
 
 const void EnergyUsageSampler::ParseStatTokens(
-    Tokenizer* tokenizer,
-    proto::EnergyDataResponse_EnergySample* sample) {
+    Tokenizer* tokenizer, proto::EnergyDataResponse_EnergySample* sample) {
   std::string category;
   tokenizer->GetNextToken(&category);
 
@@ -65,22 +78,65 @@ const void EnergyUsageSampler::ParseStatTokens(
   //    line from dumpsys:  9,10087,l,cpu,223989,107626,3988814
   //    starting cursor: ~~~~~~~~~~~~~~~~~^
   if (category.compare("cpu") == 0) {
+    // CPU time from CPU (cpu) stat category.
     // Format: user-cpu-time-ms, system-cpu-time-ms, total-cpu-power-mAus
-    string user_time_token;
-    string system_time_token;
-    tokenizer->GetNextToken(&user_time_token);
-    tokenizer->GetNextToken(&system_time_token);
-    int32_t user_time = temp_stoi(user_time_token);
-    int32_t system_time = temp_stoi(system_time_token);
-    sample->set_cpu_user_power_usage(user_time);
-    sample->set_cpu_system_power_usage(system_time);
+    sample->set_cpu_user_power_usage(getNextInteger(tokenizer));
+    sample->set_cpu_system_power_usage(getNextInteger(tokenizer));
 
   } else if (category.compare("fg") == 0) {
-    // Format: total-time, start-count
-    string foreground_time_token;
-    tokenizer->GetNextToken(&foreground_time_token);
-    int32_t foreground_time = temp_stoi(foreground_time_token);
-    sample->set_screen_power_usage(foreground_time);
+    // App screen-on time from foreground (fg) category.
+    // Format: total-time-ms, start-count
+    sample->set_screen_power_usage(getNextInteger(tokenizer));
+
+  } else if (category.compare("wfcd") == 0) {
+    // Wifi usage time from wifi-controller-data (wfcd) category.
+    // Format: wifi-idle-time-ms, wifi-rx-time-ms,
+    //         wifi-power-counter (unreliable), {wifi-tx-time-ms}+
+    int32_t wifi_usage_ms = 0;
+    tokenizer->EatNextToken();
+    wifi_usage_ms += getNextInteger(tokenizer);  // rx time.
+    tokenizer->EatNextToken();
+
+    string wifi_tx_times;
+    while (tokenizer->GetNextToken(&wifi_tx_times)) {
+      // The reason why this loop exists is because android has many Tx
+      // power-level buckets, and depending on factors such as signal strength
+      // the system may decide to use high-power mode to boost Tx signal
+      // strength. This is not the case for Rx because Rx has only one.
+      //
+      // Currently we simply return the sum of the time spent in these buckets,
+      // which is not an accurate representation of the amount of power used.
+      // When power_profiles.xml becomes available, these should be multiplied
+      // to the proper bucket power values to accurately represent power usage.
+      wifi_usage_ms += temp_stoi(wifi_tx_times);
+    }
+
+    sample->set_wifi_network_power_usage(wifi_usage_ms);
+
+  } else if (category.compare("mcd") == 0) {
+    // Modem (radio) usage time from modem-controller-data (mcd) category.
+    // Format: modem-idle-time-ms, modem-rx-time-ms,
+    //         modem-power-counter (unreliable), {modem-tx-time-ms}+
+    int32_t modem_usage_ms = 0;
+    tokenizer->EatNextToken();
+    modem_usage_ms += getNextInteger(tokenizer);  // rx time.
+    tokenizer->EatNextToken();
+
+    string modem_tx_times;
+    while (tokenizer->GetNextToken(&modem_tx_times)) {
+      // The reason why this loop exists is because android has many Tx
+      // power-level buckets, and depending on factors such as signal strength
+      // the system may decide to use high-power mode to boost Tx signal
+      // strength. This is not the case for Rx because Rx has only one.
+      //
+      // Currently we simply return the sum of the time spent in these buckets,
+      // which is not an accurate representation of the amount of power used.
+      // When power_profiles.xml becomes available, these should be multiplied
+      // to the proper bucket power values to accurately represent power usage.
+      modem_usage_ms += temp_stoi(modem_tx_times);
+    }
+
+    sample->set_cell_network_power_usage(modem_usage_ms);
   }
 }
 
