f824d7f139b8ea81dd8910553949f4965754276a
Joe Baker-Malone <jbakermalone@google.com>
2016-08-09 14:07:55 -0700

    Updating the constraintlayout lint check
    It seems like there was some cruft left over from when the
    constraintlayout packages were in the offline maven repo rather than the
    offline SDK. Removed them.
    Also updated the lint check to look for the most recent available
    package, including remotely.
    Part of the fix for b.android.com/219556
    
    Change-Id: Id1b4c8503a4a2cb3762040ff8018fb18e6143c2f

--- sdklib/src/main/java/com/android/sdklib/repository/meta/DetailsTypes.java
+++ sdklib/src/main/java/com/android/sdklib/repository/meta/DetailsTypes.java
@@ -244,18 +244,22 @@ public final class DetailsTypes {
          *
          * @param groupId    The maven groupId
          * @param artifactId The maven artifactId
-         * @param version    The maven version
+         * @param version    The maven version. If {@code null} the final component of the path
+         *                   (the version component) will be omitted.
          * @return The {@link RepoPackage#getPath() path} for the corresponding {@link RepoPackage}.
          */
         static String getRepositoryPath(
-                @NonNull String groupId, @NonNull String artifactId, @NonNull String version) {
-            return String.join(
+                @NonNull String groupId, @NonNull String artifactId, @Nullable String version) {
+            String result = String.join(
                     String.valueOf(RepoPackage.PATH_SEPARATOR),
                     SdkConstants.FD_EXTRAS,
                     SdkConstants.FD_M2_REPOSITORY,
                     groupId.replace('.', RepoPackage.PATH_SEPARATOR),
-                    artifactId,
-                    version);
+                    artifactId);
+            if (version != null) {
+                result += RepoPackage.PATH_SEPARATOR + version;
+            }
+            return result;
         }
     }
 
