2a372f4a5b87961934f0a53607575691e50d3055
Tor Norbye <tnorbye@google.com>
2016-08-24 12:32:14 -0700

    Fix conditionalApi test project
    Change-Id: I796f55128b61c5efae818e377c97c8b43901d15d

--- build-system/integration-test/test-projects/conditionalApiUse/src/main/java/com/android/tests/conditionalApiUse/MyException.java
+++ build-system/integration-test/test-projects/conditionalApiUse/src/main/java/com/android/tests/conditionalApiUse/MyException.java
@@ -2,7 +2,9 @@ package com.android.tests.conditionalApiUse;
 
 import android.hardware.camera2.CameraAccessException;
 import android.os.Build;
+import android.support.annotation.RequiresApi;
 
+@RequiresApi(21)
 public class MyException extends CameraAccessException {
 
     public MyException(int problem, String message, Throwable cause) {
