638ed743e3d811522186ed7d34cd1964d248d266
Tamas Berghammer <tberghammer@google.com>
2016-09-05 14:41:07 +0100

    Introduce new clang-format lint task
    With this CL the lintHostRelease task will verify that all files are
    following the Google code formatting standard the way it is generated
    by clang format.
    
    Change-Id: I1741af337361188b88c366f501ec9c954b288479

--- profiler/native/cmake/toolchains/linux-host.cmake
+++ profiler/native/cmake/toolchains/linux-host.cmake
@@ -11,4 +11,6 @@ set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${COMMON_FLAGS}" CACHE STRING "C flags")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${COMMON_FLAGS} -std=c++11 -stdlib=libstdc++ -I${STL}/x86_64-linux/include/c++/4.8/ -I${STL}/x86_64-linux/include/c++/4.8/x86_64-linux/" CACHE STRING "CXX flags")
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${LINKER_FLAGS}" CACHE STRING "Executable Library linker flags")
 set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} ${LINKER_FLAGS}" CACHE STRING "Shared Library linker flags")
-set(CLANG_TIDY_PATH "${PREBUILTS}/clang/host/linux-x86/clang-stable/bin/clang-tidy")
+
+set(CLANG_FORMAT_PATH "${PREBUILTS}/clang/host/linux-x86/clang-stable/bin/clang-format")
+set(CLANG_TIDY_PATH   "${PREBUILTS}/clang/host/linux-x86/clang-stable/bin/clang-tidy")
