a3c7febb79288cbd725f910a8f8e87288ba5c38f
Dana Dahlstrom <dahlstrom@google.com>
2016-11-21 15:47:54 -0800

    Move rules from tools to tools/idea subpackages
    Change-Id: I1b9ba0e3bcf28aac77d736a29601780dc4131af3

--- bazel/plugin/src/com/android/tools/bazel/Configuration.java
+++ bazel/plugin/src/com/android/tools/bazel/Configuration.java
@@ -20,7 +20,7 @@ import java.util.List;
 import java.util.Map;
 
 interface Configuration {
-    String nameRule(String rel, String name);
+    String nameRule(String pkg, String rel, String name);
 
     String mapImportJar(String jar);
 
