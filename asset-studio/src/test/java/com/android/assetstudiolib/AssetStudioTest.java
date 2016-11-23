8046f99e5ff0852db1750566094db6954f2b193e
Juan C Nuno <juancnuno@google.com>
2016-11-22 16:10:00 -0800

    Delete the duplicate ic_rv_hookup_black_24dp.xml
    Test: AssetStudioTest.getPathForBasename
    Change-Id: I22237b0b5b9e59b6674776e07ca65abe8bc1544f

--- asset-studio/src/test/java/com/android/assetstudiolib/AssetStudioTest.java
+++ asset-studio/src/test/java/com/android/assetstudiolib/AssetStudioTest.java
@@ -15,37 +15,25 @@
  */
 package com.android.assetstudiolib;
 
-import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-import com.android.assetstudiolib.AssetStudio.Generator;
-
-import org.junit.Before;
-import org.junit.Rule;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.function.Function;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.mockito.Matchers;
 import org.mockito.Mockito;
 
-import java.util.Collections;
-
 @RunWith(JUnit4.class)
 public final class AssetStudioTest {
 
-    @Rule public final ExpectedException thrown = ExpectedException.none();
-
-    private Generator mGenerator;
-
-    @Before
-    public void mockGenerator() {
-        mGenerator = Mockito.mock(Generator.class);
-
-        Mockito.when(mGenerator.getResourceNames(Matchers.any()))
-                .thenReturn(Collections.emptyIterator());
-
-        Mockito.when(mGenerator.getResourceNames("images/material_design_icons/action/"))
-                .thenReturn(Collections.singletonList("ic_search_black_24dp.xml").iterator());
+    @Test
+    public void getPathForBasename() {
+        Object expected = "images/material_design_icons/places/ic_rv_hookup_black_24dp.xml";
+        assertEquals(expected, AssetStudio.getPathForBasename("ic_rv_hookup_black_24dp"));
     }
 
     @Test
@@ -54,15 +42,33 @@ public final class AssetStudioTest {
                 "ic_search_black_24dp",
                 "images/material_design_icons/action/ic_search_black_24dp.xml");
 
-        assertThat(AssetStudio.getBasenameToPathMap(mGenerator)).isEqualTo(expected);
+        assertEquals(expected, AssetStudio.getBasenameToPathMap(mockGenerator()));
     }
 
     @Test
     public void getBasenameToPathMapThrowsIllegalArgumentException() {
-        Mockito.when(mGenerator.getResourceNames("images/material_design_icons/device/"))
+        Function<String, Iterator<String>> generator = mockGenerator();
+
+        Mockito.when(generator.apply("images/material_design_icons/device/"))
                 .thenReturn(Collections.singletonList("ic_search_black_24dp.xml").iterator());
 
-        thrown.expect(IllegalArgumentException.class);
-        AssetStudio.getBasenameToPathMap(mGenerator);
+        try {
+            AssetStudio.getBasenameToPathMap(generator);
+            fail();
+        } catch (IllegalArgumentException ignored) {
+        }
+    }
+
+    private static Function<String, Iterator<String>> mockGenerator() {
+        @SuppressWarnings("unchecked") Function<String, Iterator<String>> generator
+                = (Function<String, Iterator<String>>) Mockito.mock(Function.class);
+
+        Mockito.when(generator.apply(Matchers.any()))
+                .thenReturn(Collections.emptyIterator());
+
+        Mockito.when(generator.apply("images/material_design_icons/action/"))
+                .thenReturn(Collections.singletonList("ic_search_black_24dp.xml").iterator());
+
+        return generator;
     }
 }
