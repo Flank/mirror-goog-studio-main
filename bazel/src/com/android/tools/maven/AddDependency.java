3177f97a468f75618b7142bbc98eb71dc493c6b6
Michal Bendowski <bendowski@google.com>
2016-11-25 13:29:13 +0000

    Fixes to AddDependency, to support AARs
    Test: Manually ran for an AAR for now.
    Change-Id: I99830d1094eb54105df7eef332a554bd2c73c1af

--- bazel/src/com/android/tools/maven/AddDependency.java
+++ bazel/src/com/android/tools/maven/AddDependency.java
@@ -17,23 +17,24 @@
 package com.android.tools.maven;
 
 import com.android.tools.utils.WorkspaceUtils;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
-
-import org.eclipse.aether.artifact.DefaultArtifact;
-import org.eclipse.aether.collection.CollectRequest;
-import org.eclipse.aether.graph.Dependency;
-import org.eclipse.aether.resolution.ArtifactResult;
-import org.eclipse.aether.resolution.DependencyRequest;
-import org.eclipse.aether.resolution.DependencyResolutionException;
-import org.eclipse.aether.resolution.DependencyResult;
-import org.eclipse.aether.util.artifact.JavaScopes;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.collection.CollectRequest;
+import org.eclipse.aether.graph.Dependency;
+import org.eclipse.aether.graph.Exclusion;
+import org.eclipse.aether.resolution.ArtifactResult;
+import org.eclipse.aether.resolution.DependencyRequest;
+import org.eclipse.aether.resolution.DependencyResolutionException;
+import org.eclipse.aether.resolution.DependencyResult;
+import org.eclipse.aether.util.artifact.JavaScopes;
+import org.eclipse.aether.util.graph.selector.ExclusionDependencySelector;
 
 /**
  * Command line tool to download a new Maven artifact to prebuilts, together with all the transitive
@@ -42,6 +43,14 @@ import java.util.stream.Collectors;
  * <p>It also creates {@code java_import} Bazel rules for newly created jars.
  */
 public class AddDependency {
+
+    /** Packages that are distributed with the SDK, we won't be able to find them in JCenter. */
+    static final ImmutableList<Exclusion> EXCLUSIONS =
+            ImmutableList.of(
+                    new Exclusion("com.android.support", "*", "*", "*"),
+                    new Exclusion("com.android.support.constraint", "*", "*", "*"),
+                    new Exclusion("com.android.support.test", "*", "*", "*"),
+                    new Exclusion("com.google.android.gms", "*", "*", "*"));
 
     public static void main(String[] argsArray) throws Exception {
         List<String> args = Lists.newArrayList(argsArray);
@@ -78,6 +87,9 @@ public class AddDependency {
                         .map(artifact -> new Dependency(artifact, JavaScopes.COMPILE))
                         .collect(Collectors.toList()));
         request.setRepositories(AetherUtils.REPOSITORIES);
+
+        mRepo.getmRepositorySystemSession()
+                .setDependencySelector(new ExclusionDependencySelector(EXCLUSIONS));
 
         DependencyResult result = mRepo.resolveDependencies(new DependencyRequest(request, null));
 
