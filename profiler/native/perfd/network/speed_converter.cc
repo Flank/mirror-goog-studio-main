2910ab14222e0ed6cd6e7f017ef7b810dcca44e0
Yuxin Yu <yuxy@google.com>
2016-11-28 11:23:16 -0800

    Fix perfd traffic bytes too small because time unit should be ns.
    Test: Unittest.
    Change-Id: I018e1acbd3ec6013cf717bc69cef68b95473a9ee

--- profiler/native/perfd/network/speed_converter.cc
+++ profiler/native/perfd/network/speed_converter.cc
@@ -19,16 +19,16 @@
 
 namespace profiler {
 
-void SpeedConverter::Add(int64_t timestamp_us, int64_t bytes) {
-  if (timestamp_us <= last_timestamp_us_ || bytes < last_bytes_) {
+void SpeedConverter::Add(int64_t timestamp_ns, int64_t bytes) {
+  if (timestamp_ns <= last_timestamp_ns_ || bytes < last_bytes_) {
     return; // Ignore as this is invalid input and should never happen
   }
 
   int64_t delta_bytes = bytes - last_bytes_;
-  Convert(last_timestamp_us_, timestamp_us, speed_, delta_bytes,
-    &speed_, &speed_time_us_);
+  Convert(last_timestamp_ns_, timestamp_ns, speed_, delta_bytes,
+    &speed_, &speed_time_ns_);
 
-  last_timestamp_us_ = timestamp_us;
+  last_timestamp_ns_ = timestamp_ns;
   last_bytes_ = bytes;
 }
 
@@ -46,8 +46,8 @@ void SpeedConverter::Add(int64_t timestamp_us, int64_t bytes) {
 // where the height at each time represents a speed value that makes sense of
 // the current bytes level (keeping in mind that the area under the curve
 // represents bytes transferred).
-void SpeedConverter::Convert(int64_t prev_time_us, int64_t curr_time_us,
-  int64_t prev_speed, int64_t bytes, int64_t* speed, int64_t* speed_time_us) {
+void SpeedConverter::Convert(int64_t prev_time_ns, int64_t curr_time_ns,
+  int64_t prev_speed, int64_t bytes, int64_t* speed, int64_t* speed_time_ns) {
 
   // To visualize what's happening here:
   //
@@ -60,15 +60,15 @@ void SpeedConverter::Convert(int64_t prev_time_us, int64_t curr_time_us,
   //
   // Since "A = 1/2(h₀ + h₁)*Δt" (where A is num bytes)
   // we can solve "h₁ = 2*bytes/Δt - h₀"
-  int64_t delta_time_us = curr_time_us - prev_time_us;
-  double prev_speed_us = (double)prev_speed / Clock::s_to_us(1);
+  int64_t delta_time_ns = curr_time_ns - prev_time_ns;
+  double prev_speed_ns = (double)prev_speed / Clock::s_to_ns(1);
 
-  // bytes per µs
-  double next_speed_us = (2.0 * bytes / delta_time_us - prev_speed_us);
+  // bytes per ns
+  double next_speed_ns = (2.0 * bytes / delta_time_ns - prev_speed_ns);
 
-  // bytes / µs * (µs / sec) = bytes / sec
-  *speed = (int64_t)(next_speed_us * Clock::s_to_us(1));
-  *speed_time_us = curr_time_us;
+  // bytes per ns * (sec / ns) = bytes / sec
+  *speed = (int64_t)(next_speed_ns * Clock::s_to_ns(1));
+  *speed_time_ns = curr_time_ns;
 
   if (*speed < 0) {
     // Special case - |bytes| is so small, that we need to drop our speed to 0
@@ -84,7 +84,7 @@ void SpeedConverter::Convert(int64_t prev_time_us, int64_t curr_time_us,
     // Since "A = 1/2(t?-t₀)h",
     // we can solve "t? = 2*A/h + t₀""
     *speed = 0;
-    *speed_time_us = (int64_t)(2.0 * bytes / prev_speed_us + prev_time_us);
+    *speed_time_ns = (int64_t)(2.0 * bytes / prev_speed_ns + prev_time_ns);
   }
 }
 
--- profiler/native/perfd/network/speed_converter_test.cc
+++ profiler/native/perfd/network/speed_converter_test.cc
@@ -25,7 +25,7 @@ TEST(SpeedConverter, NoDataAddedReturnsNoSpeed) {
   SpeedConverter converter(12345, 1000);
 
   EXPECT_EQ(0, converter.speed());
-  EXPECT_EQ(12345, converter.speed_time_us());
+  EXPECT_EQ(12345, converter.speed_time_ns());
 }
 
 TEST(SpeedConverter, OneAddProducesExpectedSpeed) {
@@ -33,33 +33,33 @@ TEST(SpeedConverter, OneAddProducesExpectedSpeed) {
 
   // A final speed of 2K / sec, starting from 0K / sec, will allow us to
   // download 1K of bytes after 1 sec
-  converter.Add(Clock::s_to_us(1), 1024);
+  converter.Add(Clock::s_to_ns(1), 1024);
   EXPECT_EQ(2048, converter.speed());
-  EXPECT_EQ(Clock::s_to_us(1), converter.speed_time_us());
+  EXPECT_EQ(Clock::s_to_ns(1), converter.speed_time_ns());
 }
 
 TEST(SpeedConverter, TwoAddsProducesExpectedSpeed) {
   SpeedConverter converter(0, 0);
-  converter.Add(Clock::s_to_us(1), 1024); // Final speek, 2K / sec
-  converter.Add(Clock::s_to_us(2), 1024 + 2048); // Maintain 2K / sec
+  converter.Add(Clock::s_to_ns(1), 1024); // Final speek, 2K / sec
+  converter.Add(Clock::s_to_ns(2), 1024 + 2048); // Maintain 2K / sec
 
   EXPECT_EQ(2048, converter.speed());
-  EXPECT_EQ(Clock::s_to_us(2), converter.speed_time_us());
+  EXPECT_EQ(Clock::s_to_ns(2), converter.speed_time_ns());
 }
 
 TEST(SpeedConverter, SpeedCanDropToZero) {
   SpeedConverter converter(0, 0);
-  converter.Add(Clock::s_to_us(1), 1024);
+  converter.Add(Clock::s_to_ns(1), 1024);
   // Only 100 bytes left to transfer, so speed will drop to 0
-  converter.Add(Clock::s_to_us(2), 1024 + 400);
+  converter.Add(Clock::s_to_ns(2), 1024 + 400);
 
   EXPECT_EQ(0, converter.speed());
-  EXPECT_LT(Clock::s_to_us(1), converter.speed_time_us());
-  EXPECT_GT(Clock::s_to_us(2), converter.speed_time_us());
+  EXPECT_LT(Clock::s_to_ns(1), converter.speed_time_ns());
+  EXPECT_GT(Clock::s_to_ns(2), converter.speed_time_ns());
 
   // Speed stays at 0...
-  converter.Add(Clock::s_to_us(3), 1024 + 400);
+  converter.Add(Clock::s_to_ns(3), 1024 + 400);
   EXPECT_EQ(0, converter.speed());
-  EXPECT_EQ(Clock::s_to_us(3), converter.speed_time_us());
+  EXPECT_EQ(Clock::s_to_ns(3), converter.speed_time_ns());
 }
 
