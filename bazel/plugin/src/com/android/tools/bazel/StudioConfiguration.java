f015134b0e3d2284e3a3073a862dfa81248fa04d
Dana Dahlstrom <dahlstrom@google.com>
2016-11-30 21:10:35 -0800

    Add iml_module rule for maven3-server-common
    Change-Id: I163aa0118bd236e437d00b4adae82f085343d8f7

--- bazel/plugin/src/com/android/tools/bazel/StudioConfiguration.java
+++ bazel/plugin/src/com/android/tools/bazel/StudioConfiguration.java
@@ -62,8 +62,7 @@ public class StudioConfiguration implements Configuration {
             || rule.getLabel().startsWith("//tools/vendor/google3/blaze/")
             || rule.getName().endsWith("devkit")  // Kotlin compilation fails
             || rule.getName().endsWith("devkit-tests")  // depends on devkit
-            || rule.getName().endsWith("maven3-server-common")  // Java compilation fails
-            || rule.getName().endsWith("maven30-server-impl")  // depends on maven3-server-common
+            || rule.getName().endsWith("maven30-server-impl")  // Java compilation fails
             || rule.getName().endsWith("maven32-server-impl")  // Java compilation fails
             || rule.getName().endsWith("cvs-core")  // Java compilation fails
             || rule.getName().endsWith("cvs-plugin")  // depends on cvs-core
