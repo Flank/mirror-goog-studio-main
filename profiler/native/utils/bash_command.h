be7f19cb918a8b64d375e8c7dce3026fe328e0f4
Shukang Zhou <shukang@google.com>
2016-12-05 11:33:12 -0800

    Make CPU profiler really use sampling for ART method tracing when asked.
    This CL doesn't fix b/33300765 ([perfd] Take bigger captures) but it
    makes the issue less severe. Sampling-based approach generates smaller
    trace files than instrumentation-based profiling. At 10000 Hz
    (100 microsecond sampling interval), a sampling-based trace file often
    grows slower than 200 KB/second. This CL defaults to 1000 Hz, which should
    produce smaller trace files. Of course, the actual growth rate depends
    on apps.
    
    Test: Added 'ActivityManagerTest'.
    
    Change-Id: I2b544176d7eb9b7b06b34ef58fc6df42b9573c7e

--- profiler/native/utils/bash_command.h
+++ profiler/native/utils/bash_command.h
@@ -27,6 +27,7 @@ class BashCommandRunner {
   // Expected executable_path can be either absolute, relative or even
   // executable name only path.
   explicit BashCommandRunner(const std::string &executable_path);
+  virtual ~BashCommandRunner() = default;
   // Returns true if the command runs successfully (exiting with 0).
   // If |output| is not null, it is populated with stdin and stderr from
   // running command.
@@ -37,7 +38,7 @@ class BashCommandRunner {
 
  private:
   const std::string executable_path_;
-  bool RunAndReadOutput(const std::string &cmd, std::string *output) const;
+  virtual bool RunAndReadOutput(const std::string &cmd, std::string *output) const;
 };
 }  // namespace profiler
 #endif  // UTILS_PROFILER_BASH_COMMAND_RUNNER_H
