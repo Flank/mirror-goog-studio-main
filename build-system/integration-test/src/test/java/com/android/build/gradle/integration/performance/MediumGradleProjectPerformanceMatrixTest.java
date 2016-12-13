faedb30bafb366e2849003b35df1de2120b58397
Chris Warrington <cmw@google.com>
2016-12-13 21:01:47 +0000

    Fix MediumGradleProjectPerformanceMatrixTest.
    It was hardcoded to use the prebuilt SDK.
    
    Change-Id: I1d0e27084fa620a5c8c7fd490cbfda7ec8ca368a
    Test: gradle performance tests.

--- build-system/integration-test/src/test/java/com/android/build/gradle/integration/performance/MediumGradleProjectPerformanceMatrixTest.java
+++ build-system/integration-test/src/test/java/com/android/build/gradle/integration/performance/MediumGradleProjectPerformanceMatrixTest.java
@@ -25,7 +25,6 @@ import com.android.build.gradle.integration.common.fixture.RunGradleTasks;
 import com.android.build.gradle.integration.common.runner.FilterableParameterized;
 import com.android.build.gradle.integration.common.utils.JackHelper;
 import com.android.build.gradle.integration.common.utils.ModelHelper;
-import com.android.build.gradle.integration.common.utils.SdkHelper;
 import com.android.build.gradle.integration.common.utils.TestFileUtils;
 import com.android.builder.core.AndroidBuilder;
 import com.android.builder.model.AndroidProject;
@@ -102,10 +101,6 @@ public class MediumGradleProjectPerformanceMatrixTest {
         Files.copy(
                 project.file("WordPress/gradle.properties-example").toPath(),
                 project.file("WordPress/gradle.properties").toPath());
-
-        Files.write(
-                project.file("local.properties").toPath(),
-                ImmutableList.of("sdk.dir=" + SdkHelper.findSdkDir()));
 
         TestFileUtils.appendToFile(
                 project.getBuildFile(),
