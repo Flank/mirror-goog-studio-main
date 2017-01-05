e5f0b1b0274df267d324f9281de68fe35623f240
Raymond Chiu <chiur@google.com>
2017-01-05 15:33:05 -0800

    Use newly added API for configuring annotation process.
    Test: existing AnnotationProcessorTest
    Change-Id: I3f321ffe0e32569038c2f3a55395dc4cf3e87e7c

--- build-system/gradle-core/src/main/java/com/android/build/gradle/tasks/JavaPreCompileTask.java
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/tasks/JavaPreCompileTask.java
@@ -16,27 +16,13 @@
 
 package com.android.build.gradle.tasks;
 
-import com.android.SdkConstants;
 import com.android.annotations.NonNull;
-import com.android.build.gradle.internal.dsl.CoreAnnotationProcessorOptions;
 import com.android.build.gradle.internal.scope.TaskConfigAction;
 import com.android.build.gradle.internal.scope.VariantScope;
 import com.android.build.gradle.internal.tasks.BaseTask;
-import com.android.build.gradle.tasks.factory.AndroidJavaCompile;
-import com.android.build.gradle.tasks.factory.JavaCompileConfigAction;
-import com.android.builder.model.SyncIssue;
 import com.android.utils.FileUtils;
-import com.google.common.base.Joiner;
-import com.google.common.base.Preconditions;
 import java.io.File;
-import java.util.Collection;
-import java.util.Map;
-import java.util.stream.Collectors;
-import org.gradle.api.Project;
-import org.gradle.api.artifacts.Configuration;
-import org.gradle.api.artifacts.ResolvedConfiguration;
 import org.gradle.api.tasks.Input;
-import org.gradle.api.tasks.InputFiles;
 import org.gradle.api.tasks.TaskAction;
 
 /**
@@ -47,63 +33,10 @@ public class JavaPreCompileTask extends BaseTask {
     @Input
     private File annotationProcessorOutputFolder;
 
-    @InputFiles
-    private Configuration annotationProcessorConfiguration;
-
-    private CoreAnnotationProcessorOptions annotationProcessorOptions;
-
-    private String javacTaskName;
-
     @TaskAction
     public void preCompile() {
         // Create directory for output of annotation processor.
         FileUtils.mkdirs(annotationProcessorOutputFolder);
-
-        Preconditions.checkNotNull(annotationProcessorOptions.getIncludeCompileClasspath());
-
-        // Resolve configuration.
-        ResolvedConfiguration resolvedConfiguration =
-                annotationProcessorConfiguration.getResolvedConfiguration();
-        if (resolvedConfiguration.hasError()) {
-            resolvedConfiguration.rethrowFailure();
-        }
-        Collection<File> processorPath =
-                annotationProcessorConfiguration
-                        .getFiles()
-                        .stream()
-                        .filter(file -> !file.getPath().endsWith(SdkConstants.DOT_AAR))
-                        .collect(Collectors.toSet());
-
-        AndroidJavaCompile javacTask =
-                (AndroidJavaCompile) getProject().getTasks().getByName(javacTaskName);
-
-        if (!processorPath.isEmpty()) {
-            if (Boolean.TRUE.equals(annotationProcessorOptions.getIncludeCompileClasspath())) {
-                processorPath.addAll(javacTask.getClasspath().getFiles());
-            }
-            javacTask.getOptions().getCompilerArgs().add("-processorpath");
-            javacTask.getOptions().getCompilerArgs().add(FileUtils.joinFilePaths(processorPath));
-        }
-        if (!annotationProcessorOptions.getClassNames().isEmpty()) {
-            javacTask.getOptions().getCompilerArgs().add("-processor");
-            javacTask.getOptions().getCompilerArgs().add(
-                    Joiner.on(',').join(annotationProcessorOptions.getClassNames()));
-        }
-        if ((!processorPath.isEmpty() || !annotationProcessorOptions.getClassNames().isEmpty())
-                && getProject().getPlugins().hasPlugin("com.neenbedankt.android-apt")) {
-            throw new RuntimeException(
-                    // Error if using android-apt plugin, as it overwrites the annotation processor
-                    // opts
-                    "Using incompatible plugins for the annotation processing: "
-                            + "android-apt. This may result in an unexpected behavior.");
-        }
-        if (!annotationProcessorOptions.getArguments().isEmpty()) {
-            for (Map.Entry<String, String> arg :
-                    annotationProcessorOptions.getArguments().entrySet()) {
-                javacTask.getOptions().getCompilerArgs().add(
-                        "-A" + arg.getKey() + "=" + arg.getValue());
-            }
-        }
     }
 
     public static class ConfigAction implements TaskConfigAction<JavaPreCompileTask> {
@@ -130,27 +63,6 @@ public class JavaPreCompileTask extends BaseTask {
         public void execute(@NonNull JavaPreCompileTask task) {
             task.setVariantName(scope.getFullVariantName());
             task.annotationProcessorOutputFolder = scope.getAnnotationProcessorOutputDir();
-            task.javacTaskName = new JavaCompileConfigAction(scope).getName();
-            task.annotationProcessorOptions =
-                    scope.getVariantConfiguration().getJavaCompileOptions()
-                            .getAnnotationProcessorOptions();
-            task.annotationProcessorConfiguration =
-                    scope.getVariantData().getVariantDependency()
-                            .getAnnotationProcessorConfiguration();
-
-            Project project = scope.getGlobalScope().getProject();
-            if ((!task.annotationProcessorConfiguration.getAllDependencies().isEmpty()
-                    || !task.annotationProcessorOptions.getClassNames().isEmpty())
-                    && project.getPlugins().hasPlugin("com.neenbedankt.android-apt")) {
-                // warn user if using android-apt plugin, as it overwrites the annotation processor
-                // opts
-                scope.getGlobalScope().getAndroidBuilder().getErrorReporter().handleSyncWarning(
-                        null,
-                        SyncIssue.TYPE_GENERIC,
-                        "Using incompatible plugins for the annotation processing: "
-                                + "android-apt. This may result in an unexpected behavior.");
-
-            }
         }
     }
 }
