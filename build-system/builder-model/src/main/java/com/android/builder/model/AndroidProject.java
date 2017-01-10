987805e9fd177df1e635b49491d12416635f9d49
Daniel Colascione <dancol@google.com>
2017-01-10 09:27:24 -0800

    Start Instant Run server via content provider, not service
    By starting the IR server by side effect during the creation of a
    dummy content provider instead of relying on the IDE to issue an
    explicit "am startservice", we can both avoid the ANR that could arise
    if a process is stalled in its main loop (e.g., broken into a
    debugger) while the IDE runs a concurrent "am startservice" as well as
    simplify all the code paths in the IDE and Gradle that try to
    launch processes.
    
    Test: existing; included
    Change-Id: I09f3d023eea72a6b3a5394f318eb6a579e109ffb

--- build-system/builder-model/src/main/java/com/android/builder/model/AndroidProject.java
+++ build-system/builder-model/src/main/java/com/android/builder/model/AndroidProject.java
@@ -62,7 +62,11 @@ public interface AndroidProject {
     String PROPERTY_BUILD_ABI = "android.injected.build.abi";
     String PROPERTY_BUILD_DENSITY = "android.injected.build.density";
 
+    // Has the effect of telling the Gradle plugin to
+    //   1) Generate machine-readable errors
+    //   2) Generate build metadata JSON files
     String PROPERTY_INVOKED_FROM_IDE = "android.injected.invoked.from.ide";
+
     String PROPERTY_GENERATE_SOURCES_ONLY = "android.injected.generateSourcesOnly";
 
     String PROPERTY_RESTRICT_VARIANT_PROJECT = "android.injected.restrict.variant.project";
