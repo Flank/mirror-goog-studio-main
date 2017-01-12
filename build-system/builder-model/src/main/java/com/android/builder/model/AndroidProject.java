4c020eb4dac96935b1006eb468c4f7e73ab0d0a7
Xavier Ducrohet <xav@google.com>
2017-01-12 08:39:20 -0800

    Make plugin check for exact level2 sync.
    Previously the code would look for level 2 or
    above to do something different.
    
    Since level 2 is not ready yet, but disabling it is non
    trivial (since we'd have to rewrite a bunch of tests),
    the new code checks for exactly level 2.
    
    We will ensure that Studio never uses level2 and skips directly
    to level3.
    
    Test: existing integ tests.
    
    Change-Id: I32cecdca73f5b4fe3d0601c1556451d69a0e0251

--- build-system/builder-model/src/main/java/com/android/builder/model/AndroidProject.java
+++ build-system/builder-model/src/main/java/com/android/builder/model/AndroidProject.java
@@ -31,10 +31,10 @@ public interface AndroidProject {
     String PROPERTY_BUILD_MODEL_ONLY = "android.injected.build.model.only";
     // Sent by Studio 1.1+
     String PROPERTY_BUILD_MODEL_ONLY_ADVANCED = "android.injected.build.model.only.advanced";
-    // Sent by Studio 2.3+. The value of the prop is a monotonically increasing integer.
+    // Sent by Studio 2.4+. The value of the prop is a monotonically increasing integer.
     // see MODEL_LEVEL_* constants
     String PROPERTY_BUILD_MODEL_ONLY_VERSIONED = "android.injected.build.model.only.versioned";
-    // Sent by Studio 2.3+. Additional model feature trigger on a case by case basis
+    // Sent by Studio 2.4+. Additional model feature trigger on a case by case basis
     // Value is simply true to enable.
     String PROPERTY_BUILD_MODEL_FEATURE_FULL_DEPENDENCIES = "android.injected.build.model.feature.full.dependencies";
 
@@ -113,11 +113,10 @@ public interface AndroidProject {
     int GENERATION_ORIGINAL = 1;
     int GENERATION_COMPONENT = 2;
 
-    int MODEL_LEVEL_0_ORIGNAL = 0 ; // studio 1.0, no support for SyncIssue
+    int MODEL_LEVEL_0_ORIGINAL = 0 ; // studio 1.0, no support for SyncIssue
     int MODEL_LEVEL_1_SYNC_ISSUE = 1; // studio 1.1+, with SyncIssue
-    int MODEL_LEVEL_2_NEW_DEP_MODEL = 2; // studio 2.3+, new model for dependencies
-    //int MODEL_LEVEL_3_FULL_DEP_GRAPH = 3; // studio 2.3+, new model for dependencies
-    int MODEL_LEVEL_LATEST = MODEL_LEVEL_2_NEW_DEP_MODEL;
+    int MODEL_LEVEL_2_DONT_USE = 2; // Don't use this. Go level 1 to level 3 when ready.
+    int MODEL_LEVEL_LATEST = MODEL_LEVEL_2_DONT_USE;
 
     int PROJECT_TYPE_APP = 0;
     int PROJECT_TYPE_LIBRARY = 1;
