583ddd9b86037dc2b9fbc644405a72527f030fe5
Michal Bendowski <bendowski@google.com>
2017-01-30 10:53:58 +0000

    Clean up PackagingOptions DSL
    Bug: https://github.com/google/android-gradle-dsl/issues/7
    Test: No automated tests for DSL contents, so manually checked.
    Change-Id: I8ed095ae257dd8bb5e2b5f3624b9b9b6b268d149
    (cherry picked from commit 6e6bf4b68328dc761c860341ff4b31c299c43871)

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/dsl/PackagingOptions.java
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/dsl/PackagingOptions.java
@@ -17,14 +17,9 @@
 package com.android.build.gradle.internal.dsl;
 
 import com.android.annotations.NonNull;
-import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
-
-import org.gradle.api.tasks.Input;
-
-import java.nio.file.PathMatcher;
-import java.util.Map;
 import java.util.Set;
+import org.gradle.api.tasks.Input;
 
 /**
  * DSL object for configuring APK packaging options.
@@ -32,38 +27,37 @@ import java.util.Set;
  * <p>Packaging options are configured with three sets of paths: first-picks, merges and excludes:
  *
  * <dl>
- *     <dt>First-pick
- *     <dd>Paths that match a first-pick pattern will be selected into the APK. If more than one
- *     path matches the first-pick, only the first found will be selected.
- *
- *     <dt>Merge
- *     <dd>Paths that match a merge pattern will be concatenated and merged into the APK. When
- *     merging two files, a newline will be appended to the end of the first file, if it doesn't
- *     end with a newline already. This is done for all files, regardless of the type of contents.
- *
- *     <dt>Exclude
- *     <dd>Paths that match an exclude pattern will not be included in the APK.
+ *   <dt>First-pick
+ *   <dd>Paths that match a first-pick pattern will be selected into the APK. If more than one path
+ *       matches the first-pick, only the first found will be selected.
+ *   <dt>Merge
+ *   <dd>Paths that match a merge pattern will be concatenated and merged into the APK. When merging
+ *       two files, a newline will be appended to the end of the first file, if it doesn't end with
+ *       a newline already. This is done for all files, regardless of the type of contents.
+ *   <dt>Exclude
+ *   <dd>Paths that match an exclude pattern will not be included in the APK.
  * </dl>
  *
  * To decide the action on a specific path, the following algorithm is used:
+ *
  * <ol>
- *     <li>If any of the first-pick patterns match the path and that path has not been included in
- *     the APK, add it to the APK.
- *     <li>If any of the first-pick patterns match the path and that path has already been included
- *     in the APK, do not include the path in the APK.
- *     <li>If any of the merge patterns match the path and that path has not been included in the
- *     APK, add it to the APK.
- *     <li>If any of the merge patterns match the path and that path has already been included in
- *     the APK, concatenate the contents of the file to the ones already in the APK.
- *     <li>If any of the exclude patterns match the path, do not include it in the APK.
- *     <li>If none of the patterns above match the path and the path has not been included in the
- *     APK, add it to the APK.
- *     <li>Id none of the patterns above match the path and the path has been included in the APK,
- *     fail the build and signal a duplicate path error.
+ *   <li>If any of the first-pick patterns match the path and that path has not been included in the
+ *       APK, add it to the APK.
+ *   <li>If any of the first-pick patterns match the path and that path has already been included in
+ *       the APK, do not include the path in the APK.
+ *   <li>If any of the merge patterns match the path and that path has not been included in the APK,
+ *       add it to the APK.
+ *   <li>If any of the merge patterns match the path and that path has already been included in the
+ *       APK, concatenate the contents of the file to the ones already in the APK.
+ *   <li>If any of the exclude patterns match the path, do not include it in the APK.
+ *   <li>If none of the patterns above match the path and the path has not been included in the APK,
+ *       add it to the APK.
+ *   <li>Id none of the patterns above match the path and the path has been included in the APK,
+ *       fail the build and signal a duplicate path error.
  * </ol>
  *
- * <p>Patterns in packaging options are specified as globs following the syntax in the
- * <a href="https://docs.oracle.com/javase/8/docs/api/java/nio/file/FileSystem.html#getPathMatcher-java.lang.String-">
+ * <p>Patterns in packaging options are specified as globs following the syntax in the <a
+ * href="https://docs.oracle.com/javase/8/docs/api/java/nio/file/FileSystem.html#getPathMatcher-java.lang.String-">
  * Java Filesystem API</a>. All paths should be configured using forward slashes ({@code /}).
  *
  * <p>All paths to be matched are provided as absolute paths from the root of the apk archive. So,
@@ -74,11 +68,12 @@ import java.util.Set;
  * the same pattern.
  *
  * <p>The default values are:
+ *
  * <ul>
- *     <li>Pick first: none
- *     <li>Merge: {@code /META-INF/services/&#042;&#042;}
- *     <li>Exclude:
- *     <ul>
+ *   <li>Pick first: none
+ *   <li>Merge: <code>/META-INF/services/&#042;&#042;</code>
+ *   <li>Exclude:
+ *       <ul>
  *         <li>{@code /META-INF/LICENCE}
  *         <li>{@code /META-INF/LICENCE.txt}
  *         <li>{@code /META-INF/NOTICE}
@@ -91,7 +86,8 @@ import java.util.Set;
  *         <li><code>&#042;&#042;/CVS/&#042;&#042;</code> (all {@code CVS} directory contents)
  *         <li><code>&#042;&#042;/SCCS/&#042;&#042;</code> (all {@code SCCS} directory contents)
  *         <li><code>&#042;&#042;/.&#042;</code> (all UNIX hidden files)
- *         <li><code>&#042;&#042;/.&#042;/&#042;&#042;</code> (all contents of UNIX hidden directories)
+ *         <li><code>&#042;&#042;/.&#042;/&#042;&#042;</code> (all contents of UNIX hidden
+ *             directories)
  *         <li><code>&#042;&#042;/&#042;~</code> (temporary files)
  *         <li><code>&#042;&#042;/thumbs.db</code>
  *         <li><code>&#042;&#042;/picasa.ini</code>
@@ -100,19 +96,22 @@ import java.util.Set;
  *         <li><code>&#042;&#042;/overview.html</code>
  *         <li><code>&#042;&#042;/_&#042;</code>
  *         <li><code>&#042;&#042;/_&#042;/&#042;&#042;</code>
- *     </ul>
+ *       </ul>
+ *
  * </ul>
  *
  * <p>Example that adds the first {@code anyFileWillDo} file found and ignores all the others and
  * that excludes anything inside a {@code secret-data} directory that exists in the root:
+ *
  * <pre>
  * packagingOptions {
- *     pickFirst anyFileWillDo
- *     exclude /secret-data/&#042;&#042;
+ *     pickFirst "anyFileWillDo"
+ *     exclude "/secret-data/&#042;&#042;"
  * }
  * </pre>
  *
  * <p>Example that removes all patterns:
+ *
  * <pre>
  * packagingOptions {
  *     pickFirsts = [] // Not really needed because the default is empty.
@@ -122,27 +121,18 @@ import java.util.Set;
  * </pre>
  *
  * <p>Example that merges all {@code LICENCE.txt} files in the root.
+ *
  * <pre>
  * packagingOptions {
- *     merges = "/LICENCE.txt"
- *     excludes -= ["/LICENCE.txt"] // Not needed because merges take precedence over excludes
+ *     merge "/LICENCE.txt" // Same as: merges += ["/LICENSE.txt"]
+ *     excludes -= ["/LICENCE.txt"] // Not really needed because merges take precedence over excludes.
  * }
  * </pre>
  */
 public class PackagingOptions implements com.android.builder.model.PackagingOptions {
 
-    private Set<String> excludes = Sets.newHashSet();
-    private Set<String> pickFirsts = Sets.newHashSet();
-    private Set<String> merges = Sets.newHashSet();
-    private Set<String> doNotStrip = Sets.newHashSet();
-
-    /**
-     * Cache with compiled patterns.
-     */
-    @NonNull
-    private final Map<String, PathMatcher> compiledPatterns = Maps.newHashMap();
-
     public PackagingOptions() {
+        // ATTENTION - keep this in sync with JavaDoc above.
         exclude("/META-INF/LICENCE");
         exclude("/META-INF/LICENSE.txt");
         exclude("/META-INF/NOTICE");
@@ -180,6 +170,15 @@ public class PackagingOptions implements com.android.builder.model.PackagingOpti
         merge("/META-INF/services/**");
     }
 
+    // This is a bit embarrassing, but our DSL generator gets confused for some reason, if the constructor
+    // above is immediately followed by a property. DSL generation needs to be reworked anyway, so for now
+    // I'll just move the fields down here.
+
+    private Set<String> excludes = Sets.newHashSet();
+    private Set<String> pickFirsts = Sets.newHashSet();
+    private Set<String> merges = Sets.newHashSet();
+    private Set<String> doNotStrip = Sets.newHashSet();
+
     /**
      * Returns the list of excluded paths.
      */
@@ -204,7 +203,8 @@ public class PackagingOptions implements com.android.builder.model.PackagingOpti
     }
 
     /**
-     * Returns the list of patterns where the first occurrence is packaged in the APK.
+     * Returns the list of patterns where the first occurrence is packaged in the APK. First pick
+     * patterns do get packaged in the APK, but only the first occurrence found gets packaged.
      */
     @Override
     @NonNull
@@ -214,8 +214,7 @@ public class PackagingOptions implements com.android.builder.model.PackagingOpti
     }
 
     /**
-     * Adds a first-pick pattern. First pick patterns do get packaged in the APK, but only the
-     * first occurrence found gets packaged.
+     * Adds a first-pick pattern.
      *
      * @param pattern the path to add.
      */
@@ -253,6 +252,8 @@ public class PackagingOptions implements com.android.builder.model.PackagingOpti
 
     /**
      * Returns the list of patterns for native library that should not be stripped of debug symbols.
+     *
+     * <p>Example: <code>packagingOptions.doNotStrip "&#42;/armeabi-v7a/libhello-jni.so"</code>
      */
     @Override
     @NonNull
@@ -260,6 +261,7 @@ public class PackagingOptions implements com.android.builder.model.PackagingOpti
         return Sets.newHashSet(doNotStrip);
     }
 
+    @SuppressWarnings("unused") // Exposed in DSL.
     public void setDoNotStrip(Set<String> doNotStrip) {
         this.doNotStrip = Sets.newHashSet(doNotStrip);
     }
@@ -269,6 +271,7 @@ public class PackagingOptions implements com.android.builder.model.PackagingOpti
      *
      * @param pattern the pattern, as packaged in the APK
      */
+    @SuppressWarnings("unused") // Exposed in DSL.
     public void doNotStrip(String pattern) {
         doNotStrip.add(pattern);
     }
