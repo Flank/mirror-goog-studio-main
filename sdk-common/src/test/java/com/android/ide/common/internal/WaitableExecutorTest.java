9d8e76c99b7258cae19c113788f0129d31e38217
Ivan Gavrilovic <gavra@google.com>
2017-02-02 16:23:50 +0000

    Throw exception if any of WaitableExecutor tasks fails
    InstantRunTransform will now throw an exception if any
    of the tasks fails, which will make debugging easier.
    Tests for the WaitableExecutor have been added as well.
    
    Test: WaitableExecutorTest
    Change-Id: I742c0cca2fd481a09981c9dff365e2dff26f3f1c

---
+++ sdk-common/src/test/java/com/android/ide/common/internal/WaitableExecutorTest.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.ide.common.internal;
+
+import static org.junit.Assert.fail;
+
+import com.android.annotations.NonNull;
+import com.google.common.truth.Truth;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import org.junit.Before;
+import org.junit.Test;
+
+/** Tests for the {@link WaitableExecutor}. */
+public class WaitableExecutorTest {
+
+    @NonNull private WaitableExecutor<Integer> executor;
+
+    @Before
+    public void setUp() {
+        executor = WaitableExecutor.useGlobalSharedThreadPool();
+    }
+
+    @Test
+    public void checkTaskResults() throws InterruptedException {
+        executor.execute(() -> 1);
+        executor.execute(() -> 2);
+        executor.execute(() -> 3);
+
+        List<Integer> results =
+                executor.waitForAllTasks().stream().map(e -> e.value).collect(Collectors.toList());
+        Truth.assertThat(results).containsExactly(1, 2, 3);
+    }
+
+    @Test
+    public void checkNoTaskResults() throws InterruptedException {
+        List<Integer> results =
+                executor.waitForAllTasks().stream().map(e -> e.value).collect(Collectors.toList());
+        Truth.assertThat(results).isEmpty();
+    }
+
+    @Test
+    public void checkOneFails() throws InterruptedException {
+        executor.execute(() -> 1);
+        executor.execute(() -> 2);
+        executor.execute(
+                () -> {
+                    throw new RuntimeException("Fail this task");
+                });
+
+        List<WaitableExecutor.TaskResult<Integer>> results = executor.waitForAllTasks();
+        List<Integer> values = results.stream().map(e -> e.value).collect(Collectors.toList());
+        Truth.assertThat(values).containsExactly(1, 2, null);
+
+        List<String> exceptions =
+                results.stream()
+                        .map(e -> e.exception)
+                        .filter(Objects::nonNull)
+                        .map(Throwable::getMessage)
+                        .collect(Collectors.toList());
+        Truth.assertThat(exceptions).containsExactly("Fail this task");
+    }
+
+    @Test
+    public void checkExceptionThrownIfOneFails() throws InterruptedException {
+        executor.execute(() -> 1);
+        executor.execute(() -> 2);
+        executor.execute(
+                () -> {
+                    throw new RuntimeException("Fail this task");
+                });
+
+        try {
+            executor.waitForTasksWithQuickFail(false);
+            fail();
+        } catch (Exception e) {
+            Truth.assertThat(e.getCause().getMessage()).contains("Fail this task");
+        }
+    }
+}
