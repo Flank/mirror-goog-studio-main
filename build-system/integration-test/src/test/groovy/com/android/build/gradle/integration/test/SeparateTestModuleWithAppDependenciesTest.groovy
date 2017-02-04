e41db9b32a5d833c1685224df572e8cf6be4d9de
Xavier Ducrohet <xav@google.com>
2017-02-04 09:39:53 -0800

    Fix issue with missing dependency.
    The new mechanism makes the test project runtimeClasspath not
    extend from the tested app. This made a previously upgraded dependency
    not found (the request version is not in the prebuilts SDK, but the
    upgraded is).
    
    Just force the test to upgrade itself to the newer version.
    
    Test: fixing them
    Change-Id: I11a9c9fa7e631aefff46410d19305ac782e1f4ee

--- build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/test/SeparateTestModuleWithAppDependenciesTest.groovy
+++ build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/test/SeparateTestModuleWithAppDependenciesTest.groovy
@@ -78,6 +78,7 @@ public class FooActivity extends AppCompatActivity {
         project.getSubproject("test").getBuildFile() << """
 dependencies {
     compile 'com.android.support.test:rules:$GradleTestProject.TEST_SUPPORT_LIB_VERSION'
+    compile 'com.android.support:support-annotations:$GradleTestProject.SUPPORT_LIB_VERSION'
 }
         """
 
