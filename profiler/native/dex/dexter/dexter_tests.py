bfbbcd56e941e9efe2363449b8a12fc80018f360
Leonard Mosescu <mosescu@google.com>
2017-02-06 15:09:25 -0800

    The .dex reader (incrementally creating .dex IR)
    This is the first part of the .dex manipulation pipeline: the
    reader parses an in-memory .dex image and offers a low-level interface
    for iterating the main .dex indexes, as well as the ability to
    create .dex IR incrementally (once class at a time). Adding the .dex IR
    for a class will automatically include the trasitive closure of all the
    declarations referenced from that class so we always have all the .dex IR
    that describes a partial but valid .dex file.
    
    Also included a custom driver for end-to-end testing based on comparing
    the expected ('golden') output with the actual output. Right now this is
    fairly simple but we could make it more generic if we want to use this
    for other components.
    
    Test: added end-to-end tests (bazel only)
    
    Change-Id: I36b26ffe872d89745ca532b2daf8e8890d34b94c

---
+++ profiler/native/dex/dexter/dexter_tests.py
@@ -0,0 +1,53 @@
+
+import re
+import os
+import glob
+import sys
+import subprocess
+import argparse
+
+# test command line arguments
+argParser = argparse.ArgumentParser(description = 'dexter end-to-end test driver')
+argParser.add_argument('-cmd', required = True)
+args = argParser.parse_args()
+
+# the bazel sandbox test data root
+data_root = 'tools/base/profiler/native/testdata/dex'
+
+# list of test cases
+# ( <test_name> : { <test_case_config> } )
+test_cases = {
+  'map' : { 'args' : '-m', 'input' : '*.dex' }
+}
+
+# run a shell command and returns the stdout content
+def Run(cmd):
+  return subprocess.Popen(
+    args = cmd,
+    shell = True,
+    stdout = subprocess.PIPE,
+    stderr = subprocess.STDOUT).communicate()[0]
+
+# for each test_case, run dexter over the specified input (ex. *.dex)
+#
+# the expected ('golden') output has the same base name as the input .dex,
+# for example (test_name = 'map') :
+#
+#    'hello.dex' -> 'hello.map.expected'
+#
+for test_name, test_config in sorted(test_cases.iteritems()):
+  input_files = glob.glob(os.path.join(data_root, test_config['input']))
+
+  for input in input_files:
+    # run dexter with the test arguments
+    cmd = '%s %s %s' % (args.cmd, test_config['args'], input)
+    actual_output = Run(cmd)
+
+    # build the expected filename
+    expected_filename = re.sub(r'\.dex', ('.%s.expected' % test_name), input)
+
+    # compare the actual output with the expected output
+    with open(expected_filename) as f:
+      if actual_output != f.read():
+        print('expected output mismatch (%s)' % os.path.basename(expected_filename))
+        exit(1)
