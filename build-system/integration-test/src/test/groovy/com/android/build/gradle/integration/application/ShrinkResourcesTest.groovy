91494b9ca8c12226eafb7c365dead0d7d7cde1bf
Michal Bendowski <bendowski@google.com>
2017-02-22 15:38:35 +0000

    Fix a bug where we never shrunk resources
    I made a typo earlier this year and it was not caught, because the
    integration test was disabled. I think I know why the test was flaky
    before, so I'll re-enabled it now.
    
    Test: ShrinkResourcesTest
    Change-Id: I9ddecdd8b420ea3789454f1a81cfd2ce688b5f20

--- build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/application/ShrinkResourcesTest.groovy
+++ build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/application/ShrinkResourcesTest.groovy
@@ -28,7 +28,6 @@ import com.google.common.io.Closer
 import groovy.transform.CompileStatic
 import org.junit.Assume
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 
@@ -77,7 +76,6 @@ class ShrinkResourcesTest {
     }
 
     @Test
-    @Ignore("Flaky: http://b.android.com/224406")
     void "check shrink resources"() {
         project.execute("clean", "assembleRelease", "assembleDebug", "assembleProguardNoShrink")
 
