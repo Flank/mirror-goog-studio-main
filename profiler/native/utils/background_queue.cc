9f18d30ade4244462ea4acd59393a100dbd76aba
David Herman <davidherman@google.com>
2017-02-23 07:41:54 -0800

    Remove busy-looping from BackgroundQueue
    Currently, BackgroundQueue runs a thread which actively loops with
    yields. This was too aggressive and shows up quite noisily in the CPU
    profiler. Now, we put the thread to sleep until it's notified that
    there's new work to be processed.
    
    Test: N/A
    Change-Id: Iff279403f51d9ce6d33544d598e0f025ebe99bc0

--- profiler/native/utils/background_queue.cc
+++ profiler/native/utils/background_queue.cc
@@ -31,19 +31,23 @@ using std::queue;
 namespace profiler {
 
 BackgroundQueue::BackgroundQueue(string thread_name)
-    : is_task_running_(false), task_thread_name_(thread_name) {
+    : is_task_running_(false),
+      task_thread_name_(thread_name),
+      task_enqueued_latch_(1) {
   is_ready_ = true;
   task_thread_ = thread(&BackgroundQueue::TaskThread, this);
 }
 
 BackgroundQueue::~BackgroundQueue() {
   is_ready_ = false;
+  EnqueueTask([]() {});  // Releases latch so background thread can run and die
   task_thread_.join();
 }
 
 void BackgroundQueue::EnqueueTask(function<void()> task) {
   lock_guard<mutex> lock(task_queue_mutex_);
   task_queue_.push(task);
+  task_enqueued_latch_.CountDown();
 }
 
 void BackgroundQueue::Reset() {
@@ -74,28 +78,25 @@ void BackgroundQueue::TaskThread() {
   SetThreadName(task_thread_name_);
 
   while (is_ready_) {
-    function<void()> task = []() {};
+    task_enqueued_latch_.Await();
+
+    function<void()> task;
 
     {
       lock_guard<mutex> lock(task_queue_mutex_);
-      if (!task_queue_.empty()) {
-        task = task_queue_.front();
-        task_queue_.pop();
-        is_task_running_ = true;
-      }
+      task = task_queue_.front();
+      task_queue_.pop();
+      is_task_running_ = true;
     }
 
     task();
 
-    bool is_task_queue_empty;
     {
       lock_guard<mutex> lock(task_queue_mutex_);
       is_task_running_ = false;
-      is_task_queue_empty = task_queue_.empty();
-    }
-
-    if (is_task_queue_empty) {
-      std::this_thread::yield();
+      if (task_queue_.empty()) {
+        task_enqueued_latch_.Reset();
+      }
     }
   }
 }
