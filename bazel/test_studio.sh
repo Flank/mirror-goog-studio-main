bc1c1b6545a8c902ac2241b3f78b420b1b721ca2
Peter Xu <peterx@google.com>
2017-03-22 09:10:29 -0700

    Zip and copy logs to artifact directory in test_studio.sh
    Requested by vsiva@, zip and copy log files to the artifiacts directory
    ($DIST).
    
    Test: n/a
    Change-Id: Ib5678d2c29d5d7fc980d1d70c1e5fd8220e596c6

--- bazel/test_studio.sh
+++ bazel/test_studio.sh
@@ -1,4 +1,14 @@
 #!/bin/bash -ex
 # Invoked by Android Build Launchcontrol for continuous builds.
-SCRIPT_DIR="$(dirname "$0")"
-"${SCRIPT_DIR}/bazel" --batch test --test_output=errors $(< "${SCRIPT_DIR}/targets")
+
+# Expected arguments:
+readonly out_dir="$1"
+readonly dist_dir="$2"
+readonly build_number="$3"
+
+readonly script_dir="$(dirname "$0")"
+readonly testlogs_dir="$(${script_dir}/bazel info output_path)/local-fastbuild/testlogs"
+
+"${script_dir}/bazel" --batch test --test_output=errors $(< "${script_dir}/targets")
+
+(cd "${testlogs_dir}" && zip -qr "${dist_dir}/bazel_testlogs_${build_number}.zip" *)
