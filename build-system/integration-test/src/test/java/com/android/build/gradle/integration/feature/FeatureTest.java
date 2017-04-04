862a2eaa6af28bd2969d462521442919f15a88f6
Jerome Dochez <jedo@google.com>
2017-04-04 16:19:56 -0700

    Add minimalistic model integration tests
    Test: added integration tests
    
    Change-Id: I5c9a0ac2dcf20fd204acb48a7fccb57a3381d280

--- build-system/integration-test/src/test/java/com/android/build/gradle/integration/feature/FeatureTest.java
+++ build-system/integration-test/src/test/java/com/android/build/gradle/integration/feature/FeatureTest.java
@@ -22,15 +22,30 @@ import static com.android.build.gradle.integration.common.truth.TruthHelper.asse
 import static com.android.build.gradle.integration.common.truth.TruthHelper.assertThatApk;
 import static com.android.testutils.truth.MoreTruth.assertThatZip;
 
+import com.android.SdkConstants;
+import com.android.build.OutputFile;
+import com.android.build.VariantOutput;
+import com.android.build.gradle.integration.common.fixture.GradleBuildResult;
 import com.android.build.gradle.integration.common.fixture.GradleTestProject;
 import com.android.build.gradle.integration.common.truth.ApkSubject;
 import com.android.build.gradle.internal.aapt.AaptGeneration;
 import com.android.build.gradle.internal.tasks.featuresplit.FeatureSplitDeclaration;
 import com.android.build.gradle.internal.tasks.featuresplit.FeatureSplitPackageIds;
 import com.android.builder.model.AndroidProject;
+import com.android.builder.model.ProjectBuildOutput;
+import com.android.builder.model.VariantBuildOutput;
 import com.android.testutils.truth.ZipFileSubject;
 import com.android.utils.FileUtils;
+import com.google.common.base.Charsets;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterators;
+import com.google.common.collect.Lists;
+import com.google.common.io.Files;
 import java.io.File;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
@@ -167,5 +182,198 @@ public class FeatureTest {
             baseFeatureApk.containsClass("Lcom/example/android/multiproject/library/PersonView;");
             baseFeatureApk.containsClass("Lcom/example/android/multiproject/base/PersonView2;");
         }
+    }
+
+    @Test
+    public void testMinimalisticModel() throws Exception {
+        sProject.executor()
+                .with(AaptGeneration.AAPT_V2_JNI)
+                .withEnabledFeatureSplitTransitionalAttributes(true)
+                .run("assemble");
+
+        // get the initial minimalistic model.
+        Map<String, ProjectBuildOutput> multi = sProject.model().getMulti(ProjectBuildOutput.class);
+
+        ProjectBuildOutput projectBuildOutput = multi.get(":feature");
+        assertThat(projectBuildOutput).isNotNull();
+
+        Collection<VariantBuildOutput> variantsBuildOutput =
+                projectBuildOutput.getVariantsBuildOutput();
+        assertThat(variantsBuildOutput).hasSize(4);
+
+        List<String> expectedVariantNames =
+                Lists.newArrayList(
+                        "debugFeature", "releaseFeature", // for the feature split
+                        "debug", "release"); // for the aar part.
+
+        variantsBuildOutput.forEach(
+                variantBuildOutput -> {
+                    assertThat(expectedVariantNames.remove(variantBuildOutput.getName())).isTrue();
+                    OutputFile output =
+                            Iterators.getOnlyElement(variantBuildOutput.getOutputs().iterator());
+                    assertThat(output.getOutputType())
+                            .isEqualTo(VariantOutput.OutputType.MAIN.toString());
+                    assertThat(output.getFilters()).isEmpty();
+                });
+        assertThat(expectedVariantNames).isEmpty();
+    }
+
+    @Test
+    public void incrementalAllVariantsBuild() throws Exception {
+        sProject.executor()
+                .with(AaptGeneration.AAPT_V2_JNI)
+                .withEnabledFeatureSplitTransitionalAttributes(true)
+                .run("assemble");
+
+        GradleTestProject featureProject = sProject.getSubproject(":feature");
+
+        // get the initial minimalistic model.
+        Map<String, ProjectBuildOutput> multi = sProject.model().getMulti(ProjectBuildOutput.class);
+
+        ProjectBuildOutput projectBuildOutput = multi.get(":feature");
+        assertThat(projectBuildOutput).isNotNull();
+
+        Map<String, Long> originalApkTimestamps =
+                getVariantNameToTimestamp(projectBuildOutput.getVariantsBuildOutput());
+
+        // now change a source file
+        String javaCode = generateClass();
+
+        File addedSource =
+                featureProject.file(
+                        "src/main/java/com/example/android/multiproject/feature/Hello.java");
+
+        Files.write(javaCode, addedSource, Charsets.UTF_8);
+
+        GradleBuildResult assemble =
+                sProject.executor()
+                        .with(AaptGeneration.AAPT_V2_JNI)
+                        .withEnabledFeatureSplitTransitionalAttributes(true)
+                        .run("assemble");
+
+        multi = sProject.model().getMulti(ProjectBuildOutput.class);
+
+        assertThat(assemble.getNotUpToDateTasks()).contains(":feature:assembleDebug");
+        assertThat(assemble.getNotUpToDateTasks()).contains(":baseFeature:assemble");
+
+        Map<String, File> modifiedApks =
+                getVariantNameToOutputFileMap(multi.get(":feature").getVariantsBuildOutput());
+
+        // assert that all APKs are present and contains the new class.
+        for (Map.Entry<String, File> stringFileEntry : modifiedApks.entrySet()) {
+            File file = stringFileEntry.getValue();
+            assertThat(originalApkTimestamps).containsKey(stringFileEntry.getKey());
+            assertThat(file).exists();
+            if (file.getName().endsWith(SdkConstants.DOT_ANDROID_PACKAGE)) {
+                try (ApkSubject apk = assertThatApk(file)) {
+                    apk.containsClass("Lcom/example/android/multiproject/feature/Hello;");
+                }
+            }
+        }
+        assertThat(addedSource.delete()).isTrue();
+    }
+
+    @Test
+    public void incrementalBuild() throws Exception {
+        sProject.executor()
+                .with(AaptGeneration.AAPT_V2_JNI)
+                .withEnabledFeatureSplitTransitionalAttributes(true)
+                .run("assemble");
+
+        GradleTestProject featureProject = sProject.getSubproject(":feature");
+
+        // get the initial minimalistic model.
+        Map<String, ProjectBuildOutput> multi = sProject.model().getMulti(ProjectBuildOutput.class);
+
+        ProjectBuildOutput projectBuildOutput = multi.get(":feature");
+        assertThat(projectBuildOutput).isNotNull();
+
+        Map<String, Long> originalApkTimestamps =
+                getVariantNameToOutputFileMap(projectBuildOutput.getVariantsBuildOutput())
+                        .entrySet()
+                        .stream()
+                        .collect(
+                                Collectors.toMap(
+                                        Map.Entry::getKey, e -> e.getValue().lastModified()));
+
+        // now change a source file
+        String javaCode = generateClass();
+
+        File addedSource =
+                featureProject.file(
+                        "src/main/java/com/example/android/multiproject/feature/Hello.java");
+
+        Files.write(javaCode, addedSource, Charsets.UTF_8);
+
+        GradleBuildResult assembleDebug =
+                sProject.executor()
+                        .with(AaptGeneration.AAPT_V2_JNI)
+                        .withEnabledFeatureSplitTransitionalAttributes(true)
+                        .run("assembleDebug");
+
+        multi = sProject.model().getMulti(ProjectBuildOutput.class);
+
+        assertThat(assembleDebug.getNotUpToDateTasks()).contains(":feature:assembleDebug");
+        assertThat(assembleDebug.getUpToDateTasks().contains(":feature:assembleRelease"));
+        assertThat(assembleDebug.getNotUpToDateTasks()).contains(":baseFeature:assembleDebug");
+
+        Map<String, File> modifiedApks =
+                getVariantNameToOutputFileMap(multi.get(":feature").getVariantsBuildOutput());
+
+        // assert that all APKs were rebuilt.
+        for (Map.Entry<String, File> stringFileEntry : modifiedApks.entrySet()) {
+
+            String variantName = stringFileEntry.getKey();
+            File file = stringFileEntry.getValue();
+            assertThat(originalApkTimestamps).containsKey(variantName);
+            if (variantName.contains("release")) {
+                assertThat(originalApkTimestamps.get(variantName)).isEqualTo(file.lastModified());
+            } else {
+                // check the .class file got added.
+                assertThat(file).exists();
+                if (file.getName().endsWith(SdkConstants.DOT_ANDROID_PACKAGE)) {
+                    try (ApkSubject apk = assertThatApk(file)) {
+                        apk.containsClass("Lcom/example/android/multiproject/feature/Hello;");
+                    }
+                }
+            }
+        }
+
+        assertThat(addedSource.delete()).isTrue();
+    }
+
+    private static String generateClass() {
+        return "package com.example.android.multiproject.feature; \n"
+                + "import android.app.Activity;\n"
+                + "import android.os.Bundle;\n"
+                + "\n"
+                + "public class Hello extends Activity {\n"
+                + "   @Override\n"
+                + "   public void onCreate(Bundle savedInstanceState) {"
+                + "   }\n"
+                + "}\n";
+    }
+
+    private static Map<String, File> getVariantNameToOutputFileMap(
+            Collection<VariantBuildOutput> variantsBuildOutput) {
+
+        ImmutableMap.Builder<String, File> variantNameToApkTimestamp = ImmutableMap.builder();
+        variantsBuildOutput.forEach(
+                variantBuildOutput -> {
+                    OutputFile output =
+                            Iterators.getOnlyElement(variantBuildOutput.getOutputs().iterator());
+                    variantNameToApkTimestamp.put(
+                            variantBuildOutput.getName(), output.getOutputFile());
+                });
+        return variantNameToApkTimestamp.build();
+    }
+
+    private static Map<String, Long> getVariantNameToTimestamp(
+            Collection<VariantBuildOutput> variantsBuildOutput) {
+
+        return getVariantNameToOutputFileMap(variantsBuildOutput)
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().lastModified()));
     }
 }
