20305362ed58b810d0b3a791bffae0fafd800428
Izabela Orlowska <imorlowska@google.com>
2017-05-10 18:51:20 +0100

    Fallback to out of process AAPT2 on Windows for linking
    Fallback to out of process AAPT2 for the aapt link call if on Windows
    and using AAPT2 JNI.
    
    Test: gwt
    Change-Id: I7a512227b622a98a0135c136d366ec089ea06c4e

--- build-system/gradle-core/src/main/java/com/android/build/gradle/tasks/ProcessAndroidResources.java
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/tasks/ProcessAndroidResources.java
@@ -504,10 +504,17 @@ public class ProcessAndroidResources extends IncrementalTask {
                         symbolOutputDir,
                         proguardOutputFile);
             } else {
+                AaptGeneration aaptGen = aaptGeneration;
+                if (SdkConstants.CURRENT_PLATFORM == SdkConstants.PLATFORM_WINDOWS
+                        && aaptGeneration == AaptGeneration.AAPT_V2_JNI) {
+                    // If we're on Windows we should fall back to out of process AAPT2 instead of
+                    // AAPT2 JNI for the link phase.
+                    aaptGen = AaptGeneration.AAPT_V2;
+                }
 
                 Aapt aapt =
                         AaptGradleFactory.make(
-                                aaptGeneration,
+                                aaptGen,
                                 builder,
                                 processOutputHandler,
                                 true,
