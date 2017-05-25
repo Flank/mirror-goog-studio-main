6cd66c103d78c9c56c5da5a28c2627cdf0d0ac17
Kainan Wang <knwang@google.com>
2017-05-25 15:04:22 -0700

    Fix minor bugs for QA Android Profiler App
    The CL corrects some typos and simplifies event scenarios.
    It also fills the array to make sure that exact amount of
    memory is allocated.
    
    Change-Id: I04b0ac0bda8f8c93f6e4984ec4be01c3394064e5

---
+++ profiler/integration-tests/ProfilerTester/app/src/main/java/android/com/java/profilertester/event/EventConfigurations.java
@@ -0,0 +1,9 @@
+package android.com.java.profilertester.event;
+
+public class EventConfigurations {
+    public  enum ActionNumber {
+        BASIC_EVENTS,
+        TYPE_WORDS,
+        SWITCH_ACTIVITY
+    }
+}
