03f821de01492cafeb60bdb55acc4c3a898b080c
Ivan Gavrilovic <gavra@google.com>
2017-06-14 10:03:07 +0100

    Fix KotlinApp to compile with both android 25 and 26
    Method signature changed, and compilation fails for
    android 26. This CL fixes allows this app to be compiled
    against android-25 and android-26.
    
    Test: KotlinAppTest
    BUG=62528938
    
    Change-Id: I15fcf833f408c1e6d275c4fef611f6c54c82ab83

--- build-system/integration-test/test-projects/kotlinApp/src/main/kotlin/com/example/android/kotlin/MainActivity.kt
+++ build-system/integration-test/test-projects/kotlinApp/src/main/kotlin/com/example/android/kotlin/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.android.kotlin
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
@@ -12,13 +13,13 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_layout)
-        val tmpTxt = findViewById(R.id.someText)
+        val tmpTxt: View = findViewById(R.id.someText)
         text = tmpTxt as TextView
         if (text != null) {
             text?.setText("testing kotlin")
             android.util.Log.d("kotlin", text?.getText().toString())
         }
-        val tmpClick = findViewById(R.id.click)
+        val tmpClick: View = findViewById(R.id.click)
         val click = tmpClick as Button
         click?.setOnClickListener { text?.setText("clicked!") }
     }
