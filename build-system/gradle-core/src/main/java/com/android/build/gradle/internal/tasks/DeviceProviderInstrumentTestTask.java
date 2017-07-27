5f89be21e77cfd5b0b9f4ed6b4ddf6e7baaa2a7d
Ivan Gavrilovic <gavra@google.com>
2017-07-27 11:01:43 +0100

    Add missing/fix copyright headers
    Test: n/a
    Change-Id: I40604c77afa4ef98686198434c847a739d1a7b0d

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/tasks/DeviceProviderInstrumentTestTask.java
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/tasks/DeviceProviderInstrumentTestTask.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.android.build.gradle.internal.tasks;
 
 import static com.android.builder.core.BuilderConstants.CONNECTED;
