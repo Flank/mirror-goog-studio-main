692352b660e9a44456755d447b46bdd2c0138af4
Michal Bendowski <bendowski@google.com>
2017-08-02 16:31:13 +0100

    Fix java 6 issues
    - Stop using java 8 in builder-model tests, as gradle doesn't seem to
    support different language levels for tests.
    - Update IR BUILD files to better reflect build.gradle files.
    
    Test: ./gradlew :base:builder-model:compileTestJava
    Change-Id: Ia97486810cc71a02f20d96a4599f824a9a55ba8b

--- build-system/builder-model/src/test/java/com/android/builder/model/TestOptionsTest.java
+++ build-system/builder-model/src/test/java/com/android/builder/model/TestOptionsTest.java
@@ -1,19 +1,19 @@
 package com.android.builder.model;
 
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 import org.junit.Assert;
 import org.junit.Test;
 
+@SuppressWarnings("Convert2Diamond") // This needs to be java 6.
 public class TestOptionsTest {
 
     @Test
     public void executionEnumNames() throws Exception {
-        Set<String> valuesNames =
-                Arrays.stream(TestOptions.Execution.values())
-                        .map(Enum::name)
-                        .collect(Collectors.toSet());
+        Set<String> valuesNames = new HashSet<String>();
+        for (TestOptions.Execution enumValue : TestOptions.Execution.values()) {
+            valuesNames.add(enumValue.name());
+        }
 
         // These values are already used in the IDE, so cannot be renamed.
         Assert.assertTrue(valuesNames.contains("ANDROID_TEST_ORCHESTRATOR"));
