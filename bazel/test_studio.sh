b41f2de8afebe343e68ff0d9af519fdd89dea2fb
Peter Xu <peterx@google.com>
2017-08-16 10:37:00 -0700

    Update test_studio.sh to reference testlogs dir
    Test: n/a
    Change-Id: I920c62b1c0a90ba1ca213e62384401e7de5e91bd

--- bazel/test_studio.sh
+++ bazel/test_studio.sh
@@ -7,8 +7,7 @@ readonly dist_dir="$2"
 readonly build_number="$3"
 
 readonly script_dir="$(dirname "$0")"
-readonly testlogs_dir="$(${script_dir}/bazel info output_path)/local-fastbuild/testlogs"
-
 "${script_dir}/bazel" test --test_tag_filters=-gradle_integration --test_output=errors --test_summary=detailed $(< "${script_dir}/targets")
 
+readonly testlogs_dir="$(${script_dir}/bazel info bazel-testlogs)"
 (cd "${testlogs_dir}" && zip -qr "${dist_dir}/bazel_testlogs_${build_number}.zip" *)
