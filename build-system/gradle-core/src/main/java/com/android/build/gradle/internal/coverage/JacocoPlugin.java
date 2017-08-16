07c4afe1fe199784b55e10810752d7b25dd8074e
Ivan Gavrilovic <gavra@google.com>
2017-08-16 11:10:09 +0100

    Improve Jacoco version calculation
    This CL uses the plugin's classloader
    to get the Jacoco version that is used
    to instrument classes. In addition to that,
    resolution strategy is added that forces
    the agent runtime to be the same version as
    used for offline instrumentation.
    
    BUG=37116868
    Test: JacocoDependenciesTest
    
    Change-Id: Ieb5740b134dffb477042796a55f68f9344430164

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/coverage/JacocoPlugin.java
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/coverage/JacocoPlugin.java
@@ -16,14 +16,11 @@
 
 package com.android.build.gradle.internal.coverage;
 
-import com.android.annotations.Nullable;
-import java.util.Set;
+import com.android.annotations.NonNull;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
-import org.gradle.api.artifacts.ModuleVersionIdentifier;
-import org.gradle.api.artifacts.ResolvedArtifact;
-
+import org.jacoco.core.JaCoCo;
 
 /**
  * Jacoco plugin. This is very similar to the built-in support for Jacoco but we dup it in order
@@ -36,18 +33,17 @@ public class JacocoPlugin implements Plugin<Project> {
     public static final String ANT_CONFIGURATION_NAME = "androidJacocoAnt";
     public static final String AGENT_CONFIGURATION_NAME = "androidJacocoAgent";
 
-    /** This version must be kept in sync with the version that the gradle plugin depends on. */
-    private static final String DEFAULT_JACOCO_VERSION = "0.7.4.201502262128";
-
     private Project project;
-
-    @Nullable
-    private String jacocoVersion;
 
     @Override
     public void apply(Project project) {
         this.project = project;
         addJacocoConfigurations();
+    }
+
+    @NonNull
+    public String getAgentRuntimeDependency() {
+        return "org.jacoco:org.jacoco.agent:" + getJacocoVersion() + ":runtime";
     }
 
     /**
@@ -61,10 +57,7 @@ public class JacocoPlugin implements Plugin<Project> {
         config.setCanBeConsumed(false);
         config.setDescription("The Jacoco agent to use to get coverage data.");
 
-        project.getDependencies()
-                .add(
-                        AGENT_CONFIGURATION_NAME,
-                        "org.jacoco:org.jacoco.agent:" + getJacocoVersion() + ":runtime");
+        project.getDependencies().add(AGENT_CONFIGURATION_NAME, getAgentRuntimeDependency());
 
         config = this.project.getConfigurations().create(ANT_CONFIGURATION_NAME);
 
@@ -77,42 +70,11 @@ public class JacocoPlugin implements Plugin<Project> {
                 .add(ANT_CONFIGURATION_NAME, "org.jacoco:org.jacoco.ant:" + getJacocoVersion());
     }
 
-    @Nullable
+    @NonNull
     private String getJacocoVersion() {
-        if (jacocoVersion != null) {
-            return jacocoVersion;
-        }
-
-        Project candidateProject = project;
-        boolean shouldFailWithException = false;
-
-        while (candidateProject != null) {
-            Set<ResolvedArtifact> resolvedArtifacts =
-                    candidateProject.getBuildscript().getConfigurations().getByName("classpath")
-                            .getResolvedConfiguration().getResolvedArtifacts();
-            for (ResolvedArtifact artifact : resolvedArtifacts) {
-                ModuleVersionIdentifier moduleVersion = artifact.getModuleVersion().getId();
-                if ("org.jacoco.core".equals(moduleVersion.getName())) {
-                    jacocoVersion = moduleVersion.getVersion();
-                    return jacocoVersion;
-                }
-            }
-            if (!resolvedArtifacts.isEmpty()) {
-                // not in the DSL test case, where nothing will have been resolved.
-                shouldFailWithException = true;
-            }
-
-            candidateProject = candidateProject.getParent();
-        }
-
-        if (shouldFailWithException) {
-            throw new IllegalStateException(
-                    "Could not find project build script dependency on org.jacoco.core");
-        }
-
-        project.getLogger().error(
-                "No resolved dependencies found when searching for the jacoco version.");
-        jacocoVersion = DEFAULT_JACOCO_VERSION;
-        return jacocoVersion;
+        // Version of Jacoco might not be the one AGP depends on, as it can be changed
+        // by adding another classpath dependency. To get the actual runtime version, we
+        // use Jacoco itself to extract info about its version.
+        return JaCoCo.VERSION;
     }
 }
