bfee2b1f1d46aab362e0f40d5fdbbef99711fbcd
Amaury Medeiros <amaurym@google.com>
2017-08-25 10:27:43 +0100

    Minor cleanup in process_manager.h
    Fix minor documentation typo and run clang-format in the file.
    
    Test: N/A
    Change-Id: I46fc3dfa7a2e315a7f4893d6fe86c21df0f198cb

--- profiler/native/utils/process_manager.h
+++ profiler/native/utils/process_manager.h
@@ -26,8 +26,8 @@ namespace profiler {
 // Record storing all information retrieved from /proc/pids folders
 struct Process {
  public:
-  Process(pid_t pid, const std::string &cmdline,
-          const std::string &binary_name);
+  Process(pid_t pid, const std::string& cmdline,
+          const std::string& binary_name);
   pid_t pid;
   std::string cmdline;
   std::string binary_name;
@@ -40,7 +40,7 @@ class ProcessManager {
   // This method purpose is to match an app with a process id and the
   // expectation is that only one app with this package name will be running.
   // Therefore, it returns the first match.
-  int GetPidForBinary(const std::string &binary_name) const;
+  int GetPidForBinary(const std::string& binary_name) const;
 
   // Return true is process |pid| is currently running (present in /proc).
   bool IsPidAlive(int pid) const;
@@ -48,11 +48,11 @@ class ProcessManager {
   static std::string GetCmdlineForPid(int pid);
 
   // Get the package name associate with the application name. If the
-  // application of interest is a service running as its own process, its'
+  // application of interest is a service running as its own process, its
   // app_name can be of the format PACKAGE_NAME:SERVICE_NAME. We need
   // to extract the package name for operations like run-as and data folder path
   // retrieval, which works on the package instead of the app.
-  static std::string GetPackageNameFromAppName(const std::string &app_name);
+  static std::string GetPackageNameFromAppName(const std::string& app_name);
 
   static std::string GetAttachAgentCommand();
 
