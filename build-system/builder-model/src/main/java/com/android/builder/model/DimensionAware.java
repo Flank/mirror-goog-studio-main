3abbbbaf227fe5db3b1526a6d771ec1217d12a1c
Adarsh Fernando <adarshf@google.com>
2017-09-01 15:07:38 -0700

    Docs: ProductFlavor and BuildType doc scrub for plugin 3.0.0
    - Focuses on only the DSL properties that have new/changed behavior
      in 3.0.0. Doc scrub for all other properties will be included in a
      follow-up CL.
    
    Test: ./gradlew base:docs:dslHtml (docs only)
    bug: 32490371
    Change-Id: Iba093adb0f594238015c10934b77e5b22dbcffd3
    (cherry picked from commit 0c49d21e80d836347d1be1e61342eb7de37b68a8)

--- build-system/builder-model/src/main/java/com/android/builder/model/DimensionAware.java
+++ build-system/builder-model/src/main/java/com/android/builder/model/DimensionAware.java
@@ -24,7 +24,65 @@ import com.android.annotations.Nullable;
 public interface DimensionAware {
 
     /**
-     * Returns the dimension or null if the object do not have a dimension.
+     * Specifies the flavor dimension that this product flavor belongs to.
+     *
+     * <p>When configuring product flavors with Android plugin 3.0.0 and higher, you must specify at
+     * least one flavor dimension, using the <a
+     * href="com.android.build.gradle.BaseExtension.html#com.android.build.gradle.BaseExtension:flavorDimensions(java.lang.String[])">
+     * <code>flavorDimensions</code></a> property, and then assign each flavor to a dimension.
+     * Otherwise, you will get the following build error:
+     *
+     * <pre>
+     * Error:All flavors must now belong to a named flavor dimension.
+     * The flavor 'flavor_name' is not assigned to a flavor dimension.
+     * </pre>
+     *
+     * <p>By default, when you specify only one dimension, all flavors you configure automatically
+     * belong to that dimension. If you specify more than one dimension, you need to manually assign
+     * each flavor to a dimension, as shown in the sample below:
+     *
+     * <pre>
+     * android {
+     *     ...
+     *     // Specifies the flavor dimensions you want to use. The order in which you
+     *     // list each dimension determines its priority, from highest to lowest,
+     *     // when Gradle merges variant sources and configurations. You must assign
+     *     // each product flavor you configure to one of the flavor dimensions.
+     *     flavorDimensions 'api', 'version'
+     *
+     *     productFlavors {
+     *       demo {
+     *         // Assigns this product flavor to the 'version' flavor dimension.
+     *         dimension 'version'
+     *         ...
+     *     }
+     *
+     *       full {
+     *         dimension 'version'
+     *         ...
+     *       }
+     *
+     *       minApi24 {
+     *         // Assigns this flavor to the 'api' dimension.
+     *         dimension 'api'
+     *         minSdkVersion '24'
+     *         versionNameSuffix "-minApi24"
+     *         ...
+     *       }
+     *
+     *       minApi21 {
+     *         dimension "api"
+     *         minSdkVersion '21'
+     *         versionNameSuffix "-minApi21"
+     *         ...
+     *       }
+     *    }
+     * }
+     * </pre>
+     *
+     * <p>To learn more about configuring flavor dimensions, read <a
+     * href="https://developer.android.com/studio/build/build-variants.html#flavor-dimensions">
+     * Combine multiple flavors</a>.
      */
     @Nullable
     String getDimension();
