429e6a8f139e46fc5e786cae3d5b8baf0ab8f687
Ying Chen <chying@google.com>
2017-10-06 13:14:35 -0700

    Make JavaModelBuilder to support all versions of Gradle.
    JavaModelBuilder used to call some APIs that are not available
    in old versions of Gradle. For example,
    SourceSet.getCompileClasspathConfigurationName is only available
    since 2.12, and ResolvedArtifact.getId is available since 2.6.
    
    With this change, the code checks for gradle version before calling
    these APIs, if current version is older than expected, then use
    alternative ways.
    Although we can make the code always use the old APIs and avoid version
    check, the new APIs should perform better than the old ones, and also
    more reliable.
    
    Bug: 67420000
    Test: JavaModelBuilderTest, GradleSyncWithOlderPluginTest
    Change-Id: Ib6bfe78f7211487fabaf907ebfcb06884ffa5eb6

--- build-system/java-lib-plugin/java-lib-model-builder/src/main/java/com/android/java/model/builder/JavaModelBuilder.java
+++ build-system/java-lib-plugin/java-lib-model-builder/src/main/java/com/android/java/model/builder/JavaModelBuilder.java
@@ -16,6 +16,7 @@
 
 package com.android.java.model.builder;
 
+import com.android.annotations.VisibleForTesting;
 import com.android.java.model.JavaLibrary;
 import com.android.java.model.JavaProject;
 import com.android.java.model.SourceSet;
@@ -23,22 +24,19 @@ import com.android.java.model.impl.JavaLibraryImpl;
 import com.android.java.model.impl.JavaProjectImpl;
 import com.android.java.model.impl.SourceSetImpl;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Set;
 import org.gradle.api.Project;
-import org.gradle.api.artifacts.Configuration;
-import org.gradle.api.artifacts.Dependency;
-import org.gradle.api.artifacts.ProjectDependency;
-import org.gradle.api.artifacts.ResolvedArtifact;
-import org.gradle.api.artifacts.SelfResolvingDependency;
-import org.gradle.api.artifacts.UnresolvedDependency;
+import org.gradle.api.artifacts.*;
 import org.gradle.api.artifacts.component.ComponentIdentifier;
 import org.gradle.api.artifacts.component.ProjectComponentIdentifier;
 import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.api.plugins.JavaPluginConvention;
 import org.gradle.api.specs.Specs;
 import org.gradle.tooling.provider.model.ToolingModelBuilder;
+import org.gradle.util.VersionNumber;
 
 /**
  * Builder for the custom Java library model.
@@ -58,8 +56,8 @@ public class JavaModelBuilder implements ToolingModelBuilder {
         if (!project.getPlugins().hasPlugin(JavaPlugin.class)) {
             return null;
         }
-        JavaPluginConvention javaPlugin = project.getConvention()
-                .findPlugin(JavaPluginConvention.class);
+        JavaPluginConvention javaPlugin =
+                project.getConvention().findPlugin(JavaPluginConvention.class);
 
         Collection<SourceSet> sourceSets = new ArrayList<>();
         for (org.gradle.api.tasks.SourceSet sourceSet : javaPlugin.getSourceSets()) {
@@ -67,52 +65,64 @@ public class JavaModelBuilder implements ToolingModelBuilder {
         }
 
         return new JavaProjectImpl(
-                project.getName(),
-                sourceSets,
-                javaPlugin.getSourceCompatibility().toString());
+                project.getName(), sourceSets, javaPlugin.getSourceCompatibility().toString());
     }
 
-    private static SourceSet createSourceSets(Project project,
-            org.gradle.api.tasks.SourceSet sourceSet) {
+    private static SourceSet createSourceSets(
+            Project project, org.gradle.api.tasks.SourceSet sourceSet) {
+        String compileConfigurationName;
+        if (isGradleAtLeast(project.getGradle().getGradleVersion(), "2.12")) {
+            compileConfigurationName = sourceSet.getCompileClasspathConfigurationName();
+        } else {
+            compileConfigurationName = sourceSet.getCompileConfigurationName();
+        }
         return new SourceSetImpl(
                 sourceSet.getName(),
                 sourceSet.getAllJava().getSrcDirs(),
                 sourceSet.getResources().getSrcDirs(),
                 sourceSet.getOutput().getClassesDir(),
                 sourceSet.getOutput().getResourcesDir(),
-                getLibrariesForConfiguration(project.getConfigurations()
-                        .getAt(sourceSet.getCompileClasspathConfigurationName()))
-        );
+                getLibrariesForConfiguration(project, compileConfigurationName));
+    }
+
+    @VisibleForTesting
+    static boolean isGradleAtLeast(String gradleVersion, String expectedVersion) {
+        VersionNumber currentVersion = VersionNumber.parse(gradleVersion);
+        VersionNumber givenVersion = VersionNumber.parse(expectedVersion);
+        return currentVersion.compareTo(givenVersion) >= 0;
     }
 
     private static Collection<JavaLibrary> getLibrariesForConfiguration(
-            Configuration configuration) {
+            Project project, String configurationName) {
+        Configuration configuration = project.getConfigurations().getAt(configurationName);
         Collection<JavaLibrary> javaLibraries = new ArrayList<>();
 
         // Since this plugin is always called from IDE, it should not break on unresolved dependencies.
-        Set<ResolvedArtifact> allArtifacts = configuration.getResolvedConfiguration()
-                .getLenientConfiguration()
-                .getArtifacts(Specs.satisfyAll());
+        Set<ResolvedArtifact> allArtifacts =
+                configuration
+                        .getResolvedConfiguration()
+                        .getLenientConfiguration()
+                        .getArtifacts(Specs.satisfyAll());
         for (ResolvedArtifact artifact : allArtifacts) {
-            String projectPath = null;
-            File jarFile = null;
-            ComponentIdentifier id = artifact.getId().getComponentIdentifier();
-            if (id instanceof ProjectComponentIdentifier) {
-                projectPath = ((ProjectComponentIdentifier) id).getProjectPath().intern();
-            } else {
-                jarFile = artifact.getFile();
-            }
-            javaLibraries.add(new JavaLibraryImpl(projectPath, artifact.getName().intern(), jarFile));
+            String projectPath = getProjectPath(project, artifact);
+            File jarFile = artifact.getFile();
+            javaLibraries.add(
+                    new JavaLibraryImpl(projectPath, artifact.getName().intern(), jarFile));
         }
 
         // Add unresolved dependencies, mark by adding prefix UNRESOLVED_DEPENDENCY_PREFIX to name.
         // This follows idea plugin.
-        Set<UnresolvedDependency> unresolvedDependencies = configuration.getResolvedConfiguration()
-                .getLenientConfiguration().getUnresolvedModuleDependencies();
+        Set<UnresolvedDependency> unresolvedDependencies =
+                configuration
+                        .getResolvedConfiguration()
+                        .getLenientConfiguration()
+                        .getUnresolvedModuleDependencies();
         for (UnresolvedDependency dependency : unresolvedDependencies) {
-            String unresolvedName = UNRESOLVED_DEPENDENCY_PREFIX + dependency.getSelector()
-                    .toString().replaceAll(":", " ");
-            javaLibraries.add(new JavaLibraryImpl(null, unresolvedName.intern(), new File(unresolvedName)));
+            String unresolvedName =
+                    UNRESOLVED_DEPENDENCY_PREFIX
+                            + dependency.getSelector().toString().replaceAll(":", " ");
+            javaLibraries.add(
+                    new JavaLibraryImpl(null, unresolvedName.intern(), new File(unresolvedName)));
         }
 
         // Collect jars from local directory
@@ -126,5 +136,40 @@ public class JavaModelBuilder implements ToolingModelBuilder {
             }
         }
         return javaLibraries;
+    }
+
+    /** Returns project path if artifact is a module dependency, returns null otherwise. */
+    private static String getProjectPath(Project project, ResolvedArtifact artifact) {
+        if (isGradleAtLeast(project.getGradle().getGradleVersion(), "2.6")) {
+            ComponentIdentifier id = artifact.getId().getComponentIdentifier();
+            if (id instanceof ProjectComponentIdentifier) {
+                return ((ProjectComponentIdentifier) id).getProjectPath().intern();
+            }
+        } else {
+            Set<Project> allProjects = project.getRootProject().getAllprojects();
+            for (Project subProject : allProjects) {
+                if (contains(subProject.getBuildDir(), artifact.getFile())) {
+                    return subProject.getPath().intern();
+                }
+            }
+        }
+        return null;
+    }
+
+    /** Returns true if file is inside of directory or any of its sub-directories. */
+    private static boolean contains(File directory, File file) {
+        try {
+            File canonicalFile = file.getCanonicalFile().getParentFile();
+            File canonicalDirectory = directory.getCanonicalFile();
+            while (canonicalFile != null) {
+                if (canonicalFile.equals(canonicalDirectory)) {
+                    return true;
+                }
+                canonicalFile = canonicalFile.getParentFile();
+            }
+        } catch (IOException ex) {
+            return false;
+        }
+        return false;
     }
 }
