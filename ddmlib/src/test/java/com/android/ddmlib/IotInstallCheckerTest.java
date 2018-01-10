49afac0e01d2d65f6eaca002a14e18a3905b7d8a
Anh Tuan Nguyen <anhtnguyen@google.com>
2018-01-10 11:47:30 -0800

    Fix IotInstallChecker to only collect third-party apps.
    Android Studio 3.1 Preview proposes to uninstall existing apps with
    intent filter IOT_LAUNCHER when installing a new Things app. However,
    it does so even for system apps like IoTLauncher or oem apps with that
    filter. Instead, Android Studio should suggest to only uninstall
    third-party apps with that intent filter.
    Even though we have temporarily disabled the uninstall feature, we
    plan to bring it back, and this logic of only uninstalling third-party
    apps still applies.
    
    Test: IotInstallCheckerTest
    Bug: 71806709
    Change-Id: Id5a2aa3eecb1ed0d29c2b3c946cfe46090d3ff72

--- ddmlib/src/test/java/com/android/ddmlib/IotInstallCheckerTest.java
+++ ddmlib/src/test/java/com/android/ddmlib/IotInstallCheckerTest.java
@@ -25,36 +25,122 @@ import java.io.IOException;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import org.junit.Test;
+import org.mockito.internal.util.StringUtil;
 
 public class IotInstallCheckerTest {
-    private static final String[] DUMPSYS_RESULT_WITHOUT_IOT_PACKAGE =
+    private static final String[] DUMPSYS_RESULT_WITHOUT_THIRD_PARTY_LAUNCHERS =
             new String[] {
+                "Activity Resolver Table:",
+                "Non-Data Actions:",
                 "android.intent.action.MAIN:",
-                "abc49e0 com.android.settings/.Settings$WifiCallingSuggestionActivity filter 7470c66",
+                "a59037c com.android.iotlauncher/.IoTHome filter adae581",
                 "Action: \"android.intent.action.MAIN\"",
-                "Category: \"com.android.settings.suggested.category.SETTINGS_ONLY\"",
-                "AutoVerify=false"
-            };
-    private static final String[] DUMPSYS_RESULT_WITH_IOT_PACKAGE =
-            new String[] {
-                "android.intent.action.MAIN:",
-                "abc49e0 com.android.settings/.Settings$WifiCallingSuggestionActivity filter 7470c66",
-                "Action: \"android.intent.action.MAIN\"",
-                "Category: \"com.android.settings.suggested.category.SETTINGS_ONLY\"",
-                "AutoVerify=false",
-                "3ede499 com.anhtnguyen.twinkle/.MainActivity filter b91ee63",
+                "Category: \"android.intent.category.HOME\"",
+                "Category: \"android.intent.category.DEFAULT\"",
+                "4de7405 com.android.iotlauncher/.DefaultIoTLauncher filter 3e73626",
                 "Action: \"android.intent.action.MAIN\"",
                 "Category: \"android.intent.category.IOT_LAUNCHER\"",
                 "Category: \"android.intent.category.DEFAULT\"",
-                "AutoVerify=false"
+                "Category: \"android.intent.category.MONKEY\"",
+                "mPriority=-1000, mHasPartialTypes=false",
+                "2ac4614 com.anhtnguyen.twinkle/.MainActivity filter 15a0d29",
+                "Action: \"android.intent.action.MAIN\"",
+                "Category: \"android.intent.category.LAUNCHER\"",
+                "2ac4614 com.anhtnguyen.twinkle/.MainActivity filter a1048ae",
+                "Action: \"android.intent.action.MAIN\"",
+                "Category: \"android.intent.category.IOT_LAUNCHER\"",
+                "Category: \"android.intent.category.DEFAULT\"",
+                "Packages:",
+                "Package [com.android.iotlauncher] (8cd1023):",
+                "userId=10034",
+                "flags=[ SYSTEM DEBUGGABLE HAS_CODE ALLOW_CLEAR_USER_DATA TEST_ONLY ALLOW_BACKUP ]",
+                "Package [com.anhtnguyen.twinkle] (8cd1023):",
+                "userId=10034",
+                "pkg=Package{84cb920 com.anhtnguyen.twinkle}",
+                "codePath=/oem/app/app-debug.apk",
+                "resourcePath=/oem/app/app-debug.apk",
+                "legacyNativeLibraryDir=/oem/lib/app-debug",
+                "primaryCpuAbi=null",
+                "secondaryCpuAbi=null",
+                "versionCode=1 minSdk=26 targetSdk=26",
+                "versionName=1.0",
+                "splits=[base]",
+                "apkSigningVersion=2",
+                "applicationInfo=ApplicationInfo{7d2c2d9 com.anhtnguyen.twinkle}",
+                "flags=[ SYSTEM DEBUGGABLE HAS_CODE ALLOW_CLEAR_USER_DATA TEST_ONLY ALLOW_BACKUP ]",
+                "privateFlags=[ PRIVATE_FLAG_ACTIVITIES_RESIZE_MODE_RESIZEABLE_VIA_SDK_VERSION ]",
+                "dataDir=/data/user/0/com.anhtnguyen.twinkle",
             };
+    private static final String[] DUMPSYS_RESULT_WITH_THIRD_PARTY_LAUNCHERS =
+            new String[] {
+                "Activity Resolver Table:",
+                "Non-Data Actions:",
+                "android.intent.action.MAIN:",
+                "a59037c com.android.iotlauncher/.IoTHome filter adae581",
+                "Action: \"android.intent.action.MAIN\"",
+                "Category: \"android.intent.category.HOME\"",
+                "Category: \"android.intent.category.DEFAULT\"",
+                "4de7405 com.android.iotlauncher/.DefaultIoTLauncher filter 3e73626",
+                "Action: \"android.intent.action.MAIN\"",
+                "Category: \"android.intent.category.IOT_LAUNCHER\"",
+                "Category: \"android.intent.category.DEFAULT\"",
+                "Category: \"android.intent.category.MONKEY\"",
+                "mPriority=-1000, mHasPartialTypes=false",
+                "2ac4614 com.anhtnguyen.twinkle/.MainActivity filter 15a0d29",
+                "Action: \"android.intent.action.MAIN\"",
+                "Category: \"android.intent.category.LAUNCHER\"",
+                "2ac4614 com.anhtnguyen.twinkle/.MainActivity filter a1048ae",
+                "Action: \"android.intent.action.MAIN\"",
+                "Category: \"android.intent.category.IOT_LAUNCHER\"",
+                "Category: \"android.intent.category.DEFAULT\"",
+                "9b25933 com.example.anhtnguyen.dummyapp/.MainActivity filter a8f9fef",
+                "Action: \"android.intent.action.MAIN\"",
+                "Category: \"android.intent.category.LAUNCHER\"",
+                "9b25933 com.example.anhtnguyen.dummyapp/.MainActivity filter 3bc1afc",
+                "Action: \"android.intent.action.MAIN\"",
+                "Category: \"android.intent.category.IOT_LAUNCHER\"",
+                "Category: \"android.intent.category.DEFAULT\"",
+                "Packages:",
+                "Package [com.android.iotlauncher] (8cd1023):",
+                "userId=10034",
+                "flags=[ SYSTEM DEBUGGABLE HAS_CODE ALLOW_CLEAR_USER_DATA TEST_ONLY ALLOW_BACKUP ]",
+                "Package [com.anhtnguyen.twinkle] (8cd1023):",
+                "userId=10034",
+                "pkg=Package{84cb920 com.anhtnguyen.twinkle}",
+                "codePath=/oem/app/app-debug.apk",
+                "resourcePath=/oem/app/app-debug.apk",
+                "legacyNativeLibraryDir=/oem/lib/app-debug",
+                "primaryCpuAbi=null",
+                "secondaryCpuAbi=null",
+                "versionCode=1 minSdk=26 targetSdk=26",
+                "versionName=1.0",
+                "splits=[base]",
+                "apkSigningVersion=2",
+                "applicationInfo=ApplicationInfo{7d2c2d9 com.anhtnguyen.twinkle}",
+                "flags=[ DEBUGGABLE HAS_CODE ALLOW_CLEAR_USER_DATA TEST_ONLY ALLOW_BACKUP ]",
+                "privateFlags=[ PRIVATE_FLAG_ACTIVITIES_RESIZE_MODE_RESIZEABLE_VIA_SDK_VERSION ]",
+                "dataDir=/data/user/0/com.anhtnguyen.twinkle",
+            };
+    private static final String IOT_LAUNCHER = "com.android.iotlauncher";
     private static final String PACKAGE_TO_BE_INSTALLED = "com.anhtnguyen.twinkle";
+    private static final String DUMMY_THIRD_PARTY_PACKAGE = "com.example.anhtnguyen.dummyapp";
 
     @Test
-    public void testCustomReceiver() {
-        IotInstallChecker.IntentListReceiver receiver = new IotInstallChecker.IntentListReceiver();
-        receiver.processNewLines(DUMPSYS_RESULT_WITH_IOT_PACKAGE);
-        assertThat(receiver.getPackagesWithIotLauncher()).containsExactly("com.anhtnguyen.twinkle");
+    public void testIntentListReceiver() {
+        System.out.println(StringUtil.join(DUMPSYS_RESULT_WITH_THIRD_PARTY_LAUNCHERS));
+        IotInstallChecker.LauncherPackagesReceiver receiver =
+                new IotInstallChecker.LauncherPackagesReceiver();
+        receiver.processNewLines(DUMPSYS_RESULT_WITH_THIRD_PARTY_LAUNCHERS);
+        assertThat(receiver.getMatchingPackages())
+                .containsExactly(IOT_LAUNCHER, PACKAGE_TO_BE_INSTALLED, DUMMY_THIRD_PARTY_PACKAGE);
+    }
+
+    @Test
+    public void testSystemPackagesReceiver() {
+        IotInstallChecker.SystemPackagesReceiver receiver =
+                new IotInstallChecker.SystemPackagesReceiver();
+        receiver.processNewLines(DUMPSYS_RESULT_WITH_THIRD_PARTY_LAUNCHERS);
+        assertThat(receiver.getMatchingPackages()).containsExactly(IOT_LAUNCHER);
     }
 
     @Test
@@ -68,6 +154,7 @@ public class IotInstallCheckerTest {
     public void testCheckNoIotPackageInstalled() {
         IDevice device = createMockDevice(true, false);
         Set<String> packages = (new IotInstallChecker()).getInstalledIotLauncherApps(device);
+        // Should not contain IOT_LAUNCHER nor PACKAGE_TO_BE_INSTALLED since they are system apps.
         assertThat(packages).isEmpty();
     }
 
@@ -75,12 +162,13 @@ public class IotInstallCheckerTest {
     public void testCheckIotPackageAlreadyInstalled() {
         IDevice device = createMockDevice(true, true);
         Set<String> packages = (new IotInstallChecker()).getInstalledIotLauncherApps(device);
-        assertThat(packages).containsExactly(PACKAGE_TO_BE_INSTALLED);
+        // Should not contain IOT_LAUNCHER since it is a system app.
+        assertThat(packages).containsExactly(PACKAGE_TO_BE_INSTALLED, DUMMY_THIRD_PARTY_PACKAGE);
     }
 
     /** Helper method that creates a mock device. */
     static IDevice createMockDevice(
-            boolean supportsEmbedded, boolean isIotPackageAlreadyInstalled) {
+            boolean supportsEmbedded, boolean isThirdPartyLauncherPackageAlreadyInstalled) {
         IDevice mockDevice = mock(IDevice.class);
         when(mockDevice.supportsFeature(IDevice.HardwareFeature.EMBEDDED))
                 .thenReturn(supportsEmbedded);
@@ -92,9 +180,9 @@ public class IotInstallCheckerTest {
                                     IShellOutputReceiver receiver =
                                             (IShellOutputReceiver) invocation.getArguments()[1];
                                     String[] output =
-                                            isIotPackageAlreadyInstalled
-                                                    ? DUMPSYS_RESULT_WITH_IOT_PACKAGE
-                                                    : DUMPSYS_RESULT_WITHOUT_IOT_PACKAGE;
+                                            isThirdPartyLauncherPackageAlreadyInstalled
+                                                    ? DUMPSYS_RESULT_WITH_THIRD_PARTY_LAUNCHERS
+                                                    : DUMPSYS_RESULT_WITHOUT_THIRD_PARTY_LAUNCHERS;
                                     byte[] bytes = String.join("\n", output).getBytes();
                                     receiver.addOutput(bytes, 0, bytes.length);
                                     return null;
