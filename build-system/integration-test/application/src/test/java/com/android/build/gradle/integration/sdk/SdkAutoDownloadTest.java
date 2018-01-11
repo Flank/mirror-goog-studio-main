2cc6547454e48517493edfc6c943efe7985d4f62
Ivan Gavrilovic <gavra@google.com>
2018-01-11 12:57:01 +0000

    Updated SDK with build-tools;27.0.3
    Change-Id: I077a0d517f9941a7859c7777e52a986d30007ddc

--- build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/sdk/SdkAutoDownloadTest.java
+++ build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/sdk/SdkAutoDownloadTest.java
@@ -71,7 +71,7 @@ public class SdkAutoDownloadTest {
             + System.lineSeparator()
             + "target_link_libraries(hello-jni log)";
 
-    private static final String BUILD_TOOLS_VERSION = "27.0.1";
+    private static final String BUILD_TOOLS_VERSION = "27.0.3";
     private static final String PLATFORM_VERSION = "26";
 
     @Rule
