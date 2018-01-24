52bc6070b70756ffd441a85e1c695c948f20e19d
Raymond Chiu <chiur@google.com>
2018-01-23 17:16:59 -0800

    Fix missing striping for deprecated ABI
    Test: existing tests (e.g. CmakeBasicProjectTest) with updated NDK to r16.
    Change-Id: I45964bbf112489a7abf3734f14c43d5580ade27e

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/transforms/StripDebugSymbolTransform.java
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/transforms/StripDebugSymbolTransform.java
@@ -85,7 +85,7 @@ public class StripDebugSymbolTransform extends Transform {
         this.isLibrary = isLibrary;
         checkArgument(ndkHandler.isConfigured());
 
-        for (Abi abi : ndkHandler.getDefaultAbis()) {
+        for (Abi abi : ndkHandler.getSupportedAbis()) {
             stripExecutables.put(abi, ndkHandler.getStripExecutable(abi));
         }
         this.project = project;
