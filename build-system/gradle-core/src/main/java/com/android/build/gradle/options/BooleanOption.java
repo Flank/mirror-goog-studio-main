761e855f49b049536ae56bca4ab3c0d635574593
Ivan Gavrilovic <gavra@google.com>
2018-02-01 12:19:08 +0000

    Enable d8 desugaring
    Test: existing
    Change-Id: I4135680e8e97c4f4f3e3708822552224b24c8923

--- build-system/gradle-core/src/main/java/com/android/build/gradle/options/BooleanOption.java
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/options/BooleanOption.java
@@ -41,7 +41,7 @@ public enum BooleanOption implements Option<Boolean> {
     ENABLE_CORE_LAMBDA_STUBS("android.enableCoreLambdaStubs", true),
 
     ENABLE_D8("android.enableD8", true, DeprecationReporter.DeprecationTarget.LEGACY_DEXER),
-    ENABLE_D8_DESUGARING("android.enableD8.desugaring", false),
+    ENABLE_D8_DESUGARING("android.enableD8.desugaring", true),
     ENABLE_D8_MAIN_DEX_LIST(
             "android.enableD8MainDexList",
             true,
