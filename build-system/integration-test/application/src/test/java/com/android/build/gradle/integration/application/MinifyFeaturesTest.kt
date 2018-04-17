546eeeaeb372d33ffdb4a8c8dad7a72aac515d88
Scott Pollom <spollom@google.com>
2018-04-17 09:56:35 -0700

    Use classes.dex from base when packaging features
    This CL is the 6th in a series to provide proguard/R8
    support for multi-APK projects.
    
    This CL changes PackageAndroidArtifact to use the
    classes.dex files published from the base module when
    the DexSplitterTransform is run.
    
    Test: MinifyFeaturesTest
    Bug: 74433581
    Change-Id: I9c85651683f6aea524301fc606df9dfd5fd23ff0

--- build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/application/MinifyFeaturesTest.kt
+++ build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/application/MinifyFeaturesTest.kt
@@ -23,7 +23,6 @@ import com.android.build.gradle.integration.common.runner.FilterableParameterize
 import com.android.build.gradle.integration.common.truth.TruthHelper.assertThat
 import com.android.build.gradle.internal.scope.CodeShrinker
 import com.android.build.gradle.options.BooleanOption
-import com.android.build.gradle.tasks.ResourceUsageAnalyzer
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -35,16 +34,18 @@ import org.junit.runners.Parameterized
  * Project roughly structured as follows (see implementation below for exact structure) :
  *
  * <pre>
- *   instantApp  --->                --->  library2  ------>
- *                     otherFeature                           library1
- *   app  ---------->                --->  baseFeature  --->
+ *                  --->  library2  ------>
+ *   otherFeature1                           library1
+ *                  --->  baseFeature  --->
+ *   otherFeature2
  *
  * More explicitly,
- *   instantApp  depends on  otherFeature, baseFeature
- *          app  depends on  otherFeature, baseFeature
- * otherFeature  depends on  library2, baseFeature
- *  baseFeature  depends on  library1
- *     library2  depends on  library1
+ *    instantApp  depends on  otherFeature1, otherFeature2, baseFeature  (not pictured)
+ *           app  depends on  otherFeature1, otherFeature2, baseFeature  (not pictured)
+ * otherFeature1  depends on  library2, baseFeature
+ * otherFeature2  depends on  baseFeature
+ *   baseFeature  depends on  library1
+ *      library2  depends on  library1
  * </pre>
  */
 @RunWith(FilterableParameterized::class)
@@ -186,15 +187,15 @@ class MinifyFeaturesTest(val codeShrinker: CodeShrinker, useDexArchive: Boolean)
                             "proguard-rules.pro",
                             """-keep public class com.example.baseFeature.EmptyClassToKeep""")
 
-    private val otherFeature =
-            MinimalSubProject.feature("com.example.otherFeature")
+    private val otherFeature1 =
+            MinimalSubProject.feature("com.example.otherFeature1")
                     .appendToBuild(
                             "android { buildTypes { minified { initWith(buildTypes.debug) }}}")
                     .withFile(
                             "src/main/AndroidManifest.xml",
                             """<?xml version="1.0" encoding="utf-8"?>
                             <manifest xmlns:android="http://schemas.android.com/apk/res/android"
-                                  package="com.example.otherFeature">
+                                  package="com.example.otherFeature1">
                                 <application android:label="app_name">
                                     <activity android:name=".Main"
                                               android:label="app_name">
@@ -224,10 +225,10 @@ class MinifyFeaturesTest(val codeShrinker: CodeShrinker, useDexArchive: Boolean)
                                         android:text=""
                                         android:id="@+id/dateText" />
                             </LinearLayout>""")
-                    .withFile("src/main/resources/other_java_res.txt", "other")
+                    .withFile("src/main/resources/other_java_res_1.txt", "other")
                     .withFile(
-                            "src/main/java/com/example/otherFeature/Main.java",
-                            """package com.example.otherFeature;
+                            "src/main/java/com/example/otherFeature1/Main.java",
+                            """package com.example.otherFeature1;
 
                             import android.app.Activity;
                             import android.os.Bundle;
@@ -263,9 +264,94 @@ class MinifyFeaturesTest(val codeShrinker: CodeShrinker, useDexArchive: Boolean)
                                 }
                             }""")
                     .withFile(
-                            "src/main/java/com/example/otherFeature/EmptyClassToRemove.java",
-                            """package com.example.otherFeature;
+                            "src/main/java/com/example/otherFeature1/EmptyClassToKeep.java",
+                            """package com.example.otherFeature1;
+                            public class EmptyClassToKeep {
+                            }""")
+                    .withFile(
+                            "src/main/java/com/example/otherFeature1/EmptyClassToRemove.java",
+                            """package com.example.otherFeature1;
                             public class EmptyClassToRemove {
+                            }""")
+                    .withFile(
+                            "proguard-rules.pro",
+                            """-keep public class com.example.otherFeature1.EmptyClassToKeep""")
+
+    private val otherFeature2 =
+        MinimalSubProject.feature("com.example.otherFeature2")
+            .appendToBuild(
+                "android { buildTypes { minified { initWith(buildTypes.debug) }}}")
+            .withFile(
+                "src/main/AndroidManifest.xml",
+                """<?xml version="1.0" encoding="utf-8"?>
+                            <manifest xmlns:android="http://schemas.android.com/apk/res/android"
+                                  package="com.example.otherFeature2">
+                                <application android:label="app_name">
+                                    <activity android:name=".Main"
+                                              android:label="app_name">
+                                        <intent-filter>
+                                            <action android:name="android.intent.action.MAIN" />
+                                            <category android:name="android.intent.category.LAUNCHER" />
+                                        </intent-filter>
+                                    </activity>
+                                </application>
+                            </manifest>""")
+            .withFile(
+                "src/main/res/layout/other_main.xml",
+                """<?xml version="1.0" encoding="utf-8"?>
+                            <LinearLayout
+                                    xmlns:android="http://schemas.android.com/apk/res/android"
+                                    android:orientation="vertical"
+                                    android:layout_width="fill_parent"
+                                    android:layout_height="fill_parent" >
+                                <TextView
+                                        android:layout_width="fill_parent"
+                                        android:layout_height="wrap_content"
+                                        android:text="Test App - Basic"
+                                        android:id="@+id/text" />
+                                <TextView
+                                        android:layout_width="fill_parent"
+                                        android:layout_height="wrap_content"
+                                        android:text=""
+                                        android:id="@+id/dateText" />
+                            </LinearLayout>""")
+            .withFile("src/main/resources/other_java_res_2.txt", "other")
+            .withFile(
+                "src/main/java/com/example/otherFeature2/Main.java",
+                """package com.example.otherFeature2;
+
+                            import android.app.Activity;
+                            import android.os.Bundle;
+                            import android.widget.TextView;
+
+                            import java.lang.Exception;
+                            import java.lang.RuntimeException;
+
+                            import com.example.baseFeature.StringProvider;
+
+                            public class Main extends Activity {
+
+                                private int foo = 1234;
+
+                                private final StringProvider stringProvider = new StringProvider();
+
+                                /** Called when the activity is first created. */
+                                @Override
+                                public void onCreate(Bundle savedInstanceState) {
+                                    super.onCreate(savedInstanceState);
+                                    setContentView(R.layout.other_main);
+
+                                    TextView tv = (TextView) findViewById(R.id.dateText);
+                                    tv.setText(getStringProvider().getString(foo));
+                                }
+
+                                public StringProvider getStringProvider() {
+                                    return stringProvider;
+                                }
+
+                                public void handleOnClick(android.view.View view) {
+                                    // This method should be kept by the default ProGuard rules.
+                                }
                             }""")
 
     private val app =
@@ -280,19 +366,25 @@ class MinifyFeaturesTest(val codeShrinker: CodeShrinker, useDexArchive: Boolean)
                     .subproject(":lib1", lib1)
                     .subproject(":lib2", lib2)
                     .subproject(":baseFeature", baseFeature)
-                    .subproject(":foo:otherFeature", otherFeature)
+                    .subproject(":otherFeature1", otherFeature1)
+                    .subproject(":otherFeature2", otherFeature2)
                     .subproject(":app", app)
                     .subproject(":instantApp", instantApp)
-                    .dependency(app, otherFeature)
-                    .dependency(otherFeature, lib2)
-                    .dependency(otherFeature, baseFeature)
+                    .dependency(app, baseFeature)
+                    .dependency(app, otherFeature1)
+                    .dependency(app, otherFeature2)
+                    .dependency(otherFeature1, lib2)
+                    .dependency(otherFeature1, baseFeature)
+                    .dependency(otherFeature2, baseFeature)
                     .dependency(lib2, lib1)
                     .dependency(baseFeature, lib1)
                     .dependency(instantApp, baseFeature)
-                    .dependency(instantApp, otherFeature)
+                    .dependency(instantApp, otherFeature1)
+                    .dependency(instantApp, otherFeature2)
                     // Reverse dependencies for the instant app.
                     .dependency("application", baseFeature, app)
-                    .dependency("feature", baseFeature, otherFeature)
+                    .dependency("feature", baseFeature, otherFeature1)
+                    .dependency("feature", baseFeature, otherFeature2)
                     .build()
 
     @get:Rule
@@ -310,7 +402,7 @@ class MinifyFeaturesTest(val codeShrinker: CodeShrinker, useDexArchive: Boolean)
                 .run("assembleMinified")
 
         val baseFeatureApk =
-                project.getSubproject(":baseFeature")
+                project.getSubproject("baseFeature")
                         .getFeatureApk(object: GradleTestProject.ApkType {
                             override fun getBuildType() = "minified"
                             override fun getTestName(): String? = null
@@ -324,10 +416,38 @@ class MinifyFeaturesTest(val codeShrinker: CodeShrinker, useDexArchive: Boolean)
         }
         assertThat(baseFeatureApk).containsClass("Lcom/example/baseFeature/EmptyClassToKeep;")
         assertThat(baseFeatureApk).containsJavaResource("base_java_res.txt")
-        assertThat(baseFeatureApk).containsJavaResource("other_java_res.txt")
+        assertThat(baseFeatureApk).containsJavaResource("other_java_res_1.txt")
+        assertThat(baseFeatureApk).containsJavaResource("other_java_res_2.txt")
         assertThat(baseFeatureApk).doesNotContainClass(
             "Lcom/example/baseFeature/EmptyClassToRemove;")
-        assertThat(baseFeatureApk).doesNotContainClass("Lcom/example/otherFeature/Main;")
+        assertThat(baseFeatureApk).doesNotContainClass("Lcom/example/otherFeature1/Main;")
+        assertThat(baseFeatureApk).doesNotContainClass("Lcom/example/otherFeature2/Main;")
+
+        val otherFeature1Apk =
+                project.getSubproject("otherFeature1")
+                        .getFeatureApk(object: GradleTestProject.ApkType {
+                            override fun getBuildType() = "minified"
+                            override fun getTestName(): String? = null
+                            override fun isSigned() = true
+                        })
+        assertThat(otherFeature1Apk).containsClass("Lcom/example/otherFeature1/Main;")
+        assertThat(otherFeature1Apk).doesNotContainJavaResource("other_java_res_1.txt")
+        assertThat(otherFeature1Apk).doesNotContainClass(
+            "Lcom/example/otherFeature1/EmptyClassToRemove;")
+        assertThat(otherFeature1Apk).doesNotContainClass("Lcom/example/baseFeature/Main;")
+        assertThat(otherFeature1Apk).doesNotContainClass("Lcom/example/otherFeature2/Main;")
+
+        val otherFeature2Apk =
+                project.getSubproject("otherFeature2")
+                        .getFeatureApk(object: GradleTestProject.ApkType {
+                            override fun getBuildType() = "minified"
+                            override fun getTestName(): String? = null
+                            override fun isSigned() = true
+                        })
+        assertThat(otherFeature2Apk).containsClass("Lcom/example/otherFeature2/Main;")
+        assertThat(otherFeature2Apk).doesNotContainJavaResource("other_java_res_2.txt")
+        assertThat(otherFeature2Apk).doesNotContainClass("Lcom/example/baseFeature/Main;")
+        assertThat(otherFeature2Apk).doesNotContainClass("Lcom/example/otherFeature1/Main;")
     }
 }
 
