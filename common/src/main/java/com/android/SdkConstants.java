51fee1088ab1447887875c17c05f034112e1c42c
Kelvin Ma <kelvinhanma@google.com>
2018-04-18 12:13:37 -0700

    Enable apk analyzer for bundle
    This only allows analyzer to see and open a bundle (.aab) file. There
    will be more work to support bundle internals (.pb etc)
    
    Bug: 69364580
    Test: TODO
    Change-Id: I4cc6296c8c31c146dfa25c6c2477f075ed14e96b

--- common/src/main/java/com/android/SdkConstants.java
+++ common/src/main/java/com/android/SdkConstants.java
@@ -1882,6 +1882,8 @@ public final class SdkConstants {
     public static final String EXT_RES = "ap_"; //$NON-NLS-1$
     /** Extension for pre-processable images. Right now pngs */
     public static final String EXT_PNG = "png"; //$NON-NLS-1$
+    /** Extension of bundle files, i.e. "aab" */
+    public static final String EXT_BUNDLE = "aab"; //$NON-NLS-1$
 
     public static final String EXT_HPROF = "hprof"; //$NON-NLS-1$
     public static final String EXT_GZ = "gz"; //$NON-NLS-1$
