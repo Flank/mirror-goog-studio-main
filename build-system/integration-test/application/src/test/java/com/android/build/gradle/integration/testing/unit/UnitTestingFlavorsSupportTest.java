6212b316ae25d14c6a48bb3f3d716fb9d8305eb7
Chris Warrington <cmw@google.com>
2018-05-03 10:13:12 +0100

    Investigate Gradle integration tests timeouts.
    Split unit testing tests into a separate target.
    
    Test: bazel test //tools/base/build-system/integration-test/application/...
    Bug: 78568459
    Change-Id: I69960a7700df2ffe16c20ddc7ddeeb1291e33f20

--- build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/testing/unit/UnitTestingFlavorsSupportTest.java
+++ build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/testing/unit/UnitTestingFlavorsSupportTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 The Android Open Source Project
+ * Copyright (C) 2018 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,10 +14,10 @@
  * limitations under the License.
  */
 
-package com.android.build.gradle.integration.testing;
+package com.android.build.gradle.integration.testing.unit;
 
-import static com.android.build.gradle.integration.testing.JUnitResults.Outcome.FAILED;
-import static com.android.build.gradle.integration.testing.JUnitResults.Outcome.PASSED;
+import static com.android.build.gradle.integration.testing.unit.JUnitResults.Outcome.FAILED;
+import static com.android.build.gradle.integration.testing.unit.JUnitResults.Outcome.PASSED;
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.fail;
 
