b95265dfc10aa9c74f7d3e14b6f67d8209115e9d
Armand Navabi <navabi@google.com>
2018-05-03 16:28:15 -0700

    Update docker image to have support for UTF-8 filenames.
    And add a FoundryTest for UTF-8 filename support.
    
    Bug: 72180168
    Test: existing //tools/base/bazel/foundry:test
    Change-Id: I2f2158d6d83ad69855d5b60ccb3d6b3eccaf6d9f

--- bazel/foundry/FoundryTest.java
+++ bazel/foundry/FoundryTest.java
@@ -1,9 +1,14 @@
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.security.KeyStore;
 import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
+import java.util.Arrays;
+import java.util.Locale;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.X509TrustManager;
@@ -39,5 +44,22 @@ public final class FoundryTest {
         assertTrue(rnd.isDirectory());
         // Running this with -Djava.security.debug=all shows in the logs that it fails to use /dev/random
         byte[] seed = SecureRandom.getInstance("SHA1PRNG").generateSeed(24);
+    }
+
+    @Test
+    public void testCreateFile() {
+        File tempDir = new File("/tmp");
+        String filename = "你所有的基地都属于我们.txt";
+        System.out.println("locale info: " + Locale.getDefault());
+        System.out.println("defaultCharset: " + Charset.defaultCharset());
+        try {
+            File file = new File(tempDir, filename);
+            file.createNewFile();
+            System.out.println("File created.");
+            System.out.println("tempDir.list(): " + Arrays.toString(tempDir.list()));
+            assertTrue(Arrays.asList(tempDir.list()).contains(filename));
+        } catch (IOException e) {
+            fail("File with non-ascii name failed to be created.");
+        }
     }
 }
