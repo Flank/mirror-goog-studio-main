fc93e6bcc2bbfe3eeff95f17d393f31321f3a275
Scott Pollom <spollom@google.com>
2018-05-23 13:50:00 -0700

    Check for public task property setters
    This CL adds a test to check that no task property setters
    are public, except those which are white-listed.
    
    Bug: 73482993
    Test: TaskMethodModifiersAndAnnotationsTest
    Change-Id: Ibf59bd269f060aa781ce6ddc8d6bf5849c6ad2fb

--- build-system/gradle-core/src/test/java/com/android/build/gradle/internal/tasks/TaskMethodModifiersAndAnnotationsTest.kt
+++ build-system/gradle-core/src/test/java/com/android/build/gradle/internal/tasks/TaskMethodModifiersAndAnnotationsTest.kt
@@ -17,6 +17,9 @@
 package com.android.build.gradle.internal.tasks
 
 import com.google.common.reflect.ClassPath
+import com.google.common.reflect.TypeToken
+import com.google.common.truth.Truth
+import org.gradle.api.Task
 import org.gradle.api.tasks.Classpath
 import org.gradle.api.tasks.CompileClasspath
 import org.gradle.api.tasks.Console
@@ -91,6 +94,167 @@ class TaskMethodModifiersAndAnnotationsTest {
         }
         throw AssertionError(error.toString())
     }
+
+    @Test
+    fun `check for public task setters`() {
+
+        val whiteListedSetters =
+            listOf(
+                "com.android.build.gradle.internal.coverage.JacocoReportTask::setCoverageFile",
+                "com.android.build.gradle.internal.coverage.JacocoReportTask::setJacocoClasspath",
+                "com.android.build.gradle.internal.coverage.JacocoReportTask::setReportDir",
+                "com.android.build.gradle.internal.coverage.JacocoReportTask::setReportName",
+                "com.android.build.gradle.internal.coverage.JacocoReportTask::setTabWidth",
+                "com.android.build.gradle.internal.feature.BundleFeatureClasses::setJavacClasses",
+                "com.android.build.gradle.internal.feature.BundleFeatureClasses::setOutputJar",
+                "com.android.build.gradle.internal.feature.BundleFeatureClasses::setPostJavacClasses",
+                "com.android.build.gradle.internal.feature.BundleFeatureClasses::setPreJavacClasses",
+                "com.android.build.gradle.internal.res.GenerateLibraryRFileTask::setInputResourcesDir",
+                "com.android.build.gradle.internal.res.LinkApplicationAndroidResourcesTask::setAaptMainDexListProguardOutputFile",
+                "com.android.build.gradle.internal.res.LinkApplicationAndroidResourcesTask::setAaptOptions",
+                "com.android.build.gradle.internal.res.LinkApplicationAndroidResourcesTask::setDebuggable",
+                "com.android.build.gradle.internal.res.LinkApplicationAndroidResourcesTask::setEnableAapt2",
+                "com.android.build.gradle.internal.res.LinkApplicationAndroidResourcesTask::setMergeBlameLogFolder",
+                "com.android.build.gradle.internal.res.LinkApplicationAndroidResourcesTask::setProguardOutputFile",
+                "com.android.build.gradle.internal.res.LinkApplicationAndroidResourcesTask::setPseudoLocalesEnabled",
+                "com.android.build.gradle.internal.res.LinkApplicationAndroidResourcesTask::setSourceOutputDir",
+                "com.android.build.gradle.internal.res.LinkApplicationAndroidResourcesTask::setType",
+                "com.android.build.gradle.internal.res.namespaced.AutoNamespaceDependenciesTask::setLog",
+                "com.android.build.gradle.internal.tasks.AndroidBuilderTask::setAndroidBuilder",
+                "com.android.build.gradle.internal.tasks.AndroidReportTask::setIgnoreFailures",
+                "com.android.build.gradle.internal.tasks.AndroidReportTask::setReportType",
+                "com.android.build.gradle.internal.tasks.AndroidReportTask::setReportsDir",
+                "com.android.build.gradle.internal.tasks.AndroidReportTask::setResultsDir",
+                "com.android.build.gradle.internal.tasks.AndroidReportTask::setWillRun",
+                "com.android.build.gradle.internal.tasks.AndroidVariantTask::setVariantName",
+                "com.android.build.gradle.internal.tasks.CheckManifest::setManifest",
+                "com.android.build.gradle.internal.tasks.CheckManifest::setOptional",
+                "com.android.build.gradle.internal.tasks.CombineModuleInfoTask::setLocalModuleInfo",
+                "com.android.build.gradle.internal.tasks.CombineModuleInfoTask::setOutputProvider",
+                "com.android.build.gradle.internal.tasks.CombineModuleInfoTask::setSubModules",
+                "com.android.build.gradle.internal.tasks.DependencyReportTask::setVariants",
+                "com.android.build.gradle.internal.tasks.DeviceProviderInstrumentTestTask::setCoverageDir",
+                "com.android.build.gradle.internal.tasks.DeviceProviderInstrumentTestTask::setDeviceProvider",
+                "com.android.build.gradle.internal.tasks.DeviceProviderInstrumentTestTask::setFlavorName",
+                "com.android.build.gradle.internal.tasks.DeviceProviderInstrumentTestTask::setIgnoreFailures",
+                "com.android.build.gradle.internal.tasks.DeviceProviderInstrumentTestTask::setInstallOptions",
+                "com.android.build.gradle.internal.tasks.DeviceProviderInstrumentTestTask::setProcessExecutor",
+                "com.android.build.gradle.internal.tasks.DeviceProviderInstrumentTestTask::setReportsDir",
+                "com.android.build.gradle.internal.tasks.DeviceProviderInstrumentTestTask::setResultsDir",
+                "com.android.build.gradle.internal.tasks.DeviceProviderInstrumentTestTask::setTestData",
+                "com.android.build.gradle.internal.tasks.GatherAndroidModuleInfoTask::setExtension",
+                "com.android.build.gradle.internal.tasks.GatherModuleInfoTask::setHasKotlin",
+                "com.android.build.gradle.internal.tasks.GatherModuleInfoTask::setOutputProvider",
+                "com.android.build.gradle.internal.tasks.GatherModuleInfoTask::setPluginName",
+                "com.android.build.gradle.internal.tasks.GenerateApkDataTask::setMinSdkVersion",
+                "com.android.build.gradle.internal.tasks.GenerateApkDataTask::setResOutputDir",
+                "com.android.build.gradle.internal.tasks.GenerateApkDataTask::setTargetSdkVersion",
+                "com.android.build.gradle.internal.tasks.IncrementalTask::setIncrementalFolder",
+                "com.android.build.gradle.internal.tasks.InstallVariantTask::setApkDirectory",
+                "com.android.build.gradle.internal.tasks.InstallVariantTask::setInstallOptions",
+                "com.android.build.gradle.internal.tasks.InstallVariantTask::setProcessExecutor",
+                "com.android.build.gradle.internal.tasks.InstallVariantTask::setProjectName",
+                "com.android.build.gradle.internal.tasks.InstallVariantTask::setTimeOutInMs",
+                "com.android.build.gradle.internal.tasks.InstallVariantTask::setVariantData",
+                "com.android.build.gradle.internal.tasks.LintCompile::setOutputDirectory",
+                "com.android.build.gradle.internal.tasks.LintStandaloneTask::setAutoFix",
+                "com.android.build.gradle.internal.tasks.LintStandaloneTask::setFatalOnly",
+                "com.android.build.gradle.internal.tasks.LintStandaloneTask::setLintChecks",
+                "com.android.build.gradle.internal.tasks.LintStandaloneTask::setLintOptions",
+                "com.android.build.gradle.internal.tasks.LintStandaloneTask::setReportDir",
+                "com.android.build.gradle.internal.tasks.MergeFileTask::setInputFiles",
+                "com.android.build.gradle.internal.tasks.MergeFileTask::setOutputFile",
+                "com.android.build.gradle.internal.tasks.NdkTask::setNdkConfig",
+                "com.android.build.gradle.internal.tasks.SigningReportTask::setVariants",
+                "com.android.build.gradle.internal.tasks.SourceSetsTask::setConfig",
+                "com.android.build.gradle.internal.tasks.TestServerTask::setTestApks",
+                "com.android.build.gradle.internal.tasks.TestServerTask::setTestServer",
+                "com.android.build.gradle.internal.tasks.TestServerTask::setTestedApks",
+                "com.android.build.gradle.internal.tasks.UninstallTask::setTimeOutInMs",
+                "com.android.build.gradle.internal.tasks.UninstallTask::setVariant",
+                "com.android.build.gradle.internal.tasks.databinding.DataBindingGenBaseClassesTask::setV1Artifacts",
+                "com.android.build.gradle.tasks.AidlCompile::setPackageWhitelist",
+                "com.android.build.gradle.tasks.AidlCompile::setPackagedDir",
+                "com.android.build.gradle.tasks.AidlCompile::setSourceOutputDir",
+                "com.android.build.gradle.tasks.CleanBuildCache::setBuildCache",
+                "com.android.build.gradle.tasks.CompatibleScreensManifest::setMinSdkVersion",
+                "com.android.build.gradle.tasks.CompatibleScreensManifest::setOutputFolder",
+                "com.android.build.gradle.tasks.CompatibleScreensManifest::setScreenSizes",
+                "com.android.build.gradle.tasks.ExtractAnnotations::setBootClasspath",
+                "com.android.build.gradle.tasks.ExtractAnnotations::setClassDir",
+                "com.android.build.gradle.tasks.ExtractAnnotations::setEncoding",
+                "com.android.build.gradle.tasks.ExtractAnnotations::setOutput",
+                "com.android.build.gradle.tasks.GenerateBuildConfig::setBuildTypeName",
+                "com.android.build.gradle.tasks.GenerateBuildConfig::setSourceOutputDir",
+                "com.android.build.gradle.tasks.GenerateResValues::setItems",
+                "com.android.build.gradle.tasks.GenerateResValues::setResOutputDir",
+                "com.android.build.gradle.tasks.InvokeManifestMerger::setMainManifestFile",
+                "com.android.build.gradle.tasks.InvokeManifestMerger::setOutputFile",
+                "com.android.build.gradle.tasks.InvokeManifestMerger::setSecondaryManifestFiles",
+                "com.android.build.gradle.tasks.MainApkListPersistence::setApkData",
+                "com.android.build.gradle.tasks.MainApkListPersistence::setOutputFile",
+                "com.android.build.gradle.tasks.ManifestProcessorTask::setAaptFriendlyManifestOutputDirectory",
+                "com.android.build.gradle.tasks.ManifestProcessorTask::setInstantRunManifestOutputDirectory",
+                "com.android.build.gradle.tasks.ManifestProcessorTask::setManifestOutputDirectory",
+                "com.android.build.gradle.tasks.ManifestProcessorTask::setReportFile",
+                "com.android.build.gradle.tasks.MergeManifests::setVariantConfiguration",
+                "com.android.build.gradle.tasks.MergeResources::setBlameLogFolder",
+                "com.android.build.gradle.tasks.MergeResources::setOutputDir",
+                "com.android.build.gradle.tasks.MergeResources::setPublicFile",
+                "com.android.build.gradle.tasks.MergeResources::setResources",
+                "com.android.build.gradle.tasks.MergeSourceSetFolders::setLibraries",
+                "com.android.build.gradle.tasks.MergeSourceSetFolders::setOutputDir",
+                "com.android.build.gradle.tasks.NdkCompile::setDebuggable",
+                "com.android.build.gradle.tasks.NdkCompile::setForTesting",
+                "com.android.build.gradle.tasks.NdkCompile::setGeneratedMakefile",
+                "com.android.build.gradle.tasks.NdkCompile::setNdkCygwinMode",
+                "com.android.build.gradle.tasks.NdkCompile::setNdkDirectory",
+                "com.android.build.gradle.tasks.NdkCompile::setNdkRenderScriptMode",
+                "com.android.build.gradle.tasks.NdkCompile::setObjFolder",
+                "com.android.build.gradle.tasks.NdkCompile::setSoFolder",
+                "com.android.build.gradle.tasks.PackageAndroidArtifact::setAbiFilters",
+                "com.android.build.gradle.tasks.PackageAndroidArtifact::setDebugBuild",
+                "com.android.build.gradle.tasks.PackageAndroidArtifact::setJniDebugBuild",
+                "com.android.build.gradle.tasks.PackageAndroidArtifact::setSigningConfig",
+                "com.android.build.gradle.tasks.ProcessManifest::setVariantConfiguration",
+                "com.android.build.gradle.tasks.ProcessTestManifest::setTestManifestFile",
+                "com.android.build.gradle.tasks.ProcessTestManifest::setTmpDir",
+                "com.android.build.gradle.tasks.RenderscriptCompile::setDebugBuild",
+                "com.android.build.gradle.tasks.RenderscriptCompile::setImportDirs",
+                "com.android.build.gradle.tasks.RenderscriptCompile::setLibOutputDir",
+                "com.android.build.gradle.tasks.RenderscriptCompile::setNdkMode",
+                "com.android.build.gradle.tasks.RenderscriptCompile::setObjOutputDir",
+                "com.android.build.gradle.tasks.RenderscriptCompile::setOptimLevel",
+                "com.android.build.gradle.tasks.RenderscriptCompile::setResOutputDir",
+                "com.android.build.gradle.tasks.RenderscriptCompile::setSourceOutputDir",
+                "com.android.build.gradle.tasks.RenderscriptCompile::setSupportMode",
+                "com.android.build.gradle.tasks.ShaderCompile::setDefaultArgs",
+                "com.android.build.gradle.tasks.ShaderCompile::setOutputDir",
+                "com.android.build.gradle.tasks.ShaderCompile::setScopedArgs",
+                "com.android.build.gradle.tasks.ShaderCompile::setSourceDir",
+                "com.android.build.gradle.tasks.ir.FastDeployRuntimeExtractorTask::setOutputFile"
+            )
+
+        val classPath = ClassPath.from(this.javaClass.classLoader)
+        val taskInterface = TypeToken.of(Task::class.java)
+        val publicSetters =
+            classPath
+                .getTopLevelClassesRecursive("com.android.build")
+                .map { classInfo -> classInfo.load() as Class<*> }
+                .filter { clazz -> TypeToken.of(clazz).types.contains(taskInterface) }
+                .flatMap { it.declaredMethods.asIterable() }
+                .filter {
+                    it.name.startsWith("set")
+                            && !it.name.contains('$')
+                            && Modifier.isPublic(it.modifiers)
+                }
+                .map { "${it.declaringClass.toString().substringAfter(" ")}::${it.name}" }
+
+        Truth.assertThat(publicSetters)
+            .named("Task public setters")
+            .containsExactlyElementsIn(whiteListedSetters)
+    }
+
 
     private fun AnnotatedElement.hasGradleInputOrOutputAnnotation(): Boolean {
         // look for all org.gradle.api.tasks annotations, except @CacheableTask, @Internal, and
