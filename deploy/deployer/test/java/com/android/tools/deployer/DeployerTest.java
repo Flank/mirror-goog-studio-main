f4d8f88d648ba3ad3d2b1144888e2ad67172229d
estebandlc <estebandlc@google.com>
2018-07-12 12:23:05 -0700

    Adds a module for the deployer
    Creates the IJ targets and iml files
    Fixes dependencies and packages
    
    Test: existing
    Bug: 111215639
    Change-Id: Icb4da851ac6c2d553035ec1d03e8a957a7015945

---
+++ deploy/deployer/test/java/com/android/tools/deployer/DeployerTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tools.deployer;
+
+import static org.junit.Assert.assertEquals;
+
+import com.android.testutils.TestUtils;
+import java.io.File;
+import java.util.HashMap;
+import org.junit.Test;
+
+public class DeployerTest {
+
+    private static final String BASE = "tools/base/deploy/deployer/test/resource/";
+
+    @Test
+    public void testCentralDirectoryParse() {
+        File file = TestUtils.getWorkspaceFile(BASE + "base.apk.remotecd");
+        ZipCentralDirectory zcd = new ZipCentralDirectory(file);
+        HashMap<String, Long> crcs = new HashMap<>();
+        zcd.getCrcs(crcs);
+        assertEquals(1007, crcs.size());
+
+        long manifestCrc = crcs.get("AndroidManifest.xml");
+        assertEquals(0x5804A053, manifestCrc);
+    }
+}
