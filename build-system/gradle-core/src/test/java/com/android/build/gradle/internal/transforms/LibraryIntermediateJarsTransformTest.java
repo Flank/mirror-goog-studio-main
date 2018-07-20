e6db1941afa9244b92d3a113dba4ac52001aa619
Chris Warrington <cmw@google.com>
2018-07-20 15:00:01 +0000

    Revert "Publish classes in dir between projects"
    This reverts commit f0a25da212e14f7edd377f4ef077cf1d4fd82ea9.
    
    Reason for revert: This seems to regress incremental build time b/111534927
    
    Change-Id: I30c57fd83decd7555b43a2aaa8574dbf4258cd67
    (cherry picked from commit 7d0fe26c9ff991f9c544df91ea3618c1a087fffc)

--- build-system/gradle-core/src/test/java/com/android/build/gradle/internal/transforms/LibraryIntermediateJarsTransformTest.java
+++ build-system/gradle-core/src/test/java/com/android/build/gradle/internal/transforms/LibraryIntermediateJarsTransformTest.java
@@ -22,13 +22,12 @@ import static com.android.build.api.transform.QualifiedContent.Scope.PROJECT;
 import static com.android.build.gradle.internal.transforms.TransformTestHelper.invocationBuilder;
 import static com.android.build.gradle.internal.transforms.TransformTestHelper.singleJarBuilder;
 import static com.android.testutils.truth.MoreTruth.assertThat;
-import static com.android.testutils.truth.PathSubject.assertThat;
 
 import com.android.annotations.NonNull;
+import com.android.build.api.transform.TransformException;
 import com.android.build.api.transform.TransformInvocation;
 import com.android.testutils.apk.Zip;
 import com.android.utils.FileUtils;
-import com.google.common.truth.Truth;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -40,8 +39,7 @@ import org.junit.Test;
 
 public class LibraryIntermediateJarsTransformTest {
 
-    private File mainClassJar;
-    private File mainClassDir;
+    private File mainClassLocation;
     private File resJarLocation;
     private String packageName;
 
@@ -51,23 +49,22 @@ public class LibraryIntermediateJarsTransformTest {
     public void setUp() throws Exception {
         packageName = "com.example.android.multiproject.person";
 
-        mainClassJar = Files.createTempFile(null, null).toFile();
-        mainClassDir = Files.createTempDirectory("classes_dir").toFile();
+        mainClassLocation = Files.createTempFile(null, null).toFile();
         resJarLocation = Files.createTempFile(null, null).toFile();
 
         transform =
                 new LibraryIntermediateJarsTransform(
-                        mainClassJar, mainClassDir, resJarLocation, () -> packageName, true);
+                        mainClassLocation, resJarLocation, () -> packageName, true);
     }
 
     @After
     public void tearDown() throws Exception {
-        FileUtils.deleteIfExists(mainClassJar);
+        FileUtils.deleteIfExists(mainClassLocation);
         FileUtils.deleteIfExists(resJarLocation);
     }
 
     @Test
-    public void testSimpleMainInput() throws Exception {
+    public void testSimpleMainInput() throws TransformException, InterruptedException, IOException {
         // get a simple input
         TransformInvocation invocation = invocationBuilder()
                 .setIncremental(false)
@@ -80,21 +77,20 @@ public class LibraryIntermediateJarsTransformTest {
         transform.transform(invocation);
 
         // check the output
-        try (Zip classZip = new Zip(mainClassJar);
-                Zip resZip = new Zip(resJarLocation)) {
-            // source code in classZip only
-            assertThat(classZip).contains("com/example/android/multiproject/person/People.class");
-            assertThat(resZip)
-                    .doesNotContain("com/example/android/multiproject/person/People.class");
+        final Zip classZip = new Zip(mainClassLocation);
+        final Zip resZip = new Zip(resJarLocation);
 
-            // resources in resZip only
-            assertThat(classZip).doesNotContain("file1.txt");
-            assertThat(resZip).contains("file1.txt");
+        // source code in classZip only
+        assertThat(classZip).contains("com/example/android/multiproject/person/People.class");
+        assertThat(resZip).doesNotContain("com/example/android/multiproject/person/People.class");
 
-            // R class nowhere
-            assertThat(classZip).doesNotContain("com/example/android/multiproject/person/R.class");
-            assertThat(resZip).doesNotContain("com/example/android/multiproject/person/R.class");
-        }
+        // resources in resZip only
+        assertThat(classZip).doesNotContain("file1.txt");
+        assertThat(resZip).contains("file1.txt");
+
+        // R class nowhere
+        assertThat(classZip).doesNotContain("com/example/android/multiproject/person/R.class");
+        assertThat(resZip).doesNotContain("com/example/android/multiproject/person/R.class");
 
         // no folder either. Can't check this yet.
         //assertThat(classZip).doesNotContain("com/example/");
@@ -102,7 +98,8 @@ public class LibraryIntermediateJarsTransformTest {
     }
 
     @Test
-    public void testMergingInputsJustClasses() throws Exception {
+    public void testMergingInputsJustClasses()
+            throws TransformException, InterruptedException, IOException {
         // get a simple input
         TransformInvocation invocation =
                 invocationBuilder()
@@ -122,52 +119,10 @@ public class LibraryIntermediateJarsTransformTest {
         transform.transform(invocation);
 
         // check the output
-        try (Zip classZip = new Zip(mainClassJar)) {
-            // META-INF files in classZip
-            assertThat(classZip).contains("META-INF/MANIFEST.MF");
-        }
-    }
+        final Zip classZip = new Zip(mainClassLocation);
 
-    @Test
-    public void testMergingWithDirs() throws Exception {
-        Path dir1 = Files.createTempDirectory("classes1");
-        Files.write(dir1.resolve("Foo.class"), new byte[0]);
-        Files.write(Files.createDirectories(dir1.resolve("com")).resolve("Foo.class"), new byte[0]);
-        Path dir2 = Files.createTempDirectory("classes2");
-        Files.write(dir2.resolve("Bar.class"), new byte[0]);
-        Files.write(Files.createDirectories(dir2.resolve("com")).resolve("Bar.class"), new byte[0]);
-
-        TransformInvocation invocation =
-                invocationBuilder()
-                        .setIncremental(false)
-                        .addReferenceInput(
-                                TransformTestHelper.directoryBuilder(dir1.toFile())
-                                        .setContentType(CLASSES)
-                                        .setScope(PROJECT)
-                                        .build())
-                        .addReferenceInput(
-                                TransformTestHelper.directoryBuilder(dir2.toFile())
-                                        .setContentType(CLASSES)
-                                        .setScope(PROJECT)
-                                        .build())
-                        .addReferenceInput(
-                                singleJarBuilder(getInputFile("test-jar1.jar"))
-                                        .setContentTypes(CLASSES, RESOURCES)
-                                        .setScopes(PROJECT)
-                                        .build())
-                        .build();
-
-        transform.transform(invocation);
-
-        assertThat(mainClassDir.toPath().resolve("Foo.class")).exists();
-        assertThat(mainClassDir.toPath().resolve("com/Foo.class")).exists();
-        assertThat(mainClassDir.toPath().resolve("Bar.class")).exists();
-        assertThat(mainClassDir.toPath().resolve("com/Bar.class")).exists();
-
-        assertThat(mainClassJar).exists();
-        try (Zip zip = new Zip(mainClassJar)) {
-            Truth.assertThat(zip.getEntries()).hasSize(3);
-        }
+        // META-INF files in classZip
+        assertThat(classZip).contains("META-INF/MANIFEST.MF");
     }
 
     private File getInputFile(@NonNull String name) throws IOException {
