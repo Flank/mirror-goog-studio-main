4f304bf0c598de5fc8daddf8d6e9879ef9a30bf5
Andrii Dehtiarov <adehtiarov@google.com>
2018-07-26 19:39:31 +0100

    Fetch remote repositories in parallel.
    Fetching remote repo usually means downloading dozens of small xmls
    potentially from different sites. Since the xmls are small, bandwidth
    isn't really a bottleneck so it makes sense to try and maximize its
    usage by perfoming each download on a pooled thread.
    
    The local benchmarks showed 5x difference of fetch time for the
    default studio setup (10 repo source in total, 3 source providers).
    
    This speed-up should have impact in three contexts:
    1) Studio startup (as it automatically checks for updates even before
    main window appears).
    2) SDK manager 'Looking for updates...' spinning wheel which appears
    on every SDK manager start and on some types of UI refreshes.
    3) 'Check for Updates' (as specified in the referenced bug).
    
    This CL is a follow-up on ag/4445049, but it takes a more conscious
    thread- and exception-safety approach.
    
    Bug: b/85839361 (main), b/111161100, b/111159809, b/111159035
    Test: Added testDownloadsAreParallel; also existing tests will serve to
    verify the correctness of concurrent processing of the repositories.
    Change-Id: Ie499acbe7e90ff8bedbf6d32fd87e3fdcdde764d

--- repository/src/main/java/com/android/repository/impl/manager/RemoteRepoLoaderImpl.java
+++ repository/src/main/java/com/android/repository/impl/manager/RemoteRepoLoaderImpl.java
@@ -18,17 +18,7 @@ package com.android.repository.impl.manager;
 
 import com.android.annotations.NonNull;
 import com.android.annotations.Nullable;
-import com.android.repository.api.Channel;
-import com.android.repository.api.Downloader;
-import com.android.repository.api.FallbackRemoteRepoLoader;
-import com.android.repository.api.ProgressIndicator;
-import com.android.repository.api.ProgressIndicatorAdapter;
-import com.android.repository.api.RemotePackage;
-import com.android.repository.api.Repository;
-import com.android.repository.api.RepositorySource;
-import com.android.repository.api.RepositorySourceProvider;
-import com.android.repository.api.SchemaModule;
-import com.android.repository.api.SettingsController;
+import com.android.repository.api.*;
 import com.android.repository.impl.meta.SchemaModuleUtil;
 import com.android.repository.util.InstallerUtil;
 import com.google.common.collect.Lists;
@@ -40,6 +30,9 @@ import java.net.URL;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import javax.xml.bind.JAXBException;
 import org.w3c.dom.ls.LSResourceResolver;
 
@@ -47,33 +40,42 @@ import org.w3c.dom.ls.LSResourceResolver;
  * Utility class that loads {@link Repository}s from {@link RepositorySource}s.
  */
 public class RemoteRepoLoaderImpl implements RemoteRepoLoader {
+    /**
+     * Timeout to wait for the packages to be fetched, in terms of terminating the downloads thread
+     * pool. Each time the timeout is reached, a warning will be logged but waiting for the thread
+     * pool termination will continue. It is expected that network operations will eventually time
+     * out on their own and/or throw exception in the worst case, leading to the thread pool
+     * termination anyway.
+     */
+    private static final int FETCH_PACKAGES_WAITING_ITERATION_MINUTES = 1;
 
     /**
-     * Resource resolver to use for finding imported XSDs.
+     * A lock object for a fast critical section in the sources processing worker threads to secure
+     * the resulting packages resolution when there are multiple packages with same name but from
+     * different sources and/or with different versions.
      */
+    private final Object myPackagesResolutionLock = new Object();
+
+    /** Resource resolver to use for finding imported XSDs. */
     private final LSResourceResolver mResourceResolver;
 
-    /**
-     * {@link FallbackRemoteRepoLoader} to use if we get an XML file we can't parse.
-     */
+    /** {@link FallbackRemoteRepoLoader} to use if we get an XML file we can't parse. */
     private FallbackRemoteRepoLoader mFallback;
 
-    /**
-     * The {@link RepositorySourceProvider}s to load from.
-     */
+    /** The {@link RepositorySourceProvider}s to load from. */
     private final Collection<RepositorySourceProvider> mSourceProviders;
 
     /**
      * Constructor
      *
-     * @param sources          The {@link RepositorySourceProvider}s to get the {@link
-     *                         RepositorySource}s to load from.
+     * @param sources The {@link RepositorySourceProvider}s to get the {@link RepositorySource}s to
+     *     load from.
      * @param resourceResolver The resolver to use to find imported XSDs, if necessary for the
-     *                         {@link SchemaModule}s used by the {@link RepositorySource}s.
-     * @param fallback         The {@link FallbackRemoteRepoLoader} to use if we can't parse an XML
-     *                         file.
+     *     {@link SchemaModule}s used by the {@link RepositorySource}s.
+     * @param fallback The {@link FallbackRemoteRepoLoader} to use if we can't parse an XML file.
      */
-    public RemoteRepoLoaderImpl(@NonNull Collection<RepositorySourceProvider> sources,
+    public RemoteRepoLoaderImpl(
+            @NonNull Collection<RepositorySourceProvider> sources,
             @Nullable LSResourceResolver resourceResolver,
             @Nullable FallbackRemoteRepoLoader fallback) {
         mResourceResolver = resourceResolver;
@@ -83,33 +85,66 @@ public class RemoteRepoLoaderImpl implements RemoteRepoLoader {
 
     @Override
     @NonNull
-    public Map<String, RemotePackage> fetchPackages(@NonNull ProgressIndicator progress,
-            @NonNull Downloader downloader, @Nullable SettingsController settings) {
+    public Map<String, RemotePackage> fetchPackages(
+            @NonNull ProgressIndicator progress,
+            @NonNull Downloader downloader,
+            @Nullable SettingsController settings) {
         Map<String, RemotePackage> result = Maps.newHashMap();
+        List<RepositorySource> sources = Lists.newArrayList();
+
         double progressMax = 0;
         for (RepositorySourceProvider provider : mSourceProviders) {
             progressMax += 0.1 / mSourceProviders.size();
-            List<RepositorySource> sources =
-                    provider.getSources(downloader, progress.createSubProgress(progressMax), false);
-            double progressIncrement = 0.9 / (mSourceProviders.size() * sources.size() * 2.);
+            sources.addAll(
+                    provider.getSources(
+                            downloader, progress.createSubProgress(progressMax), false));
+        }
+
+        // In this context we are not concerned that much about the precise progress reporting, because
+        // the manifests downloading is about a dozen of tiny files, so it's expected to be relatively fast
+        // and OTOH a lock-based thread-safe ProgressIndicator implementation would even likely be a bottleneck
+        // (presumably, probability of that is linearly proportional to how often the progress gets reported
+        // by the downloads).
+        boolean wasIndeterminate = progress.isIndeterminate();
+        progress.setIndeterminate(true);
+        LoggingOnlyProgressIndicator loggingOnlyProgress =
+                new LoggingOnlyProgressIndicator(progress);
+
+        ExecutorService sourceThreadPool = Executors.newCachedThreadPool();
+        try {
             for (RepositorySource source : sources) {
                 if (!source.isEnabled()) {
-                    progressMax += 2 * progressIncrement;
-                    progress.setFraction(progressMax);
                     continue;
                 }
-                try {
-                    progressMax += progressIncrement;
-                    InputStream repoStream =
-                            downloader.downloadAndStream(
-                                    new URL(source.getUrl()),
-                                    progress.createSubProgress(progressMax));
-                    progress.setFraction(progressMax);
-                    final List<String> errors = Lists.newArrayList();
+                sourceThreadPool.submit(
+                        () ->
+                                processSource(
+                                        loggingOnlyProgress, downloader, source, settings, result));
+            }
+        } finally {
+            shutdownAndJoin(sourceThreadPool, progress);
+            progress.setIndeterminate(wasIndeterminate);
+        }
 
-                    // Don't show the errors, in case the fallback loader can read it. But keep
-                    // track of them to show later in case not.
-                    ProgressIndicator unmarshalProgress = new ProgressIndicatorAdapter() {
+        return result;
+    }
+
+    private void processSource(
+            @NonNull ProgressIndicator progress,
+            @NonNull Downloader downloader,
+            @NonNull RepositorySource source,
+            @Nullable SettingsController settings,
+            @NonNull Map<String, RemotePackage> result) {
+        try {
+            InputStream repoStream =
+                    downloader.downloadAndStream(new URL(source.getUrl()), progress);
+
+            final List<String> errors = Lists.newArrayList();
+
+            // Don't show the errors, in case the fallback loader can read it. But keep
+            // track of them to show later in case not.
+            ProgressIndicator unmarshalProgress =
+                    new ProgressIndicatorAdapter() {
                         @Override
                         public void logWarning(@NonNull String s, Throwable e) {
                             errors.add(s);
@@ -127,92 +162,140 @@ public class RemoteRepoLoaderImpl implements RemoteRepoLoader {
                         }
                     };
 
-                    Repository repo = null;
-                    try {
-                        repo = (Repository) SchemaModuleUtil
-                                .unmarshal(repoStream, source.getPermittedModules(),
-                                        mResourceResolver, true, unmarshalProgress);
-                    } catch (JAXBException e) {
-                        errors.add(e.toString());
-                    }
+            Repository repo = null;
+            try {
+                repo =
+                        (Repository)
+                                SchemaModuleUtil.unmarshal(
+                                        repoStream,
+                                        source.getPermittedModules(),
+                                        mResourceResolver,
+                                        true,
+                                        unmarshalProgress);
+            } catch (JAXBException e) {
+                errors.add(e.toString());
+            }
 
-                    Collection<? extends RemotePackage> parsedPackages = null;
-                    boolean legacy = false;
-                    if (repo != null) {
-                        parsedPackages = repo.getRemotePackage();
-                    } else if (mFallback != null) {
-                        // TODO: don't require downloading again
-                        parsedPackages =
-                                mFallback.parseLegacyXml(
-                                        source,
-                                        downloader,
-                                        settings,
-                                        progress.createSubProgress(
-                                                progressMax + progressIncrement));
-                        legacy = true;
-                    }
-                    progressMax += progressIncrement;
-                    progress.setFraction(progressMax);
+            Collection<? extends RemotePackage> parsedPackages = null;
+            boolean legacy = false;
+            if (repo != null) {
+                parsedPackages = repo.getRemotePackage();
+            } else if (mFallback != null) {
+                // TODO: don't require downloading again
+                parsedPackages = mFallback.parseLegacyXml(source, downloader, settings, progress);
+                legacy = true;
+            }
 
-                    if (parsedPackages != null && !parsedPackages.isEmpty()) {
-                        for (RemotePackage pkg : parsedPackages) {
-                            RemotePackage existing = result.get(pkg.getPath());
-                            if (existing != null) {
-                                int compare = existing.getVersion().compareTo(pkg.getVersion());
-                                if (compare > 0) {
-                                    // If there are multiple versions of the same package available,
-                                    // pick the latest.
+            if (parsedPackages != null && !parsedPackages.isEmpty()) {
+                for (RemotePackage pkg : parsedPackages) {
+                    // This has to be synchronized because we update the resulting collection from multiple
+                    // threads and the update logic depends on the collection content, so just the collection
+                    // being concurrent wouldn't be enough.
+                    // These operations do not involve any high-latency parts like network connections,
+                    // so it's OK for this to be a synchronized section (i.e., a lock-free approach involving
+                    // a concurrent multi-map and the likes would only complicate the code for little to no
+                    // reason).
+                    synchronized (myPackagesResolutionLock) {
+                        RemotePackage existing = result.get(pkg.getPath());
+                        if (existing != null) {
+                            int compare = existing.getVersion().compareTo(pkg.getVersion());
+                            if (compare > 0) {
+                                // If there are multiple versions of the same package available,
+                                // pick the latest.
+                                continue;
+                            }
+                            if (compare == 0) {
+                                if (legacy) {
+                                    // If legacy and non-legacy packages are available with the
+                                    // same version, pick the non-legacy one.
                                     continue;
                                 }
-                                if (compare == 0) {
-                                    if (legacy) {
-                                        // If legacy and non-legacy packages are available with the
-                                        // same version, pick the non-legacy one.
+                                URL existingUrl =
+                                        InstallerUtil.resolveCompleteArchiveUrl(existing, progress);
+                                if (existingUrl != null) {
+                                    String existingProtocol = existingUrl.getProtocol();
+                                    if (existingProtocol.equals("file")) {
+                                        // If the existing package is local, use it.
                                         continue;
                                     }
-                                    URL existingUrl = InstallerUtil.resolveCompleteArchiveUrl(
-                                            existing, progress);
-                                    if (existingUrl != null) {
-                                        String existingProtocol = existingUrl.getProtocol();
-                                        if (existingProtocol.equals("file")) {
-                                            // If the existing package is local, use it.
-                                            continue;
-                                        }
-                                    }
                                 }
-                            }
-                            Channel settingsChannel =
-                                    settings == null || settings.getChannel() == null
-                                            ? Channel.DEFAULT : settings.getChannel();
+                        }
+                        }
+                        Channel settingsChannel =
+                                settings == null || settings.getChannel() == null
+                                        ? Channel.DEFAULT
+                                        : settings.getChannel();
 
-                            if (pkg.getArchive() != null
-                                    && pkg.getChannel().compareTo(settingsChannel) <= 0) {
-                                pkg.setSource(source);
-                                result.put(pkg.getPath(), pkg);
-                            }
+                        if (pkg.getArchive() != null
+                                && pkg.getChannel().compareTo(settingsChannel) <= 0) {
+                            pkg.setSource(source);
+                            result.put(pkg.getPath(), pkg);
                         }
-                        source.setFetchError(null);
-                    } else {
-                        progress.logWarning("Errors during XML parse:");
-                        for (String error : errors) {
-                            progress.logWarning(error);
-                        }
-                        //noinspection VariableNotUsedInsideIf
-                        if (mFallback != null) {
-                            progress.logWarning(
-                                    "Additionally, the fallback loader failed to parse the XML.");
-                        }
-                        source.setFetchError(errors.isEmpty() ? "unknown error" : errors.get(0));
                     }
-                } catch (MalformedURLException e) {
-                    source.setFetchError("Malformed URL");
-                    progress.logWarning(e.toString());
-                } catch (IOException e) {
-                    source.setFetchError(e.getMessage());
-                    progress.logWarning(e.toString());
+                    source.setFetchError(null);
                 }
+            } else {
+                progress.logWarning("Errors during XML parse:");
+                for (String error : errors) {
+                    progress.logWarning(error);
+                }
+                //noinspection VariableNotUsedInsideIf
+                if (mFallback != null) {
+                    progress.logWarning(
+                            "Additionally, the fallback loader failed to parse the XML.");
+                }
+                source.setFetchError(errors.isEmpty() ? "unknown error" : errors.get(0));
             }
+        } catch (MalformedURLException e) {
+            source.setFetchError("Malformed URL");
+            progress.logWarning(e.toString());
+        } catch (IOException e) {
+            source.setFetchError(e.getMessage());
+            progress.logWarning(e.toString());
         }
-        return result;
+    }
+
+    private static void shutdownAndJoin(
+            @NonNull ExecutorService threadPool, @NonNull ProgressIndicator progress) {
+        threadPool.shutdown();
+
+        try {
+            while (!threadPool.awaitTermination(
+                    FETCH_PACKAGES_WAITING_ITERATION_MINUTES, TimeUnit.MINUTES)) {
+                progress.logWarning("Still waiting for package manifests to be fetched remotely.");
+            }
+        } catch (InterruptedException ignored) {
+            // ignored
+        }
+    }
+
+    /**
+     * A thread-safe implementation of {@link DelegatingProgressIndicator} which does not report the
+     * fraction, but preserves the ability to report the errors/warnings, as most underlying logging
+     * implementations are thread-safe.
+     */
+    private static class LoggingOnlyProgressIndicator extends DelegatingProgressIndicator {
+        LoggingOnlyProgressIndicator(@NonNull ProgressIndicator progress) {
+            super(progress);
+        }
+
+        @Override
+        public void setFraction(double fraction) {}
+
+        @Override
+        public double getFraction() {
+            return 0;
+        }
+
+        @Override
+        public void setText(@Nullable String text) {}
+
+        @Override
+        public void setSecondaryText(@Nullable String text) {}
+
+        @Override
+        public ProgressIndicator createSubProgress(double max) {
+            return this;
+        }
     }
 }
