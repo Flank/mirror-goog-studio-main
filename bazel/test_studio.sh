17314220e283f07d0f20b3911f14d2c44ad5cb64
Armand Navabi <navabi@google.com>
2018-07-31 16:13:14 -0700

    Modify launchpad script for new go/ab bazel target to invoke bazel RBE.
    Bug: 111844253
    Test: navabi@yeezy:~/studio/studio-master-dev$ bash -c
                         tools/base/bazel/test_studio.sh ~/tmp ~/tmp 7
    
    Change-Id: I4bb2b605c7199c17d8c9155fc339677cc45d7320

--- bazel/test_studio.sh
+++ bazel/test_studio.sh
@@ -7,7 +7,7 @@ readonly dist_dir="$2"
 readonly build_number="$3"
 
 readonly script_dir="$(dirname "$0")"
-"${script_dir}/bazel" test --test_tag_filters=-gradle_integration --test_output=errors --test_summary=detailed $(< "${script_dir}/targets")
+"${script_dir}/bazel" --max_idle_secs=60 --bazelrc=/dev/null test --keep_going --bes_backend=buildeventservice.googleapis.com --auth_credentials="$HOME"/.android-studio-alphasource.json --auth_scope=https://www.googleapis.com/auth/cloud-source-tools --project_id=908081808034 --config=remote --cache_test_results=no --build_tag_filters=-no_linux --test_tag_filters=-no_linux,-no_test_linux,-qa_sanity,-qa_fast,-qa_unreliable $(< "${script_dir}/targets")
 
 readonly testlogs_dir="$(${script_dir}/bazel info bazel-testlogs)"
 (cd "${testlogs_dir}" && zip -qr "${dist_dir}/bazel_testlogs_${build_number}.zip" *)
