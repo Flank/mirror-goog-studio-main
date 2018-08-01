c783c9ae95f39730fb4f7f0a33cabb77416f7393
Hung Nguyen <hungnv@google.com>
2018-08-01 09:42:47 -0700

    Refactor MinimalSubproject.java to Kotlin.
    So that the next CL is easier to review.
    
    Bug: N/A
    Test: Existing tests (no functional changes)
    Change-Id: I1fc09e2255d8c7af84b2ae3721a49f4fca55bdb2

---
+++ build-system/integration-test/framework/src/main/java/com/android/build/gradle/integration/common/fixture/app/MinimalSubProject.kt
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.build.gradle.integration.common.fixture.app
+
+import com.android.build.gradle.integration.common.fixture.GradleTestProject
+
+/** An empty subproject.  */
+class MinimalSubProject private constructor(plugin: String, packageName: String?) :
+    AbstractAndroidTestApp(), AndroidTestApp {
+
+    init {
+        var content = "\napply plugin: '$plugin'\n"
+        if (packageName != null) {
+            content = (content
+                    + "\n"
+                    + "android.compileSdkVersion "
+                    + GradleTestProject.DEFAULT_COMPILE_SDK_VERSION
+                    + "\n")
+            val manifest = TestSourceFile(
+                "src/main",
+                "AndroidManifest.xml",
+                "<manifest xmlns:android=\"http://schemas.android.com/apk/res/android\"\n"
+                        + "    package=\""
+                        + packageName
+                        + "\">\n"
+                        + "    <application />\n"
+                        + "</manifest>"
+            )
+            addFiles(manifest)
+        }
+        val build = TestSourceFile("", "build.gradle", content)
+        addFiles(build)
+    }
+
+    override fun containsFullBuildScript(): Boolean {
+        return false
+    }
+
+    fun withFile(path: String, content: String): MinimalSubProject {
+        val pos = path.lastIndexOf('/')
+        return if (pos == -1) {
+            withFile(TestSourceFile("", path, content))
+        } else withFile(
+            TestSourceFile(path.substring(0, pos), path.substring(pos + 1), content)
+        )
+    }
+
+    fun withFile(file: TestSourceFile): MinimalSubProject {
+        addFile(file)
+        return this
+    }
+
+    fun appendToBuild(snippet: String): MinimalSubProject {
+        replaceFile(getFile("build.gradle", "").appendContent("\n" + snippet + "\n"))
+        return this
+    }
+
+    companion object {
+
+        fun lib(packageName: String): MinimalSubProject {
+            return MinimalSubProject("com.android.library", packageName)
+        }
+
+        fun feature(packageName: String): MinimalSubProject {
+            return MinimalSubProject("com.android.feature", packageName)
+        }
+
+        fun app(packageName: String): MinimalSubProject {
+            return MinimalSubProject("com.android.application", packageName)
+        }
+
+        fun dynamicFeature(packageName: String): MinimalSubProject {
+            return MinimalSubProject("com.android.dynamic-feature", packageName)
+        }
+
+        fun instantApp(): MinimalSubProject {
+            return MinimalSubProject("com.android.instantapp", null)
+        }
+
+        fun test(packageName: String): MinimalSubProject {
+            return MinimalSubProject("com.android.test", packageName)
+        }
+
+        fun javaLibrary(): MinimalSubProject {
+            return MinimalSubProject("java-library", null)
+        }
+    }
+}
