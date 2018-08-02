b77ab0e5e89a9b7489d68b94788e0c74983c91f0
Xavier Ducrohet <xav@google.com>
2018-08-02 10:24:09 -0700

    Convert TaskFactory to Kotlin
    Bug: N/A
    Test: N/A
    Change-Id: I1893e2bd8935837c6d20715db959c6bc7f2dd7d0

---
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/tasks/factory/TaskFactory.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.build.gradle.internal.tasks.factory
+
+import com.android.build.gradle.internal.scope.TaskConfigAction
+import org.gradle.api.Action
+import org.gradle.api.DefaultTask
+import org.gradle.api.Task
+
+/**
+ * Interface for a container that can create Task.
+ */
+interface TaskFactory {
+    /** Returns true if this collection contains an item with the given name.  */
+    fun containsKey(name: String): Boolean
+
+    /**
+     * Returns the [Task] named name from the current set of defined tasks.
+     *
+     * @param name the name of the requested [Task]
+     * @return the [Task] instance or null if not found.
+     */
+    fun findByName(name: String): Task?
+
+    /** Creates a task with the given name.  */
+    fun create(name: String): Task
+
+    /** Creates a task with the given name and type.  */
+    fun <S : Task> create(name: String, type: Class<S>): S
+
+    /** Creates a task with the given [TaskConfigAction]  */
+    fun <T : Task> create(configAction: TaskConfigAction<T>): T
+
+    /** Creates a task wit the given name, type and configuration action  */
+    fun <T : Task> create(
+        taskName: String, taskClass: Class<T>, configAction: Action<T>
+    ): T
+
+    /** Creates a task with the given name and config action.  */
+    fun create(taskName: String, configAction: Action<DefaultTask>): DefaultTask
+
+    /** Applies the given configAction to the task with given name.  */
+    fun configure(name: String, configAction: Action<in Task>)
+}
