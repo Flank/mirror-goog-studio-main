b23536fafe0cb8f5eec22f823e88ee0a8397f09d
estebandlc <estebandlc@google.com>
2018-08-03 15:42:12 -0700

    Updates the test deployment script
    The script now:
    . Uses the workspace it's in
    . Uses the abi of the phone that it connects to.
    
    Test: N/A
    Bug: N/A
    Change-Id: I7f05966019e10cc6fe7f86eb6664229d3b9ebe31

--- deploy/installer/install_to_device.sh
+++ deploy/installer/install_to_device.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-
-echo "Be sure to modify MY_HOME to point to where your studio-master-dev branch lives!"
-echo "If this fails, it's probably because of that."
-MY_HOME=$HOME
-
-$MY_HOME/studio-master-dev/tools/base/bazel/bazel build //tools/base/deploy/...
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+WORKSPACE=$DIR/../../../..
+$WORKSPACE/tools/base/bazel/bazel build //tools/base/deploy/...
 
 adb shell mkdir -p /data/local/tmp/.ir2
 adb shell mkdir -p /data/local/tmp/.ir2/bin
 adb shell mkdir -p /data/local/tmp/.ir2/dumps
 adb shell mkdir -p /data/local/tmp/.ir2/apps
 
-adb push $MY_HOME/studio-master-dev/bazel-bin/tools/base/deploy/installer/android/x86/ir2_installer /data/local/tmp/.ir2/bin
-adb push $MY_HOME/studio-master-dev/bazel-bin/tools/base/deploy/agent/native/android/x86/libswap.so /data/local/tmp/.ir2/bin/agent.so
-adb push $MY_HOME/studio-master-dev/bazel-out/k8-fastbuild/genfiles/tools/base/deploy/agent/instrumentation/instrumentation.jar /data/local/tmp/.ir2/bin/support.dex
+ABI=$(adb shell getprop ro.product.cpu.abi)
+adb push $WORKSPACE/bazel-bin/tools/base/deploy/installer/android/$ABI/ir2_installer /data/local/tmp/.ir2/bin
+adb push $WORKSPACE/bazel-bin/tools/base/deploy/agent/native/android/$ABI/libswap.so /data/local/tmp/.ir2/bin/agent.so
+adb push $WORKSPACE/bazel-genfiles/tools/base/deploy/agent/instrumentation/instrumentation.jar /data/local/tmp/.ir2/bin/support.dex
 
 adb shell chmod +x /data/local/tmp/.ir2/bin/ir2_installer
 adb shell "/data/local/tmp/.ir2/bin/ir2_installer swap 1" < "$1"
