40ba262bc383533aa75fb34ff42e4247a8c25383
Armand Navabi <navabi@google.com>
2018-08-09 16:19:52 -0700

    Make go/ab launch script run bazel foundry and copy results to directory for ATP
    Bug: 111844253
    Change-Id: I24fc6c84956d13590a5d32ed3c530b1dcf8d3407

--- bazel/test_studio.sh
+++ bazel/test_studio.sh
@@ -7,7 +7,15 @@ readonly dist_dir="$2"
 readonly build_number="$3"
 
 readonly script_dir="$(dirname "$0")"
-echo "${script_dir}/bazel" --max_idle_secs=60 --bazelrc=/dev/null test --keep_going --bes_backend=buildeventservice.googleapis.com --auth_credentials="$HOME"/.android-studio-alphasource.json --auth_scope=https://www.googleapis.com/auth/cloud-source-tools --project_id=908081808034 --config=remote --cache_test_results=no --build_tag_filters=-no_linux --test_tag_filters=-no_linux,-no_test_linux,-qa_sanity,-qa_fast,-qa_unreliable $(< "${script_dir}/targets")
+"${script_dir}/bazel" --max_idle_secs=60 --bazelrc=/dev/null test --keep_going --bes_backend=buildeventservice.googleapis.com --auth_credentials="$HOME"/.android-studio-alphasource.json --auth_scope=https://www.googleapis.com/auth/cloud-source-tools --project_id=908081808034 --config=remote --cache_test_results=no --build_tag_filters=-no_linux --test_tag_filters=-no_linux,-no_test_linux,-qa_sanity,-qa_fast,-qa_unreliable $(< "${script_dir}/targets")
 
-# readonly testlogs_dir="$(${script_dir}/bazel info bazel-testlogs)"
-# (cd "${testlogs_dir}" && zip -qr "${dist_dir}/bazel_testlogs_${build_number}.zip" *)
+if [[ -d "${dist_dir}" ]]; then
+  # on AB/ATP, follow conventions to use gtest-testlog-forwarding
+  readonly testlogs_dir="$(${script_dir}/bazel info bazel-testlogs)"
+  mkdir "${dist_dir}"/gtest
+  # This does not handle spaces in file names.
+  for xml_source in $(cd "${testlogs_dir}" && find -name '*.xml' -printf '%P\n'); do
+    xml_target="$(echo "${xml_source}" | tr '/' '_')"
+    cp -pv "${testlogs_dir}/${xml_source}" "${dist_dir}/gtest/${xml_target}"
+  done
+fi
