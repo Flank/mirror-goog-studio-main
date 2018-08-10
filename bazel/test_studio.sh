1f50565beb36b23950b41314da5f5db91f3c8c02
Dana Dahlstrom <dahlstrom@google.com>
2018-08-10 12:00:00 -0700

    Run //tools/base/bazel/foundry:test in studio_rbe
    … and copy testlogs into place for gtest-testlog-forwarding on ATP.
    
    Change-Id: I24fc6c84956d13590a5d32ed3c530b1dcf8d3408
    Issue: 111844253

--- bazel/test_studio.sh
+++ bazel/test_studio.sh
@@ -7,7 +7,15 @@ readonly dist_dir="$2"
 readonly build_number="$3"
 
 readonly script_dir="$(dirname "$0")"
-echo "${script_dir}/bazel" --max_idle_secs=60 --bazelrc=/dev/null test --keep_going --bes_backend=buildeventservice.googleapis.com --auth_credentials="$HOME"/.android-studio-alphasource.json --auth_scope=https://www.googleapis.com/auth/cloud-source-tools --project_id=908081808034 --config=remote --cache_test_results=no --build_tag_filters=-no_linux --test_tag_filters=-no_linux,-no_test_linux,-qa_sanity,-qa_fast,-qa_unreliable $(< "${script_dir}/targets")
+"${script_dir}/bazel" --max_idle_secs=60 --bazelrc=/dev/null test --keep_going --bes_backend=buildeventservice.googleapis.com --auth_credentials="$HOME"/.android-studio-alphasource.json --auth_scope=https://www.googleapis.com/auth/cloud-source-tools --project_id=908081808034 --config=remote --cache_test_results=no --build_tag_filters=-no_linux --test_tag_filters=-no_linux,-no_test_linux,-qa_sanity,-qa_fast,-qa_unreliable //tools/base/bazel/foundry:test  # $(< "${script_dir}/targets")
 
-# readonly testlogs_dir="$(${script_dir}/bazel info bazel-testlogs)"
-# (cd "${testlogs_dir}" && zip -qr "${dist_dir}/bazel_testlogs_${build_number}.zip" *)
+if [[ -d "${dist_dir}" ]]; then
+  # follow conventions to use gtest-testlog-forwarding on ATP
+  readonly testlogs_dir="$(${script_dir}/bazel info bazel-testlogs)"
+  mkdir "${dist_dir}"/gtest
+  # This does not handle spaces in file names.
+  for source_xml in $(cd "${testlogs_dir}" && find -name '*.xml' -printf '%P\n'); do
+    target_xml="$(echo "${source_xml}" | tr '/' '_')"
+    cp -pv "${testlogs_dir}/${source_xml}" "${dist_dir}/gtest/${target_xml}"
+  done
+fi
