e99aa54b81f2c684fcaab075db160c582b8bc279
Dana Dahlstrom <dahlstrom@google.com>
2018-08-10 19:00:00 -0700

    Add --nobuild_runfile_links option to bazel test
    … following Change I4db00d0c.
    Also add “--” separator before target patterns.
    
    Change-Id: I72a9db38d8e9a11eb765a3ad250c262f70d6be8f

--- bazel/test_studio.sh
+++ bazel/test_studio.sh
@@ -7,7 +7,7 @@ readonly dist_dir="$2"
 readonly build_number="$3"
 
 readonly script_dir="$(dirname "$0")"
-"${script_dir}/bazel" --max_idle_secs=60 --bazelrc=/dev/null test --keep_going --bes_backend=buildeventservice.googleapis.com --auth_credentials="$HOME"/.android-studio-alphasource.json --auth_scope=https://www.googleapis.com/auth/cloud-source-tools --project_id=908081808034 --config=remote --cache_test_results=no --build_tag_filters=-no_linux --test_tag_filters=-no_linux,-no_test_linux,-qa_sanity,-qa_fast,-qa_unreliable $(< "${script_dir}/targets")
+"${script_dir}/bazel" --max_idle_secs=60 --bazelrc=/dev/null test --keep_going --nobuild_runfile_links --bes_backend=buildeventservice.googleapis.com --auth_credentials="$HOME"/.android-studio-alphasource.json --auth_scope=https://www.googleapis.com/auth/cloud-source-tools --project_id=908081808034 --config=remote --cache_test_results=no --build_tag_filters=-no_linux --test_tag_filters=-no_linux,-no_test_linux,-qa_sanity,-qa_fast,-qa_unreliable -- $(< "${script_dir}/targets")
 
 if [[ -d "${dist_dir}" ]]; then
   # follow conventions to use gtest-testlog-forwarding on ATP
