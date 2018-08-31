b291748c5d70dc38719ab7a26498d1957ef98257
Armand Navabi <navabi@google.com>
2018-08-31 15:28:50 -0700

    Add baseline tests for CPU, memory and disk performance on RBE.
    These tests are intended to provide a baseline for CPU, memory and disk usage
    for remote build execution from our CI. We expect very little variance across
    builds for these metrics. Any changes in the performance of these metrics should
    indicate changes on the VM's that run our remote builds in RBE.
    
    Bug: N/A
    Test: bazel test foundry:perfgate-baseline-tests
    Change-Id: Ie013759f0f531048b098190ed589fbfaa3e9eecb

---
+++ bazel/foundry/FoundryPerfgateMetrics.java
@@ -0,0 +1,82 @@
+
+import com.android.tools.perflogger.Benchmark;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Vector;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class FoundryPerfgateMetrics {
+
+    private static Benchmark benchmark = new Benchmark.Builder("Foundry Baseline Tests").build();
+
+    @Test
+    public void testPerfgateCPUTest() {
+        System.out.println("testPerfgateCPUTest");
+        long foo = 0; // arbitrary variable for arbitrary CPU loop
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) for (int j = 0; j < 10000; j++) foo += Math.tan(i + j);
+        long elapsedTime = System.currentTimeMillis() - startTime;
+        System.out.println("elapsedTime: " + elapsedTime);
+        benchmark.log("cpu_test", elapsedTime);
+    }
+
+    private static void doubleCopyFile(File src, File dst) throws IOException {
+        try (InputStream is = new FileInputStream(src);
+                OutputStream os = new FileOutputStream(dst)) {
+            byte[] buffer = new byte[5120];
+            int length;
+            while ((length = is.read(buffer)) > 0) {
+                os.write(buffer, 0, length);
+                os.write(buffer, 0, length);
+                os.flush();
+            }
+        }
+    }
+
+    @Test
+    public void testPerfgateDiskTest() throws IOException {
+        System.out.println("testPerfgateDiskTest");
+        long startTime = System.currentTimeMillis();
+        File src = File.createTempFile("foo", ".out");
+        FileWriter writer = new FileWriter(src);
+        writer.write("foobar baz foo bazfoobaz bar");
+        writer.close();
+        for (int i = 0; i < 14; i++) {
+            System.out.println("src file size: " + src.length());
+            File dst = File.createTempFile("bar", ".out");
+            doubleCopyFile(src, dst);
+            doubleCopyFile(dst, src);
+            dst.delete();
+        }
+        src.delete();
+        long elapsedTime = System.currentTimeMillis() - startTime;
+        System.out.println("elapsedTime: " + elapsedTime);
+        benchmark.log("disk_test", elapsedTime);
+    }
+
+    @Test
+    public void testPerfgateMemoryTest() {
+        int byteArraySize = 1024 * 1024 * 16;
+        System.out.println("testPerfgateMemoryTest");
+        Vector v = new Vector();
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < 2048; i++) {
+            // Allocate a large byte array and keep it around to avoid GC
+            byte b[] = new byte[byteArraySize];
+            v.add(b);
+            if (i % 200 == 0)
+                System.out.println("free memory: " + Runtime.getRuntime().freeMemory());
+        }
+        long elapsedTime = System.currentTimeMillis() - startTime;
+        System.out.println("elapsedTime: " + elapsedTime);
+        benchmark.log("memory_test", elapsedTime);
+    }
+}
