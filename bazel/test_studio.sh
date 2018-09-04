5f4024aa69c3bb1aee8f8d7cca1facfc94c0c95b
Christopher Pilch <cpilch@google.com>
2018-09-04 07:18:02 -0700

    Create a Mac and Windows OS specific codepath
    Create a Mac/Windows OS specific codepath that will simply echo a
    confirmation statement. We wish to set up a both Mac and Windows
    based AndroidBuild targets, and we require a specified
    target/branch combination to do so. This checkin allows us to
    start running a new target/branch without interrupting existing
    builds or machine resources.
    
    Change-Id: Ifd62f9e91d3e97338f44f51b3c64a979ac6251b9

--- bazel/test_studio.sh
+++ bazel/test_studio.sh
@@ -6,6 +6,22 @@ readonly out_dir="$1"
 readonly dist_dir="$2"
 readonly build_number="$3"
 
+# We wish to set up a new Mac based build branch.  This initial check
+# is to allow a simple echo statement confirmation on the buildbot.
+unamestr=`uname`
+if [[ "$unamestr" == 'Darwin' ]]; then
+  echo "Mac Build Detected.  No Build to perform."
+  exit 0;
+fi
+
+# For Windows, we know that the string will contain Cygwin (if
+# executed through Cygwin) combined with NT Version.  Since NT
+# version is variable due to updates, check for CYGWIN in regex string.
+if [[ "$unamestr" == *'CYGWIN'* ]]; then
+  echo "Windows Cygwin Build Detected.  No Build to perform."
+  exit 0;
+fi
+
 readonly script_dir="$(dirname "$0")"
 "${script_dir}/bazel" --max_idle_secs=60 --bazelrc=/dev/null test --keep_going --nobuild_runfile_links --bes_backend=buildeventservice.googleapis.com --auth_credentials="$HOME"/.android-studio-alphasource.json --auth_scope=https://www.googleapis.com/auth/cloud-source-tools --project_id=908081808034 --config=remote --cache_test_results=no --build_tag_filters=-no_linux --test_tag_filters=-no_linux,-no_test_linux,-qa_sanity,-qa_fast,-qa_unreliable,-slow -- $(< "${script_dir}/targets")
 
