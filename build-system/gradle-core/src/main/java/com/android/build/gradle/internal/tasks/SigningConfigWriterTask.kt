825bb616b495629080fcb980ab667aa07e32241c
Udam Saini <udam@google.com>
2018-09-12 17:44:45 -0700

    Create a task to write the signing config to a file.
    We need this task so that dynamic-features can read the signing config
    from the base module. The signing config should not be read for each
    dynamic-feature, but shared with the base module.
    
    Future cl's will read the signing config and move any dynamic-features
    that read the signing config from the variant configuration to read it
    based on a task. This will also configure the instant run packaging too
    later.
    
    bug:114626324
    Test: added unit test
    Change-Id: I946eec4a67b9c25e00eea1acc2aab89ec7f9d107

---
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/tasks/SigningConfigWriterTask.kt
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.build.gradle.internal.tasks
+
+import com.android.annotations.VisibleForTesting
+import com.android.build.gradle.internal.dsl.CoreSigningConfig
+import com.android.build.gradle.internal.scope.InternalArtifactType
+import com.android.build.gradle.internal.scope.VariantScope
+import com.android.build.gradle.tasks.AnnotationProcessingTaskCreationAction
+import com.android.build.gradle.tasks.Initial
+import com.android.build.gradle.tasks.InternalID
+import com.google.gson.GsonBuilder
+import org.apache.commons.io.FileUtils
+import org.gradle.api.file.Directory
+import org.gradle.api.provider.Provider
+import java.io.File
+import java.io.IOException
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.TaskAction
+
+/**
+ * Task that writes the SigningConfig information and publish it for dynamic-feature modules.
+ */
+open class SigningConfigWriterTask : AndroidVariantTask() {
+
+    @VisibleForTesting
+    internal val PERSISTED_FILE_NAME = "signing-config.json"
+
+    @get:OutputDirectory
+    @get:InternalID(InternalArtifactType.METADATA_SIGNING_CONFIG)
+    @get:Initial(out="")
+    lateinit var outputDirectory: Provider<Directory>
+        internal set
+
+    @get:Input
+    lateinit var signingConfig: CoreSigningConfig
+        internal set
+
+    @TaskAction
+    @Throws(IOException::class)
+    fun fullTaskAction() {
+        val outputFile = File(outputDirectory.get().asFile, PERSISTED_FILE_NAME)
+        val gsonBuilder = GsonBuilder()
+        val gson = gsonBuilder.create()
+        FileUtils.write(outputFile, gson.toJson(signingConfig))
+    }
+
+    class CreationAction(variantScope: VariantScope) :
+        AnnotationProcessingTaskCreationAction<SigningConfigWriterTask>(
+            variantScope,
+            variantScope.getTaskName("signingConfigWriter"),
+            SigningConfigWriterTask::class.java) {
+
+        override fun configure(task: SigningConfigWriterTask) {
+            super.configure(task)
+
+            task.signingConfig = variantScope.variantConfiguration.signingConfig!!
+        }
+    }
+}
