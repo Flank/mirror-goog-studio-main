a1b3d4b6fdf3bc75d45bad5c86dc4ff031b44886
Tamas Kenez <tamaskenez@google.com>
2018-09-24 12:36:15 +0200

    Update D8 R8 master to 1.3.22-dev
    Version: master abe98d4deff31ea1ce86169baa8486b64d5910c7
    This is a development snapshot, it's fine to use for studio canary build, but
    not for BETA or release, for those we would need a release version of R8
    binaries.
    This build IS suitable for preview release but IS NOT suitable for public release.
    
    Built here: go/r8-releases/raw/master/abe98d4deff31ea1ce86169baa8486b64d5910c7/
    Test: ./gradlew check
    Bug:
    
    Change-Id: Idc9b938c299b35e5e5a9c6f6ff547ca753fe4ef8

--- build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/sanity/JarContentsTest.java
+++ build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/sanity/JarContentsTest.java
@@ -297,6 +297,7 @@ public class JarContentsTest {
                 "com/android/tools/r8/ir/optimize/lambda/",
                 "com/android/tools/r8/ir/optimize/lambda/kotlin/",
                 "com/android/tools/r8/ir/optimize/staticizer/",
+                "com/android/tools/r8/ir/optimize/string/",
                 "com/android/tools/r8/ir/regalloc/",
                 "com/android/tools/r8/ir/synthetic/",
                 "com/android/tools/r8/it/",
