45f475f49a861ec5df5bca55d64a878bb5402ff9
Christopher Pilch <cpilch@google.com>
2018-09-26 15:08:32 -0700

    Updates script to create upsalite redirect file
    Updates script to capture the upsalite id returned from the bazel
    invocation, and insert this into a redirect html file in dist_dirs.
    This will allow developers easier access to Upsalite test reporting when
    viewing data on go/ab branches.
    
    Test:  Ran local.
    Bug:  N/A
    Change-Id: Ib1433f702cc0845aff766639279b177aa050f9a5

--- bazel/test_studio.sh
+++ bazel/test_studio.sh
@@ -23,11 +23,20 @@ if [[ "$unamestr" == *'CYGWIN'* ]]; then
 fi
 
 readonly script_dir="$(dirname "$0")"
+
+# Grab the location of the command_log file for bazel daemon so we can search it later.
+readonly command_log="$(${script_dir}/bazel info command_log)"
+
+# Run Bazel
 "${script_dir}/bazel" --max_idle_secs=60 --bazelrc=/dev/null test --keep_going --nobuild_runfile_links --bes_backend=buildeventservice.googleapis.com --auth_credentials="$HOME"/.android-studio-alphasource.json --auth_scope=https://www.googleapis.com/auth/cloud-source-tools --project_id=908081808034 --config=remote --cache_test_results=no --build_tag_filters=-no_linux --test_tag_filters=-no_linux,-no_test_linux,-qa_sanity,-qa_fast,-qa_unreliable,-slow -- $(< "${script_dir}/targets")
 
 if [[ -d "${dist_dir}" ]]; then
+  # Grab the upsalite_id from the stdout of the bazel command.  This is captured in command.log
+  readonly upsalite_id=`sed -n -e 's/.*invocation_id: //p' $command_log`
+  echo "<meta http-equiv=\"refresh\" content=\"0; URL='https://source.cloud.google.com/results/invocations/$upsalite_id\" />" > "${dist_dir}"/upsalite_test_results.html
+
   # follow conventions to use gtest-testlog-forwarding on ATP
-  readonly testlogs_dir="$(${script_dir}/bazel info -c opt bazel-testlogs)"
+  readonly testlogs_dir="$(${script_dir}/bazel info bazel-testlogs)"
   mkdir "${dist_dir}"/gtest
   # This does not handle spaces in file names.
   for source_xml in $(cd "${testlogs_dir}" && find -name '*.xml' -printf '%P\n'); do
