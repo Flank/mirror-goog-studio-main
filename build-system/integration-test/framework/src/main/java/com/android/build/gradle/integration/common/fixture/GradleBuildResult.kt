476d15377bc99f1a238a0ec7bf03ca3cba82fb99
Stephen Atwood <stephenatwood@google.com>
2018-10-22 15:39:20 -0700

    Convert GradleBuildResult to Kotlin
    GradleBuildResult containined some stuff that looked like it would be
    much cleaner in Kotlin, including lazy initialization and a handful of
    "get___()" methods, which translated nicely into properties.
    
    Bug: N/A
    Test: Test change
    Change-Id: I31608b2477701d3e6a078b4c9134e284318e6f2a

---
+++ build-system/integration-test/framework/src/main/java/com/android/build/gradle/integration/common/fixture/GradleBuildResult.kt
@@ -0,0 +1,143 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.build.gradle.integration.common.fixture
+
+import com.google.common.truth.Truth.assert_
+import com.android.build.gradle.integration.common.truth.GradleOutputFileSubject
+import com.android.build.gradle.integration.common.truth.GradleOutputFileSubjectFactory
+import com.android.build.gradle.integration.common.truth.TaskStateList
+import com.google.api.client.repackaged.com.google.common.base.Preconditions
+import com.google.api.client.repackaged.com.google.common.base.Throwables
+import com.google.common.base.Splitter
+import com.google.common.collect.ImmutableList
+import com.google.common.collect.Sets
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.util.HashSet
+import org.gradle.api.ProjectConfigurationException
+import org.gradle.api.tasks.TaskExecutionException
+import org.gradle.internal.serialize.PlaceholderException
+import org.gradle.tooling.BuildException
+import org.gradle.tooling.GradleConnectionException
+import org.gradle.tooling.events.ProgressEvent
+
+/**
+ * The result from running a build.
+ * See [GradleTestProject.executor] and [GradleTaskExecutor].
+ *
+ * @property exception The exception from the build, null if the build succeeded.
+ */
+class GradleBuildResult(
+    stdout: ByteArrayOutputStream,
+    stderr: ByteArrayOutputStream,
+    private val taskEvents: ImmutableList<ProgressEvent>,
+    val exception: GradleConnectionException?
+) {
+
+    val stdout: String = stdout.toString()
+    val stderr: String = stderr.toString()
+
+    /**
+     * Most tests don't examine the state of the build's tasks and [TaskStateList] is relatively
+     * expensive to initialize, so this is done lazily.
+     */
+    private val taskStates: TaskStateList by lazy {
+        TaskStateList(taskEvents, this.stdout)
+    }
+
+    /**
+     * Returns the short (single-line) message that Gradle would print out in the console, without
+     * `--stacktrace`. If the build succeeded, returns null.
+     */
+    val failureMessage: String?
+        get() = exception?.let {
+            val causalChain = Throwables.getCausalChain(exception)
+            // Try the common scenarios: configuration or task failure.
+            for (throwable in causalChain) {
+                // Because of different class loaders involved, we are forced to do stringly-typed
+                // programming.
+                val throwableType = throwable.javaClass.name
+                if (throwableType == ProjectConfigurationException::class.java.name) {
+                    return throwable.cause?.message ?: throw AssertionError(
+                        "Exception had unexpected structure.",
+                        exception
+                    )
+                } else if (throwableType == PlaceholderException::class.java.name) {
+                    if (throwable.toString().startsWith(TaskExecutionException::class.java.name)) {
+                        var cause = throwable
+                        // there can be several levels of PlaceholderException when dealing with
+                        // Worker API failures.
+                        while (cause.javaClass.name == PlaceholderException::class.java.name && cause.cause != null) {
+                            cause = cause.cause
+                        }
+                        return cause.message
+                    }
+                }
+            }
+
+            // Look for any BuildException, for other cases.
+            for (throwable in causalChain) {
+                val throwableType = throwable.javaClass.name
+                if (throwableType == BuildException::class.java.name) {
+                    return throwable.cause?.message ?: throw AssertionError(
+                        "Exception had unexpected structure.",
+                        exception
+                    )
+                }
+            }
+
+            throw AssertionError("Failed to determine the failure message.", exception)
+        }
+
+    val stdoutAsLines: List<String>
+        get() = Splitter.on(System.lineSeparator()).omitEmptyStrings().split(stdout).toList()
+
+    val tasks: List<String>
+        get() = taskStates.plannedForExecutionTasks
+
+    val upToDateTasks: Set<String>
+        get() = taskStates.upToDateTasks
+
+    /** Use [didWorkTasks] instead for clearer semantics.  */
+    val notUpToDateTasks: Set<String>
+        get() = Sets.difference(tasks.toSet(), upToDateTasks)
+
+    val fromCacheTasks: Set<String>
+        get() = taskStates.fromCacheTasks
+
+    val didWorkTasks: Set<String>
+        get() = taskStates.didWorkTasks
+
+    val skippedTasks: Set<String>
+        get() = taskStates.skippedTasks
+
+    val failedTasks: Set<String>
+        get() = taskStates.failedTasks
+
+    /**
+     * Truth style assert to check changes to a file.
+     */
+    fun assertThatFile(subject: File): GradleOutputFileSubject {
+        return assert_().about(GradleOutputFileSubjectFactory.factory(stdout)).that(subject)
+    }
+
+    fun getTask(name: String): TaskStateList.TaskInfo {
+        Preconditions.checkArgument(name.startsWith(":"), "Task name must start with :")
+        return taskStates.getTask(name)
+    }
+
+}
