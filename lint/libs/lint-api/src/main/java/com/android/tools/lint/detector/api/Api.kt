b81b7d59cbec775a0d675061a2f13ff8c9007cd4
Tor Norbye <tnorbye@google.com>
2018-10-23 15:53:10 -0700

    Bump lint API level
    My last CL to bump the API level only updated the
    API-level-to-string method for API 4, but didn't actually
    rev the current level.
    
    Bug: N/A
    Test: Covered by JarFileIssueRegistryTest
    Change-Id: Ib7951f90ce4abd9714a9f2363de3817e4f790927

--- lint/libs/lint-api/src/main/java/com/android/tools/lint/detector/api/Api.kt
+++ lint/libs/lint-api/src/main/java/com/android/tools/lint/detector/api/Api.kt
@@ -25,7 +25,7 @@ import com.android.tools.lint.client.api.IssueRegistry
  * value, not read the current value from the hosting lint environment
  * when the custom lint checks are loaded into lint.
  */
-const val CURRENT_API = 3
+const val CURRENT_API = 4
 
 /** Describes the given API level */
 fun describeApi(api: Int): String {
