1d0913c68ac7e4406db56343523de00efbd2a988
Dana Dahlstrom <dahlstrom@google.com>
2018-11-10 14:30:00 -0700

    Move --config option before the info key
    … according to prescribed syntax.
    
    While touching the line, also quote variable expansions.
    
    Change-Id: I3b3d87f9aeb708ff0b8f5d20ddcde592a68e5ee9

--- bazel/test_studio.sh
+++ bazel/test_studio.sh
@@ -17,7 +17,7 @@ fi
 readonly script_dir="$(dirname "$0")"
 
 # Grab the location of the command_log file for bazel daemon so we can search it later.
-readonly command_log="$(${script_dir}/bazel --bazelrc=${script_dir}/toplevel.bazel.rc info command_log --config=remote)"
+readonly command_log="$("${script_dir}"/bazel --bazelrc="${script_dir}"/toplevel.bazel.rc info --config=remote command_log)"
 
 # Run Bazel
 "${script_dir}/bazel" --max_idle_secs=60 --bazelrc=${script_dir}/toplevel.bazel.rc test --keep_going --nobuild_runfile_links --bes_backend=buildeventservice.googleapis.com --auth_credentials="$HOME"/.android-studio-alphasource.json --auth_scope=https://www.googleapis.com/auth/cloud-source-tools --project_id=908081808034 --config=remote --cache_test_results=no --build_tag_filters=-no_linux --test_tag_filters=-no_linux,-no_test_linux,-qa_sanity,-qa_fast,-qa_unreliable,-slow -- $(< "${script_dir}/targets")
