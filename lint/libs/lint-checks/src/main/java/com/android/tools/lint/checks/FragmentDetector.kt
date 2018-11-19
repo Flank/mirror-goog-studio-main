6a39df8375e7875e662fcbf4ccdd3f70bf4eb574
Tor Norbye <tnorbye@google.com>
2018-11-19 09:51:45 -0800

    Port FragmentDetector to Kotlin
    No semantic changes. This is prep before making other
    changes to it.
    
    Bug: N/A
    Test: Covered by existing tests
    Change-Id: I4649f636709f31917c8540d78d335db339fd371e

---
+++ lint/libs/lint-checks/src/main/java/com/android/tools/lint/checks/FragmentDetector.kt
@@ -0,0 +1,130 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tools.lint.checks
+
+import com.android.SdkConstants.CLASS_FRAGMENT
+import com.android.SdkConstants.CLASS_V4_FRAGMENT
+import com.android.tools.lint.detector.api.Category
+import com.android.tools.lint.detector.api.Detector
+import com.android.tools.lint.detector.api.Implementation
+import com.android.tools.lint.detector.api.Issue
+import com.android.tools.lint.detector.api.JavaContext
+import com.android.tools.lint.detector.api.Scope
+import com.android.tools.lint.detector.api.Severity
+import com.android.tools.lint.detector.api.SourceCodeScanner
+import org.jetbrains.uast.UAnonymousClass
+import org.jetbrains.uast.UClass
+
+/**
+ * Checks that Fragment subclasses can be instantiated via Class.newInstance: the
+ * class is public, static, and has a public null constructor.
+ *
+ * This helps track down issues like
+ * http://stackoverflow.com/questions/8058809/fragment-activity-crashes-on-screen-rotate
+ * (and countless duplicates)
+ */
+class FragmentDetector : Detector(), SourceCodeScanner {
+    companion object {
+        /** Are fragment subclasses instantiatable? */
+        @JvmField
+        val ISSUE = Issue.create(
+            id = "ValidFragment",
+            briefDescription = "Fragment not instantiatable",
+            explanation = """
+                From the Fragment documentation:
+                **Every** fragment must have an empty constructor, so it can be instantiated \
+                when restoring its activity's state. It is strongly recommended that subclasses \
+                do not have other constructors with parameters, since these constructors will \
+                not be called when the fragment is re-instantiated; instead, arguments can be \
+                supplied by the caller with `setArguments(Bundle)` and later retrieved by the \
+                Fragment with `getArguments()`.
+                """,
+            category = Category.CORRECTNESS,
+            androidSpecific = true,
+            priority = 6,
+            severity = Severity.ERROR,
+            moreInfo = "http://developer.android.com/reference/android/app/Fragment.html#Fragment()",
+            implementation = Implementation(FragmentDetector::class.java, Scope.JAVA_FILE_SCOPE)
+        )
+    }
+
+    // ---- implements SourceCodeScanner ----
+
+    override fun applicableSuperClasses(): List<String>? {
+        return listOf(
+            CLASS_FRAGMENT, CLASS_V4_FRAGMENT.oldName(), CLASS_V4_FRAGMENT.newName()
+        )
+    }
+
+    override fun visitClass(context: JavaContext, declaration: UClass) {
+        if (declaration is UAnonymousClass) {
+            context.report(ISSUE, declaration, context.getNameLocation(declaration),
+                "Fragments should be static such that they can be re-instantiated by the system, and anonymous classes are not static"
+            )
+            return
+        }
+
+        val evaluator = context.evaluator
+        if (evaluator.isAbstract(declaration)) {
+            return
+        }
+
+        if (!evaluator.isPublic(declaration)) {
+            context.report(ISSUE, declaration, context.getNameLocation(declaration),
+                "This fragment class should be public (${declaration.qualifiedName})"
+            )
+            return
+        }
+
+        if (declaration.containingClass != null && !evaluator.isStatic(declaration)) {
+            context.report(
+                ISSUE, declaration, context.getNameLocation(declaration),
+                "This fragment inner class should be static (${declaration.qualifiedName})"
+            )
+            return
+        }
+
+        var hasDefaultConstructor = false
+        var hasConstructor = false
+        for (constructor in declaration.constructors) {
+            hasConstructor = true
+            if (constructor.parameterList.parametersCount == 0) {
+                if (evaluator.isPublic(constructor)) {
+                    hasDefaultConstructor = true
+                } else {
+                    val location = context.getNameLocation(constructor)
+                    context.report(
+                        ISSUE, constructor, location, "The default constructor must be public"
+                    )
+                    return
+                }
+            } else {
+                val location = context.getNameLocation(constructor)
+                // TODO: Use separate issue for this which isn't an error
+                val message =
+                    "Avoid non-default constructors in fragments: use a default constructor plus `Fragment#setArguments(Bundle)` instead"
+                context.report(ISSUE, constructor, location, message)
+            }
+        }
+
+        if (!hasDefaultConstructor && hasConstructor) {
+            val message =
+                "This fragment should provide a default constructor (a public constructor with no arguments) (`${declaration.qualifiedName}`)"
+            context.report(ISSUE, declaration, context.getNameLocation(declaration), message)
+        }
+    }
+}
---
+++ lint/libs/lint-tests/src/test/java/com/android/tools/lint/checks/FragmentDetectorTest.kt
@@ -0,0 +1,142 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tools.lint.checks
+
+import com.android.tools.lint.detector.api.Detector
+
+class FragmentDetectorTest : AbstractCheckTest() {
+    override fun getDetector(): Detector {
+        return FragmentDetector()
+    }
+
+    fun testBasic() {
+        val expected = """
+            src/test/pkg/FragmentTest.java:10: Error: This fragment class should be public (test.pkg.FragmentTest.Fragment1) [ValidFragment]
+                private static class Fragment1 extends Fragment {
+                                     ~~~~~~~~~
+            src/test/pkg/FragmentTest.java:15: Error: This fragment inner class should be static (test.pkg.FragmentTest.Fragment2) [ValidFragment]
+                public class Fragment2 extends Fragment {
+                             ~~~~~~~~~
+            src/test/pkg/FragmentTest.java:21: Error: The default constructor must be public [ValidFragment]
+                    private Fragment3() {
+                            ~~~~~~~~~
+            src/test/pkg/FragmentTest.java:26: Error: This fragment should provide a default constructor (a public constructor with no arguments) (test.pkg.FragmentTest.Fragment4) [ValidFragment]
+                public static class Fragment4 extends Fragment {
+                                    ~~~~~~~~~
+            src/test/pkg/FragmentTest.java:27: Error: Avoid non-default constructors in fragments: use a default constructor plus Fragment#setArguments(Bundle) instead [ValidFragment]
+                    private Fragment4(int dummy) {
+                            ~~~~~~~~~
+            src/test/pkg/FragmentTest.java:36: Error: Avoid non-default constructors in fragments: use a default constructor plus Fragment#setArguments(Bundle) instead [ValidFragment]
+                    public Fragment5(int dummy) {
+                           ~~~~~~~~~
+            6 errors, 0 warnings
+            """
+
+        lint().files(
+            java(
+                "src/test/pkg/FragmentTest.java",
+                """
+                package test.pkg;
+
+                import android.annotation.SuppressLint;
+                import android.app.Fragment;
+
+                @SuppressWarnings("unused")
+                public class FragmentTest {
+
+                    // Should be public
+                    private static class Fragment1 extends Fragment {
+
+                    }
+
+                    // Should be static
+                    public class Fragment2 extends Fragment {
+
+                    }
+
+                    // Should have a public constructor
+                    public static class Fragment3 extends Fragment {
+                        private Fragment3() {
+                        }
+                    }
+
+                    // Should have a public constructor with no arguments
+                    public static class Fragment4 extends Fragment {
+                        private Fragment4(int dummy) {
+                        }
+                    }
+
+                    // Should *only* have the default constructor, not the
+                    // multi-argument one
+                    public static class Fragment5 extends Fragment {
+                        public Fragment5() {
+                        }
+                        public Fragment5(int dummy) {
+                        }
+                    }
+
+                    // Suppressed
+                    @SuppressLint("ValidFragment")
+                    public static class Fragment6 extends Fragment {
+                        private Fragment6() {
+                        }
+                    }
+
+                    public static class ValidFragment1 extends Fragment {
+                        public ValidFragment1() {
+                        }
+                    }
+
+                    // (Not a fragment)
+                    private class NotAFragment {
+                    }
+
+                    // Ok: Has implicit constructor
+                    public static class Fragment7 extends Fragment {
+                    }
+                }
+                """
+            ).indented()
+        ).run().expect(expected)
+    }
+
+    fun testAnonymousInnerClass() {
+        val expected = """
+            src/test/pkg/Parent.java:7: Error: Fragments should be static such that they can be re-instantiated by the system, and anonymous classes are not static [ValidFragment]
+                    return new Fragment() {
+                               ~~~~~~~~
+            1 errors, 0 warnings"""
+
+        lint().files(
+            java(
+                "src/test/pkg/Parent.java",
+                """
+                    package test.pkg;
+
+                    import android.app.Fragment;
+
+                    public class Parent {
+                        public Fragment method() {
+                            return new Fragment() {
+                            };
+                        }
+                    }
+                    """
+            ).indented()
+        ).run().expect(expected)
+    }
+}
