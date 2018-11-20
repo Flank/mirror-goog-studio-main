535b2ab225782cd5758c9b66320bc21e7c9f33df
Ivan Gavrilovic <gavra@google.com>
2018-11-20 18:15:56 +0000

    Compute provided classpath using component identifiers
    This CL changes how we compute provided classpath. When computing
    it, component identifier is used to find artifacts that are
    in the compile classpath, and not in the runtime one. We consider
    those artifacts to form the provided classpath. They are used by
    e.g. R8 and Proguard when adding "-libraryjar" keep rules.
    
    Previously, provided classpath was computed by subtracting
    compile classpath from the runtime classpath. Because library
    projects now publish two separate artifacts (files) for the
    compile and the runtime classpath, provided classpath contained
    the compile library artifact. This CL fixes that.
    
    BUG: 119758914
    Test: MinifyLibAndAppKeepRules
    Change-Id: Ia567f0f877454be198259f33cd8e867795276c9a

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/scope/VariantScopeImpl.java
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/scope/VariantScopeImpl.java
@@ -60,6 +60,7 @@ import com.android.build.gradle.internal.dependency.AndroidTestResourceArtifactC
 import com.android.build.gradle.internal.dependency.ArtifactCollectionWithExtraArtifact;
 import com.android.build.gradle.internal.dependency.FilteredArtifactCollection;
 import com.android.build.gradle.internal.dependency.FilteringSpec;
+import com.android.build.gradle.internal.dependency.ProvidedClasspath;
 import com.android.build.gradle.internal.dependency.SubtractingArtifactCollection;
 import com.android.build.gradle.internal.dependency.VariantDependencies;
 import com.android.build.gradle.internal.dsl.BuildType;
@@ -997,9 +998,10 @@ public class VariantScopeImpl extends GenericVariantScopeImpl implements Variant
     @NonNull
     @Override
     public FileCollection getProvidedOnlyClasspath() {
-        FileCollection compile = getArtifactFileCollection(COMPILE_CLASSPATH, ALL, CLASSES);
-        FileCollection pkg = getArtifactFileCollection(RUNTIME_CLASSPATH, ALL, CLASSES);
-        return compile.minus(pkg);
+        ArtifactCollection compile = getArtifactCollection(COMPILE_CLASSPATH, ALL, CLASSES);
+        ArtifactCollection runtime = getArtifactCollection(RUNTIME_CLASSPATH, ALL, CLASSES);
+
+        return ProvidedClasspath.getProvidedClasspath(compile, runtime);
     }
 
     /**
