5dba931cda3a5218a814ac75fc38384d9e3850db
Michal Bendowski <bendowski@google.com>
2018-11-26 17:26:44 +0000

    Migrate separateTestModuleWithMinifiedApp to ATSL
    Test: existing
    Bug: 112467616
    Change-Id: I22c9078a39f06adf6dfeb3a514b06356d74de862

--- build-system/integration-test/test-projects/separateTestModuleWithMinifiedApp/test/src/main/java/com/android/tests/basic/MainTest.java
+++ build-system/integration-test/test-projects/separateTestModuleWithMinifiedApp/test/src/main/java/com/android/tests/basic/MainTest.java
@@ -16,28 +16,29 @@
 
 package com.android.tests.basic;
 
-import com.android.tests.utils.Utility;
-import android.test.ActivityInstrumentationTestCase2;
-import android.test.suitebuilder.annotation.MediumTest;
-import android.widget.TextView;
+import static org.junit.Assert.*;
 
-public class MainTest extends ActivityInstrumentationTestCase2<Main> {
+import android.support.test.filters.MediumTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.widget.TextView;
+import com.android.tests.utils.Utility;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class MainTest {
+    @Rule public ActivityTestRule<Main> rule = new ActivityTestRule<>(Main.class);
 
     private TextView mTextView;
     private Main mMain;
     private Utility mUtility;
 
-    /**
-     * Creates an {@link ActivityInstrumentationTestCase2} that tests the {@link Main} activity.
-     */
-    public MainTest() {
-        super(Main.class);
-    }
-
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-        mMain = getActivity();
+    @Before
+    public void setUp() {
+        mMain = rule.getActivity();
         // ensure a valid handle to the activity has been returned
         assertNotNull(mMain);
         mTextView = (TextView) mMain.findViewById(R.id.text);
@@ -45,12 +46,12 @@ public class MainTest extends ActivityInstrumentationTestCase2<Main> {
     }
 
     /**
-     * The name 'test preconditions' is a convention to signal that if this
-     * test doesn't pass, the test case was not set up properly and it might
-     * explain any and all failures in other tests.  This is not guaranteed
-     * to run before other tests, as junit uses reflection to find the tests.
+     * The name 'test preconditions' is a convention to signal that if this test doesn't pass, the
+     * test case was not set up properly and it might explain any and all failures in other tests.
+     * This is not guaranteed to run before other tests, as junit uses reflection to find the tests.
      */
     @MediumTest
+    @Test
     public void testPreconditions() {
         assertNotNull(mTextView);
         assertEquals(mMain.getUtility().getValue(), mTextView.getText());
