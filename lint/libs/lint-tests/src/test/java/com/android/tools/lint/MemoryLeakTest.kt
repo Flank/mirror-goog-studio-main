98dad7868dffff5c72f4cc12503d50bd11b44929
Matthew Gharrity <gharrma@google.com>
2018-11-28 12:25:43 -0800

    Add Lint memory leak test
    This test attempts to catch memory Leaks by running Lint on a few files
    and asserting that there are no strongly reachable instances of
    LintCoreProjectEnvironment, PsiWhiteSpaceImpl, etc.
    
    Ideally we could test for memory leaks after running Lint on a very
    large and complex project using a Gradle daemon, but that will be
    trickier to do. Still, this test is able to successfully detect
    the bug b/119833503 fixed by change Ia17206f5. If you revert that
    change and run this test, it fails with:
    "Detected Lint memory leak; LintCoreProjectEnvironment is reachable"
    
    Test: self
    Bug: 119833503
    Change-Id: Id4cd06bea4e6ee46c9d945f5347e969f9bf5ed78

---
+++ lint/libs/lint-tests/src/test/java/com/android/tools/lint/MemoryLeakTest.kt
@@ -0,0 +1,191 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tools.lint
+
+import com.android.SdkConstants.FN_RESOURCE_TEXT
+import com.android.testutils.TestUtils
+import com.android.tools.lint.checks.BuiltinIssueRegistry
+import com.android.tools.lint.checks.PrivateResourceDetectorTest
+import com.android.tools.lint.checks.infrastructure.TestFiles.gradle
+import com.android.tools.lint.checks.infrastructure.TestFiles.java
+import com.android.tools.lint.checks.infrastructure.TestFiles.kotlin
+import com.android.tools.lint.checks.infrastructure.TestFiles.xml
+import com.android.tools.lint.checks.infrastructure.TestLintTask
+import com.google.common.base.Charsets
+import com.google.common.io.Files
+import com.intellij.psi.impl.source.tree.PsiWhiteSpaceImpl
+import com.sun.tools.attach.VirtualMachine
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import sun.tools.attach.HotSpotVirtualMachine
+import java.io.File
+import java.io.IOException
+import java.lang.management.ManagementFactory
+import java.util.Scanner
+
+class MemoryLeakTest {
+
+    private fun countLiveInstancesOf(clz: String): Int {
+        val pid = ManagementFactory.getRuntimeMXBean().name.substringBefore('@')
+        val vm = VirtualMachine.attach(pid) as HotSpotVirtualMachine
+        val heap = vm.heapHisto("-live")
+
+        var res = 0
+        heap.bufferedReader().forEachLine { line ->
+            Scanner(line).use { s ->
+                try {
+                    // Format: idx #instances #bytes className
+                    s.next()
+                    val count = s.nextInt()
+                    s.next()
+                    val currClass = s.next()
+
+                    if (currClass == clz) {
+                        res = count
+                        return@forEachLine
+                    }
+                } catch (e: NoSuchElementException) {
+                    // Skip this line.
+                }
+            }
+        }
+
+        return res
+    }
+
+    private fun lint(): TestLintTask {
+        return TestLintTask().sdkHome(TestUtils.getSdk())
+    }
+
+    @Test
+    fun testForMemoryLeak() {
+        // Regression test for
+        // 119833503: Memory leak due to Lint's custom class loader when running with Gradle
+        //
+        // This test cannot guarantee that there are no memory leaks, but it gives us a
+        // chance at catching some automatically by running Lint and searching
+        // for surviving instances of LintCoreProjectEnvironment, PsiWhiteSpaceImpl, etc.
+        //
+        // A more thorough test would run Lint multiple times on a large and
+        // complex project using Gradle (with a daemon) in order to get better
+        // code coverage (and thus a better chance at catching a leak).
+
+        lint().files(
+
+            gradle("""
+                apply plugin: 'com.android.application'
+
+                android {
+                    compileSdkVersion 28
+                    defaultConfig {
+                        applicationId "com.gharrma.sampleapp"
+                        minSdkVersion 15
+                        targetSdkVersion 28
+                        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
+                    }
+                }
+
+                dependencies {
+                    implementation 'androidx.appcompat:appcompat:1.0.2'
+                }
+            """.trimIndent()),
+
+            kotlin("""
+                package com.gharrma.sampleapp
+
+                import androidx.appcompat.app.AppCompatActivity
+                import android.os.Bundle
+
+                class MainActivity : AppCompatActivity() {
+
+                    override fun onCreate(savedInstanceState: Bundle?) {
+                        super.onCreate(savedInstanceState)
+                        setContentView(R.layout.activity_main)
+                        Utils.foo()
+                    }
+
+                    companion object {
+                        fun bar() {
+                            println("bar")
+                        }
+                    }
+                }
+            """.trimIndent()),
+
+            java("""
+                package com.gharrma.sampleapp;
+
+                public class Utils {
+
+                    public static void foo() {
+                        System.out.println("foo");
+                        MainActivity.Companion.bar();
+                    }
+                }
+            """.trimIndent()),
+
+            xml(
+                "res/layout/activity_main.xml",
+                """
+                <?xml version="1.0" encoding="utf-8"?>
+                <androidx.constraintlayout.widget.ConstraintLayout
+                        xmlns:android="http://schemas.android.com/apk/res/android"
+                        xmlns:tools="http://schemas.android.com/tools"
+                        xmlns:app="http://schemas.android.com/apk/res-auto"
+                        android:layout_width="match_parent"
+                        android:layout_height="match_parent"
+                        tools:context=".MainActivity">
+
+                    <TextView
+                            android:layout_width="wrap_content"
+                            android:layout_height="wrap_content"
+                            android:text="Hello World!"
+                            app:layout_constraintBottom_toBottomOf="parent"
+                            app:layout_constraintLeft_toLeftOf="parent"
+                            app:layout_constraintRight_toRightOf="parent"
+                            app:layout_constraintTop_toTopOf="parent"/>
+
+                </androidx.constraintlayout.widget.ConstraintLayout>
+            """.trimIndent())
+        )
+            // Needed to allow PrivateResourceDetector to run.
+            .modifyGradleMocks(PrivateResourceDetectorTest.mockModifier)
+
+            // Needed to allow GradleDetector to run.
+            .networkData(
+                "http://search.maven.org/solrsearch/select" +
+                        "?q=g:%22androidx.appcompat%22+AND+a:%22appcompat%22&core=gav&wt=json",
+                "" // Response doesn't matter for this test.
+            )
+
+            .issues(*BuiltinIssueRegistry().issues.toTypedArray())
+            .run()
+            .expectClean()
+
+        assertTrue(
+            "Utility function `countLiveInstancesOf()` appears to be broken.",
+            countLiveInstancesOf(Object::class.java.name) > 0)
+
+        assertTrue(
+            "Detected Lint memory leak; LintCoreProjectEnvironment is reachable",
+            countLiveInstancesOf(LintCoreProjectEnvironment::class.java.name) == 0)
+
+        assertTrue(
+            "Detected Lint memory leak; PsiWhiteSpaceImpl is reachable",
+            countLiveInstancesOf(PsiWhiteSpaceImpl::class.java.name) == 0)
+    }
+}
