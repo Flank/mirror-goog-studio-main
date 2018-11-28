c509fb73126ee8cfc411648709ad46e2b05bd388
Dana Dahlstrom <dahlstrom@google.com>
2018-11-28 11:45:00 -0800

    Add //tools/base/bazel/foundry:test back
    It was removed by Change I7f52d96c.
    
    Change-Id: Ie5a3de9af25e343488dc885c3578a1d50506d864
    Issue: 118391265

--- bazel/test_studio.sh
+++ bazel/test_studio.sh
@@ -20,7 +20,7 @@ readonly script_dir="$(dirname "$0")"
 readonly command_log="$("${script_dir}"/bazel --bazelrc="${script_dir}"/toplevel.bazel.rc info --config=remote command_log)"
 
 # Run Bazel
-"${script_dir}/bazel" --max_idle_secs=60 --bazelrc=${script_dir}/toplevel.bazel.rc test --keep_going --nobuild_runfile_links --bes_backend=buildeventservice.googleapis.com --auth_credentials="$HOME"/.android-studio-alphasource.json --auth_scope=https://www.googleapis.com/auth/cloud-source-tools --project_id=908081808034 --config=remote --cache_test_results=no --build_tag_filters=-no_linux --test_tag_filters=-no_linux,-no_test_linux,-qa_sanity,-qa_fast,-qa_unreliable,-slow -- $(< "${script_dir}/targets")
+"${script_dir}/bazel" --max_idle_secs=60 --bazelrc=${script_dir}/toplevel.bazel.rc test --keep_going --nobuild_runfile_links --bes_backend=buildeventservice.googleapis.com --auth_credentials="$HOME"/.android-studio-alphasource.json --auth_scope=https://www.googleapis.com/auth/cloud-source-tools --project_id=908081808034 --config=remote --cache_test_results=no --build_tag_filters=-no_linux --test_tag_filters=-no_linux,-no_test_linux,-qa_sanity,-qa_fast,-qa_unreliable,-slow -- $(< "${script_dir}/targets") //tools/base/bazel/foundry:test
 
 readonly bazel_status=$?
 
