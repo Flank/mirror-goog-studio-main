0f7bad0eb5a6c5c876a93c9419b1fa76a7f2a5e6
Eric Reed <ericreed@google.com>
2018-12-03 14:13:48 -0800

    Remove obsolete --bazelrc usage in coverage script
    Test: N/A
    Bug: N/A
    Change-Id: I1434cc142b72eeb7ee2fc0ba335452a1229ddf0c

--- bazel/studio_coverage.sh
+++ bazel/studio_coverage.sh
@@ -11,12 +11,11 @@ readonly dist_dir="$1"
 readonly script_dir="$(dirname "$0")"
 
 # Grab the location of the command_log file for bazel daemon so we can search it later.
-readonly command_log="$("${script_dir}"/bazel --bazelrc="${script_dir}"/toplevel.bazel.rc info --config=remote command_log)"
+readonly command_log="$("${script_dir}"/bazel info --config=remote command_log)"
 
 # Run Bazel with coverage instrumentation
 "${script_dir}/bazel" \
   --max_idle_secs=60 \
-  --bazelrc=${script_dir}/toplevel.bazel.rc \
   test \
   --config=remote \
   --auth_credentials="$HOME"/.android-studio-alphasource.json \
@@ -43,7 +42,7 @@ readonly production_targets_file=$(mktemp)
   | tee $production_targets_file
 
 
-readonly testlogs_dir="$(${script_dir}/bazel --bazelrc=${script_dir}/toplevel.bazel.rc info bazel-testlogs --config=remote)"
+readonly testlogs_dir="$(${script_dir}/bazel info bazel-testlogs --config=remote)"
 
 # Generate the report
 "${script_dir}/bazel" \
