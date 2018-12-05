e7ae5126a03a51d4e31307952ca4574a88632d4a
Eric Reed <ericreed@google.com>
2018-12-04 17:59:42 -0800

    Identify fake classes and uninstrumented files
    - Filters test, generated, and external files from consideration
    - Outputs unmatched classes from Jacoco as fake (no matching source)
    - Outputs unmatched filetree files as missing (uninstrumented source)
    
    Test: N/A
    Bug: 117566157
    Change-Id: I4f68d052c2690ecf3ec6122894a20dc9cd65a5a4

--- bazel/jacoco_to_lcov.py
+++ bazel/jacoco_to_lcov.py
@@ -5,12 +5,148 @@ import itertools as IT
 def main():
   # directory subtrees omitted from consideration
   skip = [
-      './tools/base/sdk-common/src/test', # actually a test directory that wasn't filtered
+      # tests
+      './tools/adt/idea/adt-ui-model/testSrc',
+      './tools/adt/idea/adt-ui/src/test',
+      './tools/adt/idea/android-adb/testSrc',
+      './tools/adt/idea/android-common/test',
+      './tools/adt/idea/android-debuggers/testSrc',
+      './tools/adt/idea/android-kotlin/android-extensions-idea/testData',
+      './tools/adt/idea/android-kotlin/android-extensions-idea/tests',
+      './tools/adt/idea/android-kotlin/android-extensions-jps/testData',
+      './tools/adt/idea/android-kotlin/idea-android/testData',
+      './tools/adt/idea/android-kotlin/idea-android/tests',
+      './tools/adt/idea/android-lang-databinding/testSrc',
+      './tools/adt/idea/android-lang/testSrc',
+      './tools/adt/idea/android-layout-inspector/testSrc',
+      './tools/adt/idea/android-test-framework/testSrc',
+      './tools/adt/idea/android-uitests/testData',
+      './tools/adt/idea/android-uitests/testSrc',
+      './tools/adt/idea/android/testData',
+      './tools/adt/idea/android/testSrc',
+      './tools/adt/idea/apkanalyzer/testSrc',
+      './tools/adt/idea/assistant/testSrc',
+      './tools/adt/idea/build-common/testSrc',
+      './tools/adt/idea/connection-assistant/testSrc',
+      './tools/adt/idea/databinding/testData',
+      './tools/adt/idea/databinding/testSrc',
+      './tools/adt/idea/deploy/testSrc',
+      './tools/adt/idea/designer/testData',
+      './tools/adt/idea/designer/testSrc',
+      './tools/adt/idea/jps-plugin/testData',
+      './tools/adt/idea/jps-plugin/testSrc',
+      './tools/adt/idea/kotlin-integration/testData',
+      './tools/adt/idea/kotlin-integration/testSrc',
+      './tools/adt/idea/native-symbolizer/testSrc',
+      './tools/adt/idea/observable-ui/testSrc',
+      './tools/adt/idea/observable/testSrc',
+      './tools/adt/idea/perfd-host/testSrc',
+      './tools/adt/idea/profilers-android/testSrc',
+      './tools/adt/idea/profilers-ui/testSrc',
+      './tools/adt/idea/profilers/testSrc',
+      './tools/adt/idea/project-system-gradle/testSrc',
+      './tools/adt/idea/project-system/testSrc',
+      './tools/adt/idea/sdk-updates/testSrc',
+      './tools/adt/idea/smali/testSrc',
+      './tools/adt/idea/swingp/testSrc',
+      './tools/adt/idea/uitest-framework-bazel/testSrc',
+      './tools/adt/idea/uitest-framework-gradle/testSrc',
+      './tools/adt/idea/uitest-framework/testSrc',
+      './tools/adt/idea/whats-new-assistant/testSrc',
+      './tools/adt/idea/wizard-model/testSrc',
+      './tools/analytics-library/crash/src/test',
+      './tools/analytics-library/publisher/src/test',
+      './tools/analytics-library/shared/src/test',
+      './tools/analytics-library/testing/src/test',
+      './tools/analytics-library/tracker/src/test',
+      './tools/apksig/src/test',
+      './tools/apkzlib/src/test',
+      './tools/base/apkparser/analyzer/src/test',
+      './tools/base/apkparser/binary-resources/src/test',
+      './tools/base/apkparser/cli/src/test',
+      './tools/base/bazel/test',
+      './tools/base/bazel/testSrc',
+      './tools/base/build-system/builder-model/src/test',
+      './tools/base/build-system/builder-test-api/src/test',
+      './tools/base/build-system/builder/src/test',
+      './tools/base/build-system/gradle-api/src/test',
+      './tools/base/build-system/gradle-core/src/test',
+      './tools/base/build-system/gradle-experimental/src/test',
+      './tools/base/build-system/instant-run-instrumentation/src/test',
+      './tools/base/build-system/integration-test/application/src/test',
+      './tools/base/build-system/integration-test/databinding/src/test',
+      './tools/base/build-system/integration-test/framework/src/test',
+      './tools/base/build-system/integration-test/java-library-model-builder/src/test',
+      './tools/base/build-system/integration-test/test-projects',
+      './tools/base/build-system/java-lib-plugin/java-lib-model-builder/src/test',
+      './tools/base/build-system/manifest-merger/src/test',
+      './tools/base/build-system/profile/src/test',
+      './tools/base/common/src/test',
+      './tools/base/ddmlib/src/test',
+      './tools/base/deploy/deployer/src/test',
+      './tools/base/deploy/test',
+      './tools/base/devicelib/src/test',
+      './tools/base/draw9patch/src/test',
+      './tools/base/fakeadbserver/src/test',
+      './tools/base/flags/src/test',
+      './tools/base/instant-run/instant-run-client/src/test',
+      './tools/base/instant-run/instant-run-runtime/src/test',
+      './tools/base/layoutinspector/testSrc',
+      './tools/base/layoutlib-api/sample/testproject',
+      './tools/base/layoutlib-api/src/test',
+      './tools/base/lint/libs/lint-gradle/src/test',
+      './tools/base/lint/libs/lint-tests/src/test',
+      './tools/base/ninepatch/src/test',
+      './tools/base/perf-logger/src/test',
+      './tools/base/perflib/src/test',
+      './tools/base/pixelprobe/src/test',
+      './tools/base/profiler/app/supportlib/src/test',
+      './tools/base/profiler/integration-tests',
+      './tools/base/profiler/tests',
+      './tools/base/repository/src/test',
+      './tools/base/sdk-common/src/test',
+      './tools/base/sdklib/src/test',
+      './tools/base/testutils/src/test',
+      './tools/base/tracer/agent/testSrc',
+      './tools/base/usb-devices/testSrc',
+      './tools/base/vector-drawable-tool/src/test',
+      './tools/buildSrc/src/test',
+      './tools/data-binding/compilationTests/src/test',
+      './tools/data-binding/compiler/src/test',
+      './tools/data-binding/compilerCommon/src/test',
+      './tools/data-binding/exec/src/test',
+      './tools/data-binding/extensions-support/library/src/androidTest',
+      './tools/data-binding/extensions/library/src/androidTest',
+      './tools/data-binding/integration-tests-support',
+      './tools/data-binding/integration-tests',
+      './tools/data-binding/samples',
+      './tools/dx/dalvik/dx/junit-tests',
+      './tools/dx/dalvik/dx/tests',
+      # generated
+      './tools/adt/idea/android-lang-databinding/gen',
+      './tools/adt/idea/android-lang/gen',
+      './tools/adt/idea/artwork/gen',
+      './tools/adt/idea/smali/gen',
+      # external
+      './tools/base/jobb',
+      './tools/external',
+      './tools/idea',
+      './tools/sherpa',
+      './tools/studio',
+      './tools/swing-testing',
+      './tools/swt',
+      './tools/vendor',
+  ]
+  # file extensions we care about
+  ends = [
+      '.java',
+      '.kt',
   ]
   print 'create a filename -> dirpath map of ./tools'
   # we're using a filename -> directory path because it makes resolving to
   # sourcefiles very fast
   filetree = {}
+  pathset = set()
   # walk down the source file tree
   for (dirpath, _, filenames) in os.walk('./tools'):
     # ignore skipped directories
@@ -18,10 +154,13 @@ def main():
       continue
     # otherwise add files to the map
     for name in filenames:
+      if not any(IT.imap(lambda ext: name.endswith(ext), ends)):
+        continue
       if name in filetree:
         filetree[name].add(dirpath)
       else:
         filetree[name] = set([dirpath])
+      pathset.add(os.path.join(dirpath, name))
 
   print 'parse jacoco xml report'
   root = ET.parse('./out/agent-coverage/tools/base/coverage_report/report.xml').getroot()
@@ -30,6 +169,8 @@ def main():
   # we're going to build a nest map like package -> (file -> path/coverage info)
   # this makes it easy to do package or file level sorting for output
   data = {}
+  matchedset = set()
+  fakeset = set()
   for pkg in root.iter('package'):
     pkg_name = pkg.get('name')
     for sfile in pkg.iter('sourcefile'):
@@ -52,6 +193,20 @@ def main():
               'instrumented' : instrumented,
               'covered' : covered,
           }
+          matchedset.add(os.path.join(path, pkg_name, sfile_name))
+        else:
+          fakeset.add(os.path.join(pkg_name, sfile_name))
+
+  print 'compute unmatched real paths'
+  unmatched = pathset - matchedset
+  missing = open('./out/missing', 'w')
+  for path in sorted(unmatched):
+    missing.write('{}\n'.format(path))
+
+  print 'write fake package/class list'
+  fake = open('./out/fake', 'w')
+  for path in sorted(fakeset):
+    fake.write('{}\n'.format(path))
 
   print 'write to lcov file'
   lcov = open('./out/lcov', 'w')
