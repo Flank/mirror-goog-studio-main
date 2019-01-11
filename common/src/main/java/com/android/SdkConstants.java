3b2820acf02837cf1dace870af7944cf9ef46634
Chris Warrington <cmw@google.com>
2019-01-11 14:22:52 +0000

    Gradle 5.1.1
    Bug: 121193171
    Test: Existing Android Gradle Plugin integration tests
    Change-Id: I9b9092d32b50b81d60eb303b913623811ff95b29

--- common/src/main/java/com/android/SdkConstants.java
+++ common/src/main/java/com/android/SdkConstants.java
@@ -2599,7 +2599,7 @@ public final class SdkConstants {
     public static final String GRADLE_PLUGIN_NAME = "com.android.tools.build:gradle:";
     public static final String GRADLE_EXPERIMENTAL_PLUGIN_NAME =
             "com.android.tools.build:gradle-experimental:";
-    public static final String GRADLE_MINIMUM_VERSION = "5.1-milestone-1";
+    public static final String GRADLE_MINIMUM_VERSION = "5.1.1";
     public static final String GRADLE_LATEST_VERSION = GRADLE_MINIMUM_VERSION;
     public static final String GRADLE_PLUGIN_MINIMUM_VERSION = "1.0.0";
     public static final String GRADLE_PLUGIN_RECOMMENDED_VERSION = "3.1.0";
