d9365b769975b8e199ea8d3e2039a0f219e942df
Bruno Buss <buss@google.com>
2019-01-14 15:21:41 +0000

    Remove code-duplication on getDirName/getDirectorySegments.
    Also simplifies a bit getDirectorySegments().
    
    Bug: n/a
    Test: existing
    Change-Id: I446bc6eb6b3e057aa6c47e15c097f235a9f69c3f

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/core/VariantConfiguration.java
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/core/VariantConfiguration.java
@@ -46,6 +46,7 @@ import com.android.ide.common.resources.ResourceMerger;
 import com.android.ide.common.resources.ResourceSet;
 import com.android.sdklib.AndroidVersion;
 import com.android.utils.StringHelper;
+import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
@@ -482,27 +483,8 @@ public class VariantConfiguration<T extends BuildType, D extends ProductFlavor, 
     @NonNull
     public String getDirName() {
         if (mDirName == null) {
-            StringBuilder sb = new StringBuilder(mFlavors.size() * 20 + 100);
-            if (mType.isHybrid()) {
-                sb.append("feature/");
-            }
-
-            if (mType.isTestComponent()) {
-                sb.append(mType.getPrefix()).append("/");
-            }
-
-            if (!mFlavors.isEmpty()) {
-                StringHelper.combineAsCamelCase(sb, mFlavors, F::getName);
-                sb.append('/').append(mBuildType.getName());
-
-            } else {
-                sb.append(mBuildType.getName());
-            }
-
-            mDirName = sb.toString();
-
+            mDirName = Joiner.on('/').join(getDirectorySegments());
         }
-
         return mDirName;
     }
 
@@ -526,17 +508,10 @@ public class VariantConfiguration<T extends BuildType, D extends ProductFlavor, 
             }
 
             if (!mFlavors.isEmpty()) {
-                StringBuilder sb = new StringBuilder(mFlavors.size() * 10);
-                for (F flavor : mFlavors) {
-                    StringHelper.appendCamelCase(sb, flavor.getName());
-                }
-                builder.add(sb.toString());
-
-                builder.add(mBuildType.getName());
-
-            } else {
-                builder.add(mBuildType.getName());
+                builder.add(StringHelper.combineAsCamelCase(mFlavors, F::getName));
             }
+
+            builder.add(mBuildType.getName());
 
             mDirSegments = builder.build();
         }
