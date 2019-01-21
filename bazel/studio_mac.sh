8c9986cec0c76b55d74da5acaa64520c54252341
Jerome Gaillard <jgaillard@google.com>
2019-01-21 11:56:57 +0000

    Revert "Update studio_mac upsalite reporting"
    This reverts commit 2006e6d7d698448723da417e0d05d563b4c5b2d6.
    
    Reason for revert: This seems to make navigation editor tests time out
    
    Change-Id: I71d63e2ba33dd15daadac6897ffc7a534a4158ab

--- bazel/studio_mac.sh
+++ bazel/studio_mac.sh
@@ -9,17 +9,29 @@ readonly build_number="$3"
 readonly script_dir="$(dirname "$0")"
 
 # Grab the location of the command_log file for bazel daemon so we can search it later.
-readonly command_log="$(${script_dir}/bazel --bazelrc=${script_dir}/toplevel.bazel.rc info command_log --config=postsubmit --config=mac-experimental --auth_credentials=/buildbot/android-studio-alphasource.json)"
+readonly command_log="$(${script_dir}/bazel --bazelrc=${script_dir}/toplevel.bazel.rc info command_log --config=postsubmit --config=local --config=mac-experimental --auth_credentials=/buildbot/android-studio-alphasource.json)"
 
 # Run Bazel
-"${script_dir}/bazel" --max_idle_secs=60 --bazelrc=${script_dir}/toplevel.bazel.rc test --config=postsubmit --config=mac-experimental --build_tag_filters=-no_mac --test_tag_filters=-no_mac,-no_test_mac,-qa_sanity,-qa_fast,-qa_unreliable --auth_credentials=/buildbot/android-studio-alphasource.json --profile=${dist_dir}/prof -- $(< "${script_dir}/targets")
+"${script_dir}/bazel" --max_idle_secs=60 --bazelrc=${script_dir}/toplevel.bazel.rc test --config=postsubmit --config=local --config=mac-experimental --build_tag_filters=-no_mac --test_tag_filters=-no_mac,-no_test_mac,-qa_sanity,-qa_fast,-qa_unreliable --auth_credentials=/buildbot/android-studio-alphasource.json --profile=${dist_dir}/prof -- $(< "${script_dir}/targets")
 
 readonly bazel_status=$?
 
 if [[ -d "${dist_dir}" ]]; then
   # Grab the upsalite_id from the stdout of the bazel command.  This is captured in command.log
-  readonly upsalite_id=`sed -n -e 's/.*invocation_id: //p' ${command_log}`
-  echo "<meta http-equiv=\"refresh\" content=\"0; URL='https://source.cloud.google.com/results/invocations/${upsalite_id}'\" />" > "${dist_dir}"/upsalite_test_results.html
+  readonly upsalite_id=`sed -n -e 's/.*invocation_id: //p' $command_log`
+  echo "<meta http-equiv=\"refresh\" content=\"0; URL='https://source.cloud.google.com/results/invocations/$upsalite_id\" />" > "${dist_dir}"/upsalite_test_results.html
+
+  # follow conventions to use gtest-testlog-forwarding on ATP
+  readonly testlogs_dir="$(${script_dir}/bazel --bazelrc=${script_dir}/toplevel.bazel.rc info bazel-testlogs --config=postsubmit --config=local --config=mac-experimental --auth_credentials=/buildbot/android-studio-alphasource.json)"
+  echo "The testlogs_dir is $testlogs_dir"
+  mkdir "${dist_dir}"/gtest
+  # This does not handle spaces in file names.
+  for source_xml in $(cd "${testlogs_dir}" && find . -name '*.xml' -exec stat -f "%N" {} \; | sed "s|^\./||"); do
+    target_xml="$(echo "${source_xml}" | tr '/' '_')"
+    cp -pv "${testlogs_dir}/${source_xml}" "${dist_dir}/gtest/${target_xml}"
+    # GTestXmlResultParser requires the testsuites element to have tests and time attributes.
+    sed -i '' 's/<testsuites>/<testsuites tests="0" time="0">/' "${dist_dir}/gtest/${target_xml}"
+  done
 
   # Create profile html in ${dist_dir} so it ends up in Artifacts.
   ${script_dir}/bazel analyze-profile --html ${dist_dir}/prof
