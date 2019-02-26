51862a738c32c2ff001d6e39c0ea331a9910e140
Mads Ager <ager@google.com>
2019-02-26 11:20:55 +0100

    Make field non-constant in obfuscation test.
    Test: ./gradlew :base:build-system:integration-test:application:cleanTest :base:build-system:integration-test:application:test --tests "com.android.build.gradle.integration.library.LibMinifyTest.checkLibraryHasItsFieldsObfuscated"
    Bug:
    
    Change-Id: Ib90efea2d6d0a0fdc9d7dede9c2e75d4f1e635df

--- build-system/integration-test/test-projects/libMinify/src/main/java/com/android/tests/basic/StringProvider.java
+++ build-system/integration-test/test-projects/libMinify/src/main/java/com/android/tests/basic/StringProvider.java
@@ -1,9 +1,13 @@
 package com.android.tests.basic;
 
 public class StringProvider {
-    private static int obfuscatedInt = 5;
+    private static int obfuscatedInt = 4;
 
     public static String getString(int foo) {
+        // R8 removes the field if it is constant. Therefore,
+        // write a new value to it in order for tests that check
+        // obfuscation to pass.
+        obfuscatedInt = 5;
         return Integer.toString(foo + obfuscatedInt);
     }
 }
