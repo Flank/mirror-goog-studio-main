1f875f25b65d28b2a3a467d18c2adc507f041488
Christopher Pilch <cpilch@google.com>
2019-03-21 15:00:36 -0700

    Clean up bazel.rc files and update Linux Docker
    Clean up arguments from the common.bazel.rc and linux.bazel.rc files
    that are no longer needed, and additionally update the Docker image
    version used on Linux.
    
    Also updated the build_docker.sh script and Dockerfile with the new
    image updates.
    
    Test: PSQ
    Bug: NA
    Change-Id: I393698458d31e5c25966d357ab3d75aa4ed79021

--- bazel/foundry/build_docker.sh
+++ bazel/foundry/build_docker.sh
@@ -1,1 +1,1 @@
-gcloud builds submit --tag gcr.io/google.com/android-studio-alphasource/rbe-ubuntu16-as docker
+gcloud --project google.com:android-studio-alphasource builds submit --tag gcr.io/google.com/android-studio-alphasource/rbe-ubuntu16-as docker
