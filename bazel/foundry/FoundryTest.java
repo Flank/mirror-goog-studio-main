47363d6cb727df0dbd99e8e27bbe3ac25bbb9bce
Christopher Pilch <cpilch@google.com>
2019-03-21 13:14:03 -0700

    Add a Foundry Test to see if we are using AS Dock
    Add a test in FoundryTests that checks a environment variable to see if
    we are running a custom Android Studio Docker Image.
    
    Test: In Code / PSQ
    Bug: NA
    Change-Id: I1933c23a3ef469c412cc9b1a12d6f35afe442e18

--- bazel/foundry/FoundryTest.java
+++ bazel/foundry/FoundryTest.java
@@ -21,40 +21,56 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public final class FoundryTest {
 
+    private static final Boolean REMOTE_PROP =
+            Boolean.valueOf(System.getProperty("android.remote.run"));
+
     @Test
     public void testAapt2() throws Exception {
-        File testLocalDir = new File(".");
-        String osName = System.getProperty("os.name").toLowerCase();
+        if (REMOTE_PROP) {
+            File testLocalDir = new File(".");
+            String osName = System.getProperty("os.name").toLowerCase();
 
-        if (osName.equalsIgnoreCase("Linux")) {
-            execute("prebuilts/tools/common/aapt/" + osName + "/aapt2 -h", null, 1);
-        } else {
-            execute(
-                    "prebuilts/tools/common/aapt/"
-                            + osName
-                            + "/aapt2 -h".replace('/', File.separatorChar),
-                    null,
-                    1);
+            if (osName.equalsIgnoreCase("Linux")) {
+                execute("prebuilts/tools/common/aapt/" + osName + "/aapt2 -h", null, 1);
+            } else {
+                execute(
+                        "prebuilts/tools/common/aapt/"
+                                + osName
+                                + "/aapt2 -h".replace('/', File.separatorChar),
+                        null,
+                        1);
+            }
         }
     }
 
     @Test
     public void testCA() throws Exception {
-        TrustManagerFactory factory =
-                TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-        factory.init((KeyStore) null);
-        for (TrustManager manager : factory.getTrustManagers()) {
-            if (manager instanceof X509TrustManager) {
-                X509Certificate[] issurers = ((X509TrustManager) manager).getAcceptedIssuers();
-                for (X509Certificate c : issurers) {
-                    System.out.println(c);
-                }
-                if (issurers.length != 0) {
-                    return;
+        if (REMOTE_PROP) {
+            TrustManagerFactory factory =
+                    TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            factory.init((KeyStore) null);
+            for (TrustManager manager : factory.getTrustManagers()) {
+                if (manager instanceof X509TrustManager) {
+                    X509Certificate[] issurers = ((X509TrustManager) manager).getAcceptedIssuers();
+                    for (X509Certificate c : issurers) {
+                        System.out.println(c);
+                    }
+                    if (issurers.length != 0) {
+                        return;
+                    }
                 }
             }
+            throw new AssertionError("No issurers found");
         }
-        throw new AssertionError("No issurers found");
+    }
+
+    @Test
+    public void testCustomDockerImage() throws Exception {
+        if (REMOTE_PROP) {
+            String studioImageEnv = System.getenv("STUDIO_IMAGE");
+            String expectedStudioImageEnv = "true";
+            assertEquals(expectedStudioImageEnv, studioImageEnv);
+        }
     }
 
     @Test
@@ -64,40 +80,47 @@ public final class FoundryTest {
 
     @Test
     public void testCreateFile() {
-        File tempDir = new File("/tmp");
-        String filename = "你所有的基地都属于我们.txt";
-        System.out.println("locale info: " + Locale.getDefault());
-        System.out.println("defaultCharset: " + Charset.defaultCharset());
-        try {
-            File file = new File(tempDir, filename);
-            file.createNewFile();
-            System.out.println("File created.");
-            System.out.println("tempDir.list(): " + Arrays.toString(tempDir.list()));
-            assertTrue(Arrays.asList(tempDir.list()).contains(filename));
-        } catch (IOException e) {
-            fail("File with non-ascii name failed to be created.");
+        if (REMOTE_PROP) {
+            File tempDir = new File("/tmp");
+            String filename = "你所有的基地都属于我们.txt";
+            System.out.println("locale info: " + Locale.getDefault());
+            System.out.println("defaultCharset: " + Charset.defaultCharset());
+            try {
+                File file = new File(tempDir, filename);
+                file.createNewFile();
+                System.out.println("File created.");
+                System.out.println("tempDir.list(): " + Arrays.toString(tempDir.list()));
+                assertTrue(Arrays.asList(tempDir.list()).contains(filename));
+            } catch (IOException e) {
+                fail("File with non-ascii name failed to be created.");
+            }
         }
     }
 
     @Test
     public void testArmLinuxLinker() {
-        execute("prebuilts/studio/sdk/linux/build-tools/28.0.3/arm-linux-androideabi-ld --help");
+        if (REMOTE_PROP) {
+            execute(
+                    "prebuilts/studio/sdk/linux/build-tools/28.0.3/arm-linux-androideabi-ld --help");
+        }
     }
 
     @Test
     public void testJdkVersion() throws IOException {
-        String javaVersionInUse = System.getProperty("java.version");
-        // Get the java version from our prebuilts jdk.
-        ProcessBuilder builder =
-                new ProcessBuilder("prebuilts/studio/jdk/linux/bin/java", "-version");
-        builder.redirectErrorStream(true);
-        Process p = builder.start();
-        BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        String firstLine = r.readLine(); // e.g. openjdk version "1.8.0_152-release"
-        int lastQuote = firstLine.lastIndexOf("\"");
-        int openQuote = firstLine.substring(0, lastQuote).lastIndexOf("\"");
-        String prebuiltsJavaVersion = firstLine.substring(openQuote + 1, lastQuote);
-        assertEquals(javaVersionInUse, prebuiltsJavaVersion);
+        if (REMOTE_PROP) {
+            String javaVersionInUse = System.getProperty("java.version");
+            // Get the java version from our prebuilts jdk.
+            ProcessBuilder builder =
+                    new ProcessBuilder("prebuilts/studio/jdk/linux/bin/java", "-version");
+            builder.redirectErrorStream(true);
+            Process p = builder.start();
+            BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String firstLine = r.readLine(); // e.g. openjdk version "1.8.0_152-release"
+            int lastQuote = firstLine.lastIndexOf("\"");
+            int openQuote = firstLine.substring(0, lastQuote).lastIndexOf("\"");
+            String prebuiltsJavaVersion = firstLine.substring(openQuote + 1, lastQuote);
+            assertEquals(javaVersionInUse, prebuiltsJavaVersion);
+        }
     }
 
     private void execute(String command) {
