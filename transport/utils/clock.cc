77a2b607b3a64030565a573933e2a1f3124e540b
Shukang Zhou <shukang@google.com>
2019-04-12 14:47:46 -0700

    Simplify timestamp calculation
    I wanted to write a regression test for b/130211086,
    but it's hard without complicating production code.
    The code that caused the problem was simple, but had
    some C++ language implication that's not obvious.
    Therefore I rewrite the production code with a tested
    function and (possibly verbose) documentation.
    
    The bug was caused by commit 3c47917e46d658f5b3e2fee451c5ec63829c8fc1,
    fixed by commit 9b0c3e79ba0b30065bcc15283bb297855ae43963.
    Hopefully this CL will make it less likely to break if
    someone refactors it in the future.
    
    Bug: 130211086
    Test: N/A; new implementation uses an already-tested
    utility function, and new logic is too simple to test.
    
    Change-Id: I2892b06941af48fa8e9261edbed7559a4977e01d

--- transport/utils/clock.cc
+++ transport/utils/clock.cc
@@ -22,7 +22,31 @@ namespace profiler {
 int64_t SteadyClock::GetCurrentTime() const {
   timespec time;
   clock_gettime(CLOCK_MONOTONIC, &time);
-  return static_cast<int64_t>(1000000000) * time.tv_sec + time.tv_nsec;
+
+  // time.tv_sec is of type 'time_t'; time.tv_nsec is of type 'long'.
+  //
+  // On a 32-bit device, they are both 4 bytes long. As a result, simplily
+  // writing
+  //     1000000000 * time.tv_sec + time.tv_nsec
+  // in C++ would easily cause integer overflow because C++ would save the
+  // multiplication result into a 32-bit integer. The overflow happens roughly
+  // every 2.2 seconds.
+  // Math: (2 ^ 31 -1) / 1000000000 ~= 2.15
+  //
+  // On a 64-bit device, time_t and long are both 8 bytes long.
+  // Consider 'time.tv_sec' as in seconds held by a 64-bit integer. When the
+  // unit changes to nanoseconds, the number becomes a billion times larger, and
+  // the new value might be too large for a 64-bit integer to hold in principle.
+  // However, in practice, Android devices reset CLOCK_MONOTONIC every time when
+  // it reboots. In order to overflow, the device needs to be run 292 years.
+  // Math: (2 ^ 63 - 1) / 1000000000 / 60 / 60 / 24 / 365 ~= 292.47
+  //
+  // Therefore, it's reasonable for our purpose to use int64_t to hold the
+  // timestamp in nanosecond unit. Clock::s_to_ns() has the right input and
+  // output types, and it works on both 32-bit and 64-bit platforms. Its
+  // signature is as follows. Note constexpr implies inline.
+  //   static constexpr int64_t s_to_ns(int64_t s);
+  return Clock::s_to_ns(time.tv_sec) + time.tv_nsec;
 }
 
 }  // namespace profiler
