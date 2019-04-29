495d4dfe2440b3e9a913b935e87de51fdd553df7
Bruno Buss <buss@google.com>
2019-04-29 15:43:12 +0100

    Introduce new model to hold all sync issues.
    Bug: 130363042
    Test: IdeProjectSyncIssuesTest
    Change-Id: Id5b83f76754dbcc571d56e3a6c2dc7d4e0fa93ff

---
+++ sdk-common/src/main/java/com/android/ide/common/gradle/model/IdeProjectSyncIssues.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.ide.common.gradle.model
+
+import com.android.builder.model.ProjectSyncIssues
+import com.android.builder.model.SyncIssue
+import com.google.common.base.MoreObjects
+import java.io.Serializable
+import java.util.Objects
+
+/** Creates a deep copy of a {@link ProjectSyncIssues}. */
+data class IdeProjectSyncIssues(val syncIssues: Collection<SyncIssue>) : Serializable {
+
+    constructor(project: ProjectSyncIssues, modelCache: ModelCache) : this(copySyncIssues(project, modelCache)) {
+        modelCache.putDisallowingReplacement(project, this)
+    }
+
+    companion object {
+        private const val serialVersionUID: Long = -664815043457105643L // from serialver
+
+        fun copySyncIssues(project: ProjectSyncIssues, modelCache: ModelCache): Collection<SyncIssue> {
+            return IdeModel.copy(
+                project.syncIssues, modelCache) { issue -> IdeSyncIssue(issue, modelCache) }
+        }
+    }
+}
---
+++ sdk-common/src/test/java/com/android/ide/common/gradle/model/IdeProjectSyncIssuesTest.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.ide.common.gradle.model
+
+
+import com.android.ide.common.gradle.model.stubs.ProjectSyncIssuesStub
+import com.android.ide.common.gradle.model.stubs.SyncIssueStub
+import com.android.testutils.Serialization
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import java.io.Serializable
+
+/** Tests for {@link IdeProjectSyncIssues}. */
+class IdeProjectSyncIssuesTest {
+    private val modelCache = ModelCache()
+
+    @Test
+    fun serializable() {
+        assertThat(IdeProjectSyncIssues::class.java).isAssignableTo(Serializable::class.java)
+    }
+
+    @Test
+    fun serialization() {
+        val expectedSyncIssue = IdeSyncIssue(SyncIssueStub(), modelCache)
+
+        val projectSyncIssues = IdeProjectSyncIssues(ProjectSyncIssuesStub(), modelCache)
+        assertThat(projectSyncIssues.syncIssues).containsExactly(expectedSyncIssue)
+        val bytes = Serialization.serialize(projectSyncIssues)
+        val o = Serialization.deserialize(bytes)
+        assertThat(o).isEqualTo(projectSyncIssues)
+        assertThat((o as IdeProjectSyncIssues).syncIssues).containsExactly(expectedSyncIssue)
+    }
+
+    @Test
+    fun constructor() {
+        val original = ProjectSyncIssuesStub()
+        val copy = IdeProjectSyncIssues(ProjectSyncIssuesStub(), modelCache)
+        val expectedIssues = original.syncIssues.map { issue -> IdeSyncIssue(issue, modelCache) }
+
+        assertThat(copy.syncIssues).isEqualTo(expectedIssues)
+        IdeModelTestUtils.verifyUsageOfImmutableCollections(copy)
+    }
+}
