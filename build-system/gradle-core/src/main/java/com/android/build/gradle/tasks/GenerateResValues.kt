53fb91bba6f8800aa88f0996c55c6218337e7b27
Stephen Atwood <stephenatwood@google.com>
2019-04-30 11:54:32 -0700

    Convert GenerateResValues to Kotlin (Part 2)
    Phase 2: Convert Java code to Kotlin
    
    Bug: N/A
    Test: Existing Tests
    Change-Id: Id5606cedb221de5ee9f8717749645d058137bfd8

--- build-system/gradle-core/src/main/java/com/android/build/gradle/tasks/GenerateResValues.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/tasks/GenerateResValues.kt
@@ -13,126 +13,94 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.build.gradle.tasks;
+package com.android.build.gradle.tasks
 
-import com.android.annotations.NonNull;
-import com.android.build.gradle.internal.scope.VariantScope;
-import com.android.build.gradle.internal.tasks.NonIncrementalTask;
-import com.android.build.gradle.internal.tasks.TaskInputHelper;
-import com.android.build.gradle.internal.tasks.factory.VariantTaskCreationAction;
-import com.android.builder.compiling.ResValueGenerator;
-import com.android.builder.model.ClassField;
-import com.android.utils.FileUtils;
-import com.google.common.collect.Lists;
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-import java.util.function.Supplier;
-import javax.xml.parsers.ParserConfigurationException;
-import org.gradle.api.tasks.CacheableTask;
-import org.gradle.api.tasks.Input;
-import org.gradle.api.tasks.Internal;
-import org.gradle.api.tasks.OutputDirectory;
-import org.gradle.api.tasks.TaskProvider;
+import com.android.build.gradle.internal.scope.VariantScope
+import com.android.build.gradle.internal.tasks.NonIncrementalTask
+import com.android.build.gradle.internal.tasks.TaskInputHelper
+import com.android.build.gradle.internal.tasks.factory.VariantTaskCreationAction
+import com.android.builder.compiling.ResValueGenerator
+import com.android.builder.model.ClassField
+import com.android.utils.FileUtils
+import com.google.common.collect.Lists
+import java.io.File
+import java.util.function.Supplier
+import org.gradle.api.tasks.CacheableTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Internal
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.TaskProvider
 
 @CacheableTask
-public class GenerateResValues extends NonIncrementalTask {
+open class GenerateResValues : NonIncrementalTask() {
 
     // ----- PUBLIC TASK API -----
 
-    private File resOutputDir;
-
-    @OutputDirectory
-    public File getResOutputDir() {
-        return resOutputDir;
-    }
-
-    public void setResOutputDir(File resOutputDir) {
-        this.resOutputDir = resOutputDir;
-    }
+    @get:OutputDirectory
+    lateinit var resOutputDir: File
 
     // ----- PRIVATE TASK API -----
 
-    @Internal // handled by getItemValues()
-    public List<Object> getItems() {
-        return items.get();
-    }
+    @get:Internal //handled by getItemValues()
+    var items: List<Any>
+        get() = itemsSupplier.get()
+        set(value) {
+            itemsSupplier = Supplier { value }
+        }
 
-    public void setItems(List<Object> items) {
-        this.items = () -> items;
-    }
-
-    private Supplier<List<Object>> items;
+    private lateinit var itemsSupplier: Supplier<List<Any>>
 
     @Input
-    public List<String> getItemValues() {
-        List<Object> resolvedItems = getItems();
-        List<String> list = Lists.newArrayListWithCapacity(resolvedItems.size() * 3);
+    fun getItemValues(): List<String> {
+        val resolvedItems = items
+        val list = Lists.newArrayListWithCapacity<String>(resolvedItems.size * 3)
 
-        for (Object object : resolvedItems) {
-            if (object instanceof String) {
-                list.add((String) object);
-            } else if (object instanceof ClassField) {
-                ClassField field = (ClassField) object;
-                list.add(field.getType());
-                list.add(field.getName());
-                list.add(field.getValue());
+        for (item in resolvedItems) {
+            if (item is String) {
+                list.add(item)
+            } else if (item is ClassField) {
+                list.add(item.type)
+                list.add(item.name)
+                list.add(item.value)
             }
         }
 
-        return list;
+        return list
     }
 
-    @Override
-    protected void doTaskAction() throws IOException, ParserConfigurationException {
-        File folder = getResOutputDir();
-        List<Object> resolvedItems = getItems();
+    override fun doTaskAction() {
+        val folder = resOutputDir
+        val resolvedItems = items
 
         if (resolvedItems.isEmpty()) {
-            FileUtils.cleanOutputDir(folder);
+            FileUtils.cleanOutputDir(folder)
         } else {
-            ResValueGenerator generator = new ResValueGenerator(folder);
-            generator.addItems(getItems());
+            val generator = ResValueGenerator(folder)
+            generator.addItems(resolvedItems)
 
-            generator.generate();
+            generator.generate()
         }
     }
 
+    class CreationAction(scope: VariantScope) :
+        VariantTaskCreationAction<GenerateResValues>(scope) {
 
-    public static class CreationAction extends VariantTaskCreationAction<GenerateResValues> {
+        override val name = scope.getTaskName("generate", "ResValues")
+        override val type = GenerateResValues::class.java
 
-        public CreationAction(@NonNull VariantScope scope) {
-            super(scope);
+        override fun handleProvider(
+            taskProvider: TaskProvider<out GenerateResValues>
+        ) {
+            super.handleProvider(taskProvider)
+            variantScope.taskContainer.generateResValuesTask = taskProvider
         }
 
-        @NonNull
-        @Override
-        public String getName() {
-            return getVariantScope().getTaskName("generate", "ResValues");
-        }
+        override fun configure(task: GenerateResValues) {
+            super.configure(task)
 
-        @NonNull
-        @Override
-        public Class<GenerateResValues> getType() {
-            return GenerateResValues.class;
-        }
+            task.itemsSupplier = TaskInputHelper.memoize { variantScope.variantConfiguration.resValues }
 
-        @Override
-        public void handleProvider(
-                @NonNull TaskProvider<? extends GenerateResValues> taskProvider) {
-            super.handleProvider(taskProvider);
-            getVariantScope().getTaskContainer().setGenerateResValuesTask(taskProvider);
-        }
-
-        @Override
-        public void configure(@NonNull GenerateResValues task) {
-            super.configure(task);
-
-            VariantScope scope = getVariantScope();
-
-            task.items = TaskInputHelper.memoize(scope.getVariantConfiguration()::getResValues);
-
-            task.setResOutputDir(scope.getGeneratedResOutputDir());
+            task.resOutputDir = variantScope.generatedResOutputDir
         }
     }
 }
