4a9aedaa2705a9ed8fcf63bd54aff4cef8de4ecb
Eric Reed <ericreed@google.com>
2019-06-17 18:45:07 -0700

    Support source filtering in coverage reports
    Extend coverage_report to allow source path prefix includes and excludes
    to enable custom filtering. This enables precise intratarget reports.
    
    Note that these filters are in addition to rather than instead of the
    master filter. If something is dropped by the master filter, then a
    custom include here will not recover it.
    
    Test: N/A
    Bug: N/A
    Change-Id: I1d4d52ce09dbad40415cf43559eee3f94cf74132

--- bazel/coverage/coverage.bzl
+++ bazel/coverage/coverage.bzl
@@ -1,4 +1,23 @@
-def coverage_report(name, tests):
+# Define a coverage report
+#
+# Arguments:
+#   name : prefix of generated targets
+#   tests : list of test targets run for the report
+#   srcpath_include : (optional) list of path prefixes used to restrict data from matching sourcefiles
+#   srcpath_exclude : (optional) list of path prefixes used to exclude data from matching sourcefiles
+#
+# Generated Targets:
+#   name.suite : test_suite of tests
+#   name.lcov.unfiltered : merger of LCOV tracefiles from individual tests
+#   name.lcov : filtered version of merged LCOV tracefile
+#
+# Sourcepath Filtering:
+#   - A path matching any prefix in srcpath_include is included
+#   - A path matching any prefix in srcpath_exclude is excluded
+#   - A path that is both included and excluded is treated as excluded
+#   - If srcpath_included is omitted or empty then all paths are treated as included
+#   - The filtered tracefile contains all paths that are included and not excluded
+def coverage_report(name, tests, srcpath_include = [], srcpath_exclude = []):
     native.test_suite(
         name = name + ".suite",
         tests = ["@{}".format(t) for t in tests],
@@ -6,9 +25,19 @@ def coverage_report(name, tests):
 
     lcovs = ["@results//:{}.LCOVTracefile".format(t[2:].replace(":", "/")) for t in tests]
     native.genrule(
-        name = "{}.lcov".format(name),
+        name = "{}.lcov.unfiltered".format(name),
         srcs = lcovs,
-        outs = ["{}/lcov".format(name)],
+        outs = ["{}/lcov.unfiltered".format(name)],
         tools = [":merge_lcov"],
         cmd = "python $(location :merge_lcov) $(SRCS) >$@",
+    )
+
+    spi = " ".join(srcpath_include)
+    spe = " ".join(["-" + x for x in srcpath_exclude])
+    native.genrule(
+        name = "{}.lcov".format(name),
+        srcs = ["{}.lcov.unfiltered".format(name)],
+        outs = ["{}/lcov".format(name)],
+        tools = [":filter_lcov"],
+        cmd = "python $(location :filter_lcov) <$< >$@ {} {}".format(spi, spe),
     )
---
+++ bazel/coverage/filter_lcov.py
@@ -0,0 +1,66 @@
+"""Filters LCOV tracefiles
+
+- Arguments are filepath prefixes to keep (or drop if prefixed with '-')
+    - A path is kept if it matches at least one include and no excludes
+    - If no includes are specified then everything not excluded is included
+- Input tracefile comes from stdin
+- Output tracefiles goes to stdout
+"""
+import sys
+
+def read_lcov():
+    file_line_cov = {} # map[file][line] = covered
+    current_sf = None
+    for line in sys.stdin:
+        line = line.strip()
+        if line[:3] == "SF:":
+            current_sf = line[3:]
+            file_line_cov[current_sf] = {}
+        elif line[:3] == "DA:":
+            [num, hit] = line[3:].split(",")
+            file_line_cov[current_sf][int(num)] = hit != "0" # convert to bool
+        else:
+            pass
+
+    return file_line_cov
+
+def is_excluded(path, excludes):
+    # if no excludes are specified then this is skipped and nothing is excluded
+    for e in excludes:
+        if path.startswith(e):
+            # matched an excluded prefix so finished with excludes
+            return True
+    return False
+
+def is_included(path, includes):
+    for i in includes:
+        if path.startswith(i):
+            # matched an included prefix so finished with includes
+            return True
+    return False
+
+def write_lcov(filtered_cov):
+    for filepath in sorted(filtered_cov):
+        sys.stdout.write('SF:{}\n'.format(filepath))
+        for line in sorted(filtered_cov[filepath]):
+            sys.stdout.write('DA:{},{}\n'.format(line, int(filtered_cov[filepath][line])))
+        sys.stdout.write('end_of_record\n')
+
+
+def main():
+    prefixes = sys.argv[1:]
+    includes = ["./" + x for x in prefixes if not x.startswith("-")]
+    excludes = ["./" + x[1:] for x in prefixes if x.startswith("-")]
+
+    file_line_cov = read_lcov()
+
+    after_excludes = {f: file_line_cov[f] for f in file_line_cov if not is_excluded(f, excludes)}
+
+    filtered = after_excludes # by default include everything
+    if len(includes) > 0: # but if there are explicit includes then only include those
+        filtered = {f: after_excludes[f] for f in after_excludes if is_included(f, includes)}
+
+    write_lcov(filtered)
+
+if __name__ == '__main__':
+    main()
