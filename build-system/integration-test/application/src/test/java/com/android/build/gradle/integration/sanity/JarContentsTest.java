6541adcccab7bc39f9133bed9ee138f29697de21
Ying Chen <chying@google.com>
2019-06-17 17:25:51 -0700

    Rename and move Java Library Plugin
    Java library plugin will soon be deprecated due to merging
    of sync infrastructures, but some model builers such as
    GradlePluginModel are still useful, also new model builers
    for sources and javadoc will be added.
    
    So these modules are renamed to android.gradle-tooling.api/impl
    to reflect that it's a general purpose tooling extension used
    by studio.
    Also move the modules to tools/adt/idea since they will always
    be bundled with Studio.
    
    Bug: 129008025
    Test: No code change, existing tests.
    Change-Id: I3374820facf83e6393288e262ae926e2ae6d29b6

--- build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/sanity/JarContentsTest.java
+++ build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/sanity/JarContentsTest.java
@@ -54,7 +54,6 @@ public class JarContentsTest {
     private static final String EXTERNAL_DEPS = "/com/android/tools/external/";
 
     private static final String GMAVEN_ZIP = "tools/base/gmaven.zip";
-    private static final String JAVALIBMODELBUILDER_ZIP = "tools/base/java-lib-model-builder.zip";
 
     private static final SetMultimap<String, String> EXPECTED;
 
@@ -641,10 +640,6 @@ public class JarContentsTest {
                 "versions-offline/com/android/databinding/group-index.xml",
                 "versions-offline/com/android/installreferrer/",
                 "versions-offline/com/android/installreferrer/group-index.xml",
-                "versions-offline/com/android/java/",
-                "versions-offline/com/android/java/tools/",
-                "versions-offline/com/android/java/tools/build/",
-                "versions-offline/com/android/java/tools/build/group-index.xml",
                 "versions-offline/com/android/support/",
                 "versions-offline/com/android/support/constraint/",
                 "versions-offline/com/android/support/constraint/group-index.xml",
@@ -1142,29 +1137,6 @@ public class JarContentsTest {
                 "META-INF/MANIFEST.MF",
                 "NOTICE");
         expected.putAll(
-                "com/android/java/tools/build/java-lib-model",
-                "com/",
-                "com/android/",
-                "com/android/java/",
-                "com/android/java/model/",
-                "META-INF/",
-                "META-INF/MANIFEST.MF",
-                "NOTICE");
-        expected.putAll(
-                "com/android/java/tools/build/java-lib-model-builder",
-                "com/",
-                "com/android/",
-                "com/android/java/",
-                "com/android/java/model/",
-                "com/android/java/model/builder/",
-                "com/android/java/model/impl/",
-                "META-INF/",
-                "META-INF/MANIFEST.MF",
-                "NOTICE",
-                "META-INF/gradle-plugins/",
-                "META-INF/gradle-plugins/com.android.java.properties");
-
-        expected.putAll(
                 "com/android/tools/apkparser/binary-resources",
                 "com/",
                 "com/google/",
@@ -1316,11 +1288,6 @@ public class JarContentsTest {
         checkGroup("androidx/databinding/databinding-compiler", GMAVEN_ZIP);
         // pre-android X
         checkGroup("com/android/databinding/baseLibrary", GMAVEN_ZIP);
-    }
-
-    @Test
-    public void checkJava() throws Exception {
-        checkGroup("com/android/java", JAVALIBMODELBUILDER_ZIP);
     }
 
     private void checkGroup(String groupPrefix, String zipLocation) throws Exception {
