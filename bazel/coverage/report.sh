989b1b9ef70113ffd5eb822172164f56e519feb5
Eric Reed <ericreed@google.com>
2019-06-19 16:38:17 -0700

    Make coverage report generation script easier to use
    - Aborts if not invoked from top-level (studio-master-dev)
    - Checks if genhtml is install and aborts with instructions if not
    - Require report destination directory (not much use otherwise)
    
    Test: N/A
    Bug: N/A
    Change-Id: Id8edea41cee5aca6fbfc03072a21a8d75ece4903

--- bazel/coverage/report.sh
+++ bazel/coverage/report.sh
@@ -3,18 +3,28 @@
 readonly report_name="$1"
 readonly html_dir="$2"
 
-readonly usage="report.sh <name of coverage report> [<output directory for html report>]"
-
-if [[ -z ${report_name} ]]; then
-  echo ${usage}
-elif [[ ! -d ${html_dir} && ! -z ${html_dir} ]]; then
-  echo ${usage}
+if [[ ! -f "./tools/base/bazel/coverage/report.sh" ]]; then
+  echo "you should run this from studio-master-dev"
+  exit 1
 fi
 
+readonly genhtml="$(which genhtml)"
+if [[ -z ${genhtml} ]]; then
+  echo "you need genhtml to make reports. run this: sudo apt install lcov"
+  exit 1
+fi
+
+readonly usage="report.sh <name of coverage report> <output directory for html report>"
+
+if [[ -z ${report_name} || -z ${html_dir} ]]; then
+  echo ${usage}
+  exit 1
+fi
+
+echo "Run tests to generate coverage data"
 bazel test --define agent_coverage=true --config=remote -- "@cov//:${report_name}.suite" || exit $?
+echo "Processing raw coverage data"
 bazel build --config=remote -- "@cov//:${report_name}.lcov" || exit $?
-if [[ -d ${html_dir} ]]; then
-  echo "Generating HTML report in ${html_dir}"
-  genhtml -o ${html_dir} -p $(pwd) --no-function-coverage "bazel-genfiles/external/cov/${report_name}/lcov" || exit $?
-fi
+echo "Generating HTML report in ${html_dir}"
+genhtml -o ${html_dir} -p $(pwd) --no-function-coverage "bazel-genfiles/external/cov/${report_name}/lcov" || exit $?
 echo "Done"
