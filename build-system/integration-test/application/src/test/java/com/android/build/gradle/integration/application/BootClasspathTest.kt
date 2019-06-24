008ed0e2de8aa679679989a3e43fca8513ebc5b2
Jean-Luc Coelho <jeanlucncoelho@google.com>
2019-06-24 15:11:10 -0700

    Converting BootClasspathTest to kt
    Bug: None
    Test: BootClasspathTest
    Change-Id: I308775e801253938c48c2406242474a9a656aace

--- build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/application/BootClasspathTest.kt
+++ build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/application/BootClasspathTest.kt
@@ -1,43 +1,39 @@
-package com.android.build.gradle.integration.application;
+package com.android.build.gradle.integration.application
 
-import com.android.build.gradle.integration.common.fixture.GradleTestProject;
-import com.android.build.gradle.integration.common.fixture.app.HelloWorldApp;
-import com.android.build.gradle.integration.common.utils.TestFileUtils;
-import java.io.IOException;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import com.android.build.gradle.integration.common.fixture.GradleTestProject
+import com.android.build.gradle.integration.common.fixture.app.HelloWorldApp
+import com.android.build.gradle.integration.common.utils.TestFileUtils
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 
-/** Test BaseExtension.getBootClasspath can be use before afterEvaluate. */
-public class BootClasspathTest {
+/** Test BaseExtension.getBootClasspath can be use before afterEvaluate.  */
+class BootClasspathTest {
 
-    @Rule
-    public GradleTestProject project =
-            GradleTestProject.builder().fromTestApp(HelloWorldApp.noBuildFile()).create();
+    @get:Rule
+    var project = GradleTestProject.builder().fromTestApp(HelloWorldApp.noBuildFile()).create()
 
     @Before
-    public void setUp() throws IOException {
+    fun setUp() {
         TestFileUtils.appendToFile(
-                project.getBuildFile(),
-                "\n"
-                        + "apply plugin: 'com.android.application'\n"
-                        + "\n"
-                        + "android {\n"
-                        + "    compileSdkVersion "
-                        + GradleTestProject.DEFAULT_COMPILE_SDK_VERSION
-                        + "\n"
-                        + "    buildToolsVersion '"
-                        + GradleTestProject.DEFAULT_BUILD_TOOL_VERSION
-                        + "'\n"
-                        + "}\n"
-                        + "\n"
-                        + "task checkBootClasspath {\n"
-                        + "    assert android.getBootClasspath() != null\n"
-                        + "}\n");
+            project.buildFile,
+            """|
+               |apply plugin: 'com.android.application'
+               |
+               |android {
+               |    compileSdkVersion ${GradleTestProject.DEFAULT_COMPILE_SDK_VERSION}
+               |
+               |    buildToolsVersion '${GradleTestProject.DEFAULT_BUILD_TOOL_VERSION}'
+               |}
+               |
+               |task checkBootClasspath {
+               |    assert android.getBootClasspath() != null
+               |}""".trimMargin("|")
+        )
     }
 
     @Test
-    public void checkBootClasspathCanBeCalled() throws IOException, InterruptedException {
-        project.execute("checkBootClasspath");
+    fun checkBootClasspathCanBeCalled() {
+        project.execute("checkBootClasspath")
     }
 }
