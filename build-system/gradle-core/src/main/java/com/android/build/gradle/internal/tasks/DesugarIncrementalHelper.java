28d1afd851a4e202fd9403bfe23e7b047728c254
Ivan Gavrilovic <gavra@google.com>
2019-06-29 13:04:41 +0100

    Rename dexing task and tests
    Rename files in order to make diff in the following
    changes easier to read.
    
    BUG: 136244270
    Test: n/a
    Change-Id: Id35bbd15bd15fa103e5b3ecebdaed8ac29beb29f

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/tasks/DesugarIncrementalHelper.java
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/tasks/DesugarIncrementalHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2019 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.build.gradle.internal.transforms;
+package com.android.build.gradle.internal.tasks;
 
 import static com.android.builder.desugaring.DesugaringClassAnalyzer.analyze;
 
@@ -48,7 +48,8 @@ class DesugarIncrementalTransformHelper {
 
     @NonNull
     private static final LoggerWrapper logger =
-            LoggerWrapper.getLogger(DesugarIncrementalTransformHelper.class);
+            LoggerWrapper.getLogger(
+                    DesugarIncrementalTransformHelper.class);
 
     @NonNull private final String projectVariant;
     @NonNull private final Iterable<Path> allInputs;
