de89c14aa8a05140ae0d7712fd55fce087af1839
Valery Cherepanov <qumeric@google.com>
2019-07-02 11:17:06 +0100

    Add builders and implementations of Parameter
    Also add Constraint class
    
    Bug: 135397531
    Test: N/A
    Change-Id: I24bcc07c6b10c7a0fb721566266ba162bfa097bd

--- wizard/template-plugin/src/com/android/tools/idea/wizard/template/Parameter.kt
+++ wizard/template-plugin/src/com/android/tools/idea/wizard/template/Parameter.kt
@@ -15,6 +15,55 @@
  */
 package com.android.tools.idea.wizard.template
 
+import kotlin.reflect.KClass
+
+/**
+ * Constraints that can be applied to a parameter which helps the UI add a validator etc for user input.
+ * These are typically combined into a set of constraints via an EnumSet.
+ */
+enum class Constraint {
+  /**
+   * This value must be unique. This constraint usually only makes sense when other constraints are specified, such as [LAYOUT],
+   * which means that the parameter should designate a name that does not represent an existing layout resource name.
+   */
+  UNIQUE,
+  /**
+   * This value must already exist. This constraint usually only makes sense when other constraints are specified, such as [LAYOUT],
+   * which means that the parameter should designate a name that already exists as a resource name.
+   */
+  EXISTS,
+  /** The associated value must not be empty. */
+  NONEMPTY,
+  /** The associated value should represent a fully qualified activity class name. */
+  ACTIVITY,
+  /** The associated value should represent an API level. */
+  API_LEVEL,
+  /** The associated value should represent a valid class name. */
+  CLASS,
+  /** The associated value should represent a valid package name. */
+  PACKAGE,
+  /** The associated value should represent a valid Android application package name. */
+  APP_PACKAGE,
+  /** The associated value should represent a valid Module name. */
+  MODULE,
+  /** The associated value should represent a valid layout resource name. */
+  LAYOUT,
+  /** The associated value should represent a valid drawable resource name. */
+  DRAWABLE,
+  /** The associated value should represent a valid values file name. */
+  NAVIGATION,
+  /** The associated value should represent a valid navigation resource name. */
+  VALUES,
+  /** The associated value should represent a valid id resource name. */
+  ID,
+  /** The associated value should represent a valid source directory name. */
+  SOURCE_SET_FOLDER,
+  /** The associated value should represent a valid string resource name. */
+  STRING,
+  /**  The associated value should represent a valid URI authority. Format: [userinfo@]host[:port] */
+  URI_AUTHORITY;
+}
+
 /**
  * This is a parameter which is a part of [Template].
  *
@@ -22,15 +71,11 @@ package com.android.tools.idea.wizard.template
  * A user should provide [value]s to all parameters via interacting with UI.
  * Later this data is passed to the [Recipe] and used to render actual template files.
  */
-abstract class Parameter<T>(
-  private val visible: WizardParameterData.() -> Boolean = { true }
-) {
+// TODO(qumeric): consider renaming/changing the whole hierarchy to make users more aware that it is used for rendering UI
+sealed class Parameter<T> {
+  /** Name of the parameter. Should be unique. */
   abstract val name: String
   open val help: String? = null
-  /**
-   * Returns false if the [Parameter] should be completely ignored.
-   */
-  open val enabled: () -> Boolean = { true }
   abstract val defaultValue: T
   abstract var value: T
   // should be updated only by [Parameters]
@@ -38,11 +83,101 @@ abstract class Parameter<T>(
 
   /**
    * Tells if the [Parameter] should be shown in UI.
+   *
    * We do not show parameters which are not visible in UI, but use them (fill with data and send to the [Recipe]).
    * @see enabled
    */
-  val isVisible: Boolean
-    get() = enabled() && wizardParameterData.visible()
+  val isVisibleAndEnabled: Boolean
+    get() = enabled && visible
+
+  /**
+   * Returns false if the [Parameter] should be completely ignored (sometimes will be rendered in gray in UI).
+   */
+  abstract val enabled: Boolean
+  abstract val visible: Boolean
+}
+
+/**
+ * A wrapper which overrides some [Parameter] methods with given lambdas.
+ *
+ * Actual parameters should inherit this, not [Parameter].
+ */
+sealed class DslParameter<T>(
+  private val _visible: WizardParameterData.() -> Boolean = { true },
+  private val _enabled: WizardParameterData.() -> Boolean = { true }
+): Parameter<T>() {
+  override val enabled get() = wizardParameterData._enabled()
+  override val visible get()= wizardParameterData._visible()
+}
+
+/**
+ * String parameter. Rendered as a text field in UI.
+ */
+data class StringParameter(
+  override val name: String,
+  override val help: String? = null,
+  private val _visible: WizardParameterData.() -> Boolean = { true },
+  private val _enabled: WizardParameterData.() -> Boolean = { true },
+  override val defaultValue: String,
+  val constraints: List<Constraint>,
+  private val _suggest: () -> String? = { null },
+  val type: Type = Type.STRING
+) : DslParameter<String>(_visible, _enabled) {
+  override var value: String = defaultValue
+
+  /**
+   * Value suggested by the Studio. If it was evaluated to null, then [defaultValue] is used.
+   * Often calculated using different parameters, e.g "activity_super" layout name generated from "SuperActivity".
+   */
+  fun suggest() = _suggest()
+
+  /**
+   * Different [Type]s of StringParameter have different UIs.
+   */
+  enum class Type {
+    STRING,
+    LANGUAGE,
+    PACKAGE_NAME
+  }
+}
+
+/**
+ * Enum parameter. Rendered as a combo box in UI.
+ */
+data class EnumParameter<T: Enum<T>>(
+  private val enumClass: KClass<T>,
+  override val name: String,
+  override val help: String? = null,
+  private val _visible: WizardParameterData.() -> Boolean = { true },
+  private val _enabled: WizardParameterData.() -> Boolean = { true },
+  override val defaultValue: T
+) : DslParameter<T>(_visible, _enabled) {
+  override var value: T = defaultValue
+  val options: Array<T> = enumClass.java.enumConstants
+}
+
+/**
+ * Boolean parameter. Rendered as a checkbox in UI.
+ */
+data class BooleanParameter(
+  override val name: String,
+  override val help: String? = null,
+  private val _visible: WizardParameterData.() -> Boolean = { true },
+  private val _enabled: WizardParameterData.() -> Boolean = { true },
+  override val defaultValue: Boolean
+) : DslParameter<Boolean>(_visible, _enabled) {
+  override var value: Boolean = defaultValue
+}
+
+/**
+ * Separator. Not passed to the render, only shows in UI.
+ */
+class Separator: DslParameter<Nothing?>({true}, {true}) {
+  override val name: String = ""
+  override val defaultValue: Nothing? get() = throw UnsupportedOperationException()
+  override var value: Nothing?
+    get() = throw UnsupportedOperationException()
+    set(_) = throw UnsupportedOperationException()
 }
 
 /**
