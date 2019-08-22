62baa51dd1a0fdf9be859e9e89432dee274a8730
Stephen Atwood <stephenatwood@google.com>
2019-08-21 19:23:04 -0700

    Convert BundleInstantApp to Kotlin
    Step Two of conversion to Kotlin: convert the file
    contents to Kotlin.
    
    Bug: N/A
    Test: N/A
    Change-Id: I12eafda9d66ad9f296490fac487411828a810fe2

--- build-system/gradle-core/src/main/java/com/android/build/gradle/tasks/BundleInstantApp.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/tasks/BundleInstantApp.kt
@@ -14,187 +14,155 @@
  * limitations under the License.
  */
 
-package com.android.build.gradle.tasks;
+package com.android.build.gradle.tasks
 
-import static com.android.SdkConstants.DOT_ZIP;
+import com.android.SdkConstants.DOT_ZIP
+import com.android.build.gradle.internal.publishing.AndroidArtifacts
+import com.android.build.gradle.internal.scope.BuildArtifactsHolder
+import com.android.build.gradle.internal.scope.ExistingBuildElements
+import com.android.build.gradle.internal.scope.InstantAppOutputScope
+import com.android.build.gradle.internal.scope.InternalArtifactType
+import com.android.build.gradle.internal.scope.VariantScope
+import com.android.build.gradle.internal.tasks.ModuleMetadata
+import com.android.build.gradle.internal.tasks.NonIncrementalTask
+import com.android.build.gradle.internal.tasks.Workers
+import com.android.build.gradle.internal.tasks.factory.TaskCreationAction
+import com.android.tools.build.apkzlib.zip.ZFile
+import com.android.tools.build.apkzlib.zip.ZFileOptions
+import com.android.tools.build.apkzlib.zip.compress.DeflateExecutionCompressor
+import com.android.utils.FileUtils
+import java.io.File
+import java.io.FileInputStream
+import java.io.IOException
+import java.io.Serializable
+import java.io.UncheckedIOException
+import java.util.ArrayList
+import java.util.TreeSet
+import java.util.zip.Deflater
+import javax.inject.Inject
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.file.FileCollection
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
+import org.gradle.api.tasks.TaskProvider
 
-import com.android.annotations.NonNull;
-import com.android.build.gradle.internal.publishing.AndroidArtifacts;
-import com.android.build.gradle.internal.scope.BuildArtifactsHolder;
-import com.android.build.gradle.internal.scope.BuildOutput;
-import com.android.build.gradle.internal.scope.ExistingBuildElements;
-import com.android.build.gradle.internal.scope.InstantAppOutputScope;
-import com.android.build.gradle.internal.scope.InternalArtifactType;
-import com.android.build.gradle.internal.scope.VariantScope;
-import com.android.build.gradle.internal.tasks.ModuleMetadata;
-import com.android.build.gradle.internal.tasks.NonIncrementalTask;
-import com.android.build.gradle.internal.tasks.Workers;
-import com.android.build.gradle.internal.tasks.factory.TaskCreationAction;
-import com.android.ide.common.workers.ExecutorServiceAdapter;
-import com.android.ide.common.workers.WorkerExecutorFacade;
-import com.android.tools.build.apkzlib.zip.ZFile;
-import com.android.tools.build.apkzlib.zip.ZFileOptions;
-import com.android.tools.build.apkzlib.zip.compress.DeflateExecutionCompressor;
-import com.android.utils.FileUtils;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.Serializable;
-import java.io.UncheckedIOException;
-import java.util.ArrayList;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.zip.Deflater;
-import javax.inject.Inject;
-import org.gradle.api.file.DirectoryProperty;
-import org.gradle.api.file.FileCollection;
-import org.gradle.api.tasks.Input;
-import org.gradle.api.tasks.InputFiles;
-import org.gradle.api.tasks.OutputDirectory;
-import org.gradle.api.tasks.PathSensitive;
-import org.gradle.api.tasks.PathSensitivity;
-import org.gradle.api.tasks.TaskProvider;
+/** Task to bundle a bundle of feature APKs.  */
+abstract class BundleInstantApp : NonIncrementalTask() {
 
-/** Task to bundle a bundle of feature APKs. */
-public abstract class BundleInstantApp extends NonIncrementalTask {
+    @get:OutputDirectory
+    abstract val bundleDirectory: DirectoryProperty
 
-    @Override
-    protected void doTaskAction() throws IOException {
+    @get:Input
+    abstract val bundleName: Property<String>
+
+    @get:InputFiles
+    @get:PathSensitive(PathSensitivity.RELATIVE)
+    lateinit var applicationMetadataFile: FileCollection
+        private set
+
+    @get:InputFiles
+    @get:PathSensitive(PathSensitivity.RELATIVE)
+    lateinit var apkDirectories: FileCollection
+        private set
+
+    override fun doTaskAction() {
         // FIXME: Make this task incremental.
-        try (WorkerExecutorFacade workers = getWorkerFacadeWithWorkers()) {
+        getWorkerFacadeWithWorkers().use { workers ->
             workers.submit(
-                    BundleInstantAppRunnable.class,
-                    new BundleInstantAppParams(
-                            getProjectName(),
-                            getPath(),
-                            getBundleDirectory().get().getAsFile(),
-                            bundleName,
-                            ModuleMetadata.load(applicationMetadataFile.getSingleFile())
-                                    .getApplicationId(),
-                            new TreeSet<>(apkDirectories.getFiles())));
+                BundleInstantAppRunnable::class.java,
+                BundleInstantAppParams(
+                    projectName,
+                    path,
+                    bundleDirectory.get().asFile,
+                    bundleName.get(),
+                    ModuleMetadata.load(applicationMetadataFile.singleFile)
+                        .applicationId,
+                    TreeSet(apkDirectories.files)
+                )
+            )
         }
     }
 
-    @OutputDirectory
-    @NonNull
-    public abstract DirectoryProperty getBundleDirectory();
+    class CreationAction(private val scope: VariantScope, private val bundleDirectory: File) :
+        TaskCreationAction<BundleInstantApp>() {
 
-    @Input
-    @NonNull
-    public String getBundleName() {
-        return bundleName;
+        override val name: String
+            get() = scope.getTaskName("package", "InstantAppBundle")
+
+        override val type: Class<BundleInstantApp>
+            get() = BundleInstantApp::class.java
+
+        override fun handleProvider(taskProvider: TaskProvider<out BundleInstantApp>) {
+            super.handleProvider(taskProvider)
+            scope.artifacts
+                .producesDir(
+                    InternalArtifactType.INSTANTAPP_BUNDLE,
+                    BuildArtifactsHolder.OperationType.INITIAL,
+                    taskProvider,
+                    BundleInstantApp::bundleDirectory,
+                    bundleDirectory.absolutePath,
+                    ""
+                )
+        }
+
+        override fun configure(task: BundleInstantApp) {
+            task.variantName = scope.fullVariantName
+
+            task.bundleName.set(
+                "${scope.globalScope.projectBaseName}-${scope.variantConfiguration.baseName}$DOT_ZIP"
+            )
+
+            task.applicationMetadataFile = scope.getArtifactFileCollection(
+                AndroidArtifacts.ConsumedConfigType.COMPILE_CLASSPATH,
+                AndroidArtifacts.ArtifactScope.PROJECT,
+                AndroidArtifacts.ArtifactType.FEATURE_APPLICATION_ID_DECLARATION
+            )
+
+            task.apkDirectories = scope.getArtifactFileCollection(
+                AndroidArtifacts.ConsumedConfigType.RUNTIME_CLASSPATH,
+                AndroidArtifacts.ArtifactScope.PROJECT,
+                AndroidArtifacts.ArtifactType.APK
+            )
+        }
     }
 
-    @InputFiles
-    @PathSensitive(PathSensitivity.RELATIVE)
-    @NonNull
-    public FileCollection getApplicationMetadataFile() {
-        return applicationMetadataFile;
-    }
+    private class BundleInstantAppRunnable @Inject
+    constructor(private val params: BundleInstantAppParams) : Runnable {
 
-    @InputFiles
-    @PathSensitive(PathSensitivity.RELATIVE)
-    @NonNull
-    public FileCollection getApkDirectories() {
-        return apkDirectories;
-    }
-
-    private String bundleName;
-    private FileCollection applicationMetadataFile;
-    private FileCollection apkDirectories;
-
-    public static class CreationAction extends TaskCreationAction<BundleInstantApp> {
-
-        public CreationAction(@NonNull VariantScope scope, @NonNull File bundleDirectory) {
-            this.scope = scope;
-            this.bundleDirectory = bundleDirectory;
-        }
-
-        @NonNull
-        @Override
-        public String getName() {
-            return scope.getTaskName("package", "InstantAppBundle");
-        }
-
-        @NonNull
-        @Override
-        public Class<BundleInstantApp> getType() {
-            return BundleInstantApp.class;
-        }
-
-        @Override
-        public void handleProvider(@NonNull TaskProvider<? extends BundleInstantApp> taskProvider) {
-            super.handleProvider(taskProvider);
-            scope.getArtifacts()
-                    .producesDir(
-                            InternalArtifactType.INSTANTAPP_BUNDLE.INSTANCE,
-                            BuildArtifactsHolder.OperationType.INITIAL,
-                            taskProvider,
-                            BundleInstantApp::getBundleDirectory,
-                            bundleDirectory.getAbsolutePath(),
-                            "");
-        }
-
-        @Override
-        public void configure(@NonNull BundleInstantApp bundleInstantApp) {
-            bundleInstantApp.setVariantName(scope.getFullVariantName());
-            bundleInstantApp.bundleName =
-                    scope.getGlobalScope().getProjectBaseName()
-                            + "-"
-                            + scope.getVariantConfiguration().getBaseName()
-                            + DOT_ZIP;
-            bundleInstantApp.applicationMetadataFile =
-                    scope.getArtifactFileCollection(
-                            AndroidArtifacts.ConsumedConfigType.COMPILE_CLASSPATH,
-                            AndroidArtifacts.ArtifactScope.PROJECT,
-                            AndroidArtifacts.ArtifactType.FEATURE_APPLICATION_ID_DECLARATION);
-            bundleInstantApp.apkDirectories =
-                    scope.getArtifactFileCollection(
-                            AndroidArtifacts.ConsumedConfigType.RUNTIME_CLASSPATH,
-                            AndroidArtifacts.ArtifactScope.PROJECT,
-                            AndroidArtifacts.ArtifactType.APK);
-        }
-
-        private final VariantScope scope;
-        private final File bundleDirectory;
-    }
-
-    private static class BundleInstantAppRunnable implements Runnable {
-        private final BundleInstantAppParams params;
-
-        @Inject
-        public BundleInstantAppRunnable(BundleInstantAppParams params) {
-            this.params = params;
-        }
-
-        @Override
-        public void run() {
+        override fun run() {
             try {
-                FileUtils.mkdirs(params.bundleDirectory);
+                FileUtils.mkdirs(params.bundleDirectory)
 
-                File bundleFile = new File(params.bundleDirectory, params.bundleName);
-                FileUtils.deleteIfExists(bundleFile);
+                val bundleFile = File(params.bundleDirectory, params.bundleName)
+                FileUtils.deleteIfExists(bundleFile)
 
-                ZFileOptions zFileOptions = new ZFileOptions();
+                val zFileOptions = ZFileOptions()
 
-                try (ExecutorServiceAdapter executor =
-                        Workers.INSTANCE.withThreads(params.projectName, params.taskOwnerName)) {
-                    zFileOptions.setCompressor(
-                            new DeflateExecutionCompressor(
-                                    (compressJob) ->
-                                            executor.submit(
-                                                    CompressorRunnable.class,
-                                                    new CompressorParams(compressJob)),
-                                    Deflater.DEFAULT_COMPRESSION));
-                    try (ZFile file = ZFile.openReadWrite(bundleFile, zFileOptions)) {
-                        for (File apkDirectory : params.apkDirectories) {
-                            for (BuildOutput buildOutput :
-                                    ExistingBuildElements.from(
-                                            InternalArtifactType.APK.INSTANCE, apkDirectory)) {
-                                File apkFile = buildOutput.getOutputFile();
-                                try (FileInputStream fileInputStream =
-                                        new FileInputStream(apkFile)) {
-                                    file.add(apkFile.getName(), fileInputStream);
+                Workers.withThreads(params.projectName, params.taskOwnerName).use { executor ->
+                    zFileOptions.compressor = DeflateExecutionCompressor(
+                        { compressJob ->
+                            executor.submit(
+                                CompressorRunnable::class.java,
+                                CompressorParams(compressJob)
+                            )
+                        },
+                        Deflater.DEFAULT_COMPRESSION
+                    )
+                    ZFile.openReadWrite(bundleFile, zFileOptions).use { file ->
+                        for (apkDirectory in params.apkDirectories) {
+                            for (buildOutput in ExistingBuildElements.from(
+                                InternalArtifactType.APK, apkDirectory
+                            )) {
+                                val apkFile = buildOutput.outputFile
+                                FileInputStream(apkFile).use { fileInputStream ->
+                                    file.add(
+                                        apkFile.name,
+                                        fileInputStream
+                                    )
                                 }
                             }
                         }
@@ -202,61 +170,40 @@ public abstract class BundleInstantApp extends NonIncrementalTask {
                 }
 
                 // Write the json output.
-                InstantAppOutputScope instantAppOutputScope =
-                        new InstantAppOutputScope(
-                                params.applicationId,
-                                bundleFile,
-                                new ArrayList<>(params.apkDirectories));
-                instantAppOutputScope.save(params.bundleDirectory);
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
+                val instantAppOutputScope = InstantAppOutputScope(
+                    params.applicationId,
+                    bundleFile,
+                    ArrayList(params.apkDirectories)
+                )
+                instantAppOutputScope.save(params.bundleDirectory)
+            } catch (e: IOException) {
+                throw UncheckedIOException(e)
+            }
+
+        }
+
+        internal class CompressorRunnable @Inject
+        constructor(params: CompressorParams) : Runnable {
+            private val compressJob: Runnable
+
+            init {
+                this.compressJob = params.compressJob
+            }
+
+            override fun run() {
+                compressJob.run()
             }
         }
 
-        static class CompressorRunnable implements Runnable {
-            private final Runnable compressJob;
-
-            @Inject
-            CompressorRunnable(CompressorParams params) {
-                this.compressJob = params.compressJob;
-            }
-
-            @Override
-            public void run() {
-                compressJob.run();
-            }
-        }
-
-        static class CompressorParams implements Serializable {
-            private final Runnable compressJob;
-
-            CompressorParams(Runnable compressJob) {
-                this.compressJob = compressJob;
-            }
-        }
+        private class CompressorParams(val compressJob: Runnable) : Serializable
     }
 
-    private static class BundleInstantAppParams implements Serializable {
-        @NonNull private final String projectName;
-        @NonNull private final String taskOwnerName;
-        @NonNull private final File bundleDirectory;
-        @NonNull private final String bundleName;
-        @NonNull private final String applicationId;
-        @NonNull private final Set<File> apkDirectories;
-
-        BundleInstantAppParams(
-                @NonNull String projectName,
-                @NonNull String taskOwnerName,
-                @NonNull File bundleDirectory,
-                @NonNull String bundleName,
-                @NonNull String applicationId,
-                @NonNull Set<File> apkDirectories) {
-            this.projectName = projectName;
-            this.taskOwnerName = taskOwnerName;
-            this.bundleDirectory = bundleDirectory;
-            this.bundleName = bundleName;
-            this.applicationId = applicationId;
-            this.apkDirectories = apkDirectories;
-        }
-    }
+    private class BundleInstantAppParams internal constructor(
+        val projectName: String,
+        val taskOwnerName: String,
+        val bundleDirectory: File,
+        val bundleName: String,
+        val applicationId: String,
+        val apkDirectories: Set<File>
+    ) : Serializable
 }
