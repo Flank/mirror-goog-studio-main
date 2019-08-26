e965d8a8a87dbd1044629da54ff4983651b675b3
Matt Bayes <mbayes@google.com>
2019-08-26 15:22:02 -0700

    Run tests on crostini one at a time
    The UI tests must run one at a time due to performance restraints of the
    chromebox
    
    Bug: 139817030
    Test: n/a
    Change-Id: I028ff023c7392fc8182e1124befda65391d00f6b

--- bazel/qa_sanity_linux.sh
+++ bazel/qa_sanity_linux.sh
@@ -14,48 +14,86 @@ readonly lsb_release="$(grep -oP '(?<=DISTRIB_CODENAME=).*' /etc/lsb-release)"
 # Invalidate local cache to avoid picking up obsolete test result xmls
 "${script_dir}/bazel" clean --async
 
-#Have crostini tests run locally
-if [[ $lsb_release != "crostini" ]]; then
-  config_options="--config=remote"
-else
+#Have crostini tests run locally and one at a time
+if [[ $lsb_release == "crostini" ]]; then
   config_options="--config=cloud_resultstore"
-fi
+  target_filters=qa_sanity,-qa_unreliable,-no_linux,-no_test_linux,-requires_emulator,-perfgate_only,-no_crostini
 
-# Generate a UUID for use as the bazel invocation id
-readonly invocation_id="$(uuidgen)"
+  # Generate a UUID for use as the bazel invocation id
+  readonly build_invocation_id="$(uuidgen)"
 
-# The sanity tests are ran in 2 groups.
-# The first group is all the tests that do not use an Android emulator.
-# The second group is all the tests that use the Android emulator.
-# We need to run in 2 groups because the 2 sets of tests run with different
-# options.
+  #Build the project in parallel for crostini
+  "${script_dir}/bazel" \
+    --max_idle_secs=60 \
+    build \
+    ${config_options} \
+    --invocation_id=${build_invocation_id} \
+    --define=meta_android_build_number=${build_number} \
+    --build_tag_filters=${target_filters} \
+    --tool_tag=${script_name} \
+    -- \
+    //tools/adt/idea/android-uitests/...
 
-# Run Bazel tests - no emulator tests should run here
-target_filters=qa_sanity,-qa_unreliable,-no_linux,-no_test_linux,-requires_emulator,-perfgate_only
-"${script_dir}/bazel" \
-  --max_idle_secs=60 \
-  test \
-  --keep_going \
-  ${config_options} \
-  --invocation_id=${invocation_id} \
-  --define=meta_android_build_number=${build_number} \
-  --build_tag_filters=${target_filters} \
-  --test_tag_filters=${target_filters} \
-  --tool_tag=${script_name} \
-  -- \
-  //tools/adt/idea/android-uitests/...
+  readonly test_invocation_id="$(uuidgen)"
 
-readonly bazel_status_no_emu=$?
+  #Run the tests one at a time for crostini
+  "${script_dir}/bazel" \
+    --max_idle_secs=60 \
+    test \
+    --keep_going \
+    ${config_options} \
+    --jobs=1 \
+    --invocation_id=${test_invocation_id} \
+    --define=meta_android_build_number=${build_number} \
+    --build_tag_filters=${target_filters} \
+    --test_tag_filters=${target_filters} \
+    --tool_tag=${script_name} \
+    -- \
+    //tools/adt/idea/android-uitests/...
 
-if [[ -d "${dist_dir}" ]]; then
-  echo "<meta http-equiv=\"refresh\" content=\"0; URL='https://source.cloud.google.com/results/invocations/${invocation_id}'\" />" > "${dist_dir}"/upsalite_test_results.html
-fi
+  readonly bazel_status_no_emu=$?
 
-# Generate a UUID for use as the bazel invocation id
-readonly invocation_id_emu="$(uuidgen)"
+  if [[ -d "${dist_dir}" ]]; then
+    echo "<meta http-equiv=\"refresh\" content=\"0; URL='https://source.cloud.google.com/results/invocations/${test_invocation_id}'\" />" > "${dist_dir}"/upsalite_test_results.html
+  fi
 
-# Skips emulator tests on crostini because they are not currently supported
-if [[ $lsb_release != "crostini" ]]; then
+  readonly bazel_status_emu=0
+else #Executes normally on linux as before
+  config_options="--config=remote"
+
+  # Generate a UUID for use as the bazel invocation id
+  readonly invocation_id="$(uuidgen)"
+
+  # The sanity tests are ran in 2 groups.
+  # The first group is all the tests that do not use an Android emulator.
+  # The second group is all the tests that use the Android emulator.
+  # We need to run in 2 groups because the 2 sets of tests run with different
+  # options.
+
+  # Run Bazel tests - no emulator tests should run here
+  target_filters=qa_sanity,-qa_unreliable,-no_linux,-no_test_linux,-requires_emulator,-perfgate_only
+  "${script_dir}/bazel" \
+    --max_idle_secs=60 \
+    test \
+    --keep_going \
+    ${config_options} \
+    --invocation_id=${invocation_id} \
+    --define=meta_android_build_number=${build_number} \
+    --build_tag_filters=${target_filters} \
+    --test_tag_filters=${target_filters} \
+    --tool_tag=${script_name} \
+    -- \
+    //tools/adt/idea/android-uitests/...
+
+  readonly bazel_status_no_emu=$?
+
+  if [[ -d "${dist_dir}" ]]; then
+    echo "<meta http-equiv=\"refresh\" content=\"0; URL='https://source.cloud.google.com/results/invocations/${invocation_id}'\" />" > "${dist_dir}"/upsalite_test_results.html
+  fi
+
+  # Generate a UUID for use as the bazel invocation id
+  readonly invocation_id_emu="$(uuidgen)"
+
   # Run Bazel tests - only emulator tests should run here
   target_filters=qa_sanity_emu,-qa_unreliable,-no_linux,-no_test_linux
   QA_ANDROID_SDK_ROOT=${HOME}/Android_emulator/sdk "${script_dir}/bazel" \
@@ -77,8 +115,6 @@ if [[ $lsb_release != "crostini" ]]; then
   if [[ -d "${dist_dir}" ]]; then
     echo "<meta http-equiv=\"refresh\" content=\"0; URL='https://source.cloud.google.com/results/invocations/${invocation_id_emu}'\" />" > "${dist_dir}"/upsalite_emu_test_results.html
   fi
-else
-  readonly bazel_status_emu=0
 fi
 
 if [[ -d "${dist_dir}" ]]; then
