87a9d9dc67c0ae3e0ae9bfb6e165b6c4a5e91255
Ivan Gavrilovic <gavra@google.com>
2019-08-30 15:56:49 +0200

    Fix Gradle nightly tests on mac
    Gradle 5.7-20190815220048+0000 daemon was unable
    to start when running tests with bazel. This CL uses
    5.6.1 to run instant execution tests.
    
    BUG: 140284162
    Test: existing
    Change-Id: I65b2980f1accc3015e81274a154a8ba7354b543f

--- build-system/integration-test/application/src/testNightly/java/com/android/build/gradle/integration/instantexection/BasicInstantExecutionTest.kt
+++ build-system/integration-test/application/src/testNightly/java/com/android/build/gradle/integration/instantexection/BasicInstantExecutionTest.kt
@@ -33,7 +33,6 @@ class BasicInstantExecutionTest {
     @JvmField
     @Rule
     var project = GradleTestProject.builder()
-        .setTargetGradleVersion("5.7-20190815220048+0000")
         .fromTestApp(
             MultiModuleTestProject.builder()
                 .subproject(":app", app)
