aeb2c8f30047812673ebc888a188e320683ddebd
Izabela Orlowska <imorlowska@google.com>
2019-09-05 11:33:01 +0100

    Enable the flag for excluding res sources in release bundles
    Fixes: 139438126
    Test: DynamicAppTest
    Change-Id: Id651c05424cef3708ef70b4c5935443d2165bea5

--- build-system/gradle-core/src/main/java/com/android/build/gradle/options/BooleanOption.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/options/BooleanOption.kt
@@ -115,7 +115,7 @@ enum class BooleanOption(
     USE_NEW_JAR_CREATOR("android.useNewJarCreator", true),
     PRECOMPILE_DEPENDENCIES_RESOURCES("android.precompileDependenciesResources", true, Option.Status.STABLE),
     USE_NEW_APK_CREATOR("android.useNewApkCreator", true),
-    EXCLUDE_RES_SOURCES_FOR_RELEASE_BUNDLES("android.bundle.excludeResSourcesForRelease", false),
+    EXCLUDE_RES_SOURCES_FOR_RELEASE_BUNDLES("android.bundle.excludeResSourcesForRelease", true),
 
     // ---------------
     // Lifecycle flags: Deprecated stage, feature is stable and we want to get rid of the ability to revert to older code path
