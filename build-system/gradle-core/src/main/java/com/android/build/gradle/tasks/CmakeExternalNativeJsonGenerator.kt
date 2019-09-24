a0d344e2add466144d65020cb8975fa89b06397a
Jean-Luc Coelho <jeanlucncoelho@google.com>
2019-09-24 12:56:58 -0700

    Converting CmakeExternalNativeJsonGenerator to kt
    Bug: None
    Test: Existing
    Change-Id: I11ef1638160f15e25bd9aad344e45756068542f8

--- build-system/gradle-core/src/main/java/com/android/build/gradle/tasks/CmakeExternalNativeJsonGenerator.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/tasks/CmakeExternalNativeJsonGenerator.kt
@@ -14,72 +14,74 @@
  * limitations under the License.
  */
 
-package com.android.build.gradle.tasks;
+package com.android.build.gradle.tasks
 
-import static com.android.build.gradle.internal.cxx.cmake.MakeCmakeMessagePathsAbsoluteKt.makeCmakeMessagePathsAbsolute;
-import static com.android.build.gradle.internal.cxx.configure.CmakeCommandLineKt.convertCmakeCommandLineArgumentsToStringList;
-import static com.android.build.gradle.internal.cxx.logging.LoggingEnvironmentKt.errorln;
-import static com.android.build.gradle.internal.cxx.settings.CxxAbiModelCMakeSettingsRewriterKt.getFinalCmakeCommandLineArguments;
-
-import com.android.annotations.NonNull;
-import com.android.annotations.Nullable;
-import com.android.build.gradle.internal.core.Abi;
-import com.android.build.gradle.internal.cxx.configure.CommandLineArgument;
-import com.android.build.gradle.internal.cxx.model.CxxAbiModel;
-import com.android.build.gradle.internal.cxx.model.CxxBuildModel;
-import com.android.build.gradle.internal.cxx.model.CxxCmakeModuleModel;
-import com.android.build.gradle.internal.cxx.model.CxxModuleModelKt;
-import com.android.build.gradle.internal.cxx.model.CxxVariantModel;
-import com.android.build.gradle.internal.ndk.Stl;
-import com.android.ide.common.process.ProcessException;
-import com.android.ide.common.process.ProcessInfoBuilder;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.google.wireless.android.sdk.stats.GradleBuildVariant;
-import com.google.wireless.android.sdk.stats.GradleNativeAndroidModule;
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.stream.Collectors;
-import org.gradle.api.tasks.InputFile;
-import org.gradle.api.tasks.Optional;
-import org.gradle.api.tasks.PathSensitive;
-import org.gradle.api.tasks.PathSensitivity;
+import com.android.build.gradle.internal.cxx.cmake.makeCmakeMessagePathsAbsolute
+import com.android.build.gradle.internal.cxx.configure.convertCmakeCommandLineArgumentsToStringList
+import com.android.build.gradle.internal.cxx.logging.errorln
+import com.android.build.gradle.internal.cxx.settings.getFinalCmakeCommandLineArguments
+import com.android.build.gradle.internal.core.Abi
+import com.android.build.gradle.internal.cxx.configure.CommandLineArgument
+import com.android.build.gradle.internal.cxx.model.CxxAbiModel
+import com.android.build.gradle.internal.cxx.model.CxxBuildModel
+import com.android.build.gradle.internal.cxx.model.CxxCmakeModuleModel
+import com.android.build.gradle.internal.cxx.model.CxxVariantModel
+import com.android.build.gradle.internal.cxx.model.cmakeSettingsFile
+import com.android.build.gradle.internal.ndk.Stl
+import com.android.ide.common.process.ProcessException
+import com.android.ide.common.process.ProcessInfoBuilder
+import com.google.wireless.android.sdk.stats.GradleBuildVariant
+import com.google.wireless.android.sdk.stats.GradleNativeAndroidModule
+import java.io.File
+import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
+import java.io.IOException
 
 /**
  * CMake JSON generation logic. This is separated from the corresponding CMake task so that JSON can
  * be generated during configuration.
  */
-abstract class CmakeExternalNativeJsonGenerator extends ExternalNativeJsonGenerator {
-    @NonNull protected final CxxCmakeModuleModel cmake;
+internal abstract class CmakeExternalNativeJsonGenerator(
+    build: CxxBuildModel,
+    variant: CxxVariantModel,
+    abis: List<CxxAbiModel>,
+    stats: GradleBuildVariant.Builder
+) : ExternalNativeJsonGenerator(build, variant, abis, stats) {
+    @JvmField
+    protected val cmake: CxxCmakeModuleModel
 
-    CmakeExternalNativeJsonGenerator(
-            @NonNull CxxBuildModel build,
-            @NonNull CxxVariantModel variant,
-            @NonNull List<CxxAbiModel> abis,
-            @NonNull GradleBuildVariant.Builder stats) {
-        super(build, variant, abis, stats);
-        this.stats.setNativeBuildSystemType(GradleNativeAndroidModule.NativeBuildSystemType.CMAKE);
-        this.cmake = Objects.requireNonNull(variant.getModule().getCmake());
+    @InputFile
+    @PathSensitive(PathSensitivity.RELATIVE)
+    @Optional
+    fun getCmakeSettingsJson(): File? {
+        val cmakeSettings = variant.module.cmakeSettingsFile
+        return if (cmakeSettings.isFile) cmakeSettings else null
+    }
+
+    init {
+        this.stats.nativeBuildSystemType = GradleNativeAndroidModule.NativeBuildSystemType.CMAKE
+        this.cmake = variant.module.cmake!!
 
         // Check some basic requirements. This code executes at sync time but any call to
         // recordConfigurationError will later cause the generation of json to fail.
-        File cmakelists = getMakefile();
-        if (cmakelists.isDirectory()) {
+        val cmakelists = makefile
+        if (cmakelists.isDirectory) {
             errorln(
-                    "Gradle project cmake.path %s is a folder. It must be CMakeLists.txt",
-                    cmakelists);
-        } else if (cmakelists.isFile()) {
-            String filename = cmakelists.getName();
-            if (!filename.equals("CMakeLists.txt")) {
+                "Gradle project cmake.path %s is a folder. It must be CMakeLists.txt",
+                cmakelists
+            )
+        } else if (cmakelists.isFile) {
+            val filename = cmakelists.name
+            if (filename != "CMakeLists.txt") {
                 errorln(
-                        "Gradle project cmake.path specifies %s but it must be CMakeLists.txt",
-                        filename);
+                    "Gradle project cmake.path specifies %s but it must be CMakeLists.txt",
+                    filename
+                )
             }
         } else {
-            errorln("Gradle project cmake.path is %s but that file doesn't exist", cmakelists);
+            errorln("Gradle project cmake.path is %s but that file doesn't exist", cmakelists)
         }
     }
 
@@ -89,51 +91,39 @@ abstract class CmakeExternalNativeJsonGenerator extends ExternalNativeJsonGenera
      *
      * @return Returns the combination of STDIO and STDERR from running the process.
      */
-    @NonNull
-    public abstract String executeProcessAndGetOutput(@NonNull CxxAbiModel abi)
-            throws ProcessException, IOException;
+    @Throws(IOException::class, ProcessException::class)
+    abstract fun executeProcessAndGetOutput(abi: CxxAbiModel): String
 
-    @NonNull
-    @Override
-    public String executeProcess(@NonNull CxxAbiModel abi) throws ProcessException, IOException {
-        String output = executeProcessAndGetOutput(abi);
-        return makeCmakeMessagePathsAbsolute(output, getMakefile().getParentFile());
+    public override fun executeProcess(abi: CxxAbiModel): String {
+        val output = executeProcessAndGetOutput(abi)
+        return makeCmakeMessagePathsAbsolute(output, makefile.parentFile)
     }
 
-    @Override
-    void processBuildOutput(@NonNull String buildOutput, @NonNull CxxAbiModel abi) {
+    override fun processBuildOutput(buildOutput: String, abi: CxxAbiModel) {}
+
+    override fun getProcessBuilder(abi: CxxAbiModel): ProcessInfoBuilder {
+        val builder = ProcessInfoBuilder()
+
+        builder.setExecutable(cmake.cmakeExe)
+        val arguments = mutableListOf<CommandLineArgument>()
+        arguments.addAll(abi.getFinalCmakeCommandLineArguments())
+        builder.addArgs(arguments.convertCmakeCommandLineArgumentsToStringList())
+        return builder
     }
 
-    @NonNull
-    @Override
-    ProcessInfoBuilder getProcessBuilder(@NonNull CxxAbiModel abi) {
-        ProcessInfoBuilder builder = new ProcessInfoBuilder();
-
-        builder.setExecutable(cmake.getCmakeExe());
-        List<CommandLineArgument> arguments = Lists.newArrayList();
-        arguments.addAll(getFinalCmakeCommandLineArguments(abi));
-        builder.addArgs(convertCmakeCommandLineArgumentsToStringList(arguments));
-        return builder;
+    override fun getNativeBuildSystem(): NativeBuildSystem {
+        return NativeBuildSystem.CMAKE
     }
 
-    @NonNull
-    @Override
-    public NativeBuildSystem getNativeBuildSystem() {
-        return NativeBuildSystem.CMAKE;
-    }
-
-    @NonNull
-    @Override
-    Map<Abi, File>
-            getStlSharedObjectFiles() { // Search for ANDROID_STL build argument. Process in order / later flags take precedent.
-        Stl stl = null;
-        for (String argument : getBuildArguments()) {
-            argument = argument.replace(" ", "");
+    override fun getStlSharedObjectFiles(): Map<Abi, File> {
+        // Search for ANDROID_STL build argument. Process in order / later flags take precedent.
+        var stl: Stl? = null
+        for (argument in buildArguments.map { it.replace(" ", "") }) {
             if (argument.startsWith("-DANDROID_STL=")) {
-                String stlName = argument.split("=", 2)[1];
-                stl = Stl.fromArgumentName(stlName);
+                val stlName = argument.split("=".toRegex(), 2).toTypedArray()[1]
+                stl = Stl.fromArgumentName(stlName)
                 if (stl == null) {
-                    errorln("Unrecognized STL in arguments: %s", stlName);
+                    errorln("Unrecognized STL in arguments: %s", stlName)
                 }
             }
         }
@@ -147,24 +137,12 @@ abstract class CmakeExternalNativeJsonGenerator extends ExternalNativeJsonGenera
         // https://github.com/android-ndk/ndk/issues/744 wants to change the default for both to
         // c++_shared, but that can't happen until we stop assuming the default does not need to be
         // packaged.
-        if (stl == null) {
-            return Maps.newHashMap();
+        return if (stl == null) {
+            mapOf()
+        } else {
+            variant.module.stlSharedObjectMap.getValue(stl)
+                .filter { e -> getAbis().contains(e.key) }
         }
 
-        return variant.getModule().getStlSharedObjectMap().get(stl).entrySet().stream()
-                .filter(e -> getAbis().contains(e.getKey()))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-    }
-
-    @InputFile
-    @PathSensitive(PathSensitivity.RELATIVE)
-    @Nullable
-    @Optional
-    public File getCmakeSettingsJson() {
-        File cmakeSettings = CxxModuleModelKt.getCmakeSettingsFile(variant.getModule());
-        if (cmakeSettings.isFile()) {
-            return cmakeSettings;
-        }
-        return null;
     }
 }
