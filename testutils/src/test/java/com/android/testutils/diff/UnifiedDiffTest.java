faee4d687554429e027ded5f9af1477eb64eb947
Siva Velusamy <vsiva@google.com>
2019-10-19 19:47:49 -0700

    Use UTF_8 when constructing strings
    Test: updated
    Bug: 142972246
    Change-Id: I6bcbc42aa14c3bd454502b49dda4abc1c3fb449a

--- testutils/src/test/java/com/android/testutils/diff/UnifiedDiffTest.java
+++ testutils/src/test/java/com/android/testutils/diff/UnifiedDiffTest.java
@@ -19,6 +19,7 @@ import static org.junit.Assert.*;
 
 import com.android.testutils.TestUtils;
 import com.android.utils.FileUtils;
+import com.google.common.base.Charsets;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -138,7 +139,9 @@ public class UnifiedDiffTest {
             assertEquals(
                     expected.toPath().relativize(e).toString(),
                     value.toPath().relativize(r).toString());
-            assertEquals(new String(Files.readAllBytes(e)), new String(Files.readAllBytes(r)));
+            assertEquals(
+                    new String(Files.readAllBytes(e), Charsets.UTF_8),
+                    new String(Files.readAllBytes(r), Charsets.UTF_8));
         }
         if (it.length > ex.length) {
             fail("Unexpected file " + it[ex.length] + " was found.");
