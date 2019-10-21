7de785203d380e6fbb76794f04cd2a14a2eaebb2
Fabien Sanglard <sanglardf@google.com>
2019-10-21 15:14:34 -0700

    Zipflinger: Add debugging tools
    To investigate problems with zip or apks, this CL introduces tools
    to verify and map.
    
    Verify: Check that top-down parsing and bottom-up parsing return
    the same list of files. This is a good way to diagnose if a zip
    file contains "holes" since top-down parsing will stop at the
    first hole.
    
    Map: Returns the sorted list of entries (bottom-up built).
    
    Test: N/A
    Bug: N/A
    Change-Id: I2dd008d5c13df85a3cc6729923c91294935cf2ae

---
+++ zipflinger/tools/src/com/android/zipflinger/Mapper.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.zipflinger;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Mapper {
+    public static void main(String[] args) throws IOException {
+        for (String archivePath : args) {
+            map(archivePath);
+        }
+    }
+
+    private static void map(String archivePath) throws IOException {
+        Map<String, Entry> entries = ZipMap.from(new File(archivePath), true).getEntries();
+        ArrayList<Entry> sortedEntries = new ArrayList(entries.values());
+        sortedEntries.sort((e1, e2) -> (int) (e1.getLocation().first - e2.getLocation().last));
+
+        for (Entry e : sortedEntries) {
+            Location l = e.getLocation();
+            System.out.println(l.first + "-" + l.last + " (size=" + l.size() + ") :" + e.getName());
+        }
+    }
+}
---
+++ zipflinger/tools/src/com/android/zipflinger/Verify.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.zipflinger;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class Verify {
+    public static void main(String[] args) throws IOException {
+        for (String archivePath : args) {
+            verifyArchive(new File(archivePath));
+        }
+    }
+
+    private static Map<String, Entry> verifyArchive(File archiveFile) throws IOException {
+        System.out.println("Verifying: '" + archiveFile + "'");
+        HashMap<String, ZipEntry> topDownEntries = new HashMap<>();
+        try (ZipInputStream zis = new ZipInputStream(new FileInputStream(archiveFile))) {
+            byte[] buffer = new byte[10_240];
+            ZipEntry zipEntry = zis.getNextEntry();
+            while (zipEntry != null) {
+                ByteArrayOutputStream fos = new ByteArrayOutputStream();
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+                fos.close();
+                if (!zipEntry.getName().isEmpty()) {
+                    topDownEntries.put(zipEntry.getName(), zipEntry);
+                }
+                zipEntry = zis.getNextEntry();
+            }
+            zis.closeEntry();
+        }
+
+        Map<String, Entry> bottomUpEntries = ZipArchive.listEntries(archiveFile);
+
+        if (topDownEntries.size() != bottomUpEntries.size()) {
+            System.out.println("Bottom-up and Top-down # entries don't match!");
+        } else {
+            System.out.println(
+                    "Both BU and TD parsing found " + topDownEntries.size() + " entries.");
+        }
+
+        for (String name : bottomUpEntries.keySet()) {
+            if (!topDownEntries.keySet().contains(name)) {
+                System.out.println(name + " found in bottom-up but not in top-down");
+            }
+        }
+
+        for (String name : topDownEntries.keySet()) {
+            if (!bottomUpEntries.keySet().contains(name)) {
+                System.out.println(name + " found in top-down but not in bottom-up");
+            }
+        }
+
+        // TODO: Compare sizes and crcs
+        for (String name : bottomUpEntries.keySet()) {
+            Entry e = bottomUpEntries.get(name);
+            ZipEntry o = topDownEntries.get(name);
+            long crc = e.getCrc() & 0xFFFFFFFFL;
+            if (crc != o.getCrc()) {
+                System.out.println("Entry " + name + " crcs don't match");
+            }
+            if (e.getCompressedSize() != o.getCompressedSize()) {
+                System.out.println("Entry " + name + " compressed size don't match");
+            }
+            if (e.getUncompressedSize() != o.getSize()) {
+                System.out.println("Entry " + name + " uncompressed size don't match");
+            }
+        }
+
+        return bottomUpEntries;
+    }
+}
