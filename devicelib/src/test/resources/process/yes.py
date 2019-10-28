9d661741a7a70eb471e833d97341fcc2a326245c
Siva Velusamy <vsiva@google.com>
2019-10-28 12:34:12 -0700

    Delete devicelib
    I worked on this a long time ago, but never finished it. It hasn't
    been adopted and looking at it now, I'd do it differently. So we
    don't need this in our repository anymore.
    
    Bug: 133161845
    Test: removed
    Change-Id: I9f6dbdf07a51646be0295436bb1c67759b3591a7

--- devicelib/src/test/resources/process/yes.py
+++
@@ -1,9 +0,0 @@
-# A simple script that just keeps printing "yes", and never terminates
-import sys
-
-if (len(sys.argv) > 1):
-    sys.stderr.write("Usage: yes")
-    sys.exit(-1)
-
-while True:
-    print "yes"
