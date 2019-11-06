f5cca4759bd843a03e8d5aed2bd0edef6e15f4f4
Takeshi Hagikura <thagikura@google.com>
2019-11-06 17:50:47 +0900

    Add the Kotlin version of TabbedActivity template
    Rewrote the major part of the conversion script and now these files
    were mostly generated by the script (except for the recipe).
    
    Bug: 138544273
    Test: TemplateTest.testCompareTabbedActivityWithKotlin
    Change-Id: I02db5414e40848d29c9b3e5233df4e6974569325

--- wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/WizardTemplateProviderImpl.kt
+++ wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/WizardTemplateProviderImpl.kt
@@ -20,10 +20,11 @@ import com.android.tools.idea.wizard.template.Template
 import com.android.tools.idea.wizard.template.WizardTemplateProvider
 import com.android.tools.idea.wizard.template.impl.basicActivity.basicActivityTemplate
 import com.android.tools.idea.wizard.template.impl.emptyActivity.emptyActivityTemplate
+import com.android.tools.idea.wizard.template.impl.tabbedActivity.tabbedActivityTemplate
 
 /**
  * Implementation of the Android Wizard Template plugin extension point.
  */
 class WizardTemplateProviderImpl : WizardTemplateProvider() {
-  override fun getTemplates(): List<Template> = listOf(basicActivityTemplate, emptyActivityTemplate)
+  override fun getTemplates(): List<Template> = listOf(basicActivityTemplate, emptyActivityTemplate, tabbedActivityTemplate)
 }
---
+++ wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/tabbedActivity/src/app_package/ui/main/placeholderFragmentKt.kt
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tools.idea.wizard.template.impl.tabbedActivity.src.app_package.ui.main
+
+import com.android.tools.idea.wizard.template.getMaterialComponentName
+
+fun placeholderFragmentKt(
+  packageName: String,
+  useAndroidX: Boolean) =
+
+  """package ${packageName}.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import ${getMaterialComponentName("android.support.v4.app.Fragment", useAndroidX)}
+import ${getMaterialComponentName("android.arch.lifecycle.Observer", useAndroidX)}
+import ${getMaterialComponentName("android.arch.lifecycle.ViewModelProviders", useAndroidX)}
+import ${packageName}.R
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+class PlaceholderFragment : Fragment() {
+
+    private lateinit var pageViewModel: PageViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        pageViewModel = ViewModelProviders.of(this).get(PageViewModel::class.java).apply {
+            setIndex(arguments?.getInt(ARG_SECTION_NUMBER) ?: 1)
+        }
+    }
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_main, container, false)
+        val textView: TextView = root.findViewById(R.id.section_label)
+        pageViewModel.text.observe(this, Observer<String> {
+            textView.text = it
+        })
+        return root
+    }
+
+    companion object {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private const val ARG_SECTION_NUMBER = "section_number"
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        @JvmStatic
+        fun newInstance(sectionNumber: Int): PlaceholderFragment {
+            return PlaceholderFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(ARG_SECTION_NUMBER, sectionNumber)
+                }
+            }
+        }
+    }
+}"""
