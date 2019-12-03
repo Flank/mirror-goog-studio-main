30ab2e05859376573ec7f910fc473c02e4bad5ab
Tom Renn <tomrenn@google.com>
2019-12-03 13:21:11 -0800

    Add --remote_download_minimal flag for config=remote
    This may lead to speed improvements for RBE usage. Details
    of this flag are described in the following bazel blog post
    https://blog.bazel.build/2019/05/07/builds-without-bytes.html
    
    BUG: 139655502
    Test: None
    Change-Id: Icf8bf9187e4b340a2ff59819b4f356a4121ebad6

--- bazel/studio_coverage.sh
+++ bazel/studio_coverage.sh
@@ -23,6 +23,7 @@ fi
   test \
   --keep_going \
   --config=remote \
+  --remote_download_outputs=toplevel \
   --invocation_id=${invocation_id} \
   ${auth_options} \
   --test_tag_filters=-no_linux,-no_test_linux,coverage-test,-perfgate \
@@ -43,6 +44,7 @@ fi
 "${script_dir}/bazel" \
   build \
   --config=remote \
+  --remote_download_outputs=toplevel \
   --invocation_id=${report_invocation_id} \
   ${auth_options} \
   -- \
