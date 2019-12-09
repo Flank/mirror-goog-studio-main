3b5b4d1063360c7cddba644c32f33b8cd3ee21d5
Chris Warrington <cmw@google.com>
2019-12-09 08:18:39 +0000

    Split out databinding incremental tests.
    To try and isolate the hangs, these tests have build system invocations
    that seem to hang (or at least don't finish within the 5 minute timeout)
    about 1% of the time.
    
    Bug: 131240943
    Test: Refactoring tests.
    Change-Id: I16d3a7173e564b27ef8ac03ab57c8233c46a28cf

--- build-system/integration-test/databinding/incremental/src/test/java/com/android/build/gradle/integration/databinding/incremental/DataBindingIncrementalTest.java
+++ build-system/integration-test/databinding/incremental/src/test/java/com/android/build/gradle/integration/databinding/incremental/DataBindingIncrementalTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 The Android Open Source Project
+ * Copyright (C) 2019 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.build.gradle.integration.databinding;
+package com.android.build.gradle.integration.databinding.incremental;
 
 import static com.android.build.gradle.integration.common.fixture.GradleTestProject.ApkType.DEBUG;
 import static com.android.build.gradle.integration.common.truth.TruthHelper.assertThat;
