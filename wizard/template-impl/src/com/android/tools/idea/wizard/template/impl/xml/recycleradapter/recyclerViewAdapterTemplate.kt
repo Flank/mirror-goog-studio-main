6f20d3d59a1bf830c73152d1772aaf9488d3329e
Isaac Chai <ichai@google.com>
2019-12-18 16:15:46 +0900

    Create a template for recycler view.
    Cl focused on creating a recycler view specific wizards.
    It is similar to the existing Fragment (list) setup wizard
    but focuses on recycler view specific boiler plate codes.
    
    Also it uses the newer format templates, that are kotlin
    based rather than IntelliJ Components/ plugin.
    
    To be used with ag/9936164
    
    Bug: 146472440
    Test: Manual test
    
    Change-Id: I163a020f0220decab81758b503ea2a15f2fb731c

---
+++ wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/xml/recycleradapter/recyclerViewAdapterTemplate.kt
@@ -0,0 +1,181 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.wizard.template.impl.xml.recycleradapter
+
+import com.android.tools.idea.wizard.template.Category
+import com.android.tools.idea.wizard.template.Constraint.CLASS
+import com.android.tools.idea.wizard.template.Constraint.LAYOUT
+import com.android.tools.idea.wizard.template.Constraint.NONEMPTY
+import com.android.tools.idea.wizard.template.Constraint.PACKAGE
+import com.android.tools.idea.wizard.template.Constraint.UNIQUE
+import com.android.tools.idea.wizard.template.EnumWidget
+import com.android.tools.idea.wizard.template.FormFactor
+import com.android.tools.idea.wizard.template.LanguageWidget
+import com.android.tools.idea.wizard.template.ModuleTemplateData
+import com.android.tools.idea.wizard.template.PackageNameWidget
+import com.android.tools.idea.wizard.template.TemplateData
+import com.android.tools.idea.wizard.template.TextFieldWidget
+import com.android.tools.idea.wizard.template.WizardUiContext
+import com.android.tools.idea.wizard.template.enumParameter
+import com.android.tools.idea.wizard.template.extractLetters
+import com.android.tools.idea.wizard.template.impl.fragments.listFragment.ColumnCount
+import com.android.tools.idea.wizard.template.stringParameter
+import com.android.tools.idea.wizard.template.template
+import java.io.File
+
+@Suppress("EnumEntryName", "Unused")
+enum class ColumnCount {
+  `1 (List)`,
+  `2 (Grid)`,
+  `3`,
+  `4`
+}
+
+var currentRecyclerViewLayout = "recycler_view_layout"
+val recyclerViewAdapterFragmentTemplate
+  get() = template {
+    revision = 1
+    name = "Adapter with Fragment"
+    description = "Creates a new empty fragment containing a list that can be rendered as a grid. Compatible back to API level 14."
+    minApi = 14
+    minBuildApi = 14
+
+    category = Category.Fragment
+    formFactor = FormFactor.Mobile
+    screens = listOf(WizardUiContext.FragmentGallery, WizardUiContext.MenuEntry)
+
+    val packageName = stringParameter {
+      name = "Package name"
+      default = "com.mycompany.myapp"
+      constraints = listOf(PACKAGE)
+    }
+
+    val objectKind = stringParameter {
+      name = "Object Kind"
+      default = "Item"
+      help = "Other examples are Person, Book, etc."
+      constraints = listOf(NONEMPTY)
+    }
+
+    val fragmentClass = stringParameter {
+      name = "Fragment class name"
+      default = "ItemFragment"
+      constraints = listOf(NONEMPTY, CLASS, UNIQUE)
+      suggest = { "${extractLetters(objectKind.value)}Fragment" }
+    }
+
+    val columnCount = enumParameter<ColumnCount> {
+      name = "Column Count"
+      default = ColumnCount.`1 (List)`
+      help = "The number of columns in the grid"
+    }
+
+    val recyclerViewItem = stringParameter {
+      name = "Object content layout file name"
+      default = "recycler_item"
+      constraints = listOf(LAYOUT, NONEMPTY, UNIQUE)
+      //suggest = { fragmentToLayout(fragmentClass.value) }
+    }
+
+    val recyclerViewLayout = stringParameter {
+      name = "List layout file name"
+      default = "recycler_view_layout"
+      constraints = listOf(LAYOUT, NONEMPTY, UNIQUE)
+      suggest = { currentRecyclerViewLayout }
+    }
+
+    val adapterClassName = stringParameter {
+      name = "Adapter class name"
+      default = "MyItemRecyclerViewAdapter"
+      constraints = listOf(NONEMPTY, CLASS, UNIQUE)
+      suggest = { "My${extractLetters(objectKind.value)}RecyclerViewAdapter" }
+    }
+
+    widgets(
+      PackageNameWidget(packageName),
+      TextFieldWidget(objectKind),
+      TextFieldWidget(fragmentClass),
+      EnumWidget(columnCount),
+      TextFieldWidget(recyclerViewItem),
+      TextFieldWidget(recyclerViewLayout),
+      TextFieldWidget(adapterClassName),
+      LanguageWidget()
+    )
+
+    thumb { File("template_list_fragment.png") }
+
+    recipe = { data: TemplateData ->
+      recyclerViewAdapterRecipe(data as ModuleTemplateData, packageName.value,
+        fragmentClass.value, columnCount.value,
+                                recyclerViewItem.value, recyclerViewLayout.value, adapterClassName.value)
+    }
+  }
+
+val recyclerViewAdapterNoFragmentTemplate
+  get() = template {
+    revision = 1
+    name = "Adapter Only"
+    description = "Generates RecyclerView adapter and other boilerplate codes. Compatible back to API level 14."
+    minApi = 14
+    minBuildApi = 14
+
+    category = Category.XML
+    formFactor = FormFactor.Mobile
+    screens = listOf(WizardUiContext.FragmentGallery, WizardUiContext.MenuEntry)
+
+    val packageName = stringParameter {
+      name = "Package name"
+      default = "com.mycompany.myapp"
+      constraints = listOf(PACKAGE)
+    }
+
+    val objectKind = stringParameter {
+      name = "Object Kind"
+      default = "Item"
+      help = "Other examples are Person, Book, etc."
+      constraints = listOf(NONEMPTY)
+    }
+
+    val recyclerViewItem = stringParameter {
+      name = "Object content layout file name"
+      default = "recycler_item"
+      constraints = listOf(LAYOUT, NONEMPTY, UNIQUE)
+    }
+
+    val adapterClassName = stringParameter {
+      name = "Adapter class name"
+      default = "MyItemRecyclerViewAdapter"
+      constraints = listOf(NONEMPTY, CLASS, UNIQUE)
+      suggest = { "My${extractLetters(objectKind.value)}RecyclerViewAdapter" }
+    }
+
+    widgets(
+      PackageNameWidget(packageName),
+      TextFieldWidget(objectKind),
+      TextFieldWidget(recyclerViewItem),
+      TextFieldWidget(adapterClassName),
+      LanguageWidget()
+    )
+
+    thumb { File("template_list_fragment.png") }
+
+    recipe = { data: TemplateData ->
+      recyclerViewAdapterRecipe(data as ModuleTemplateData, packageName.value,
+                                null, null,
+                                //fragmentClass.value, columnCount.value,
+                                recyclerViewItem.value, currentRecyclerViewLayout, adapterClassName.value)
+    }
+  }
