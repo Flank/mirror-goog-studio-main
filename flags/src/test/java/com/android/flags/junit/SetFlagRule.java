ebaa63e3b7a7279d419df492e98b9e4874d35de8
Joe Baker-Malone <jbakermalone@google.com>
2019-12-27 10:39:32 -0800

    Add a test rule to set and then unset a flag
    Test: this is a test framework change
    Bug: none
    Change-Id: I15d222728284da9610825eff520a9c008870fbb8

---
+++ flags/src/test/java/com/android/flags/junit/SetFlagRule.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.flags.junit;
+
+import com.android.flags.Flag;
+import org.junit.Rule;
+
+/**
+ * {@link Rule} which will ensure that a target {@link Flag} is reset back to its default value
+ * after every test.
+ *
+ * <p>If you have code behind a flag, then that means your code can exhibit different behavior
+ * depending on the flag's value. The easiest way to test all paths is to have multiple tests, with
+ * each test changing the flag's value explicitly at the beginning of it.
+ *
+ * <pre>
+ *   public class MyTest {
+ *     @Rule
+ *     public RestoreFlagRule{Boolean} myRestoreFlag = new RestoreFlagRule{}(StudioFlags.MY_FLAG);
+ *
+ *     @Test
+ *     public void testFirstBranch() throws Exception {
+ *       StudioFlags.MY_FLAG.override(false);
+ *       ...
+ *     }
+ *
+ *     @Test
+ *     public void testSecondBranch() throws Exception {
+ *       StudioFlags.MY_FLAG.override(true);
+ *       ...
+ *     }
+ *   }
+ * </pre>
+ */
+public class SetFlagRule<T> extends RestoreFlagRule<T> {
+    private final Flag<T> myFlag;
+    private final T myValue;
+
+    public SetFlagRule(Flag<T> flag, T value) {
+        super(flag);
+        myFlag = flag;
+        myValue = value;
+    }
+
+    @Override
+    protected void before() {
+        myFlag.override(myValue);
+    }
+}
