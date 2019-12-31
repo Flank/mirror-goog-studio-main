c25cfc17a620b6a056a110e61cb7a7da18b84fea
Joe Baker-Malone <jbakermalone@google.com>
2019-12-30 17:21:40 -0800

    Convert FakeClientBuilder to java
    Test: this is a test-only change
    Bug: none
    Change-Id: I5c83e01956dfa584b573ec68879557ca65351eaf

---
+++ ddmlib/src/test/java/com/android/ddmlib/FakeClientBuilder.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.ddmlib;
+
+import static com.android.ddmlib.JdwpPacket.JDWP_HEADER_LEN;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+
+import com.android.ddmlib.jdwp.JdwpInterceptor;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import org.mockito.ArgumentMatcher;
+
+public class FakeClientBuilder {
+    Client client = mock(Client.class);
+
+    public FakeClientBuilder registerResponse(
+            ArgumentMatcher<JdwpPacket> request, int responseType, ByteBuffer payload)
+            throws IOException {
+        payload.rewind();
+        int size = payload.remaining();
+        ByteBuffer rawBuf = ChunkHandler.allocBuffer(size + JDWP_HEADER_LEN);
+        JdwpPacket returnPacket = new JdwpPacket(rawBuf);
+        ByteBuffer buf = ChunkHandler.getChunkDataBuf(rawBuf);
+        buf.put(payload);
+        ChunkHandler.finishChunkPacket(returnPacket, responseType, size);
+
+        doAnswer(
+                        invocation ->
+                                invocation
+                                        .<JdwpInterceptor>getArgument(1)
+                                        .intercept(client, returnPacket))
+                .when(client)
+                .send(argThat(request), any());
+        return this;
+    }
+
+    public Client build() {
+        return client;
+    }
+}
