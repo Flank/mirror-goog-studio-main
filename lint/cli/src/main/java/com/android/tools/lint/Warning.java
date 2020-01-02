6538304eaa5162a4c3364734a2c1ba328d9273ad
Matthew Gharrity <gharrma@google.com>
2020-01-02 12:17:31 -0800

    Simplify Warning#compareTo and #equals
    ~100 lines removed.
    
    Bug: n/a
    Test: existing (WarningTest)
    Change-Id: I58fbf6d6cfabe23d70ee55fd5a13495403d0e571

--- lint/cli/src/main/java/com/android/tools/lint/Warning.java
+++ lint/cli/src/main/java/com/android/tools/lint/Warning.java
@@ -23,15 +23,17 @@ import com.android.tools.lint.client.api.LintClient;
 import com.android.tools.lint.detector.api.Issue;
 import com.android.tools.lint.detector.api.LintFix;
 import com.android.tools.lint.detector.api.Location;
+import com.android.tools.lint.detector.api.Position;
 import com.android.tools.lint.detector.api.Project;
 import com.android.tools.lint.detector.api.Severity;
-import com.google.common.base.Objects;
+import com.google.common.collect.ComparisonChain;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -66,154 +68,45 @@ public class Warning implements Comparable<Warning> {
         this.project = project;
     }
 
-    // ---- Implements Comparable<Warning> ----
-    @SuppressWarnings({"VariableNotUsedInsideIf", "ConstantConditions"})
     @Override
-    public int compareTo(@NonNull Warning other) {
-        // Sort by category, then by priority, then by id,
-        // then by file, then by line
-        int categoryDelta = issue.getCategory().compareTo(other.issue.getCategory());
-        if (categoryDelta != 0) {
-            return categoryDelta;
-        }
-        // DECREASING priority order
-        int priorityDelta = other.issue.getPriority() - issue.getPriority();
-        if (priorityDelta != 0) {
-            return priorityDelta;
-        }
-        String id1 = issue.getId();
-        String id2 = other.issue.getId();
-        assert id1 != null;
-        assert id2 != null;
-        int idDelta = id1.compareTo(id2);
-        if (idDelta != 0) {
-            return idDelta;
-        }
-        if (file != null) {
-            if (other.file != null) {
-                int fileDelta = file.getName().compareTo(other.file.getName());
-                if (fileDelta != 0) {
-                    return fileDelta;
-                }
-            } else {
-                return -1;
-            }
-        } else if (other.file != null) {
-            return 1;
-        }
-        if (line != other.line) {
-            return line - other.line;
-        }
+    public int compareTo(@NonNull Warning o) {
+        String fileName1 = file != null ? file.getName() : null;
+        String fileName2 = o.file != null ? o.file.getName() : null;
 
-        int delta = message.compareTo(other.message);
-        if (delta != 0) {
-            return delta;
-        }
-
-        if (file != null) {
-            if (other.file != null) {
-                delta = file.compareTo(other.file);
-                if (delta != 0) {
-                    return delta;
-                }
-            } else {
-                return -1;
-            }
-        } else if (other.file != null) {
-            return 1;
-        }
-
-        // This handles the case where you have a huge XML document without hewlines,
-        // such that all the errors end up on the same line.
-        if (location != null
-                && other.location != null
-                && location.getStart() != null
-                && other.location.getStart() != null) {
-            delta = location.getStart().getColumn() - other.location.getStart().getColumn();
-            if (delta != 0) {
-                return delta;
-            }
-        }
+        Position start1 = location != null ? location.getStart() : null;
+        Position start2 = o.location != null ? o.location.getStart() : null;
+        Integer col1 = start1 != null ? start1.getColumn() : null;
+        Integer col2 = start2 != null ? start2.getColumn() : null;
 
         Location secondary1 = location != null ? location.getSecondary() : null;
-        File secondaryFile1 = secondary1 != null ? secondary1.getFile() : null;
-        Location secondary2 = other.location != null ? other.location.getSecondary() : null;
-        File secondaryFile2 = secondary2 != null ? secondary2.getFile() : null;
-        if (secondaryFile1 != null) {
-            if (secondaryFile2 != null) {
-                delta = secondaryFile1.compareTo(secondaryFile2);
-                //noinspection RedundantIfStatement
-                if (delta != 0) {
-                    return delta;
-                }
-            } else {
-                return -1;
-            }
-        } else if (secondaryFile2 != null) {
-            return 1;
-        }
+        Location secondary2 = o.location != null ? o.location.getSecondary() : null;
+        File secondFile1 = secondary1 != null ? secondary1.getFile() : null;
+        File secondFile2 = secondary2 != null ? secondary2.getFile() : null;
 
-        return 0;
+        return ComparisonChain.start()
+                .compare(issue.getCategory(), o.issue.getCategory())
+                .compare(issue.getPriority(), o.issue.getPriority(), Comparator.reverseOrder())
+                .compare(issue.getId(), o.issue.getId())
+                .compare(fileName1, fileName2, Comparator.nullsLast(Comparator.naturalOrder()))
+                .compare(line, o.line)
+                .compare(message, o.message)
+                .compare(file, o.file, Comparator.nullsLast(Comparator.naturalOrder()))
+                // This handles the case where you have a huge XML document without newlines,
+                // such that all the errors end up on the same line.
+                .compare(col1, col2, Comparator.nullsLast(Comparator.naturalOrder()))
+                .compare(secondFile1, secondFile2, Comparator.nullsLast(Comparator.naturalOrder()))
+                .result();
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) {
+    public boolean equals(Object other) {
+        if (this == other) {
             return true;
         }
-        if (o == null || getClass() != o.getClass()) {
+        if (other == null || getClass() != other.getClass()) {
             return false;
         }
-
-        Warning warning = (Warning) o;
-
-        if (line != warning.line) {
-            return false;
-        }
-        if (file != null ? !file.equals(warning.file) : warning.file != null) {
-            return false;
-        }
-        if (!issue.getCategory().equals(warning.issue.getCategory())) {
-            return false;
-        }
-        if (issue.getPriority() != warning.issue.getPriority()) {
-            return false;
-        }
-        if (!issue.getId().equals(warning.issue.getId())) {
-            return false;
-        }
-        //noinspection RedundantIfStatement
-        if (!message.equals(warning.message)) {
-            return false;
-        }
-
-        Location secondary1 = location != null ? location.getSecondary() : null;
-        Location secondary2 = warning.location != null ? warning.location.getSecondary() : null;
-        if (secondary1 != null) {
-            if (secondary2 != null) {
-                if (!Objects.equal(secondary1.getFile(), secondary2.getFile())) {
-                    return false;
-                }
-            } else {
-                return false;
-            }
-        } else //noinspection VariableNotUsedInsideIf
-        if (secondary2 != null) {
-            return false;
-        }
-
-        // This handles the case where you have a huge XML document without newlines,
-        // such that all the errors end up on the same line.
-        //noinspection RedundantIfStatement
-        if (location != null
-                && warning.location != null
-                && location.getStart() != null
-                && warning.location.getStart() != null
-                && location.getStart().getColumn() != warning.location.getStart().getColumn()) {
-            return false;
-        }
-
-        return true;
+        return this.compareTo((Warning) other) == 0;
     }
 
     @Override
