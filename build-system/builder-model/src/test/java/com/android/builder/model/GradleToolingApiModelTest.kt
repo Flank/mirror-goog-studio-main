59d727de00e95d525d37e5d12bcae4607f6fb4a2
Chris Warrington <cmw@google.com>
2020-01-14 14:27:52 +0000

    New DSL: Preparatory work to unblock kotlin conversion.
    Some DSL classes implement tooling model interfaces,
    and those will need to be converted to kotlin not to prevent the use of
    kotlin fields.
    
    Bug: 140406102
    Test: Added GradleToolingApiModelTest
    Change-Id: I03823d4fd7b7fffc877b99a1a4fb4edcee8006b9

---
+++ build-system/builder-model/src/test/java/com/android/builder/model/GradleToolingApiModelTest.kt
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.builder.model
+
+import com.android.testutils.ApiTester
+import com.google.common.collect.ImmutableSet
+import com.google.common.io.Resources
+import com.google.common.reflect.ClassPath
+import org.junit.Test
+import java.io.IOException
+
+class GradleToolingApiModelTest {
+    @Test
+    @Throws(Exception::class)
+    fun stableApiElements() {
+        getApiTester().checkApiElements()
+    }
+
+    companion object {
+        private val EXCLUDED_CLASSES = ImmutableSet.of<Class<*>>(
+            GradleToolingApiModelTest::class.java,
+            TestOptionsTest::class.java
+        )
+        private val STABLE_API_URL = Resources.getResource(
+            GradleToolingApiModelTest::class.java, "tooling-api-model-api.txt"
+        )
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            getApiTester().updateFile(
+                "tools/base/build-system/builder-model/src/test/resources/com/android/builder/model/"
+            )
+        }
+
+        private fun getApiTester(): ApiTester {
+
+            val classes =
+                ClassPath
+                    .from(GradleToolingApiModelTest::class.java.classLoader)
+                    .getTopLevelClassesRecursive("com.android.builder.model")
+                    .filter { !EXCLUDED_CLASSES.contains(it.load()) }
+            return ApiTester(
+                "Android Gradle Plugin Tooling Model API.",
+                classes,
+                ApiTester.Filter.ALL,
+                "The Android Gradle Plugin Tooling Model API."
+                        + " API has changed, either revert "
+                        + "the api change or re-run GradleToolingApiModelTest.main[] from the IDE "
+                        + "to update the API file.\n"
+                        + "GradleToolingApiModelTest will apply the following changes if run:\n",
+                STABLE_API_URL
+            )
+        }
+    }
+}
