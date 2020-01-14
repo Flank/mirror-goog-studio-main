1d8d46dce1174c3ca4a60505d4968b75d6eee81f
Amr Afifiy <amrmahmoud@google.com>
2020-01-14 17:11:03 +0000

    New DSL: Convert SigningConfig to kt
    Bug: 140406102
    Test: existing DSL tests
    Change-Id: If423fc6628f768c16516c5248109e46a6c8695f4

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/dsl/SigningConfig.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/dsl/SigningConfig.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 The Android Open Source Project
+ * Copyright (C) 2020 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,134 +14,40 @@
  * limitations under the License.
  */
 
-package com.android.build.gradle.internal.dsl;
+package com.android.build.gradle.internal.dsl
 
-import com.android.annotations.NonNull;
-import com.android.builder.signing.DefaultSigningConfig;
-import com.google.common.base.MoreObjects;
-import java.io.File;
-import java.io.Serializable;
-import javax.inject.Inject;
-import org.gradle.api.Named;
+import com.android.builder.signing.DefaultSigningConfig
+import com.google.common.base.MoreObjects
+import org.gradle.api.Named
+import java.io.Serializable
+import javax.inject.Inject
 
-/** DSL object for configuring signing configs. */
-public class SigningConfig extends DefaultSigningConfig
-implements Serializable, Named, com.android.build.api.dsl.SigningConfig {
-    private static final long serialVersionUID = 1L;
+open class SigningConfig @Inject constructor(name: String) : DefaultSigningConfig(name),
+    Serializable, Named, com.android.build.api.dsl.SigningConfig {
 
-    /**
-     * Creates a SigningConfig with a given name.
-     *
-     * @param name the name of the signingConfig.
-     */
-    @Inject
-    public SigningConfig(@NonNull String name) {
-        super(name);
+    fun initWith(that: com.android.builder.model.SigningConfig): SigningConfig? {
+        setStoreFile(that.storeFile)
+        setStorePassword(that.storePassword)
+        setKeyAlias(that.keyAlias)
+        setKeyPassword(that.keyPassword)
+        internalSetV1SigningEnabled(that.isV1SigningEnabled)
+        internalSetV2SigningEnabled(that.isV2SigningEnabled)
+        setStoreType(that.storeType)
+        return this
     }
 
-    public SigningConfig initWith(com.android.builder.model.SigningConfig that) {
-        setStoreFile(that.getStoreFile());
-        setStorePassword(that.getStorePassword());
-        setKeyAlias(that.getKeyAlias());
-        setKeyPassword(that.getKeyPassword());
-        internalSetV1SigningEnabled(that.isV1SigningEnabled());
-        internalSetV2SigningEnabled(that.isV2SigningEnabled());
-        setStoreType(that.getStoreType());
-        return this;
-    }
-
-    /**
-     * Store file used when signing.
-     *
-     * <p>See <a href="http://developer.android.com/tools/publishing/app-signing.html">
-     * Signing Your Applications</a>
-     */
-    @Override
-    public File getStoreFile() {
-        return super.getStoreFile();
-    }
-
-    /**
-     * Store password used when signing.
-     *
-     * <p>See <a href="http://developer.android.com/tools/publishing/app-signing.html">
-     * Signing Your Applications</a>
-     */
-    @Override
-    public String getStorePassword() {
-        return super.getStorePassword();
-    }
-
-    /**
-     * Key alias used when signing.
-     *
-     * <p>See <a href="http://developer.android.com/tools/publishing/app-signing.html">
-     * Signing Your Applications</a>
-     */
-    @Override
-    public String getKeyAlias() {
-        return super.getKeyAlias();
-    }
-
-    /**
-     * Key password used when signing.
-     *
-     * <p>See <a href="http://developer.android.com/tools/publishing/app-signing.html">
-     * Signing Your Applications</a>
-     */
-    @Override
-    public String getKeyPassword() {
-        return super.getKeyPassword();
-    }
-
-    /**
-     * Store type used when signing.
-     *
-     * <p>See <a href="http://developer.android.com/tools/publishing/app-signing.html">
-     * Signing Your Applications</a>
-     */
-    @Override
-    public String getStoreType() {
-        return super.getStoreType();
-    }
-
-    /**
-     * Whether signing using JAR Signature Scheme (aka v1 signing) is enabled.
-     *
-     * <p>See <a href="https://developer.android.com/tools/publishing/app-signing.html">
-     * Signing Your Applications</a>
-     */
-    @Override
-    public boolean isV1SigningEnabled() {
-        return super.isV1SigningEnabled();
-    }
-
-    /**
-     * Whether signing using APK Signature Scheme v2 (aka v2 signing) is enabled.
-     *
-     * <p>See <a href="https://developer.android.com/tools/publishing/app-signing.html">
-     * Signing Your Applications</a>
-     */
-    @Override
-    public boolean isV2SigningEnabled() {
-        return super.isV2SigningEnabled();
-    }
-
-    @Override
-    public String toString() {
+    override fun toString(): String {
         return MoreObjects.toStringHelper(this)
-            .add("name", getName())
-            .add(
-                "storeFile",
-                getStoreFile() != null ? getStoreFile().getAbsolutePath() : "null")
-        .add("storePassword", getStorePassword())
-        .add("keyAlias", getKeyAlias())
-        .add("keyPassword", getKeyPassword())
-        .add("storeType", getStoreType())
-        .add("v1SigningEnabled", isV1SigningEnabled())
-        .add("v2SigningEnabled", isV2SigningEnabled())
-        .add("v1SigningConfigured", isV1SigningConfigured())
-        .add("v2SigningConfigured", isV2SigningConfigured())
-        .toString();
+            .add("name", name)
+            .add("storeFile", storeFile?.absolutePath ?: "null")
+            .add("storePassword", storePassword)
+            .add("keyAlias", keyAlias)
+            .add("keyPassword", keyPassword)
+            .add("storeType", storeType)
+            .add("v1SigningEnabled", isV1SigningEnabled)
+            .add("v2SigningEnabled", isV2SigningEnabled)
+            .add("v1SigningConfigured", isV1SigningConfigured)
+            .add("v2SigningConfigured", isV2SigningConfigured)
+            .toString()
     }
 }
