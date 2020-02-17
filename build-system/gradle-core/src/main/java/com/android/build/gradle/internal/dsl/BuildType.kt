3b43359b6de8b544946ccef9f79bd710a1dac4a7
Amr Afifiy <amrmahmoud@google.com>
2020-02-17 17:47:08 +0000

    [New DSL] Use ExternalNativeBuildOptions interface
    Bug: 140406102
    Test: existing dsl test
    Change-Id: I6bc653cc66800989680368dad2460e2fc20233df

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/dsl/BuildType.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/dsl/BuildType.kt
@@ -132,9 +132,14 @@ open class BuildType @Inject constructor(
     }
 
     override val ndkConfig: NdkOptions = dslServices.newInstance(NdkOptions::class.java)
-    override val externalNativeBuildOptions: ExternalNativeBuildOptions = dslServices.newInstance(
-        ExternalNativeBuildOptions::class.java, dslServices
-    )
+
+    override val externalNativeBuild: ExternalNativeBuildOptions =
+        dslServices.newInstance(ExternalNativeBuildOptions::class.java, dslServices)
+
+    override val externalNativeBuildOptions: ExternalNativeBuildOptions
+        get() {
+            return this.externalNativeBuild
+        }
 
     private val _postProcessing: PostProcessingBlock = dslServices.newInstance(
         PostProcessingBlock::class.java,
@@ -543,6 +548,10 @@ open class BuildType @Inject constructor(
     fun externalNativeBuild(action: Action<ExternalNativeBuildOptions>): ExternalNativeBuildOptions {
         action.execute(externalNativeBuildOptions)
         return externalNativeBuildOptions
+    }
+
+    override fun externalNativeBuild(action: com.android.build.api.dsl.ExternalNativeBuildOptions.() -> Unit) {
+        action.invoke(externalNativeBuild)
     }
 
     /**
