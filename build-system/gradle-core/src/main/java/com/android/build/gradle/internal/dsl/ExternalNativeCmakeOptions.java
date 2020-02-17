5ef9f7416ee659fbece2162b78c3f2ae95471047
Amr Afifiy <amrmahmoud@google.com>
2020-02-17 14:56:34 +0000

    [New DSL] Add ExternalNativeCmakeOptions interface
    Bug: 140406102
    Test: existing dsl tests
    Change-Id: Id07f11378d9d80b124ab54d3e5af8a73d2674573

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/dsl/ExternalNativeCmakeOptions.java
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/dsl/ExternalNativeCmakeOptions.java
@@ -24,15 +24,9 @@ import java.util.List;
 import java.util.Set;
 import javax.inject.Inject;
 
-/**
- * DSL object for per-variant CMake options, such as CMake arguments and compiler flags.
- *
- * <p>To learn more about including CMake builds to your Android Studio projects, read <a
- * href="https://developer.android.com/studio/projects/add-native-code.html">Add C and C++ Code to
- * Your Project</a>. You can also read more documentation about <a
- * href="https://developer.android.com/ndk/guides/cmake.html">the Android CMake toolchain</a>.
- */
-public class ExternalNativeCmakeOptions implements CoreExternalNativeCmakeOptions {
+public class ExternalNativeCmakeOptions
+        implements CoreExternalNativeCmakeOptions,
+                com.android.build.api.dsl.ExternalNativeCmakeOptions {
 
     @NonNull
     private final List<String> arguments = Lists.newArrayList();
@@ -48,78 +42,93 @@ public class ExternalNativeCmakeOptions implements CoreExternalNativeCmakeOption
     @Inject
     public ExternalNativeCmakeOptions() {}
 
-    /** {@inheritDoc} */
     @NonNull
     @Override
     public List<String> getArguments() {
         return arguments;
     }
 
+    @Override
     public void setArguments(@NonNull List<String> arguments) {
         this.arguments.addAll(arguments);
     }
 
-    public void arguments(@NonNull String ...arguments) {
+    @Override
+    public void arguments(@NonNull String... arguments) {
         Collections.addAll(this.arguments, arguments);
     }
 
-    /** {@inheritDoc} */
     @NonNull
     @Override
-    public List<String> getcFlags() {
+    public List<String> getCFlags() {
         return cFlags;
     }
 
-    public void setcFlags(@NonNull List<String> flags) {
-        this.cFlags.addAll(flags);
+    @Override
+    public void setCFlags(@NonNull List<String> cFlags) {
+        this.cFlags.addAll(cFlags);
     }
 
-    public void cFlags(@NonNull String ...flags) {
+    @NonNull
+    @Override
+    public List<String> getcFlags() {
+        return getCFlags();
+    }
+
+    public void setcFlags(@NonNull List<String> flags) {
+        setCFlags(flags);
+    }
+
+    @Override
+    public void cFlags(@NonNull String... flags) {
         Collections.addAll(this.cFlags, flags);
     }
 
-    /** {@inheritDoc} */
     @NonNull
     @Override
     public List<String> getCppFlags() {
         return cppFlags;
     }
 
+    @Override
     public void setCppFlags(@NonNull List<String> flags) {
         this.cppFlags.addAll(flags);
     }
 
-    public void cppFlags(@NonNull String ...flags) {
+    @Override
+    public void cppFlags(@NonNull String... flags) {
         Collections.addAll(this.cppFlags, flags);
     }
 
-    /** {@inheritDoc} */
     @NonNull
     @Override
     public Set<String> getAbiFilters() {
         return abiFilters;
     }
 
+    @Override
     public void setAbiFilters(@NonNull Set<String> abiFilters) {
         this.abiFilters.addAll(abiFilters);
     }
 
-    public void abiFilters(@NonNull String ...abiFilters) {
+    @Override
+    public void abiFilters(@NonNull String... abiFilters) {
         Collections.addAll(this.abiFilters, abiFilters);
     }
 
-    /** {@inheritDoc} */
     @NonNull
     @Override
     public Set<String> getTargets() {
         return targets;
     }
 
+    @Override
     public void setTargets(@NonNull Set<String> targets) {
         this.targets.addAll(targets);
     }
 
-    public void targets(@NonNull String ...targets) {
+    @Override
+    public void targets(@NonNull String... targets) {
         Collections.addAll(this.targets, targets);
     }
 
