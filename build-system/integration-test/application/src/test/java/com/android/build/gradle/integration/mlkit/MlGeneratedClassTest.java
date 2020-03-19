8b2735435d7b6f4968d9d64992ed64bd98eb88cc
Lei Yu <jackqdyulei@google.com>
2020-03-19 12:48:21 -0700

    Improve mlkit AGP test to verify generated methods
    1. Verify in apk it has model class with valid API
    2. Verify generated source code is correct, even
    for implementation.
    
    Bug: 151953847
    Test: This is test
    Change-Id: Ic5d5a329bd3e27670582cd5212c4d381a8dbadf6

--- build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/mlkit/MlGeneratedClassTest.java
+++ build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/mlkit/MlGeneratedClassTest.java
@@ -16,22 +16,33 @@
 
 package com.android.build.gradle.integration.mlkit;
 
-import static com.android.build.gradle.integration.common.truth.TruthHelper.assertThatApk;
-
 import com.android.build.gradle.integration.common.fixture.GradleTestProject;
 import com.android.build.gradle.integration.common.fixture.app.MinimalSubProject;
 import com.android.build.gradle.integration.common.utils.TestFileUtils;
 import com.android.testutils.TestUtils;
 import com.android.testutils.apk.Apk;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.truth.Truth;
 import java.io.File;
 import java.io.IOException;
+import java.util.Set;
 import org.apache.commons.io.FileUtils;
+import org.jf.dexlib2.iface.reference.MethodReference;
+import org.jf.dexlib2.immutable.ImmutableMethod;
+import org.jf.dexlib2.immutable.util.ParamUtil;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 /** Assemble tests for mlkit generated model class. */
 public class MlGeneratedClassTest {
+
+    private static final String MODEL = "Lcom/android/app/ml/Model;";
+    private static final String MODEL_OUTPUT = "Lcom/android/app/ml/Model$Outputs;";
+    private static final String TENSOR_IMAGE = "Lorg/tensorflow/lite/support/image/TensorImage;";
+    private static final String TENSOR_BUFFER =
+            "Lorg/tensorflow/lite/support/tensorbuffer/TensorBuffer;";
+    private static final String TENSOR_LABEL = "Lorg/tensorflow/lite/support/label/TensorLabel;";
 
     @Rule public GradleTestProject project;
 
@@ -64,50 +75,140 @@ public class MlGeneratedClassTest {
 
     @Test
     public void testMobileNetModelClass() throws IOException, InterruptedException {
-        verifyModelClass("mobilenet_quant_metadata.tflite");
+        Set<MethodReference> modelMethods =
+                ImmutableSet.of(
+                        createNewInstanceMethodReference(),
+                        createProcessMethodReference(TENSOR_IMAGE));
+        Set<MethodReference> outputsMethods =
+                ImmutableSet.of(
+                        createOutputGetterMethodReference(
+                                "getProbabilityAsTensorLabel", TENSOR_LABEL));
+        verifyModelClass("mobilenet_quant_metadata.tflite", modelMethods, outputsMethods);
     }
 
     @Test
     public void testMobileNetModelClassWithoutMetadata() throws IOException, InterruptedException {
-        verifyModelClass("mobilenet_quant_no_metadata.tflite");
+        Set<MethodReference> modelMethods =
+                ImmutableSet.of(
+                        createNewInstanceMethodReference(),
+                        createProcessMethodReference(TENSOR_BUFFER));
+        Set<MethodReference> outputsMethods =
+                ImmutableSet.of(
+                        createOutputGetterMethodReference(
+                                "getOutputFeature0AsTensorBuffer", TENSOR_BUFFER));
+        verifyModelClass("mobilenet_quant_no_metadata.tflite", modelMethods, outputsMethods);
     }
 
     @Test
     public void testDeepLabModelClass() throws IOException, InterruptedException {
-        verifyModelClass("deeplab_image_seg_metadata.tflite");
+        Set<MethodReference> modelMethods =
+                ImmutableSet.of(
+                        createNewInstanceMethodReference(),
+                        createProcessMethodReference(TENSOR_IMAGE));
+        Set<MethodReference> outputsMethods =
+                ImmutableSet.of(
+                        createOutputGetterMethodReference(
+                                "getSegmentationmasksAsTensorBuffer", TENSOR_BUFFER));
+        verifyModelClass("deeplab_image_seg_metadata.tflite", modelMethods, outputsMethods);
     }
 
     @Test
     public void testInceptionModelClass() throws IOException, InterruptedException {
-        verifyModelClass("inception_quant_metadata.tflite");
+        Set<MethodReference> modelMethods =
+                ImmutableSet.of(
+                        createNewInstanceMethodReference(),
+                        createProcessMethodReference(TENSOR_IMAGE));
+        Set<MethodReference> outputsMethods =
+                ImmutableSet.of(
+                        createOutputGetterMethodReference(
+                                "getProbabilityAsTensorLabel", TENSOR_LABEL));
+        verifyModelClass("inception_quant_metadata.tflite", modelMethods, outputsMethods);
     }
 
     @Test
     public void testMnasnetModelClass() throws IOException, InterruptedException {
-        verifyModelClass("mnasnet_metadta.tflite");
+        Set<MethodReference> modelMethods =
+                ImmutableSet.of(
+                        createNewInstanceMethodReference(),
+                        createProcessMethodReference(TENSOR_IMAGE));
+        Set<MethodReference> outputsMethods =
+                ImmutableSet.of(
+                        createOutputGetterMethodReference(
+                                "getProbabilityAsTensorLabel", TENSOR_LABEL));
+        verifyModelClass("mnasnet_metadta.tflite", modelMethods, outputsMethods);
     }
 
     @Test
     public void testSsdOdtModelClass() throws IOException, InterruptedException {
-        verifyModelClass("ssd_mobilenet_odt_metadata.tflite");
+        Set<MethodReference> modelMethods =
+                ImmutableSet.of(
+                        createNewInstanceMethodReference(),
+                        createProcessMethodReference(TENSOR_IMAGE));
+        Set<MethodReference> outputsMethods =
+                ImmutableSet.of(
+                        createOutputGetterMethodReference(
+                                "getClassesAsTensorBuffer", TENSOR_BUFFER),
+                        createOutputGetterMethodReference(
+                                "getLocationsAsTensorBuffer", TENSOR_BUFFER),
+                        createOutputGetterMethodReference(
+                                "getNumberofdetectionsAsTensorBuffer", TENSOR_BUFFER),
+                        createOutputGetterMethodReference(
+                                "getScoresAsTensorBuffer", TENSOR_BUFFER));
+        verifyModelClass("ssd_mobilenet_odt_metadata.tflite", modelMethods, outputsMethods);
     }
 
     @Test
     public void testStyleTransferModelClass() throws IOException, InterruptedException {
-        verifyModelClass("style_transfer_quant_metadata.tflite");
+        Set<MethodReference> modelMethods =
+                ImmutableSet.of(
+                        createNewInstanceMethodReference(),
+                        createProcessMethodReference(TENSOR_IMAGE + TENSOR_BUFFER));
+        Set<MethodReference> outputsMethods =
+                ImmutableSet.of(
+                        createOutputGetterMethodReference(
+                                "getStyledimageAsTensorBuffer", TENSOR_BUFFER));
+        verifyModelClass("style_transfer_quant_metadata.tflite", modelMethods, outputsMethods);
     }
 
-    private void verifyModelClass(String modelFileName) throws IOException, InterruptedException {
+    private static MethodReference createNewInstanceMethodReference() {
+        return new ImmutableMethod(
+                MODEL,
+                "newInstance",
+                ParamUtil.parseParamString("Landroid/content/Context;"),
+                "Lcom/android/app/ml/Model;",
+                0,
+                null,
+                null);
+    }
+
+    private static MethodReference createProcessMethodReference(String params) {
+        return new ImmutableMethod(
+                MODEL, "process", ParamUtil.parseParamString(params), MODEL_OUTPUT, 0, null, null);
+    }
+
+    private static MethodReference createOutputGetterMethodReference(
+            String name, String returnType) {
+        return new ImmutableMethod(
+                MODEL_OUTPUT, name, ParamUtil.parseParamString(""), returnType, 0, null, null);
+    }
+
+    private void verifyModelClass(
+            String modelFileName,
+            Set<MethodReference> expectedModelMethods,
+            Set<MethodReference> expectedOutputMethods)
+            throws IOException, InterruptedException {
         // Add model file to ml folder
         File modelFile = project.file("src/main/ml/model.tflite");
         FileUtils.copyFile(
                 TestUtils.getWorkspaceFile(
-                        "prebuilts/tools/common/mlkit/testData/" + modelFileName),
+                        "prebuilts/tools/common/mlkit/testData/models/" + modelFileName),
                 modelFile);
-
         project.executor().run(":assembleDebug");
 
         Apk apk = project.getApk(GradleTestProject.ApkType.DEBUG);
-        assertThatApk(apk).containsClass("Lcom/android/app/ml/Model;");
+        Truth.assertThat(apk.getClass(MODEL).getMethods())
+                .containsAtLeastElementsIn(expectedModelMethods);
+        Truth.assertThat(apk.getClass(MODEL_OUTPUT).getMethods())
+                .containsAtLeastElementsIn(expectedOutputMethods);
     }
 }
