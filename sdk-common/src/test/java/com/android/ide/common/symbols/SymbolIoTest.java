88961a66782084a2a4f265039bdb9851779b8eb5
Chris Warrington <cmw@google.com>
2020-03-27 12:04:11 +0000

    Strengthen symbol interning tests
    Assert that the canonical name is the same instance.
    
    (cherry picked from commit 56e4e2e735c08facf2a5d74c07d2e4b2d251e596)
    Bug: 152323103
    Test: Correctness covered by existing tests
    Test: Expanded SymbolIoTest.
    Change-Id: I0a9d12b781c91db8959c38460f532d53ec4cb99a

--- sdk-common/src/test/java/com/android/ide/common/symbols/SymbolIoTest.java
+++ sdk-common/src/test/java/com/android/ide/common/symbols/SymbolIoTest.java
@@ -109,6 +109,16 @@ public class SymbolIoTest {
                                         "xml", "authenticator", "int", "0x7f040000"))
                         .build();
         assertEquals(expected, table);
+        assertCanonicalizationDoesNotProduceDuplicateStrings(table);
+    }
+
+    /** Check that cannonicalization doesn't produce duplicate strings. */
+    private static void assertCanonicalizationDoesNotProduceDuplicateStrings(SymbolTable table) {
+        for (Symbol symbol : table.getSymbols().values()) {
+            if (symbol.getCanonicalName().equals(symbol.getName())) {
+                assertThat(symbol.getCanonicalName()).isSameAs(symbol.getName());
+            }
+        }
     }
 
     @Test
@@ -137,6 +147,7 @@ public class SymbolIoTest {
                                         "xml", "authenticator", "int", "0x7f040000"))
                         .build();
         assertEquals(expected, table);
+        assertCanonicalizationDoesNotProduceDuplicateStrings(table);
     }
 
     @Test
@@ -1010,6 +1021,8 @@ public class SymbolIoTest {
                                 ResourceVisibility.PRIVATE_XML_ONLY),
                         new Symbol.NormalSymbol(
                                 ResourceType.TRANSITION, "t", 0, ResourceVisibility.PUBLIC));
+
+        assertCanonicalizationDoesNotProduceDuplicateStrings(table);
     }
 
     @Test
@@ -1084,6 +1097,7 @@ public class SymbolIoTest {
         SymbolTable tableLoadedFromFile = symbolIo.readRDef(rDefFile);
 
         assertThat(tableLoadedFromFile).isEqualTo(originalTable);
+        assertCanonicalizationDoesNotProduceDuplicateStrings(tableLoadedFromFile);
     }
 
     @Test
@@ -1199,12 +1213,16 @@ public class SymbolIoTest {
 
         UnmodifiableIterator<SymbolTable> iterator = symbolTables.iterator();
         SymbolTable lib = iterator.next();
+        Symbol drawable = lib.getSymbols().get(ResourceType.DRAWABLE, "foobar");
+        Symbol layout = lib.getSymbols().get(ResourceType.STYLEABLE, "LimitedSizeLinearLayout");
         SymbolTable lib2 = iterator.next();
 
-        assertThat(lib2.getSymbols().get(ResourceType.DRAWABLE, "foobar"))
-                .isSameAs(lib.getSymbols().get(ResourceType.DRAWABLE, "foobar"));
+        assertThat(lib2.getSymbols().get(ResourceType.DRAWABLE, "foobar")).isSameAs(drawable);
 
         assertThat(lib2.getSymbols().get(ResourceType.STYLEABLE, "LimitedSizeLinearLayout"))
-                .isSameAs(lib.getSymbols().get(ResourceType.STYLEABLE, "LimitedSizeLinearLayout"));
+                .isSameAs(layout);
+
+        assertThat(drawable.getCanonicalName()).isSameAs(drawable.getName());
+        assertThat(layout.getCanonicalName()).isSameAs(layout.getName());
     }
 }
