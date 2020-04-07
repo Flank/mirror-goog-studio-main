8df76fd74572ac313256f5bd3f7357bb3c0bf29a
Lei Yu <jackqdyulei@google.com>
2020-04-07 11:08:31 -0700

    Add Cast op to TensorBuffer input.
    User need to create TensorBuffer, in codegen we cast it to
    accepted type incase user created it wrong.
    
    Bug: n/A
    Test: Existing test still pass
    Change-Id: I0e85d4b73ea03a037359e49f637ca0d4bfcf005b

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/tasks/mlkit/codegen/codeinjector/codeblock/processor/DefaultPreprocessorInitInjector.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/tasks/mlkit/codegen/codeinjector/codeblock/processor/DefaultPreprocessorInitInjector.kt
@@ -17,6 +17,7 @@ package com.android.build.gradle.internal.tasks.mlkit.codegen.codeinjector.codeb
 
 import com.android.build.gradle.internal.tasks.mlkit.codegen.ClassNames
 import com.android.build.gradle.internal.tasks.mlkit.codegen.codeinjector.codeblock.CodeBlockInjector
+import com.android.build.gradle.internal.tasks.mlkit.codegen.getDataType
 import com.android.build.gradle.internal.tasks.mlkit.codegen.getFloatArrayString
 import com.android.build.gradle.internal.tasks.mlkit.codegen.getProcessorBuilderName
 import com.android.build.gradle.internal.tasks.mlkit.codegen.getProcessorName
@@ -44,11 +45,18 @@ class DefaultPreprocessorInitInjector : CodeBlockInjector() {
         val quantizationParams =
             tensorInfo.quantizationParams
         methodBuilder.addCode(
-            "  .add(new \$T(\$Lf, \$Lf));\n",
+            "  .add(new \$T(\$Lf, \$Lf))\n",
             ClassNames.QUANTIZE_OP,
             quantizationParams.zeroPoint,
             quantizationParams.scale
         )
+        methodBuilder.addCode(
+            "  .add(new \$T(\$T.\$L));\n",
+            ClassNames.CAST_OP,
+            ClassNames.DATA_TYPE,
+            getDataType(tensorInfo.dataType)
+        )
+
         methodBuilder.addStatement(
             "\$L = \$L.build()",
             getProcessorName(tensorInfo),
