Iandmee/base_tools

Use correct minSdkVersion when fetching desugar_lib_dex from transform
When users run debug build from studio, minSdkVersion used by d8/r8 is
override by injected device api level. L8Task and L8Transform are also
using the same minSdkVersion as d8/r8. However, we didn't use the same
one override by injected device api when fetching desugar_lib_dex from
L8Transform which is fixed in this CL.

This CL also does some clean-up in L8Task and L8Transform regarding how
we get api level to make it consistent across post compilation pipeline.

Bug: 154145638
Test: manually
Change-Id: I214509dc0f53a24a5260b23224afe5666482b518

build-system/gradle-core/src/main/java/com/android/build/gradle/internal/dependency/L8DexDesugarLibTransform.kt
@@ -107,7 +107,7 @@ fun getDesugarLibConfigurations(components: Collection<ComponentPropertiesImpl>)
 private fun getDesugarLibConfiguration(component: ComponentPropertiesImpl): DesugarLibConfiguration {
     val libConfiguration = getDesugarLibConfig(component.globalScope.project)
     val bootClasspath = component.variantScope.bootClasspath.filter { it.name == SdkConstants.FN_FRAMEWORK_LIBRARY }
-    val minSdkVersion = component.variantDslInfo.minSdkVersionWithTargetDeviceApi.apiLevel
+    val minSdkVersion = component.variantDslInfo.minSdkVersionWithTargetDeviceApi.featureLevel
 
     return DesugarLibConfiguration(libConfiguration, bootClasspath, minSdkVersion)
 }

build-system/gradle-core/src/main/java/com/android/build/gradle/internal/tasks/L8DexDesugarLibTask.kt
@@ -117,7 +117,7 @@ abstract class L8DexDesugarLibTask : NonIncrementalTask() {
             task.desugarLibJar.from(getDesugarLibJarFromMaven(creationConfig.globalScope.project))
             task.androidJar.set(creationConfig.globalScope.sdkComponents.androidJarProvider)
             task.minSdkVersion.set(
-                creationConfig.variantDslInfo.minSdkVersionWithTargetDeviceApi.apiLevel)
+                creationConfig.variantDslInfo.minSdkVersionWithTargetDeviceApi.featureLevel)
 
             val attributes = getDexingArtifactConfiguration(creationConfig).getAttributes()
 

build-system/gradle-core/src/main/java/com/android/build/gradle/internal/utils/DesugarLibUtils.kt
@@ -76,7 +76,9 @@ fun getDesugarLibDexFromTransform(creationConfig: BaseCreationConfig): FileColle
     }
 
     val configuration = getDesugarLibConfiguration(creationConfig.globalScope.project)
-    return getDesugarLibDexFromTransform(configuration, creationConfig.minSdkVersion.featureLevel)
+    return getDesugarLibDexFromTransform(
+        configuration,
+        creationConfig.variantDslInfo.minSdkVersionWithTargetDeviceApi.featureLevel)
 }
 
 /**

Flank/mirror-goog-studio-main

(cherry picked from commit 9aadaecdd012776ba69825be921a57e0c98fe3d1)

build-system/gradle-core/src/main/java/com/android/build/gradle/internal/dependency/L8DexDesugarLibTransform.kt
@@ -104,7 +104,7 @@ fun getDesugarLibConfigurations(scopes: Collection<VariantScope>): Set<DesugarLi
 private fun getDesugarLibConfiguration(scope: VariantScope): DesugarLibConfiguration {
     val libConfiguration = getDesugarLibConfig(scope.globalScope.project)
     val bootClasspath = scope.bootClasspath.filter { it.name == SdkConstants.FN_FRAMEWORK_LIBRARY }
-    val minSdkVersion = scope.variantDslInfo.minSdkVersionWithTargetDeviceApi.apiLevel
+    val minSdkVersion = scope.variantDslInfo.minSdkVersionWithTargetDeviceApi.featureLevel
 
     return DesugarLibConfiguration(libConfiguration, bootClasspath, minSdkVersion)
 }

build-system/gradle-core/src/main/java/com/android/build/gradle/internal/tasks/L8DexDesugarLibTask.kt
@@ -110,7 +110,7 @@ abstract class L8DexDesugarLibTask : NonIncrementalTask() {
             task.desugarLibJar.from(getDesugarLibJarFromMaven(variantScope.globalScope.project))
             task.androidJar.set(variantScope.globalScope.sdkComponents.androidJarProvider)
             task.minSdkVersion.set(
-                variantScope.variantDslInfo.minSdkVersionWithTargetDeviceApi.apiLevel)
+                variantScope.variantDslInfo.minSdkVersionWithTargetDeviceApi.featureLevel)
 
             val attributes = getDexingArtifactConfiguration(variantScope).getAttributes()
 

build-system/gradle-core/src/main/java/com/android/build/gradle/internal/utils/DesugarLibUtils.kt
@@ -64,7 +64,9 @@ fun getDesugarLibDexFromTransform(variantScope: VariantScope): FileCollection {
     }
 
     val configuration = getDesugarLibConfiguration(variantScope.globalScope.project)
-    return getDesugarLibDexFromTransform(configuration, variantScope.minSdkVersion.featureLevel)
+    return getDesugarLibDexFromTransform(
+        configuration,
+        variantScope.variantDslInfo.minSdkVersionWithTargetDeviceApi.featureLevel)
 }
 
 /**