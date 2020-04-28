6e5e71de8a96d003080c40c3ff8459e21278bfa5
parentej <parentej@google.com>
2020-04-28 12:08:27 +0100

    [NPW] Update compose templates to dev09
    ... to support latest compose code
    
    Bug: 154895146
    Test: Run TemplateTests and UI Tests in package com.android.tools.idea.tests.gui.compose
    Change-Id: I819e6c2cf4ff7b02eea5cf54f57600e06ab2073f

--- wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/activities/composeActivity/src/app_package/mainActivityKt.kt
+++ wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/activities/composeActivity/src/app_package/mainActivityKt.kt
@@ -29,8 +29,8 @@ package ${escapeKotlinIdentifier(packageName)}
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
-import androidx.ui.core.Text
 import androidx.ui.core.setContent
+import androidx.ui.foundation.Text
 import androidx.ui.material.MaterialTheme
 import androidx.ui.tooling.preview.Preview
 
