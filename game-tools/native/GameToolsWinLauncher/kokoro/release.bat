aeb5df115810068d7e5c204343b29c13bf38de6b
Tianyu Geng <tgeng@google.com>
2020-05-04 09:28:47 -0700

    Fix Kokoro script to build game tools launcher
    I am regretting for not setting up a presubmit for this one time build
    thing :(
    
    Bug: 11355240
    Test: N/A
    Change-Id: I044c7d4a019450f2d17333896b2f8b9f22a87b3e

--- game-tools/native/GameToolsWinLauncher/kokoro/release.bat
+++ game-tools/native/GameToolsWinLauncher/kokoro/release.bat
@@ -4,7 +4,7 @@ set PATH=C:\Program Files\Java\jdk1.7.0_75\bin;%PATH%
 :: Code under repo is checked out to %KOKORO_ARTIFACTS_DIR%\git.
 :: The final directory name in this path is determined by the scm name specified
 :: in the job configuration
-cd %KOKORO_ARTIFACTS_DIR%\git\aosp-tools-base\tools\base\game-tools\native\GameToolsWinLauncher
+cd %KOKORO_ARTIFACTS_DIR%\git\aosp-tools-base\game-tools\native\GameToolsWinLauncher
 
 set VS_ROOT=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community
 :: Make MSBuild available
