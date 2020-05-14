1d84dca6665ac337c4b488a712d2b0580d94fdd9
Joshua Gilpatrick <gijosh@google.com>
2020-05-14 10:03:44 -0700

    Add helper function get get default value if SqlValue is null
    This fixes a bug where null long_values had an undefined behavior
    This results in the UI being wrong.
    
    Test: manual
    Fixes: 156617261
    Change-Id: If15f912710fc197e14950984b8e8cb371918e7e6

--- profiler/native/trace_processor_daemon/memory/memory_request_handler.cc
+++ profiler/native/trace_processor_daemon/memory/memory_request_handler.cc
@@ -22,7 +22,12 @@
 #endif
 
 using namespace profiler::perfetto;
+using perfetto::trace_processor::SqlValue;
 using profiler::perfetto::proto::NativeAllocationContext;
+
+long GetLongOrDefault(SqlValue value, long defaultValue) {
+  return value.is_null() ? defaultValue : value.long_value;
+}
 
 void MemoryRequestHandler::PopulateEvents(NativeAllocationContext* batch) {
   if (batch == nullptr) {
@@ -32,9 +37,9 @@ void MemoryRequestHandler::PopulateEvents(NativeAllocationContext* batch) {
   auto callsite = processor_->ExecuteQuery(
       "select id, parent_id, frame_id from stack_profile_callsite");
   while (callsite.Next()) {
-    auto id = callsite.Get(0).long_value;
-    auto parent_id = callsite.Get(1).long_value;
-    auto frame_id = callsite.Get(2).long_value;
+    auto id = GetLongOrDefault(callsite.Get(0), -1);
+    auto parent_id = GetLongOrDefault(callsite.Get(1), -1);
+    auto frame_id = GetLongOrDefault(callsite.Get(2), -1);
     auto frame = profiler::perfetto::proto::StackPointer();
     frame.set_parent_id(parent_id);
     frame.set_frame_id(frame_id);
@@ -53,7 +58,7 @@ void MemoryRequestHandler::PopulateEvents(NativeAllocationContext* batch) {
     auto module_name_field = frames.Get(2);
     auto module_name =
         module_name_field.is_null() ? nullptr : module_name_field.string_value;
-    auto rel_pc = frames.Get(3).long_value;
+    auto rel_pc = GetLongOrDefault(frames.Get(3), -1);
     auto frame = batch->add_frames();
     char* demangled_name = nullptr;
     // TODO (b/151081845): Enable demangling support on windows.
@@ -94,10 +99,10 @@ void MemoryRequestHandler::PopulateEvents(NativeAllocationContext* batch) {
   auto alloc = processor_->ExecuteQuery(
       "select ts, count, size, callsite_id from heap_profile_allocation");
   while (alloc.Next()) {
-    auto timestamp = alloc.Get(0).long_value;
-    auto count = alloc.Get(1).long_value;
-    auto size = alloc.Get(2).long_value;
-    auto frame_id = alloc.Get(3).long_value;
+    auto timestamp = GetLongOrDefault(alloc.Get(0), 0);
+    auto count = GetLongOrDefault(alloc.Get(1), 0);
+    auto size = GetLongOrDefault(alloc.Get(2), 0);
+    auto frame_id = GetLongOrDefault(alloc.Get(3), -1);
     auto allocation = batch->add_allocations();
     allocation->set_timestamp(timestamp);
     allocation->set_size(size);
