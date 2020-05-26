e8eb70d9ccaa0834b8d120aee2633c09b08ed20d
Joshua Gilpatrick <gijosh@google.com>
2020-05-26 11:42:53 -0700

    Stop caching MPRQ, HPIF, and REAQ request
    Caching of MPRQ, HPIF, and REQA request can cause issues with
    systems that depend on the status returned by these request.
    In addition to not caching the reply the id of the packet needed
    to be fixed to allow the DDMLIB client to properly map a reply
    packet to a request
    
    Test: added
    Fixes: 157103406
    Fixes: 156643273
    Fixes: 157101164
    Change-Id: I86de5ab7e2c8ba6b5ea2fd1a8f460d98e248acad
    (cherry picked from commit db3d3293b430bc2100c591c66909594f8dedf0b6)

--- ddmlib/src/main/java/com/android/ddmlib/internal/jdwp/interceptor/ClientInitializationInterceptor.java
+++ ddmlib/src/main/java/com/android/ddmlib/internal/jdwp/interceptor/ClientInitializationInterceptor.java
@@ -22,7 +22,6 @@ import static com.android.ddmlib.internal.jdwp.chunkhandler.HandleHeap.CHUNK_REA
 import static com.android.ddmlib.internal.jdwp.chunkhandler.HandleHello.CHUNK_FEAT;
 import static com.android.ddmlib.internal.jdwp.chunkhandler.HandleHello.CHUNK_HELO;
 import static com.android.ddmlib.internal.jdwp.chunkhandler.HandleProfiling.CHUNK_MPRQ;
-import static com.android.ddmlib.internal.jdwp.chunkhandler.HandleWait.CHUNK_WAIT;
 
 import com.android.annotations.NonNull;
 import com.android.ddmlib.TimeoutException;
@@ -36,38 +35,57 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * Interceptor that is responsible for caching specific request/reply packets.
+ * Interceptor that is responsible for caching and fixing reply packet ids.
  *
- * Because the device manages internal state specific packets being sent at an unexpected time may cause undefined behavior. This interceptor
- * listens for various request from clients and either forwards the request on to the device or replies with a cached response previously
+ * <p>Because the device manages internal state specific packets being sent at an unexpected time
+ * may cause undefined behavior. This interceptor listens for various request from clients and
+ * either forwards the request on to the device or replies with a cached response previously
  * captured from device.
  *
- * This allows the device to talk to the proxy as a single client. While at the same time allowing multiple instances of DDMLIB to treat the proxy
- * as a single device client. Example
- * DDMLIB_1 -> HELO -> ClientInterceptor (First time seeing HELO) -> HELO -> DEVICE
- * DDMLIB_2 -> HELO -> ClientInterceptor (I am waiting on a response do nothing)
- * DEVICE -> HELO -> ClientInterceptor (2 instances are waiting for a response)
- *                                          -> HELO -> DDMLIB_1
- *                                          -> HELO -> DDMLIB_2
- * ... Some time later ...
- * DDMLIB_3 -> HELO -> ClientInterceptor (I have a response already send cache) -> HELO -> DDMLIB_3
+ * <p>This allows the device to talk to the proxy as a single client. While at the same time
+ * allowing multiple instances of DDMLIB to treat the proxy as a single device client. Example
+ * DDMLIB_1 -> HELO -> ClientInterceptor (First time seeing HELO) -> HELO -> DEVICE DDMLIB_2 -> HELO
+ * -> ClientInterceptor (I am waiting on a response do nothing) DEVICE -> HELO -> ClientInterceptor
+ * (2 instances are waiting for a response) -> HELO -> DDMLIB_1 -> HELO -> DDMLIB_2 ... Some time
+ * later ... DDMLIB_3 -> HELO -> ClientInterceptor (I have a response already send cache) -> HELO ->
+ * DDMLIB_3
  *
- * Some things to note in the example above, the device only sees HELO 1 time no matter how many instances connect.
- * Responses to all request tracked by this interceptor are only sent back as replies to instances that request them.
+ * <p>Some things to note in the example above, the device only sees HELO 1 time no matter how many
+ * instances connect. Responses to all request tracked by this interceptor are only sent back as
+ * replies to instances that request them.
  */
 public class ClientInitializationInterceptor implements Interceptor {
-    private final Set<Integer> mPacketFilter = new HashSet<>();
-    private final Map<Integer, byte[]> cachedPackets = new HashMap<>();
-    private final HashMap<Integer, Set<JdwpProxyClient>> pendingPackets = new HashMap<>();
+    private static int PACKET_ID_OFFSET = 0x04;
+    // This set contains the packet type of packets to capture and cache the device response.
+    private final Set<Integer> mCachePacketFilter = new HashSet<>();
+    // This set contains the packet type of packets to capture and fix the packet id before sending.
+    private final Set<Integer> mReplyPacketFilter = new HashSet<>();
+    private final Map<Integer, byte[]> mCachedPackets = new HashMap<>();
+    private final HashMap<Integer, Set<ClientRequestId>> mPendingPackets = new HashMap<>();
+
+    private static class ClientRequestId {
+        public JdwpProxyClient client; // Client making the request.
+        public int requestId; // Id of the requested packet.
+
+        ClientRequestId(JdwpProxyClient client, int requestId) {
+            this.client = client;
+            this.requestId = requestId;
+        }
+    }
 
     public ClientInitializationInterceptor() {
-        mPacketFilter.add(CHUNK_HELO);
-        mPacketFilter.add(CHUNK_FEAT);
-        mPacketFilter.add(CHUNK_MPRQ);
-        mPacketFilter.add(CHUNK_HPIF);
-        mPacketFilter.add(CHUNK_REAQ);
-        mPacketFilter.add(CHUNK_WAIT);
+        // Note: mCachePacketFilter and mReplyPacketFilter should not have overlapping entries
+        // if an entry does overlap the behavior of the mCachePacketFilter will win.
+        mCachePacketFilter.add(CHUNK_HELO);
+        mCachePacketFilter.add(CHUNK_FEAT);
+        // The following commands are status request for a client and do not need to be
+        // cached to support a multi-client workflow.
+        mReplyPacketFilter.add(CHUNK_MPRQ);
+        mReplyPacketFilter.add(CHUNK_HPIF);
+        mReplyPacketFilter.add(CHUNK_REAQ);
+
     }
+
     @Override
     public boolean filterToDevice(@NonNull JdwpProxyClient from, @NonNull JdwpPacket packet) throws IOException, TimeoutException {
         if (packet.isEmpty() || packet.isError() || packet.getLength() < JdwpPacket.JDWP_HEADER_LEN + 4) {
@@ -75,42 +93,73 @@ public class ClientInitializationInterceptor implements Interceptor {
         }
         ByteBuffer payload = packet.getPayload();
         int type = payload.getInt();
-        if (!mPacketFilter.contains(type)) {
+        if (!(mCachePacketFilter.contains(type) || mReplyPacketFilter.contains(type))) {
             return false;
         }
-        if (cachedPackets.containsKey(type)) {
-            byte[] send = cachedPackets.get(type);
-            from.write(send, send.length);
+        if (mCachedPackets.containsKey(type)) {
+            sendCachedPacket(from, type, packet.getId());
             return true;
         }
-        boolean alreadyPending = pendingPackets.containsKey(type);
-        pendingPackets.computeIfAbsent(type, (key) -> new HashSet<>()).add(from);
+        boolean alreadyPending = mPendingPackets.containsKey(type);
+        mPendingPackets
+                .computeIfAbsent(type, (key) -> new HashSet<>())
+                .add(new ClientRequestId(from, packet.getId()));
         return alreadyPending;
     }
 
     @Override
-    public boolean filterToClient(@NonNull JdwpProxyClient to, @NonNull JdwpPacket packet) {
-        // Can't filter on DMS packets since REAQ is not one of them.
-        if (packet.isEmpty() || packet.isError() || packet.getLength() < JdwpPacket.JDWP_HEADER_LEN + 4) {
+    public boolean filterToClient(@NonNull JdwpProxyClient to, @NonNull JdwpPacket packet)
+            throws IOException, TimeoutException {
+        // Note: reply packets don't have the command / command set values initialized.
+        if (!packet.isReply()
+                || packet.isEmpty()
+                || packet.isError()
+                || packet.getLength() < JdwpPacket.JDWP_HEADER_LEN + 4) {
             return false;
         }
 
         ByteBuffer payload = packet.getPayload();
         int type = payload.getInt();
-        if (!pendingPackets.containsKey(type)) {
+        if (!mPendingPackets.containsKey(type)) {
             return false;
         }
-        cachedPackets.computeIfAbsent(type, key ->
-        {
+        if (mCachePacketFilter.contains(type)) {
             ByteBuffer buffer = ByteBuffer.allocate(packet.getLength());
             buffer.order(CHUNK_ORDER);
             packet.move(buffer);
-            return buffer.array();
-        });
-        boolean filterPacket = !pendingPackets.get(type).remove(to) || !to.isHandshakeComplete();
-        if (pendingPackets.get(type).isEmpty()) {
-            pendingPackets.remove(type);
+            mCachedPackets.put(type, buffer.array());
+            for (ClientRequestId pending : mPendingPackets.get(type)) {
+                sendCachedPacket(pending.client, type, pending.requestId);
+            }
+        } else if (mReplyPacketFilter.contains(type)) {
+            for (ClientRequestId pending : mPendingPackets.get(type)) {
+                sendPacketWithUpdatedPacketId(pending.client, pending.requestId, packet);
+            }
         }
-        return filterPacket;
+        mPendingPackets.remove(type);
+        return true;
+    }
+
+    // Helper function to send cached packets. This helper replaces the packet id with the reply
+    // packet id expected by the ddmlib client.
+    // Packet ids are used as a mapping between request/reply packets.
+    private void sendCachedPacket(JdwpProxyClient to, int type, int id)
+            throws IOException, TimeoutException {
+        ByteBuffer buffer = ByteBuffer.wrap(mCachedPackets.get(type));
+        buffer.order(CHUNK_ORDER);
+        buffer.putInt(PACKET_ID_OFFSET, id);
+        to.write(buffer.array(), buffer.limit());
+    }
+    // Helper function to reply to specific clients with the expected packet id. Packet ids are used
+    // to map reply packets
+    // to a request. This is needed because a reply packet does not have the SET / CMD_SET flags of
+    // a packet set.
+    private static void sendPacketWithUpdatedPacketId(JdwpProxyClient to, int id, JdwpPacket packet)
+            throws IOException, TimeoutException {
+        ByteBuffer buffer = ByteBuffer.allocate(packet.getLength());
+        buffer.order(CHUNK_ORDER);
+        packet.move(buffer);
+        buffer.putInt(PACKET_ID_OFFSET, id);
+        to.write(buffer.array(), buffer.limit());
     }
 }
--- ddmlib/src/test/java/com/android/ddmlib/internal/jdwp/interceptor/ClientInitializationInterceptorTest.java
+++ ddmlib/src/test/java/com/android/ddmlib/internal/jdwp/interceptor/ClientInitializationInterceptorTest.java
@@ -16,14 +16,23 @@
 
 package com.android.ddmlib.internal.jdwp.interceptor;
 
+import static com.android.ddmlib.internal.jdwp.chunkhandler.ChunkHandler.CHUNK_ORDER;
+import static com.android.ddmlib.internal.jdwp.chunkhandler.HandleProfiling.CHUNK_MPRQ;
 import static com.android.ddmlib.internal.jdwp.interceptor.InterceptorTest.makePacket;
 import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.android.ddmlib.internal.jdwp.JdwpProxyClient;
 import com.android.ddmlib.internal.jdwp.chunkhandler.HandleHello;
+import com.android.ddmlib.internal.jdwp.chunkhandler.HandleProfiling;
+import com.android.ddmlib.internal.jdwp.chunkhandler.JdwpPacket;
+import java.nio.ByteBuffer;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
 public class ClientInitializationInterceptorTest {
 
@@ -36,7 +45,7 @@ public class ClientInitializationInterceptorTest {
     }
 
     @Test
-    public void packetFilterIgnoresUnknownPackets() {
+    public void packetFilterIgnoresUnknownPackets() throws Exception {
         ClientInitializationInterceptor interceptor = new ClientInitializationInterceptor();
         JdwpProxyClient mock = mock(JdwpProxyClient.class);
         assertThat(interceptor.filterToClient(mock, makePacket("TEST"))).isFalse();
@@ -51,14 +60,102 @@ public class ClientInitializationInterceptorTest {
     }
 
     @Test
-    public void filterPacketsBeforeHandshake() throws Exception {
+    public void cachedPacketsReplyWithCachedData() throws Exception {
         ClientInitializationInterceptor interceptor = new ClientInitializationInterceptor();
         JdwpProxyClient mock = mock(JdwpProxyClient.class);
-        when(mock.isHandshakeComplete()).thenReturn(false);
-        // Make a round trip to put packet in cache.
+        ArgumentCaptor<byte[]> writeData = ArgumentCaptor.forClass(byte[].class);
+        int payload = 0xBEEF;
+        JdwpPacket requestA = makePacket(HandleHello.CHUNK_HELO);
+        JdwpPacket requestB = makePacket(HandleHello.CHUNK_HELO, false, 1234);
+        // Round trip and cache HELO Packet
+        assertThat(interceptor.filterToDevice(mock, requestA)).isFalse();
+        assertThat(
+                        interceptor.filterToClient(
+                                mock, makePacket(HandleHello.CHUNK_HELO, true, payload)))
+                .isTrue();
+        verify(mock, times(1)).write(writeData.capture(), anyInt());
+        verifyDataPayload(writeData.getValue(), payload, requestA.getId());
+
+        // Request a 2nd packet expecting back the cached value.
+        assertThat(interceptor.filterToDevice(mock, requestB)).isTrue();
+        verify(mock, times(2)).write(writeData.capture(), anyInt());
+        verifyDataPayload(writeData.getAllValues().get(1), payload, requestB.getId());
+    }
+
+    @Test
+    public void nonCachedPacketsReplyFromDeviceOnly() throws Exception {
+        ClientInitializationInterceptor interceptor = new ClientInitializationInterceptor();
+        JdwpProxyClient mock = mock(JdwpProxyClient.class);
+        ArgumentCaptor<byte[]> writeData = ArgumentCaptor.forClass(byte[].class);
+        int payload = 0xBEEF;
+        JdwpPacket requestA = makePacket(CHUNK_MPRQ);
+        JdwpPacket requestB = makePacket(CHUNK_MPRQ, false, 1234);
+        // Round trip MPRQ expecting it to not be cached.
+        assertThat(interceptor.filterToDevice(mock, requestA)).isFalse();
+        assertThat(interceptor.filterToClient(mock, makePacket(CHUNK_MPRQ, true, payload)))
+                .isTrue();
+        verify(mock, times(1)).write(writeData.capture(), anyInt());
+        verifyDataPayload(writeData.getValue(), payload, requestA.getId());
+
+        // Request a 2nd packet expecting it to be sent to the device.
+        assertThat(interceptor.filterToDevice(mock, requestB)).isFalse();
+    }
+
+    @Test
+    public void replyPacketIdMatchesRequestPacketId() throws Exception {
+        ClientInitializationInterceptor interceptor = new ClientInitializationInterceptor();
+        JdwpProxyClient mock = mock(JdwpProxyClient.class);
+        ArgumentCaptor<byte[]> writeData = ArgumentCaptor.forClass(byte[].class);
+        int payload = 0xBEEF;
+        JdwpPacket requestPacket = makePacket(HandleHello.CHUNK_HELO);
+        JdwpPacket replyPacket = makePacket(HandleHello.CHUNK_HELO, true, payload);
+        assertThat(requestPacket.getId()).isNotEqualTo(replyPacket.getId());
+        // Round trip HELO Packet.
+        assertThat(interceptor.filterToDevice(mock, requestPacket)).isFalse();
+        assertThat(interceptor.filterToClient(mock, replyPacket)).isTrue();
+        // Capture reply packet and validate Id
+        verify(mock, times(1)).write(writeData.capture(), anyInt());
+        verifyDataPayload(writeData.getValue(), payload, requestPacket.getId());
+    }
+
+    private void verifyDataPayload(byte[] writeData, int payload, int replyId) {
+        ByteBuffer buffer = ByteBuffer.wrap(writeData);
+        buffer.order(CHUNK_ORDER);
+        buffer.position(buffer.limit()); // need to set position so find packet has proper size.
+        JdwpPacket sentPacket = JdwpPacket.findPacket(buffer);
+        assertThat(sentPacket.getId()).isEqualTo(replyId);
+        ByteBuffer bufferPayload = sentPacket.getPayload();
+        // 0 = type
+        // 4 = length of payload
+        assertThat(bufferPayload.getInt(0x08)).isEqualTo(payload);
+    }
+
+    @Test
+    public void replyPacketsAreFiltered() throws Exception {
+        ClientInitializationInterceptor interceptor = new ClientInitializationInterceptor();
+        JdwpProxyClient mock = mock(JdwpProxyClient.class);
+        int payload = 0xBEEF;
+        // HELO is filtered on the round trip
         assertThat(interceptor.filterToDevice(mock, makePacket(HandleHello.CHUNK_HELO))).isFalse();
-        assertThat(interceptor.filterToClient(mock, makePacket(HandleHello.CHUNK_HELO))).isTrue();
-        assertThat(interceptor.filterToDevice(mock, makePacket(HandleHello.CHUNK_HELO))).isTrue();
+        assertThat(
+                        interceptor.filterToClient(
+                                mock, makePacket(HandleHello.CHUNK_HELO, true, payload)))
+                .isTrue();
+        // FEAT is filtered on the round trip.
+        assertThat(interceptor.filterToDevice(mock, makePacket(HandleHello.CHUNK_FEAT))).isFalse();
+        assertThat(
+                        interceptor.filterToClient(
+                                mock, makePacket(HandleHello.CHUNK_FEAT, true, payload)))
+                .isTrue();
+        // MPRQ is filtered on the round trip.
+        assertThat(interceptor.filterToDevice(mock, makePacket(HandleProfiling.CHUNK_MPRQ)))
+                .isFalse();
+        assertThat(
+                        interceptor.filterToClient(
+                                mock, makePacket(HandleProfiling.CHUNK_MPRQ, true, payload)))
+                .isTrue();
+        // Non reply packets are not filtered.
+        assertThat(interceptor.filterToClient(mock, makePacket(HandleHello.CHUNK_HELO))).isFalse();
     }
 
     @Test
