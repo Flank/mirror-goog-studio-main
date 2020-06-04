cb40c542a6d0291b6cc8fa4a2de61d25a254c327
Lei Yu <jackqdyulei@google.com>
2020-06-04 16:34:37 -0700

    Update strings in mlkit directory.
    Bug: 157696585
    Test: Not needed. Just string updates.
    Change-Id: Ia41406357d8fc908d72073cf651a21eda0276463

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/tasks/mlkit/codegen/TfliteModelGenerator.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/tasks/mlkit/codegen/TfliteModelGenerator.kt
@@ -62,7 +62,7 @@ class TfliteModelGenerator(
         if (!modelInfo.isMinParserVersionSatisfied) {
             androidLogger.warning(
                 "Model is not fully supported in current Android Gradle Plugin" +
-                        " and will use fallback API, please upgrade to latest version if necessary: ${modelFile.absolutePath}"
+                        " and will use fallback APIs, so please update to the latest version: ${modelFile.absolutePath}"
             )
         }
 
