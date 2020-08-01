9ca5dd52d78a796e865059b1ebe32f0ee6540cf4
Yuriy Solodkyy <solodkyy@google.com>
2020-08-01 16:19:05 +0100

    Simplify IdeAndroidGradlePluginProjectFlagsImpl
    Bug: n/a
    Test: n/a
    Change-Id: I174b3b1916b16cf48d11b862c9d339933a147ece

--- sdk-common/src/main/java/com/android/ide/common/gradle/model/impl/IdeAndroidGradlePluginProjectFlagsImpl.kt
+++ sdk-common/src/main/java/com/android/ide/common/gradle/model/impl/IdeAndroidGradlePluginProjectFlagsImpl.kt
@@ -20,65 +20,64 @@ import com.android.builder.model.AndroidGradlePluginProjectFlags
 import com.android.builder.model.AndroidGradlePluginProjectFlags.BooleanFlag
 import com.android.ide.common.gradle.model.IdeAndroidGradlePluginProjectFlags
 import java.io.Serializable
-import java.util.EnumMap
 
 /**
  * Represents flags that affect the semantic of the build in the Android Gradle Plugin
  * that also should affect the behavior of Android Studio.
  */
 data class IdeAndroidGradlePluginProjectFlagsImpl(
-    private val booleanFlagMap: EnumMap<BooleanFlag, Boolean>
+  /**
+   * Whether the R class in applications and dynamic features are constant.
+   *
+   * If they are constant they can be inlined by the java compiler and used in places that
+   * require constants such as annotations and cases of switch statements.
+   */
+  override val applicationRClassConstantIds: Boolean,
+
+  /**
+   * Whether the R class in instrumentation tests are constant.
+   *
+   * If they are constant they can be inlined by the java compiler and used in places that
+   * require constants such as annotations and cases of switch statements.
+   */
+  override val testRClassConstantIds: Boolean,
+
+  /**
+   * Whether the R class generated for this project is Transitive.
+   *
+   * If it is transitive it will contain all of the resources defined in its transitive
+   * dependencies alongside those defined in this project.
+   * If non-transitive it will only contain the resources defined in this project.
+   */
+  override val transitiveRClasses: Boolean,
+
+  /** Whether the Jetpack Compose feature is enabled for this project. */
+  override val usesCompose: Boolean,
+
+  /** Whether the ML model binding feature is enabled for this project. */
+  override val mlModelBindingEnabled: Boolean
 ) : Serializable, IdeAndroidGradlePluginProjectFlags {
 
-    /**
-     * Create based on the android gradle plugin model class.
-     */
-    constructor(flags: AndroidGradlePluginProjectFlags) : this() {
-        booleanFlagMap.putAll(flags.booleanFlagMap)
-    }
+  constructor(booleanFlagMap: Map<BooleanFlag, Boolean>) : this(
+    applicationRClassConstantIds = booleanFlagMap.getBooleanFlag(BooleanFlag.APPLICATION_R_CLASS_CONSTANT_IDS),
+    testRClassConstantIds = booleanFlagMap.getBooleanFlag(BooleanFlag.TEST_R_CLASS_CONSTANT_IDS),
+    transitiveRClasses = booleanFlagMap.getBooleanFlag(BooleanFlag.TRANSITIVE_R_CLASS),
+    usesCompose = booleanFlagMap.getBooleanFlag(BooleanFlag.JETPACK_COMPOSE),
+    mlModelBindingEnabled = booleanFlagMap.getBooleanFlag(BooleanFlag.ML_MODEL_BINDING)
+  )
 
-    /**
-     * Create an empty set of flags for older AGPs and for studio serialization.
-     */
-    constructor() : this(booleanFlagMap = EnumMap(BooleanFlag::class.java))
+  /**
+   * Create based on the android gradle plugin model class.
+   */
+  constructor(flags: AndroidGradlePluginProjectFlags) : this(flags.booleanFlagMap)
 
-    /**
-     * Whether the R class in applications and dynamic features are constant.
-     *
-     * If they are constant they can be inlined by the java compiler and used in places that
-     * require constants such as annotations and cases of switch statements.
-     */
-    override val applicationRClassConstantIds: Boolean
-        get() = getBooleanFlag(BooleanFlag.APPLICATION_R_CLASS_CONSTANT_IDS)
-
-    /**
-     * Whether the R class in instrumentation tests are constant.
-     *
-     * If they are constant they can be inlined by the java compiler and used in places that
-     * require constants such as annotations and cases of switch statements.
-     */
-    override val testRClassConstantIds: Boolean
-        get() = getBooleanFlag(BooleanFlag.TEST_R_CLASS_CONSTANT_IDS)
-
-    /**
-     * Whether the R class generated for this project is Transitive.
-     *
-     * If it is transitive it will contain all of the resources defined in its transitive
-     * dependencies alongside those defined in this project.
-     * If non-transitive it will only contain the resources defined in this project.
-     */
-    override val transitiveRClasses: Boolean
-        get() = getBooleanFlag(BooleanFlag.TRANSITIVE_R_CLASS)
-
-    /** Whether the Jetpack Compose feature is enabled for this project. */
-    override val usesCompose: Boolean
-        get() = getBooleanFlag(BooleanFlag.JETPACK_COMPOSE)
-
-    /** Whether the ML model binding feature is enabled for this project. */
-    override val mlModelBindingEnabled: Boolean
-        get() = getBooleanFlag(BooleanFlag.ML_MODEL_BINDING)
-
-    private fun getBooleanFlag(flag: BooleanFlag): Boolean {
-        return booleanFlagMap[flag] ?: flag.legacyDefault
-    }
+  /**
+   * Create an empty set of flags for older AGPs and for studio serialization.
+   */
+  constructor() : this(booleanFlagMap = emptyMap())
 }
+
+private fun Map<BooleanFlag, Boolean>.getBooleanFlag(flag: BooleanFlag): Boolean {
+  return this[flag] ?: flag.legacyDefault
+}
+
