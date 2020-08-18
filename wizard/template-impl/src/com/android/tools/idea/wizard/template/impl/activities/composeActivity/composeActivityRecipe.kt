f5edcc7b4fefc88b329a391fe0e4681658b52ef0
diegoperez <diegoperez@google.com>
2020-08-18 10:51:08 +0100

    Update template for alpha01
    Bug: 163864562
    Test: TemplateTests pass
    Change-Id: I70398d6e2f609ae4503ed49352b38ec7748ab709

--- wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/activities/composeActivity/composeActivityRecipe.kt
+++ wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/activities/composeActivity/composeActivityRecipe.kt
@@ -44,7 +44,7 @@ fun RecipeExecutor.composeActivityRecipe(
   addMaterialDependency(true) // useAndroidX should be always true for Compose
 
   val composeVersionVarName = getDependencyVarName("androidx.compose.ui:ui", "compose_version")
-  setExtVar(composeVersionVarName, "0.1.0-dev16")
+  setExtVar(composeVersionVarName, "1.0.0-alpha01")
 
   addDependency(mavenCoordinate = "androidx.compose.ui:ui:\${$composeVersionVarName}")
   addDependency(mavenCoordinate = "androidx.compose.material:material:\${$composeVersionVarName}")
