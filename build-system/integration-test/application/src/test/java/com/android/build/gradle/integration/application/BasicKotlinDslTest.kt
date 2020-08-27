79869a5ec8dc44fc6e066f6128c54370891e4527
Ivan Gavrilovic <gavra@google.com>
2020-08-27 18:51:10 +0100

    Add regression test for kts usage
    Add regression test for http://b/166468915.
    
    Bug: 166468915
    Test: adding it
    Change-Id: I0e77e1429a8e513b56cdca50910abab07c4fb3a9

---
+++ build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/application/BasicKotlinDslTest.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.build.gradle.integration.application
+
+import com.android.Version
+import com.android.build.gradle.integration.common.fixture.GradleTestProject
+import com.android.build.gradle.integration.common.fixture.app.MinimalSubProject
+import org.junit.Rule
+import org.junit.Test
+
+/** Regression test for http://b/166468915. */
+class BasicKotlinDslTest {
+
+    @JvmField
+    @Rule
+    val project = GradleTestProject.builder().fromTestApp(
+            MinimalSubProject.app("com.example.app"))
+            .create()
+
+    @Test
+    fun testAbleToBuild() {
+        project.buildFile.delete()
+        project.file("settings.gradle.kts").writeText("""
+            pluginManagement {
+                apply(from="../commonLocalRepo.gradle", to=this)
+
+                resolutionStrategy {
+                    eachPlugin {
+                        if(requested.id.namespace == "com.android") {
+                            useModule("com.android.tools.build:gradle:${Version.ANDROID_GRADLE_PLUGIN_VERSION}")
+                        }
+                    }
+                }
+            }
+        """.trimIndent())
+
+        project.file("build.gradle.kts").writeText("""
+            apply(from = "../commonHeader.gradle")
+            apply(from = "../commonLocalRepo.gradle")
+            plugins {
+                id("com.android.application")
+            }
+
+            android {
+                compileSdkVersion(${GradleTestProject.DEFAULT_COMPILE_SDK_VERSION})
+            }
+        """.trimIndent())
+
+        // add at least one property, as that was triggering http://b/166468915
+        project.file("gradle.properties").appendText("""
+            android.debug.obsoleteApi=true
+        """.trimIndent())
+
+        project.executor().run("assembleDebug")
+    }
+}
