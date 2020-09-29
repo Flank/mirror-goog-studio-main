027cebcfd7b91e4302878f9483edeefa14cf8fcb
Jose Parente <parentej@google.com>
2020-09-29 14:14:16 +0100

    [NPW] Update wear templates to latest version
    Version 2.8.1 is androidx compatible
    
    Fixes: 167953155
    Test: TemplateTest
    Change-Id: If4f16a5230fc1b6e15f96539f52486c887a76158

--- wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/other/watchFaceService/watchFaceServiceTemplate.kt
+++ wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/other/watchFaceService/watchFaceServiceTemplate.kt
@@ -46,7 +46,7 @@ val watchFaceServiceTemplate
   get() = template {
     revision = 1
     name = "Watch Face"
-    constraints = listOf(TemplateConstraint.AndroidX, TemplateConstraint.Jetifier)
+    constraints = listOf(TemplateConstraint.AndroidX)
     minApi = 23
     minBuildApi = 25
     description = "Creates a watch face for Wear OS"
