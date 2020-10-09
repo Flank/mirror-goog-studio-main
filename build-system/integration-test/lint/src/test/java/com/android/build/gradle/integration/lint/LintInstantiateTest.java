b13937057757a4dea2d4bfed0309d2d22418cfc6
Tor Norbye <tnorbye@google.com>
2020-10-09 13:09:15 -0700

    Suggest placing report files under $reportsDir
    Test: Integration tests included
    Bug: None
    Change-Id: Ib0bd68d54c24da15c134cfe91dc65972bcda2a96

--- build-system/integration-test/lint/src/test/java/com/android/build/gradle/integration/lint/LintInstantiateTest.java
+++ build-system/integration-test/lint/src/test/java/com/android/build/gradle/integration/lint/LintInstantiateTest.java
@@ -49,7 +49,7 @@ public class LintInstantiateTest {
         File lintReport = project.file("app/lint-results.txt");
         assertThat(lintReport).contains("No issues found.");
 
-        File sarifFile = project.file("app/lint-results.sarif");
+        File sarifFile = new File(project.getSubproject("app").getBuildDir(), "reports/lint-results.sarif");
         assertThat(sarifFile).exists();
         assertThat(sarifFile).contains("\"$schema\" : \"https://raw.githubusercontent.com/oasis-tcs/sarif-spec/");
     }
