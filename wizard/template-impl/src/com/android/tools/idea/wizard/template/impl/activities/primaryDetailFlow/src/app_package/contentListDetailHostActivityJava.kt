9da713989fdc5589fc2bb2ee660b763b1253bd07
Takeshi Hagikura <thagikura@google.com>
2020-10-27 13:52:51 +0900

    Rename masterDetailFlow to primaryDetailFlow
    Bug: N/A
    Test: TemplateTest
    Change-Id: I90f88102807d2b0facc5564e32b56e89d832af31

--- wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/activities/primaryDetailFlow/src/app_package/contentListDetailHostActivityJava.kt
+++ wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/activities/primaryDetailFlow/src/app_package/contentListDetailHostActivityJava.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.wizard.template.impl.activities.masterDetailFlow.src.app_package
+package com.android.tools.idea.wizard.template.impl.activities.primaryDetailFlow.src.app_package
 
 import com.android.tools.idea.wizard.template.Language
 import com.android.tools.idea.wizard.template.getMaterialComponentName
