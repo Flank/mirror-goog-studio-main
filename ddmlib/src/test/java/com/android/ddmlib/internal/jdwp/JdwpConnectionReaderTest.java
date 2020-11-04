be46fed660736585891791a18741a068f3119ab0
Joshua Gilpatrick <gijosh@google.com>
2020-11-03 21:48:21 -0800

    Fixed a bug in JdwpProxy causing OOM
    Test: added test to validate buffer resize
    Fixes: 172268107
    Fixes: 171822043
    Change-Id: I61cfe61da4a729e12a3c025a8c936bc42c3d60e8

--- ddmlib/src/test/java/com/android/ddmlib/internal/jdwp/JdwpConnectionReaderTest.java
+++ ddmlib/src/test/java/com/android/ddmlib/internal/jdwp/JdwpConnectionReaderTest.java
@@ -17,12 +17,15 @@ package com.android.ddmlib.internal.jdwp;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import com.android.ddmlib.internal.jdwp.chunkhandler.ChunkHandler;
 import com.android.ddmlib.internal.jdwp.chunkhandler.JdwpPacket;
+
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
+
 import org.junit.Test;
 
 public class JdwpConnectionReaderTest {
@@ -43,19 +46,43 @@ public class JdwpConnectionReaderTest {
         SlowSimpleServer server = new SlowSimpleServer(finalSentData.array());
         channel.connect(
                 new InetSocketAddress(InetAddress.getByName("localhost"), server.getPort()));
-        JdwpConnectionReader mReader =
+        JdwpConnectionReader reader =
                 new JdwpConnectionReader(channel, JdwpPacket.JDWP_HEADER_LEN + 1);
         Thread serverThread = new Thread(server);
         serverThread.start();
-        for (ByteBuffer packet : packets) {
-            JdwpPacket readPacket = null;
-            while (mReader.read() != -1 && readPacket == null) {
-                readPacket = mReader.readPacket();
-            }
-            ByteBuffer packetData = ByteBuffer.allocate(readPacket.getLength());
-            readPacket.copy(packetData);
-            assertThat(packet).isEqualTo(packetData);
+        validatePackets(reader, packets);
+    }
+
+    @Test
+    public void resizeProperlySetsBufferPosition() throws Exception {
+        // This tests a subtle buffer resizing behavior. A bug was introduced due to the buffer
+        // resizing and copying the contents of the previous buffer independent of how full the
+        // buffer was. This means reading 1K in a 5K buffer then resizing the buffer to 10K would
+        // copy the full 5K of memory to the new buffer.
+        // One way to test this is to create a packet that expands the default buffer, then create
+        // a packet that satisfies two conditions.
+        // 1) Is larger than the current buffer
+        // 2) When read does not fill the entire buffer.
+        // Using the SlowSimpleServer this test meets those two requirements.
+        SocketChannel channel = SocketChannel.open();
+        ByteBuffer[] packets = new ByteBuffer[2];
+        int length = 0;
+        packets[0] = JdwpTest.createPacketBuffer(JdwpTest.CHUNK_TEST, 128);
+        length += packets[0].position();
+        packets[1] = JdwpTest.createPacketBuffer(ChunkHandler.type("PKET"), 128 * 5);
+        length += packets[1].position();
+        ByteBuffer finalSentData = ByteBuffer.allocate(length);
+        for (ByteBuffer byteBuffer : packets) {
+            finalSentData.put(byteBuffer.array());
         }
+        SlowSimpleServer server = new SlowSimpleServer(finalSentData.array());
+        channel.connect(
+                new InetSocketAddress(InetAddress.getByName("localhost"), server.getPort()));
+        JdwpConnectionReader reader =
+                new JdwpConnectionReader(channel, JdwpPacket.JDWP_HEADER_LEN);
+        Thread serverThread = new Thread(server);
+        serverThread.start();
+        validatePackets(reader, packets);
     }
 
     @Test
@@ -75,10 +102,28 @@ public class JdwpConnectionReaderTest {
         }
         ByteBuffer packetData = ByteBuffer.allocate(readPacket.getLength());
         readPacket.copy(packetData);
-        assertThat(sentPacket).isEqualTo(packetData);
+        assertThat(sentPacket.array()).isEqualTo(packetData.array());
+    }
+
+    private void validatePackets(JdwpConnectionReader reader, ByteBuffer[] packets)
+            throws Exception {
+        for (ByteBuffer packet : packets) {
+            JdwpPacket readPacket = null;
+            while (reader.read() != -1) {
+                readPacket = reader.readPacket();
+                if (readPacket != null) {
+                    break;
+                }
+            }
+            ByteBuffer packetData = ByteBuffer.allocate(readPacket.getLength());
+            readPacket.copy(packetData);
+            assertThat(packet.array()).isEqualTo(packetData.array());
+            readPacket.consume();
+        }
     }
 
     class SlowSimpleServer extends SimpleServer {
+
         public SlowSimpleServer(byte[] message) throws IOException {
             super(message);
         }
@@ -91,7 +136,8 @@ public class JdwpConnectionReaderTest {
                     client.write(ByteBuffer.wrap(mConnectMessage, i, 1));
                     Thread.sleep(10);
                 }
-            } catch (Exception ex) {
+            }
+            catch (Exception ex) {
                 // End thread.
             }
         }
