8c3ece22a45dd0828d55dae20642e83585252f1e
Yahan Zhou <yahan@google.com>
2020-11-06 15:30:57 -0800

    Update to UTP 0.0.6-dev
    Test: exists
    Bug: n/a
    Change-Id: If0303f1825fcc0f09cb2fd571cbcb8efdd9a8340

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/testing/utp/UtpDependencyUtils.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/testing/utp/UtpDependencyUtils.kt
@@ -25,7 +25,7 @@ import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.Optional
 
 private const val NITROGEN_MAVEN_GROUP_ID = "com.google.testing.platform"
-private const val NITROGEN_DEFAULT_VERSION = "0.0.5-dev"
+private const val NITROGEN_DEFAULT_VERSION = "0.0.6-dev"
 
 /**
  * Available Unified Test Platform dependencies.
