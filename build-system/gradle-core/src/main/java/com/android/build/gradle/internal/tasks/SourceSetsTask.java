64751e0c078781bb479871836f7904cf79e06d32
Ivan Gavrilovic <gavra@google.com>
2020-11-16 20:05:31 +0000

    Use Gradle 6.8-rc-1 as min version in AGP
    Bug: n/a
    Test: existing
    Change-Id: I654240715983b3ce54e8d889a870938d0100c8bc

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/tasks/SourceSetsTask.java
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/tasks/SourceSetsTask.java
@@ -25,15 +25,13 @@ import com.android.builder.core.VariantType;
 import java.io.IOException;
 import java.util.stream.Collectors;
 import org.gradle.api.Project;
-import org.gradle.api.tasks.diagnostics.AbstractReportTask;
+import org.gradle.api.tasks.diagnostics.ProjectBasedReportTask;
 import org.gradle.api.tasks.diagnostics.internal.ReportRenderer;
 import org.gradle.api.tasks.diagnostics.internal.TextReportRenderer;
 import org.gradle.internal.logging.text.StyledTextOutput;
 
-/**
- * Prints out the DSL names and directory names of available source sets.
- */
-public class SourceSetsTask extends AbstractReportTask {
+/** Prints out the DSL names and directory names of available source sets. */
+public class SourceSetsTask extends ProjectBasedReportTask {
 
     private final TextReportRenderer mRenderer = new TextReportRenderer();
 
