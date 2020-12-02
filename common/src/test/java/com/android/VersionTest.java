fed737b8c1b3de671f51652578229b987ccf8358
Tor Norbye <tnorbye@google.com>
2020-12-02 12:25:58 -0800

    Update tools base version to 30.0.
    Historically, the version of the base libraries (such as the
    lint API consumed by external lint checks) was the AGP version
    plus 23. This CL updates our base version from 27.3 (which was
    the initial "4.3" version, plus 23) to 30.0.
    
    Test: Existing
    Bug: None
    Change-Id: I17cb7ab5d010c00acdf64de0a3590e279cca43a3

--- common/src/test/java/com/android/VersionTest.java
+++ common/src/test/java/com/android/VersionTest.java
@@ -20,6 +20,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -35,7 +37,7 @@ public class VersionTest {
         String path = System.getProperty("test.version.properties");
         if (path != null) {
             try (InputStream bundled = Version.class.getResourceAsStream("version.properties");
-                 InputStream source = new FileInputStream(path)) {
+                    InputStream source = new FileInputStream(path)) {
                 Properties bundledProperties = new Properties();
                 bundledProperties.load(bundled);
 
@@ -45,5 +47,28 @@ public class VersionTest {
                 Assert.assertEquals(sourceProperties, bundledProperties);
             }
         }
+    }
+
+    @Test
+    public void testAgpAndBaseRelationship() {
+        // The Base libraries such as lint are expected to be exactly
+        // the AGP version plus 23 (to make it easy for example for
+        // external lint check authors who have to depend on both to
+        // easily figure out how to combine them.)
+        String agp = Version.ANDROID_GRADLE_PLUGIN_VERSION;
+        String base = Version.ANDROID_TOOLS_BASE_VERSION;
+        // We don't have access to utility classes like GradleCoordinate
+        // here so just do pretty simple substitution; this logic may
+        // need updating if this test starts failing after a dramatic
+        // change to our version string format
+        Pattern pattern = Pattern.compile("(\\d+)\\..*");
+        Matcher agpMatcher = pattern.matcher(agp);
+        Matcher baseMatcher = pattern.matcher(base);
+        Assert.assertTrue(agp, agpMatcher.find());
+        Assert.assertTrue(base, baseMatcher.find());
+        String agpMajorString = agpMatcher.group(1);
+        int agpMajor = Integer.parseInt(agpMajorString);
+        String expected = "" + (agpMajor + 23) + agp.substring(agpMatcher.end(1));
+        Assert.assertEquals(expected, base);
     }
 }
