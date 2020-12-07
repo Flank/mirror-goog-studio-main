39c72a4ef9d6a7b41891c40148760fba3cf1e4dc
Valery Cherepanov <qumeric@google.com>
2020-12-07 18:29:20 +0300

    Use updated package for Text Compose Activity
    Use BasicText instead
    Bug: N/A
    Test: TemplateTest
    
    Change-Id: I0811e8bd7830e1487437f674d126b32cc80bbed0

--- wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/activities/composeActivity/src/app_package/mainActivityKt.kt
+++ wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/activities/composeActivity/src/app_package/mainActivityKt.kt
@@ -29,9 +29,9 @@ package ${escapeKotlinIdentifier(packageName)}
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Text
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.tooling.preview.Preview
