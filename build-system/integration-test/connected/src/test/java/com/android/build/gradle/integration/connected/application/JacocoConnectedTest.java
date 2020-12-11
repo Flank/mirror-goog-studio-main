ec8c53889200c17f971e61745d96c848d1e67002
Yuki Hamada <hummer@google.com>
2020-12-10 19:31:15 -0800

    Integration tests for test coverage with clearPackageData option
    Bug: 123987001
    Test: JacocoConnectedTest
    Change-Id: Ic9e4497e4a9be4bc14a19c0c9e939299da48067f

--- build-system/integration-test/connected/src/test/java/com/android/build/gradle/integration/connected/application/JacocoConnectedTest.java
+++ build-system/integration-test/connected/src/test/java/com/android/build/gradle/integration/connected/application/JacocoConnectedTest.java
@@ -90,12 +90,27 @@ public class JacocoConnectedTest {
 
     @Test
     public void connectedCheckWithOrchestrator() throws Exception {
+        runConnectedCheckAndAssertCoverageReportExists(/*enableClearPackageDataOption=*/ false);
+    }
+
+    /** Regression test for http://b/123987001. */
+    @Test
+    public void connectedCheckWithOrchestratorAndClearPackageDataEnabled() throws Exception {
+        runConnectedCheckAndAssertCoverageReportExists(/*enableClearPackageDataOption=*/ true);
+    }
+
+    private void runConnectedCheckAndAssertCoverageReportExists(
+            boolean enableClearPackageDataOption) throws Exception {
         TestFileUtils.appendToFile(
                 project.getBuildFile(),
                 "\n"
                         + "android.defaultConfig.minSdkVersion 16\n"
                         + "android.defaultConfig.testInstrumentationRunner 'androidx.test.runner.AndroidJUnitRunner'\n"
                         + "android.defaultConfig.testInstrumentationRunnerArguments package: 'com.example.helloworld'\n"
+                        + (enableClearPackageDataOption
+                                ? "android.defaultConfig.testInstrumentationRunnerArguments clearPackageData: 'true'\n"
+                                        + "android.defaultConfig.testInstrumentationRunnerArguments coverageFilePath: '/storage/emulated/0/coverage/'\n"
+                                : "")
                         + "android.testOptions.execution 'ANDROIDX_TEST_ORCHESTRATOR'\n"
                         // Orchestrator requires some setup time and it usually takes
                         // about an minute. Increase the timeout for running "am instrument" command
@@ -105,6 +120,7 @@ public class JacocoConnectedTest {
                         + "  androidTestImplementation 'androidx.test:core:1.3.1-alpha02'\n"
                         + "  androidTestImplementation 'androidx.test.ext:junit:1.1.3-alpha02'\n"
                         + "  androidTestImplementation 'androidx.test:monitor:1.3.1-alpha02'\n"
+                        + "  androidTestImplementation 'androidx.test:rules:1.3.1-alpha02'\n"
                         + "  androidTestImplementation 'androidx.test:runner:1.3.1-alpha02'\n"
                         + "  androidTestImplementation 'androidx.test.services:test-services:1.3.1-alpha02'\n"
                         + "  androidTestUtil 'androidx.test:orchestrator:1.3.1-alpha02'\n"
@@ -117,11 +133,17 @@ public class JacocoConnectedTest {
                 "package com.example.helloworld;\n"
                         + "\n"
                         + "import androidx.test.ext.junit.runners.AndroidJUnit4;\n"
+                        + "import androidx.test.rule.GrantPermissionRule;\n"
+                        + "import org.junit.Rule;\n"
                         + "import org.junit.Test;\n"
                         + "import org.junit.runner.RunWith;\n"
                         + "\n"
                         + "@RunWith(AndroidJUnit4.class)\n"
                         + "public class ExampleTest {\n"
+                        + "    @Rule public GrantPermissionRule mRuntimePermissionRule =\n"
+                        + "        GrantPermissionRule.grant(\n"
+                        + "            android.Manifest.permission.WRITE_EXTERNAL_STORAGE);\n"
+                        + "\n"
                         + "    @Test\n"
                         + "    public void test1() { }\n"
                         + "\n"
@@ -144,7 +166,14 @@ public class JacocoConnectedTest {
                         .resolve("src/androidTest/java/com/example/helloworld/HelloWorldTest.java");
         Files.deleteIfExists(deprecatedTest);
 
-        project.executor().withArgument("--debug").run("connectedCheck");
+        // Append WRITE_EXTERNAL_STORAGE permission.
+        TestFileUtils.searchAndReplace(
+                project.getProjectDir().toPath().resolve("src/main/AndroidManifest.xml"),
+                "</manifest>",
+                "    <uses-permission android:name=\"android.permission.WRITE_EXTERNAL_STORAGE\"/>\n"
+                        + "</manifest>");
+
+        project.executor().run("connectedCheck");
         List<File> files =
                 FileUtils.find(
                         project.file("build/outputs/code_coverage"), Pattern.compile(".*\\.ec"));
