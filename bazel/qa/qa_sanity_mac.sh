d8374d71c521b19bd7a6987874e01e9b6ec03b44
Anil Timilsina <atimilsina@google.com>
2020-12-21 12:40:03 -0800

    Remove build skip logic for mac targets
    We have started seeing issues in ab/ where the builds are being retried
    when gsutil fails to upload build artifacts arbitarily. Remove the build
    skip logic to test if it is contributing to these retries.
    Bug: 173442697
    Test: n/a
    
    Change-Id: I0e93e338c4c239cd8892f20b3ae5ad470e0b1986

--- bazel/qa/qa_sanity_mac.sh
+++ bazel/qa/qa_sanity_mac.sh
@@ -10,16 +10,16 @@ readonly build_number="$3"
 # post-submit builds
 # Use flag file, skip all builds within 12 hours of last ran build
 # Skipped builds are marked green (exit 0), in ab/
-readonly mac_timestamp_file="/buildbot/lastrun.out"
-current_time=$(date +"%s")
-if [[ -f "${mac_timestamp_file}" ]]; then
-  last_run_time=$(cat $mac_timestamp_file)
-  if [[ $(($current_time-$last_run_time)) -lt 43200 ]]; then
-    exit 0
-  fi
-fi
+#readonly mac_timestamp_file="/buildbot/lastrun.out"
+#current_time=$(date +"%s")
+#if [[ -f "${mac_timestamp_file}" ]]; then
+#  last_run_time=$(cat $mac_timestamp_file)
+#  if [[ $(($current_time-$last_run_time)) -lt 43200 ]]; then
+#    exit 0
+#  fi
+#fi
 # Update timestamp in the flag file
-echo $current_time > $mac_timestamp_file
+#echo $current_time > $mac_timestamp_file
 
 readonly script_dir="$(dirname "$0")"
 readonly script_name="$(basename "$0")"
