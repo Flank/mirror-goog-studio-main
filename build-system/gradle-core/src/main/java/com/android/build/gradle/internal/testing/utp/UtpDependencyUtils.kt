a686aaac3546c2c79c296e643601004ecb6fa870
Yuki Hamada <hummer@google.com>
2021-01-08 16:36:25 -0800

    Upgrade UTP to 0.0.8-alpha
    com.google.test.platform:core-proto:0.0.4-dev uses
    "Android Software Development Kit License Agreement" license
    which makes impossible for IntelliJ team to bundle our
    android IntelliJ plugin. UTP version 0.0.8-alpha changed
    the license to APL 2.0.
    
    Bug: n/a
    Test: exists
    Change-Id: I43b3dadd70c9c8e85e66e7cc64a355c851092099

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/testing/utp/UtpDependencyUtils.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/testing/utp/UtpDependencyUtils.kt
@@ -25,7 +25,7 @@ import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.Optional
 
 private const val NITROGEN_MAVEN_GROUP_ID = "com.google.testing.platform"
-private const val NITROGEN_DEFAULT_VERSION = "0.0.4-dev"
+private const val NITROGEN_DEFAULT_VERSION = "0.0.8-alpha01"
 
 /**
  * Available Unified Test Platform dependencies.
