649330ad4883118ec04f48b1ceefad0eb9ff4d34
Ivan Gavrilovic <gavra@google.com>
2021-02-05 17:06:08 +0000

    Emit sync warnings when jcenter() is used
    If jcenter() is used as project or buildscript repo,
    AGP will emit a warning, encouraging users to migrate off it.
    This is because jcenter() is shutting down in the future.
    
    Bug: 179387390
    Test: AgpRepositoryCheckerTest
    Change-Id: Ieead6336d7e140bc662f2016b09bc06c8ba0e0ed

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/plugins/BasePlugin.java
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/plugins/BasePlugin.java
@@ -90,6 +90,7 @@ import com.android.build.gradle.internal.services.DslServicesImpl;
 import com.android.build.gradle.internal.services.ProjectServices;
 import com.android.build.gradle.internal.services.StringCachingBuildService;
 import com.android.build.gradle.internal.services.SymbolTableBuildService;
+import com.android.build.gradle.internal.utils.AgpRepositoryChecker;
 import com.android.build.gradle.internal.utils.AgpVersionChecker;
 import com.android.build.gradle.internal.utils.GradlePluginUtils;
 import com.android.build.gradle.internal.utils.KgpUtils;
@@ -129,7 +130,6 @@ import org.gradle.api.NamedDomainObjectContainer;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
-import org.gradle.api.artifacts.repositories.FlatDirectoryArtifactRepository;
 import org.gradle.api.component.SoftwareComponentFactory;
 import org.gradle.api.invocation.Gradle;
 import org.gradle.api.logging.Logger;
@@ -649,20 +649,7 @@ public abstract class BasePlugin<
                             + "tools/java-8-support-message.html\n";
             dslServices.getIssueReporter().reportWarning(IssueReporter.Type.GENERIC, warningMsg);
         }
-
-        project.getRepositories()
-                .forEach(
-                        it -> {
-                            if (it instanceof FlatDirectoryArtifactRepository) {
-                                String warningMsg =
-                                        String.format(
-                                                "Using %s should be avoided because it doesn't support any meta-data formats.",
-                                                it.getName());
-                                dslServices
-                                        .getIssueReporter()
-                                        .reportWarning(IssueReporter.Type.GENERIC, warningMsg);
-                            }
-                        });
+        AgpRepositoryChecker.INSTANCE.checkRepositories(project);
 
         // don't do anything if the project was not initialized.
         // Unless TEST_SDK_DIR is set in which case this is unit tests and we don't return.
