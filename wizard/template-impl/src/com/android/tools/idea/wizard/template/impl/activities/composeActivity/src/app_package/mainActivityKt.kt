115ccd69372b800a088edb3146b92d84561ab949
Jose Parente <parentej@google.com>
2021-02-22 15:59:47 +0000

    [Wizard Templates] Remove AppCompat usage from Compose
    Fixes: 180698146
    Test: TemplateTest
    Change-Id: I922d6e82718c1f6b9caefa9ef86d17ab8db90f67

--- wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/activities/composeActivity/src/app_package/mainActivityKt.kt
+++ wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/activities/composeActivity/src/app_package/mainActivityKt.kt
@@ -28,8 +28,8 @@ fun mainActivityKt(
 package ${escapeKotlinIdentifier(packageName)}
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -37,7 +37,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import ${escapeKotlinIdentifier(packageName)}.ui.theme.${themeName}
 
-class ${activityClass} : AppCompatActivity() {
+class ${activityClass} : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
