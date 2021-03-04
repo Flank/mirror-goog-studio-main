aa010b7e9b1032673feab16cd5cf4db211eab737
Hung Nguyen <hungnv@google.com>
2021-03-04 16:39:38 +0000

    Update to Gradle 7.0-rc-1 (tools/base)
    Bug: 181947044
    Test: Existing tests
    Change-Id: I9f71758044b9581dc9af053adceb38ae62bd2556

--- common/src/main/java/com/android/SdkConstants.java
+++ common/src/main/java/com/android/SdkConstants.java
@@ -2804,7 +2804,7 @@ public final class SdkConstants {
      * Studio product. It need not actually be the latest version of Gradle, but it will most likely
      * be fairly recent.
      */
-    public static final String GRADLE_LATEST_VERSION = "6.8.2";
+    public static final String GRADLE_LATEST_VERSION = "7.0-rc-1";
 
     public static final String GRADLE_PLUGIN_MINIMUM_VERSION = "1.0.0";
     public static final String GRADLE_PLUGIN_RECOMMENDED_VERSION = "3.3.2";
