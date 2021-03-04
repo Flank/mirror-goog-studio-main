b2bba4bba443174cdb5770b187cd489142a02311
Chris Warrington <cmw@google.com>
2021-03-04 17:42:42 +0000

    [AGP DSL] Use decorator for AndroidResources
    And migrate all the usages within it to use
    properties managed by the DSL decorator.
    
    Bug: 140406102
    Test: Existing integration tests
    Change-Id: Idc6291c634f37bec73dde89fb2656c468b92b6f2

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/dsl/AaptOptions.java
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/dsl/AaptOptions.java
@@ -19,35 +19,25 @@ package com.android.build.gradle.internal.dsl;
 import com.android.annotations.NonNull;
 import com.android.annotations.Nullable;
 import com.android.build.gradle.internal.LoggerWrapper;
+import com.android.build.gradle.internal.services.DslServices;
+import com.android.build.gradle.options.BooleanOption;
 import com.google.common.collect.Iterables;
-import java.util.ArrayList;
+
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import javax.inject.Inject;
 
 /** DSL object for configuring aapt options. */
-public class AaptOptions implements com.android.build.api.dsl.AaptOptions, com.android.build.api.dsl.AndroidResources {
+public abstract class AaptOptions implements com.android.build.api.dsl.AaptOptions, com.android.build.api.dsl.AndroidResources {
 
     @Inject
-    public AaptOptions(boolean namespaced) {
-        this.namespaced = namespaced;
+    public AaptOptions(DslServices dslServices) {
+        this.setNamespaced(dslServices.getProjectOptions().get(BooleanOption.ENABLE_RESOURCE_NAMESPACING_DEFAULT));
     }
 
-    private boolean namespaced;
-
-    @Nullable
-    private String ignoreAssetsPattern;
-
-    private final List<String> noCompressList = new ArrayList<>();
-
-    @Nullable private Boolean cruncherEnabled;
-
-    private boolean failOnMissingConfigEntry = false;
-
-    private final List<String> additionalParameters = new ArrayList<>();
-
-    private int cruncherProcesses = 0;
+    // Use an internal type to keep the logging for now
+    protected abstract List<String> getInternalNoCompressList();
 
     public void setIgnoreAssets(@Nullable String ignoreAssetsPattern) {
         setIgnoreAssetsPattern(ignoreAssetsPattern);
@@ -59,22 +49,18 @@ public class AaptOptions implements com.android.build.api.dsl.AaptOptions, com.a
      * <p>See <code>aapt --help</code>
      */
     public String getIgnoreAssets() {
-        return ignoreAssetsPattern;
+        return getIgnoreAssetsPattern();
     }
 
     @Override
-    public void setIgnoreAssetsPattern(@Nullable String ignoreAssetsPattern) {
-        this.ignoreAssetsPattern = ignoreAssetsPattern;
-    }
+    public abstract void setIgnoreAssetsPattern(@Nullable String ignoreAssetsPattern);
 
     @Override
     @Nullable
-    public String getIgnoreAssetsPattern() {
-        return ignoreAssetsPattern;
-    }
+    public abstract String getIgnoreAssetsPattern();
 
     public void setNoCompress(String noCompress) {
-        setNoCompress(new String[] { noCompress });
+        setNoCompress(Collections.singletonList(noCompress));
     }
 
     public void setNoCompress(Iterable<String> noCompress) {
@@ -82,21 +68,14 @@ public class AaptOptions implements com.android.build.api.dsl.AaptOptions, com.a
     }
 
     public void setNoCompress(String... noCompress) {
-        for (String p : noCompress) {
-            if (p.equals("\"\"")) {
-                LoggerWrapper.getLogger(AaptOptions.class).warning("noCompress pattern '\"\"' "
-                        + "no longer matches every file. It now matches exactly two double quote "
-                        + "characters. Please use '' instead.");
-            }
-        }
-        noCompressList.clear();
-        Collections.addAll(noCompressList, noCompress);
+        getInternalNoCompressList().clear();
+        noCompress(noCompress);
     }
 
     @NonNull
     @Override
     public Collection<String> getNoCompress() {
-        return noCompressList;
+        return getInternalNoCompressList();
     }
 
     @SuppressWarnings("MethodMayBeStatic")
@@ -114,7 +93,7 @@ public class AaptOptions implements com.android.build.api.dsl.AaptOptions, com.a
     }
 
     public void setCruncherEnabled(boolean value) {
-        cruncherEnabled = value;
+        setCruncherEnabledOverride(value);
     }
 
     /**
@@ -133,12 +112,13 @@ public class AaptOptions implements com.android.build.api.dsl.AaptOptions, com.a
         // Simulate true if unset. This is not really correct, but changing it to be a tri-state
         // nullable Boolean is potentially a breaking change if the getter was being used by build
         // scripts or third party plugins.
+        Boolean cruncherEnabled = getCruncherEnabledOverride();
         return cruncherEnabled == null ? true : cruncherEnabled;
     }
 
-    public Boolean getCruncherEnabledOverride() {
-        return cruncherEnabled;
-    }
+    public abstract Boolean getCruncherEnabledOverride();
+
+    protected abstract void setCruncherEnabledOverride(Boolean value);
 
     /**
      * Whether to use the new cruncher.
@@ -148,55 +128,51 @@ public class AaptOptions implements com.android.build.api.dsl.AaptOptions, com.a
     }
 
     public void failOnMissingConfigEntry(boolean value) {
-        failOnMissingConfigEntry = value;
+        setFailOnMissingConfigEntry(value);
     }
 
     @Override
-    public void setFailOnMissingConfigEntry(boolean value) {
-        failOnMissingConfigEntry = value;
-    }
+    public abstract void setFailOnMissingConfigEntry(boolean value);
 
     @Override
-    public boolean getFailOnMissingConfigEntry() {
-        return failOnMissingConfigEntry;
-    }
+    public abstract boolean getFailOnMissingConfigEntry();
 
     // -- DSL Methods. TODO remove once the instantiator does what I expect it to do.
 
     @Override
     public void noCompress(@NonNull String noCompress) {
-        noCompressList.add(noCompress);
+        getNoCompress().add(noCompress);
     }
 
     @Override
     public void noCompress(@NonNull String... noCompress) {
-        Collections.addAll(noCompressList, noCompress);
+        for (String p : noCompress) {
+            if (p.equals("\"\"")) {
+                LoggerWrapper.getLogger(AaptOptions.class).warning("noCompress pattern '\"\"' "
+                        + "no longer matches every file. It now matches exactly two double quote "
+                        + "characters. Please use '' instead.");
+            }
+        }
+        Collections.addAll(getNoCompress(), noCompress);
     }
 
     @Override
     public void additionalParameters(@NonNull String param) {
-        additionalParameters.add(param);
+        getAdditionalParameters().add(param);
     }
 
     @Override
     public void additionalParameters(@NonNull String... params) {
-        Collections.addAll(additionalParameters, params);
+        Collections.addAll(getAdditionalParameters(), params);
     }
 
-    public void setAdditionalParameters(@NonNull List<String> parameters) {
-        additionalParameters.clear();
-        additionalParameters.addAll(parameters);
-    }
+    public abstract void setAdditionalParameters(@NonNull List<String> parameters);
 
     @Override
     @NonNull
-    public List<String> getAdditionalParameters() {
-        return additionalParameters;
-    }
+    public abstract List<String> getAdditionalParameters();
 
-    public void setCruncherProcesses(int cruncherProcesses) {
-        this.cruncherProcesses = cruncherProcesses;
-    }
+    public abstract void setCruncherProcesses(int cruncherProcesses);
 
     /**
      * Obtains the number of cruncher processes to use. More cruncher processes will crunch files
@@ -204,21 +180,15 @@ public class AaptOptions implements com.android.build.api.dsl.AaptOptions, com.a
      *
      * @return the number of cruncher processes, {@code 0} to use the default
      */
-    public int getCruncherProcesses() {
-        return cruncherProcesses;
-    }
+    public abstract int getCruncherProcesses();
 
     @Override
-    public boolean getNamespaced() {
-        return namespaced;
-    }
+    public abstract boolean getNamespaced();
 
     @Override
-    public void setNamespaced(boolean namespaced) {
-        this.namespaced = namespaced;
-    }
+    public abstract void setNamespaced(boolean namespaced);
 
     public void namespaced(boolean namespaced) {
-        this.namespaced = namespaced;
+        setNamespaced(namespaced);
     }
 }
