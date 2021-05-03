bc7cf4364d81f0c73a554b631def27a021750f87
Dan Albert <danalbert@google.com>
2021-05-03 15:50:04 -0700

    Remove extra test cruft.
    I'd cleaned up the native sources when I copied this test but missed a
    bunch of the resources and kotlin imports.
    
    Test: treehugger
    Bug: None
    Change-Id: I0d734c37cc9b48fb80e4b6e853b7abbcd8cd6978

--- build-system/integration-test/test-projects/prefabNoDeps/app/src/main/java/com/android/prefabaartest/MainActivity.kt
+++ build-system/integration-test/test-projects/prefabNoDeps/app/src/main/java/com/android/prefabaartest/MainActivity.kt
@@ -1,10 +1,6 @@
 package com.android.prefabaartest
 
-import android.annotation.SuppressLint
 import android.app.Activity
-import android.os.Bundle
-import java.io.FileOutputStream
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
     companion object {
