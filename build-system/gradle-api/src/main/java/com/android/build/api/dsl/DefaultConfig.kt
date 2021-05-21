8d67f8ad0be05181289bba76e50d898092337c44
Chris Warrington <cmw@google.com>
2021-05-21 18:21:00 +0100

    [AGP DSL] Mark DefaultConfig as stable
    Bug: 140406102
    Test: Updated API expectation files
    Change-Id: I409ccba0f646dd21ce9b548f4e6158eb0390de4e

--- build-system/gradle-api/src/main/java/com/android/build/api/dsl/DefaultConfig.kt
+++ build-system/gradle-api/src/main/java/com/android/build/api/dsl/DefaultConfig.kt
@@ -16,18 +16,16 @@
 
 package com.android.build.api.dsl
 
-import org.gradle.api.Incubating
-
 /**
  * Specifies defaults for variant properties that the Android plugin applies to all build variants.
  *
  * You can override any `defaultConfig` property when
  * [configuring product flavors](https://developer.android.com/studio/build/build-variants.html#product-flavors).
+ * See [ProductFlavor].
  *
- * @see [ProductFlavor]
+ * Each plugin has its own interface that extends this one, see [ApplicationDefaultConfig],
+ * [LibraryDefaultConfig], [DynamicFeatureDefaultConfig] and [TestDefaultConfig].
  */
-@Incubating
 interface DefaultConfig :
     BaseFlavor {
-    // TODO(b/140406102)
 }
