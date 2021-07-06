0ddfa4169d7fc1fc852a0170a16fba34c4799f4b
Diego Perez <diegoperez@google.com>
2021-07-06 14:50:27 +0100

    Pin activity-compose to 1.3.0-rc01
    This version is known to work with rc01 of
    Compose and with Kotlin 1.5.10.
    Also pin lifecycle-runtime-ktx to 2.3.1.
    
    Fixes: 192936259
    Test: Template tests pass and manually verified
    Change-Id: Iac5042b57649defa7ff6eb0e6e721f0f5f8d88e3

--- wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/activities/composeActivity/composeActivityRecipe.kt
+++ wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/activities/composeActivity/composeActivityRecipe.kt
@@ -48,8 +48,8 @@ fun RecipeExecutor.composeActivityRecipe(
   addDependency(mavenCoordinate = "androidx.compose.ui:ui-tooling:\${$composeVersionVarName}",
                 configuration = "debugImplementation")
   addDependency(mavenCoordinate = "androidx.compose.ui:ui-tooling-preview:\${$composeVersionVarName}")
-  addDependency(mavenCoordinate = "androidx.lifecycle:lifecycle-runtime-ktx:+", minRev = "2.3.0-alpha06")
-  addDependency(mavenCoordinate = "androidx.activity:activity-compose:+", minRev = "1.3.0-alpha02")
+  addDependency(mavenCoordinate = "androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+  addDependency(mavenCoordinate = "androidx.activity:activity-compose:1.3.0-rc01")
   addDependency(mavenCoordinate = "androidx.compose.ui:ui-test-junit4:\${$composeVersionVarName}", configuration="androidTestImplementation")
   generateManifest(
     moduleData, activityClass, packageName, isLauncher, true,
