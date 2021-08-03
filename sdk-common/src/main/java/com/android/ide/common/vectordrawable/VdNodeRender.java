30902aa2e99687ac1de31771fa2b10a57fe63ab6
Sergey Prigogin <sprigogin@google.com>
2021-08-02 21:01:52 -0700

    Cosmetics
    Test: existing
    Bug: N/A
    Change-Id: I0e25c08fafbcf145599c345ceb9c500f51d0d39d

--- sdk-common/src/main/java/com/android/ide/common/vectordrawable/VdNodeRender.java
+++ sdk-common/src/main/java/com/android/ide/common/vectordrawable/VdNodeRender.java
@@ -19,6 +19,8 @@ import java.awt.geom.Path2D;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.jetbrains.annotations.NotNull;
+
 /**
  * Given an array of {@link VdPath.Node}, generates a Path2D object.
  * In other words, this is the engine which converts the pathData into
@@ -28,7 +30,7 @@ import java.util.logging.Logger;
 class VdNodeRender {
     private static final Logger LOGGER = Logger.getLogger(VdNodeRender.class.getSimpleName());
 
-    public static void createPath(VdPath.Node[] nodes, Path2D path) {
+    public static void createPath(@NotNull VdPath.Node[] nodes, @NotNull Path2D path) {
         float[] current = new float[6];
         char lastCmd = ' ';
         for (VdPath.Node node : nodes) {
@@ -37,8 +39,8 @@ class VdNodeRender {
         }
     }
 
-    private static void addCommand(Path2D path, float[] current, char cmd,
-            char lastCmd, float[] val) {
+    private static void addCommand(@NotNull Path2D path, float[] current, char cmd, char lastCmd,
+            float[] val) {
         int incr = 2;
 
         float cx = current[0];
@@ -90,12 +92,11 @@ class VdNodeRender {
 
             switch (cmd) {
                 case 'm':
-                    cx += val[k + 0];
+                    cx += val[k];
                     cy += val[k + 1];
                     if (k > 0) {
-                        // According to the spec, if a moveto is followed by multiple
-                        // pairs of coordinates, the subsequent pairs are treated as
-                        // implicit lineto commands.
+                        // If a moveto is followed by multiple pairs of coordinates, the subsequent
+                        // pairs are treated as implicit lineto commands.
                         path.lineTo(cx, cy);
                     } else {
                         path.moveTo(cx, cy);
@@ -103,13 +104,13 @@ class VdNodeRender {
                         loopY = cy;
                     }
                     break;
+
                 case 'M':
-                    cx = val[k + 0];
+                    cx = val[k];
                     cy = val[k + 1];
                     if (k > 0) {
-                        // According to the spec, if a moveto is followed by multiple
-                        // pairs of coordinates, the subsequent pairs are treated as
-                        // implicit lineto commands.
+                        // If a moveto is followed by multiple pairs of coordinates, the subsequent
+                        // pairs are treated as implicit lineto commands.
                         path.lineTo(cx, cy);
                     } else {
                         path.moveTo(cx, cy);
@@ -117,133 +118,145 @@ class VdNodeRender {
                         loopY = cy;
                     }
                     break;
+
                 case 'l':
-                    cx += val[k + 0];
+                    cx += val[k];
                     cy += val[k + 1];
                     path.lineTo(cx, cy);
                     break;
+
                 case 'L':
-                    cx = val[k + 0];
+                    cx = val[k];
                     cy = val[k + 1];
                     path.lineTo(cx, cy);
                     break;
+
                 case 'z':
                 case 'Z':
                     path.closePath();
                     cx = loopX;
                     cy = loopY;
                     break;
+
                 case 'h':
-                    cx += val[k + 0];
+                    cx += val[k];
                     path.lineTo(cx, cy);
                     break;
+
                 case 'H':
-                    path.lineTo(val[k + 0], cy);
-                    cx = val[k + 0];
+                    path.lineTo(val[k], cy);
+                    cx = val[k];
                     break;
+
                 case 'v':
-                    cy += val[k + 0];
+                    cy += val[k];
                     path.lineTo(cx, cy);
                     break;
+
                 case 'V':
-                    path.lineTo(cx, val[k + 0]);
-                    cy = val[k + 0];
+                    path.lineTo(cx, val[k]);
+                    cy = val[k];
                     break;
+
                 case 'c':
-                    path.curveTo(cx + val[k + 0], cy + val[k + 1], cx + val[k + 2],
+                    path.curveTo(cx + val[k], cy + val[k + 1], cx + val[k + 2],
                             cy + val[k + 3], cx + val[k + 4], cy + val[k + 5]);
                     cpx = cx + val[k + 2];
                     cpy = cy + val[k + 3];
                     cx += val[k + 4];
                     cy += val[k + 5];
                     break;
+
                 case 'C':
-                    path.curveTo(val[k + 0], val[k + 1], val[k + 2], val[k + 3],
-                            val[k + 4], val[k + 5]);
+                    path.curveTo(val[k], val[k + 1], val[k + 2], val[k + 3], val[k + 4],
+                            val[k + 5]);
                     cx = val[k + 4];
                     cy = val[k + 5];
                     cpx = val[k + 2];
                     cpy = val[k + 3];
                     break;
+
                 case 's':
                     reflectCtrl = (lastCmd == 'c' || lastCmd == 's' || lastCmd == 'C' || lastCmd == 'S');
-                    path.curveTo(reflectCtrl ? 2 * cx - cpx : cx, reflectCtrl ? 2
-                            * cy - cpy : cy, cx + val[k + 0], cy + val[k + 1], cx
-                            + val[k + 2], cy + val[k + 3]);
-
-                    cpx = cx + val[k + 0];
+                    path.curveTo(reflectCtrl ? 2 * cx - cpx : cx, reflectCtrl ? 2 * cy - cpy : cy,
+                            cx + val[k], cy + val[k + 1], cx + val[k + 2], cy + val[k + 3]);
+                    cpx = cx + val[k];
                     cpy = cy + val[k + 1];
                     cx += val[k + 2];
                     cy += val[k + 3];
                     break;
+
                 case 'S':
                     reflectCtrl = (lastCmd == 'c' || lastCmd == 's' || lastCmd == 'C' || lastCmd == 'S');
-                    path.curveTo(reflectCtrl ? 2 * cx - cpx : cx, reflectCtrl ? 2
-                            * cy - cpy : cy, val[k + 0], val[k + 1], val[k + 2],
-                            val[k + 3]);
-                    cpx = (val[k + 0]);
+                    path.curveTo(reflectCtrl ? 2 * cx - cpx : cx, reflectCtrl ? 2 * cy - cpy : cy,
+                            val[k], val[k + 1], val[k + 2], val[k + 3]);
+                    cpx = (val[k]);
                     cpy = (val[k + 1]);
                     cx = val[k + 2];
                     cy = val[k + 3];
                     break;
+
                 case 'q':
-                    path.quadTo(cx + val[k + 0], cy + val[k + 1], cx + val[k + 2],
+                    path.quadTo(cx + val[k], cy + val[k + 1], cx + val[k + 2],
                             cy + val[k + 3]);
-                    cpx = cx + val[k + 0];
+                    cpx = cx + val[k];
                     cpy = cy + val[k + 1];
                     // Note that we have to update cpx first, since cx will be updated here.
                     cx += val[k + 2];
                     cy += val[k + 3];
                     break;
+
                 case 'Q':
-                    path.quadTo(val[k + 0], val[k + 1], val[k + 2], val[k + 3]);
+                    path.quadTo(val[k], val[k + 1], val[k + 2], val[k + 3]);
                     cx = val[k + 2];
                     cy = val[k + 3];
-                    cpx = val[k + 0];
+                    cpx = val[k];
                     cpy = val[k + 1];
                     break;
+
                 case 't':
                     reflectCtrl = (lastCmd == 'q' || lastCmd == 't' || lastCmd == 'Q' || lastCmd == 'T');
                     tempReflectedX = reflectCtrl ? 2 * cx - cpx : cx;
                     tempReflectedY = reflectCtrl ? 2 * cy - cpy : cy;
-                    path.quadTo(tempReflectedX, tempReflectedY, cx + val[k + 0], cy + val[k + 1]);
+                    path.quadTo(tempReflectedX, tempReflectedY, cx + val[k], cy + val[k + 1]);
                     cpx = tempReflectedX;
                     cpy = tempReflectedY;
-                    cx += val[k + 0];
+                    cx += val[k];
                     cy += val[k + 1];
                     break;
+
                 case 'T':
                     reflectCtrl = (lastCmd == 'q' || lastCmd == 't' || lastCmd == 'Q' || lastCmd == 'T');
                     tempReflectedX = reflectCtrl ? 2 * cx - cpx : cx;
                     tempReflectedY = reflectCtrl ? 2 * cy - cpy : cy;
-                    path.quadTo(tempReflectedX, tempReflectedY, val[k + 0], val[k + 1]);
-                    cx = val[k + 0];
+                    path.quadTo(tempReflectedX, tempReflectedY, val[k], val[k + 1]);
+                    cx = val[k];
                     cy = val[k + 1];
                     cpx = tempReflectedX;
                     cpy = tempReflectedY;
                     break;
+
                 case 'a':
                     // (rx ry x-axis-rotation large-arc-flag sweep-flag x y)
                     drawArc(path, cx, cy, val[k + 5] + cx, val[k + 6] + cy,
-                            val[k + 0], val[k + 1], val[k + 2], val[k + 3] != 0,
-                                    val[k + 4] != 0);
+                            val[k], val[k + 1], val[k + 2], val[k + 3] != 0,
+                            val[k + 4] != 0);
                     cx += val[k + 5];
                     cy += val[k + 6];
                     cpx = cx;
                     cpy = cy;
-
                     break;
+
                 case 'A':
-                    drawArc(path, cx, cy, val[k + 5], val[k + 6], val[k + 0],
-                            val[k + 1], val[k + 2], val[k + 3] != 0,
-                                    val[k + 4] != 0);
+                    drawArc(path, cx, cy, val[k + 5], val[k + 6], val[k], val[k + 1],
+                            val[k + 2], val[k + 3] != 0, val[k + 4] != 0);
                     cx = val[k + 5];
                     cy = val[k + 6];
                     cpx = cx;
                     cpy = cy;
                     break;
-
             }
+
             lastCmd = cmd;
         }
         current[0] = cx;
@@ -254,42 +267,41 @@ class VdNodeRender {
         current[5] = loopY;
     }
 
-    private static void drawArc(Path2D p, float x0, float y0, float x1,
-            float y1, float a, float b, float theta, boolean isMoreThanHalf,
-            boolean isPositiveArc) {
+    private static void drawArc(@NotNull Path2D p, double x0, double y0, double x1, double y1,
+            double a, double b, double theta, boolean isMoreThanHalf, boolean isPositiveArc) {
         LOGGER.log(Level.FINE, "(" + x0 + "," + y0 + ")-(" + x1 + "," + y1
                 + ") {" + a + " " + b + "}");
-        /* Convert rotation angle from degrees to radians */
+        // Convert rotation angle from degrees to radians.
         double thetaD = theta * Math.PI / 180.0f;
-        /* Pre-compute rotation matrix entries */
+        // Pre-compute rotation matrix entries.
         double cosTheta = Math.cos(thetaD);
         double sinTheta = Math.sin(thetaD);
-        /* Transform (x0, y0) and (x1, y1) into unit space */
-        /* using (inverse) rotation, followed by (inverse) scale */
+        // Transform (x0, y0) and (x1, y1) into unit space using (inverse) rotation,
+        // followed by (inverse) scale.
         double x0p = (x0 * cosTheta + y0 * sinTheta) / a;
         double y0p = (-x0 * sinTheta + y0 * cosTheta) / b;
         double x1p = (x1 * cosTheta + y1 * sinTheta) / a;
         double y1p = (-x1 * sinTheta + y1 * cosTheta) / b;
         LOGGER.log(Level.FINE, "unit space (" + x0p + "," + y0p + ")-(" + x1p
                 + "," + y1p + ")");
-        /* Compute differences and averages */
+        // Compute differences and averages.
         double dx = x0p - x1p;
         double dy = y0p - y1p;
         double xm = (x0p + x1p) / 2;
         double ym = (y0p + y1p) / 2;
-        /* Solve for intersecting unit circles */
+        // Solve for intersecting unit circles.
         double dsq = dx * dx + dy * dy;
         if (dsq == 0.0) {
             LOGGER.log(Level.FINE, " Points are coincident");
-            return; /* Points are coincident */
+            return; // Points are coincident.
         }
         double disc = 1.0 / dsq - 1.0 / 4.0;
         if (disc < 0.0) {
             LOGGER.log(Level.FINE, "Points are too far apart " + dsq);
-            float adjust = (float) (Math.sqrt(dsq) / 1.99999);
+            double adjust = Math.sqrt(dsq) / 1.99999;
             drawArc(p, x0, y0, x1, y1, a * adjust, b * adjust, theta,
                     isMoreThanHalf, isPositiveArc);
-            return; /* Points are too far apart */
+            return; // Points are too far apart.
         }
         double s = Math.sqrt(disc);
         double sdx = s * dx;
@@ -304,14 +316,14 @@ class VdNodeRender {
             cy = ym - sdx;
         }
 
-        double eta0 = Math.atan2((y0p - cy), (x0p - cx));
-        LOGGER.log(Level.FINE, "eta0 = Math.atan2( " + (y0p - cy) + " , "
-                + (x0p - cx) + ") = " + Math.toDegrees(eta0));
+        double eta0 = Math.atan2(y0p - cy, x0p - cx);
+        LOGGER.log(Level.FINE, "eta0 = Math.atan2(" + (y0p - cy) + ", " + (x0p - cx) + ") = "
+                + Math.toDegrees(eta0));
 
-        double eta1 = Math.atan2((y1p - cy), (x1p - cx));
-        LOGGER.log(Level.FINE, "eta1 = Math.atan2( " + (y1p - cy) + " , "
-                + (x1p - cx) + ") = " + Math.toDegrees(eta1));
-        double sweep = (eta1 - eta0);
+        double eta1 = Math.atan2(y1p - cy, x1p - cx);
+        LOGGER.log(Level.FINE, "eta1 = Math.atan2(" + (y1p - cy) + ", " + (x1p - cx) + ") = "
+                + Math.toDegrees(eta1));
+        double sweep = eta1 - eta0;
         if (isPositiveArc != (sweep >= 0)) {
             if (sweep > 0) {
                 sweep -= 2 * Math.PI;
@@ -325,12 +337,9 @@ class VdNodeRender {
         double tcx = cx;
         cx = cx * cosTheta - cy * sinTheta;
         cy = tcx * sinTheta + cy * cosTheta;
-        LOGGER.log(
-                Level.FINE,
-                        "cx, cy, a, b, x0, y0, thetaD, eta0, sweep = " + cx + " , "
-                        + cy + " , " + a + " , " + b + " , " + x0 + " , " + y0
-                        + " , " + Math.toDegrees(thetaD) + " , "
-                        + Math.toDegrees(eta0) + " , " + Math.toDegrees(sweep));
+        LOGGER.log(Level.FINE, "cx = " + cx + ", cy = " + cy + ", a = " + a + ", b = " + b
+                + ", x0 = " + x0 + ", y0 = " + y0 + ", thetaD = " + Math.toDegrees(thetaD)
+                + ", eta0 = " + Math.toDegrees(eta0) + ", sweep = " + Math.toDegrees(sweep));
 
         arcToBezier(p, cx, cy, a, b, x0, y0, thetaD, eta0, sweep);
     }
@@ -349,9 +358,8 @@ class VdNodeRender {
      * @param start The start angle of the arc on the ellipse
      * @param sweep The angle (positive or negative) of the sweep of the arc on the ellipse
      */
-    private static void arcToBezier(Path2D p, double cx, double cy, double a,
-            double b, double e1x, double e1y, double theta, double start,
-            double sweep) {
+    private static void arcToBezier(@NotNull Path2D p, double cx, double cy, double a, double b,
+            double e1x, double e1y, double theta, double start, double sweep) {
         // Taken from equations at:
         // http://spaceroots.org/documents/ellipse/node8.html
         // and http://www.spaceroots.org/documents/ellipse/node22.html
@@ -383,7 +391,7 @@ class VdNodeRender {
             double q2x = e2x - alpha * ep2x;
             double q2y = e2y - alpha * ep2y;
 
-            p.curveTo((float) q1x, (float) q1y, (float) q2x, (float) q2y, (float) e2x, (float) e2y);
+            p.curveTo(q1x, q1y, q2x, q2y, e2x, e2y);
             eta1 = eta2;
             e1x = e2x;
             e1y = e2y;
