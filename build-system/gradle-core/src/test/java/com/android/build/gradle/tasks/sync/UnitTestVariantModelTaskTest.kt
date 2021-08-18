5ea8171d958f5a51557daa9f80fb2e9a246aa424
Jerome Dochez <jedo@google.com>
2021-08-17 17:39:43 -0700

    added skeleton tasks and tests for all module types
    the task are not wired to the model yet.
    
    Test: added unit tests
    Bug: N/A
    
    Change-Id: Ib4143989e19074fb0965cbeb354e6a19f948fae1

---
+++ build-system/gradle-core/src/test/java/com/android/build/gradle/tasks/sync/UnitTestVariantModelTaskTest.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.build.gradle.tasks.sync
+
+import com.android.ide.model.sync.Variant
+import com.google.common.truth.Truth
+import org.junit.Before
+import org.junit.Test
+
+internal class UnitTestVariantModelTaskTest: VariantModelTaskAbstractTest<UnitTestVariantModelTask>() {
+
+    @Before
+    fun setUp() {
+        super.setUp(UnitTestVariantModelTask::class.java)
+    }
+
+    @Test
+    fun testTaskAction() {
+        super.testTaskAction(
+            given = {},
+            expect = {
+                Truth.assertThat(it.variantCase).isEqualTo(Variant.VariantCase.VARIANT_NOT_SET)
+            }
+        )
+    }
+}
