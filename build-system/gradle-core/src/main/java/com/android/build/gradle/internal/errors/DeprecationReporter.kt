29c00f45ffb9a2d8d3d0dc7390163b4fce5ebcdb
Scott Pollom <spollom@google.com>
2021-09-20 10:52:01 -0700

    Softly enforce android.bundle.enableUncompressedNativeLibs
    Bug: 174695257
    Test: existing
    Change-Id: I9c108e6a33c51bd446146294d4d9eb07d390aa1a

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/errors/DeprecationReporter.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/errors/DeprecationReporter.kt
@@ -82,6 +82,20 @@ interface DeprecationReporter {
             "please migrate Maven Publish Plugin. See " +
             "https://developer.android.com/studio/build/maven-publish-plugin for more information."
         ),
+
+        ENABLE_UNCOMPRESSED_NATIVE_LIBS_IN_BUNDLE(
+            Version.VERSION_8_0,
+            """
+                You can add the following to your build.gradle instead:
+                android {
+                    packagingOptions {
+                        jniLibs {
+                            useLegacyPackaging = true
+                        }
+                    }
+                }
+            """.trimIndent()
+        ),
         ;
 
         fun getDeprecationTargetMessage(): String {
