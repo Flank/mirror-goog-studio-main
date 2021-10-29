545d89d80570bd5b5e384dc7d92e8e0a60d97192
Amr Afifiy <amrmahmoud@google.com>
2021-10-29 18:48:14 +0100

    Enable CheckAll test for testFixturesApp
    Bug: 139438142
    Test: CheckAll
    Change-Id: I98cdadd71d67933940de099c6e8d9e9c93494aa9

--- build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/automatic/CheckAll.java
+++ build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/automatic/CheckAll.java
@@ -149,7 +149,6 @@ public class CheckAll {
                     "lintCustomLocalAndPublishRules", // contains integ test for lint itself
                     "simpleCompositeBuild", // broken composite build project.
                     "multiCompositeBuild", // too complex composite build project to setup
-                    "sourceDependency", // not set up fully, just used for sync tests
-                    "testFixturesApp" // disabled due to bug in Gradle, b/201391004
+                    "sourceDependency" // not set up fully, just used for sync tests
                     );
 }
