f202ad1f3b3f257ccaa70ad00d34c8a30b220c00
Stephen Atwood <stephenatwood@google.com>
2021-10-28 18:00:46 -0700

    Disable caching by default in DesugarLibKeepRulesMergeTask
    Task is not worth caching because it does so little work.
    Calculating cache hit/miss and fetching results is likely
    more expensive than simply executing the task.
    
    Bug: 200002454
    Test: N/A
    Change-Id: Id42df6d81e74c7868b127dd28299cac2adf68a92

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/tasks/DesugarLibKeepRulesMergeTask.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/tasks/DesugarLibKeepRulesMergeTask.kt
@@ -22,17 +22,22 @@ import com.android.build.gradle.internal.scope.InternalArtifactType
 import com.android.build.gradle.internal.tasks.factory.VariantTaskCreationAction
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.RegularFileProperty
-import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.PathSensitive
 import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskProvider
+import org.gradle.work.DisableCachingByDefault
 
 /**
  * A task merging desugar lib keep rules generated in dynamic feature modules.
+ *
+ * Caching disabled by default for this task because the task does very little work.
+ * Input files are read from disk and concatenated into a single output file.
+ * Calculating cache hit/miss and fetching results is likely more expensive than
+ * simply executing the task.
  */
-@CacheableTask
+@DisableCachingByDefault
 abstract class DesugarLibKeepRulesMergeTask : NonIncrementalTask() {
 
     @get:InputFiles
