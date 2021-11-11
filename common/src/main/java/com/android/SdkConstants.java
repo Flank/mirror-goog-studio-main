a06de47cd1b83a11041b03126a1f366e8623eafb
bingran <bingran@google.com>
2021-11-11 14:04:30 -0800

    Upgrade gradle version to 7.3
    Bug: 205859286
    Test: existing
    Change-Id: Icbbf9270a799815d966b23d538c03824ebe15500

--- common/src/main/java/com/android/SdkConstants.java
+++ common/src/main/java/com/android/SdkConstants.java
@@ -2862,7 +2862,7 @@ public final class SdkConstants {
      * Studio product. It need not actually be the latest version of Gradle, but it will most likely
      * be fairly recent.
      */
-    public static final String GRADLE_LATEST_VERSION = "7.3-rc-1";
+    public static final String GRADLE_LATEST_VERSION = "7.3";
 
     /**
      * The minimum released version of the Android Gradle Plugin that this version of Studio will
