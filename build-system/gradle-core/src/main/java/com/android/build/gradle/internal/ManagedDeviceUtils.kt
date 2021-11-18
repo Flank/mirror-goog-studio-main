79cb1c4dad68fc14814d6b999e2b912908545aaa
Daniel LaBar <daniellabar@google.com>
2021-11-18 10:38:56 -0800

    [AGP UTP] fix issue to reenable x86 atd images
    Fixed "host abi" logic to allow x86 atd images to be accessable.
    
    Bug: 206858611
    Test: added tests
    Change-Id: Ib33f12a074dc154d42614f631447295e14d7fe42

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/ManagedDeviceUtils.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/ManagedDeviceUtils.kt
@@ -79,6 +79,7 @@ fun computeAbiFromArchitecture(require64Bit: Boolean, apiLevel: Int, vendor: Str
         cpuArchitecture == CpuArchitecture.ARM
             || cpuArchitecture == CpuArchitecture.X86_ON_ARM-> "arm64-v8a"
         require64Bit -> "x86_64"
+        (vendor == "aosp-atd" || vendor == "google-atd") && apiLevel <= 30 -> "x86"
         // system-images;android-30;default;x86 does not exist, but the google images do.
         vendor == "aosp" && apiLevel <= 29 -> "x86"
         vendor == "google" && apiLevel <= 30 -> "x86"
