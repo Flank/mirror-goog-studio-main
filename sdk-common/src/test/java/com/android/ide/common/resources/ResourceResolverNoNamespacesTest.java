17b22ec1087bdf6007f946db02acd3bcf9e88c39
Dana Dahlstrom <dahlstrom@google.com>
2022-01-13 10:00:00 -0800

    Revise bad test code
    Stub methods call JUnit's fail method, which is bizarre at least.
    Hundreds of lines away a catch statement expects some Error subclass,
    which shouldn't be done—and these oddities turn out to be connected!
    
    Instead of calling fail, the stubs can throw reasonably appropriate
    RuntimeException instances, making the catch explicitly connected.
    
    A weird sequence involving unnecessarily scoped and mutated variables
    around the try-catch can also be replaced with the standard pattern.
    
    Change-Id: Id50d56668f4a934887d66b8d54fb34afa0ee1a79
    Issue: 214269611

--- sdk-common/src/test/java/com/android/ide/common/resources/ResourceResolverNoNamespacesTest.java
+++ sdk-common/src/test/java/com/android/ide/common/resources/ResourceResolverNoNamespacesTest.java
@@ -41,7 +41,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
-import junit.framework.AssertionFailedError;
 import junit.framework.TestCase;
 
 public class ResourceResolverNoNamespacesTest extends TestCase {
@@ -232,7 +231,7 @@ public class ResourceResolverNoNamespacesTest extends TestCase {
                     @Override
                     public void warning(
                             String tag, String message, Object viewCookie, Object data) {
-                        fail(message);
+                        throw new IllegalArgumentException(message);
                     }
 
                     @Override
@@ -242,12 +241,12 @@ public class ResourceResolverNoNamespacesTest extends TestCase {
                             Throwable throwable,
                             Object viewCookie,
                             Object data) {
-                        fail(message);
+                        throw new IllegalArgumentException(message);
                     }
 
                     @Override
                     public void error(String tag, String message, Object viewCookie, Object data) {
-                        fail(message);
+                        throw new IllegalArgumentException(message);
                     }
 
                     @Override
@@ -257,7 +256,7 @@ public class ResourceResolverNoNamespacesTest extends TestCase {
                             Throwable throwable,
                             Object viewCookie,
                             Object data) {
-                        fail(message);
+                        throw new IllegalArgumentException(message);
                     }
                 };
         resolver.setLogger(logger);
@@ -400,21 +399,18 @@ public class ResourceResolverNoNamespacesTest extends TestCase {
                                 new ResourceValueImpl(
                                         RES_AUTO, ResourceType.ID, "my_id", "@+id/some_new_id"))
                         .isFramework());
-        // error expected.
-        boolean failed = false;
-        ResourceValue val = null;
         try {
-            val =
+            ResourceValue val =
                     resolver.resolveResValue(
                             new ResourceValueImpl(
                                     RES_AUTO,
                                     ResourceType.STRING,
                                     "bright_foreground_dark",
                                     "@color/background_light"));
-        } catch (AssertionFailedError expected) {
-            failed = true;
+            fail("incorrect resource returned: " + val);
+        } catch (IllegalArgumentException expected) {
+            // pass
         }
-        assertTrue("incorrect resource returned: " + val, failed);
         ResourceValue array = resolver
                 .resolveResValue(resolver.getProjectResource(ResourceType.ARRAY, "my_array"));
         assertTrue("array" + "my_array" + "resolved incorrectly as " + array.getResourceType()
@@ -496,7 +492,7 @@ public class ResourceResolverNoNamespacesTest extends TestCase {
                                 .equals(message)) {
                             wasWarned.set(true);
                         } else {
-                            fail(message);
+                            throw new IllegalArgumentException(message);
                         }
                     }
                 };
@@ -552,7 +548,7 @@ public class ResourceResolverNoNamespacesTest extends TestCase {
                                 .equals(message)) {
                             wasWarned.set(true);
                         } else {
-                            fail(message);
+                            throw new IllegalArgumentException(message);
                         }
                     }
                 };
@@ -817,7 +813,7 @@ public class ResourceResolverNoNamespacesTest extends TestCase {
                                 .equals(message)) {
                             wasWarned.set(true);
                         } else {
-                            fail(message);
+                            throw new IllegalArgumentException(message);
                         }
                     }
                 };
