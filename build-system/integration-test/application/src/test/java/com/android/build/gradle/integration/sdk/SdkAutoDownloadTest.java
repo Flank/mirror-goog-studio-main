8a6fcbf715b436b8529aed3664b0658284a5224e
Chris Warrington <cmw@google.com>
2022-01-13 16:56:01 +0000

    Prepare for AGP tests to run against API 32
    Making them not brittle to updating of
    DEFAULT_COMPILE_SDK_VERSION
    
    Bug:
    Test: Included, follows with update
    Change-Id: I2b52d1766cfe16a2476ecec4c8eeb436dc236223

--- build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/sdk/SdkAutoDownloadTest.java
+++ build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/sdk/SdkAutoDownloadTest.java
@@ -72,6 +72,7 @@ public class SdkAutoDownloadTest {
             + System.lineSeparator()
             + "target_link_libraries(hello-jni log)";
 
+    // These all need to be kept in sync with the sibling BUILD file
     private static final String BUILD_TOOLS_VERSION = SdkConstants.CURRENT_BUILD_TOOLS_VERSION;
     private static final String PLATFORM_VERSION = "31";
     private static final String NDK_VERSION = "23.0.7344513";
