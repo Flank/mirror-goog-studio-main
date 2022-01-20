e0d25e76715af7e90958316b2629e8211cbd3e6b
Tor Norbye <tnorbye@google.com>
2022-01-20 08:47:54 -0800

    Bump lint's API constant
    This makes it possible for lint checks to declare compatibility
    with recent lint API changes.
    
    Test: Existing
    Bug: Not filed
    Change-Id: Iafe1343f3ab5039289f986a7e58717dc53ba0863

--- lint/libs/lint-api/src/main/java/com/android/tools/lint/detector/api/Api.kt
+++ lint/libs/lint-api/src/main/java/com/android/tools/lint/detector/api/Api.kt
@@ -25,12 +25,14 @@ import com.android.tools.lint.client.api.IssueRegistry
  * the hosting lint environment when the custom lint checks are loaded
  * into lint.
  */
-const val CURRENT_API = 11
+const val CURRENT_API = 13
 
 /** Describes the given API level. */
 fun describeApi(api: Int): String {
     return when (api) {
-        11 -> "7.1+" // 7.1.0-alpha06
+        13 -> "7.3+" // 7.3.0-alpha02
+        12 -> "7.2" // 7.2.0-beta02
+        11 -> "7.1" // 7.1.0-alpha06
         10 -> "7.0" // 7.0.0-alpha04
         9 -> "4.2" // 4.2.0-alpha08
         8 -> "4.1" // 4.1.0-alpha06
