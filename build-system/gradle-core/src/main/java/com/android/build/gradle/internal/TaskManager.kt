13d37b42a0e0b54bd454b744c3ad1eed6d949385
Diego Perez <diegoperez@google.com>
2022-03-15 17:57:27 +0000

    Update Compose 1.2 to alpha18
    Fixes: 223785186
    Test: bazel test //tools/base/build-system/...
    Change-Id: I4ef863fb07adeaec6f89b1d47e02ffc8d81434e8

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/TaskManager.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/TaskManager.kt
@@ -3330,7 +3330,7 @@ abstract class TaskManager<VariantBuilderT : VariantBuilderImpl, VariantT : Vari
 
         // Temporary static variables for Kotlin+Compose configuration
         const val KOTLIN_COMPILER_CLASSPATH_CONFIGURATION_NAME = "kotlinCompilerClasspath"
-        const val COMPOSE_KOTLIN_COMPILER_EXTENSION_VERSION = "1.2.0-alpha02"
+        const val COMPOSE_KOTLIN_COMPILER_EXTENSION_VERSION = "1.2.0-alpha05"
         const val CREATE_MOCKABLE_JAR_TASK_NAME = "createMockableJar"
 
         /**
