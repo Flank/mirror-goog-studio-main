7131bd362ae86b2e838af94e57adbcb30706b411
Alon Albert <aalbert@google.com>
2022-04-04 08:46:46 -0700

    Make trackDevices Start a New Channel
    So we can test exception thrown by "adb killserver"
    
    Bug: 227905221
    Bug: 191880898
    Test: N/A
    Change-Id: I0d0f7e4bdb4f0ae757d1c427b272b341d51aec24

--- adblib/src/com/android/adblib/testing/FakeAdbHostServices.kt
+++ adblib/src/com/android/adblib/testing/FakeAdbHostServices.kt
@@ -27,9 +27,11 @@ import com.android.adblib.MdnsServiceList
 import com.android.adblib.PairResult
 import com.android.adblib.SocketSpec
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.Channel.Factory.UNLIMITED
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.consumeAsFlow
 import java.io.Closeable
+import java.util.concurrent.CopyOnWriteArrayList
 
 /**
  * A fake implementation of [AdbHostServices] for tests.
@@ -40,20 +42,53 @@ class FakeAdbHostServices(override val session: AdbLibSession) : AdbHostServices
      * The response of the [devices] method.
      */
     var devices = DeviceList(emptyList(), emptyList())
-    private val trackDevicesChannel = Channel<DeviceList>(10)
+
+    // TODO(aalbert): figure out how to import a GuardedBy annotation.
+    //@GuardedBy("itself")
+    private var trackDevicesChannels =
+        CopyOnWriteArrayList<Channel<DeviceList>>()
 
     /**
      * Controls the [trackDevices] call by sending a [DeviceList] to the flow (via a channel).
+     *
+     * @param deviceList the [DeviceList] to send
+     * @param index indicates which channel to send it to. A new channel is created every time
+     * [trackDevices] is called. -1 means the latest.
      */
-    suspend fun sendDeviceList(deviceList: DeviceList) {
-        trackDevicesChannel.send(deviceList)
+    suspend fun sendDeviceList(deviceList: DeviceList, index: Int = -1) {
+        val channel = synchronized(trackDevicesChannels) {
+            try {
+                if (index >= 0) trackDevicesChannels[index] else trackDevicesChannels.last()
+            } catch (e: IndexOutOfBoundsException) {
+                throw IllegalStateException("Channel not found. Did you call trackDevices()?")
+            }
+        }
+        channel.send(deviceList)
     }
 
-    fun closeTrackDevicesFlow(e: Throwable? = null) {
-        trackDevicesChannel.close(e)
+    /**
+     * Terminates a flow from the [trackDevices] call by closing the backing [Channel]
+     *
+     * @param index indicates which channel to colse it to. A new channel is created every time
+     * [trackDevices] is called. -1 means the latest.
+     * @param e throwable to close with. null means cancellation.
+     */
+    fun closeTrackDevicesFlow(index: Int = -1, e: Throwable? = null) {
+        val channel = synchronized(trackDevicesChannels) {
+            try {
+                if (index >= 0) trackDevicesChannels[index] else trackDevicesChannels.last()
+            } catch (e: IndexOutOfBoundsException) {
+                throw IllegalStateException("Channel not found. Did you call trackDevices()?")
+            }
+        }
+        channel.close(e)
     }
+
     override fun close() {
-        closeTrackDevicesFlow()
+        synchronized(trackDevicesChannels) {
+            trackDevicesChannels.forEach { it.close() }
+            trackDevicesChannels.clear()
+        }
     }
 
     override suspend fun version(): Int = 0
@@ -61,8 +96,14 @@ class FakeAdbHostServices(override val session: AdbLibSession) : AdbHostServices
     override suspend fun devices(format: AdbHostServices.DeviceInfoFormat): DeviceList =
         DeviceList(devices, emptyList())
 
-    override fun trackDevices(format: AdbHostServices.DeviceInfoFormat): Flow<DeviceList> =
-        trackDevicesChannel.consumeAsFlow()
+    override fun trackDevices(format: AdbHostServices.DeviceInfoFormat): Flow<DeviceList> {
+        return Channel<DeviceList>(UNLIMITED).let {
+            synchronized(trackDevicesChannels) {
+                trackDevicesChannels.add(it)
+            }
+            it.consumeAsFlow()
+        }
+    }
 
     override suspend fun hostFeatures(): List<String> {
         TODO("Not yet implemented")
@@ -121,3 +162,4 @@ class FakeAdbHostServices(override val session: AdbLibSession) : AdbHostServices
         TODO("Not yet implemented")
     }
 }
+
