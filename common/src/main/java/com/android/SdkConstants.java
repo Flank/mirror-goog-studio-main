a07c2f88b20bc9cdf300f39b9798dbadc5959c6b
Bingran Li <bingran@google.com>
2022-04-14 18:33:42 +0000

    Revert "Upgrade gradle version to 7.4.2"
    Revert "Upgrade gradle version to 7.4.2"
    
    Revert "Upgrade gradle version to 7.4.2"
    
    Revert submission 17606153-7.4.2
    
    Reason for revert: <regression spotted in 7.4.2>
    Reverted Changes:
    I505ec6ac7:Upgrade gradle version to 7.4.2
    I9f7188006:Upgrade gradle version to 7.4.2
    I102f5202f:Upgrade gradle version to 7.4.2
    I1009bab68:Upgrade gradle version to 7.4.2
    
    Change-Id: I7ff05536f73a5cf5b28f82559c8e8a3163993dc4

--- common/src/main/java/com/android/SdkConstants.java
+++ common/src/main/java/com/android/SdkConstants.java
@@ -2925,7 +2925,7 @@ public final class SdkConstants {
      * Studio product. It need not actually be the latest version of Gradle, but it will most likely
      * be fairly recent.
      */
-    public static final String GRADLE_LATEST_VERSION = "7.4.2";
+    public static final String GRADLE_LATEST_VERSION = "7.4";
 
     /**
      * The minimum released version of the Android Gradle Plugin that this version of Studio will
