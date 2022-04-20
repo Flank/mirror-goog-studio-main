1370aa8808bd807a304d1734ae9cb55aef2e5ee2
Tom Renn <tomrenn@google.com>
2022-04-20 11:05:50 -0700

    Always report test results to AnTS
    Bug: 202790555
    Test: n/a
    Change-Id: I04711f6152a789fa046c7e041d697ae1f19d8f9b

--- bazel/studio_coverage.sh
+++ bazel/studio_coverage.sh
@@ -74,7 +74,6 @@ fi
 declare -a extra_test_flags
 if [[ $postsubmit ]]; then
     extra_test_flags+=(--nocache_test_results)
-    extra_test_flags+=(--config=ants)
     extra_test_flags+=(--flaky_test_attempts=2)
 fi
 
@@ -91,7 +90,7 @@ fi
 "${script_dir}/bazel" \
   "${bazelrc_flags[@]}" \
   test \
-  --config=dynamic --config=sponge \
+  --config=dynamic --config=sponge --config=ants \
   --invocation_id=${invocation_id} \
   --tool_tag="studio_coverage.sh" \
   --build_event_binary_file="${dist_dir:-/tmp}/bazel-${build_number}.bes" \
