c56c9227021f62214dc9934f845a9117e6d08e25
Mohammad Saboorian <msab@google.com>
2022-04-22 17:37:28 +0100

    Add preview params for Compose Wear template
    This uses a round device for previews which is preferred for wear.
    
    Fixes:229971705
    
    Change-Id: I8062f9b940ebe2178efc894263a19e77cc374a41
    (cherry picked from commit 6f6e325a535b372ccbcb155356348f837b448367)

--- wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/activities/composeWearActivity/src/app_package/composeWearActivityKt.kt
+++ wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/activities/composeWearActivity/src/app_package/composeWearActivityKt.kt
@@ -47,6 +47,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
@@ -90,7 +91,7 @@ fun ${greeting}(greetingName: String) {
     )
 }
 
-@Preview
+@Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
 @Composable
 fun ${defaultPreview}() {
     ${wearAppName}("Preview Android")
