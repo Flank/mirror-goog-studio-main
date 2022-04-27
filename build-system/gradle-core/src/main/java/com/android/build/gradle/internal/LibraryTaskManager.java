bd824c89229deaa03101064b765da1d2e94ee414
bingran <bingran@google.com>
2022-04-27 10:40:39 -0700

    Fix conflicts of dokka core dependency between agp and other plugins
    With this change, we use dokka-core as compileOnly dependency of AGP and
    remove it from the buildScript runtime classpath. At the same time, we
    provide dokka-core dependency via worker classloader. Therefore, the
    dokka-core that we use wouldn't override other dokka-core version used
    by other plugins(e.g. dokka gradle plugin).
    
    Due to the thread safety issue b/211725171, we limit the worker number
    to one for JavaDocGenerationTask. We could revist this thread safety
    issue later and remove the limit if our corresponding integration
    tests don't have flakiness.
    
    This CL doesn' bump up the dokka-core version to limit the scope of this
    change.
    
    Bug: 229979216
    Test: GmavenZipTest
    Change-Id: Idb53c76ff0be57641cb194a01b010f57baee4bb9

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/LibraryTaskManager.java
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/LibraryTaskManager.java
@@ -48,6 +48,7 @@ import com.android.build.gradle.internal.res.GenerateApiPublicTxtTask;
 import com.android.build.gradle.internal.res.GenerateEmptyResourceFilesTask;
 import com.android.build.gradle.internal.scope.BuildFeatureValues;
 import com.android.build.gradle.internal.scope.InternalArtifactType;
+import com.android.build.gradle.internal.services.DokkaParallelBuildService;
 import com.android.build.gradle.internal.tasks.AarMetadataTask;
 import com.android.build.gradle.internal.tasks.BundleLibraryClassesDir;
 import com.android.build.gradle.internal.tasks.BundleLibraryClassesJar;
@@ -373,6 +374,7 @@ public class LibraryTaskManager extends TaskManager<LibraryVariantBuilder, Libra
                 taskFactory.register(new SourceJarTask.CreationAction(variant));
             }
             if (components.stream().anyMatch(ComponentPublishingInfo::getWithJavadocJar)) {
+                new DokkaParallelBuildService.RegistrationAction(project).execute();
                 taskFactory.register(new JavaDocGenerationTask.CreationAction(variant));
                 taskFactory.register(new JavaDocJarTask.CreationAction(variant));
             }
