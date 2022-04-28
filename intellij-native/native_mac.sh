0b9c64ec84610bd52364d5ad64c59b5375374c6e
Eugene Yakavets <yakavets@google.com>
2022-04-28 14:13:16 -0700

    Include MacTouchBar into native mac utilities build
    Change-Id: I90d8e0ea12de28eb65e9ab6739d0fbc5b3d6f9f9

--- intellij-native/native_mac.sh
+++ intellij-native/native_mac.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
+set -eu
 
 # Build Intellij native tools for Mac
+
+function realpath() {
+   cd $1 && pwd
+}
+# Expected arguments:
+declare -r out_dir="$1"
+declare -r dist_dir="$2"
+declare -r build_number="$3"
+
+declare -r script_dir=$(realpath "$(dirname "$0")")
+declare exit_code=0
+
+(
+  echo "Building MacTouchBar..."
+  ${script_dir}/build-mac-touchbar.sh -o ${out_dir}/MacTouchBar -d ${dist_dir} || ((exit_code++))
+)
+
+echo "All Done."
+exit $exit_code
