a0d014e45a8bfe8d8da1d2f1c661a0c1e4d39899
Alexander Demurov <ademurov@google.com>
2022-05-11 20:46:14 -0700

    Move threading agent callback into a module
    Move threading agent callback interface into a separate module so that it could be used in adt/idea without adding a dependency on a threading agent.
    
    Bug: 228841408
    Test: manually tested
    Change-Id: Ice84c669626e59f4124ed08e1a0c226e66d4b01e

--- threading-agent-callback/src/com/android/tools/instrumentation/threading/agent/callback/ThreadingCheckerHook.java
+++ threading-agent-callback/src/com/android/tools/instrumentation/threading/agent/callback/ThreadingCheckerHook.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.tools.instrumentation.threading.agent;
+package com.android.tools.instrumentation.threading.agent.callback;
 
 public interface ThreadingCheckerHook {
     /**
