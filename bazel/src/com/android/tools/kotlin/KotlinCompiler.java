8c0aa7decfe1aa3bc5f9a8c7957b24ae13bd606e
Matthew Gharrity <gharrma@google.com>
2022-06-03 12:02:13 -0400

    Bazel Kotlin worker: clear jar cache after jobs
    See the bug and added code comment for details.
    
    If we wanted to be extra cautious and preclude this class of bugs,
    then we could use the --worker_quit_after_build Bazel flag
    (just like we already do on CI). However, this flag has a performance
    penalty for repeated builds.
    
    Fixes: 234840104
    Test: manual (using discovered repro scenario)
    Change-Id: I2f394479ed711ce776202638aec6ac69f23e48e7

--- bazel/src/com/android/tools/kotlin/KotlinCompiler.java
+++ bazel/src/com/android/tools/kotlin/KotlinCompiler.java
@@ -16,16 +16,20 @@
 
 package com.android.tools.kotlin;
 
-import com.android.tools.utils.BazelMultiplexWorker;
-import org.jetbrains.kotlin.cli.common.ExitCode;
-import org.jetbrains.kotlin.cli.jvm.K2JVMCompiler;
-
-import java.io.PrintStream;
-import java.util.List;
-
 import static org.jetbrains.kotlin.cli.common.CompilerSystemProperties.KOTLIN_COLORS_ENABLED_PROPERTY;
 import static org.jetbrains.kotlin.cli.common.CompilerSystemProperties.KOTLIN_COMPILER_ENVIRONMENT_KEEPALIVE_PROPERTY;
 import static org.jetbrains.kotlin.cli.jvm.compiler.CompatKt.setupIdeaStandaloneExecution;
+
+import com.android.tools.utils.BazelMultiplexWorker;
+import com.intellij.openapi.vfs.VirtualFileSystem;
+import com.intellij.openapi.vfs.impl.ZipHandler;
+import com.intellij.openapi.vfs.impl.jar.CoreJarFileSystem;
+import java.io.PrintStream;
+import java.util.List;
+import org.jetbrains.kotlin.cli.common.ExitCode;
+import org.jetbrains.kotlin.cli.jvm.K2JVMCompiler;
+import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreApplicationEnvironment;
+import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment;
 
 /** A wrapper for the Kotlin compiler. */
 public class KotlinCompiler {
@@ -51,6 +55,30 @@ public class KotlinCompiler {
             // Throw an exception to kill the Bazel persistent worker.
             throw new RuntimeException("the Kotlin compiler encountered an internal error");
         }
+        clearJarCache();
         return exit.getCode();
+    }
+
+    private static void clearJarCache() {
+        // This is adapted from CompileServiceImpl.clearJarCache in the Kotlin compiler daemon.
+        // It is needed because CoreJarFileSystem stores a global cache of jar file central
+        // directories---and this cache can become stale when it is used across multiple
+        // compilations. One would normally expect this sort of cleanup to be handled
+        // by K2JVMCompiler directly through the project disposable---but this is not the case.
+        // Instead, persistent workers must clear the caches manually. For example, in Gradle, you
+        // can see that GradleKotlinCompilerWork.compileWithDaemon invokes daemon.clearJarCache
+        // after each compilation job. Side note: it would make more sense (and improve performance)
+        // to only clear these caches after *all* jobs from a given build session have finished.
+        // However, this does not seem feasible for Bazel persistent workers. And anyway this
+        // optimization is not implemented in Gradle either (there is just a to-do comment).
+        ZipHandler.clearFileAccessorCache();
+        KotlinCoreApplicationEnvironment appEnv =
+                KotlinCoreEnvironment.Companion.getApplicationEnvironment();
+        if (appEnv != null) {
+            VirtualFileSystem jarFileSystem = appEnv.getJarFileSystem();
+            if (jarFileSystem instanceof CoreJarFileSystem) {
+                ((CoreJarFileSystem) jarFileSystem).clearHandlersCache();
+            }
+        }
     }
 }
