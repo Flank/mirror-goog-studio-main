105fab4779798a96923fc12537a394c0b26a9f43
Tom Renn <tomrenn@google.com>
2022-08-08 10:36:08 -0700

    Remove automatic retrying of flaky tests in presubmit
    Bug: 241786763
    Test: n/a
    Change-Id: Iaaf437f0282139370284731ea9d05b02f9accfc3

--- bazel/studio_linux.sh
+++ bazel/studio_linux.sh
@@ -64,23 +64,6 @@ function copy_bazel_artifacts() {(
 )}
 
 ####################################
-# Download a flake retry bazelrc file from GCS.
-# Arguments:
-#   1. File basename on GCS
-#   2. Destination directory
-# Echoes:
-#   If successful, the local path; Otherwise, nothing
-####################################
-function download_flake_retry_rc() {
-  local -r gcs_path="gs://adt-byob/known-flakes/studio-linux/${1}"
-  mkdir -p $2
-  gsutil cp $gcs_path "${2}/${1}"
-  if [[ $? -eq 0 ]]; then
-    echo "${2}/${1}"
-  fi
-}
-
-####################################
 # Generates flag values and runs bazel test.
 # Globals:
 #   ARGV
@@ -141,17 +124,10 @@ function run_bazel_test() {
   # iml_to_build_consistency_test see https://github.com/bazelbuild/bazel/issues/6038
   # This has the side effect of running it twice, but as it only takes a few seconds that seems ok.
   local -r extra_test_flags=(--runs_per_test=//tools/base/bazel:iml_to_build_consistency_test@2)
-  declare -a bazelrc_flags
-  # For presubmit builds, try download bazelrc to deflake builds
-  if [[ $BUILD_TYPE == "PRESUBMIT" && -d "${DIST_DIR}" ]]; then
-    local bazelrc=$(download_flake_retry_rc "auto-retry.bazelrc" "${DIST_DIR}/flake-retry")
-    if [[ $bazelrc ]]; then bazelrc_flags+=("--bazelrc=${bazelrc}"); fi
-  fi
 
   # Run Bazel
   "${SCRIPT_DIR}/bazel" \
     --max_idle_secs=60 \
-    "${bazelrc_flags[@]}" \
     test \
     ${CONFIG_OPTIONS} --config=ants \
     --worker_max_instances=${worker_instances} \
