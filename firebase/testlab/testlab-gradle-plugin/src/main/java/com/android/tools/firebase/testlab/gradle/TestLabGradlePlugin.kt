5d5a7cf99a52ec40a65d5ff8d3833f017435e47f
Yuki Hamada <hummer@google.com>
2022-08-31 13:31:09 -0700

    Create directory and module for FTL-GMD plugin
    Bug: 243701221
    Test: tested manually
    Change-Id: I7b89622b3b9bbd43af8bfd8585ad95a03f0febbe

---
+++ firebase/testlab/testlab-gradle-plugin/src/main/java/com/android/tools/firebase/testlab/gradle/TestLabGradlePlugin.kt
@@ -0,0 +1,13 @@
+package com.android.tools.firebase.testlab.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * An entry point for Firebase Test Lab Gradle plugin that extends Gradle Managed Device
+ * and adds support for Firebase Test Lab devices.
+ */
+class TestLabGradlePlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+    }
+}
