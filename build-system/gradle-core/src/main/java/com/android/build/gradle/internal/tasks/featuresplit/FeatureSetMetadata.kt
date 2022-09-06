2417d577c4c76e16be9dfc26d0b9e7f59bb7a38f
Chris Warrington <cmw@google.com>
2022-09-06 18:16:02 +0100

    Convert feature set metadata to kotlin
    In preparation for refactoring. No functional changes.
    
    Bug: 243502800
    Test: Existing tests
    Change-Id: I3d0d3de0395d458db6bb303dc3c2b5163877b44d

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/tasks/featuresplit/FeatureSetMetadata.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/tasks/featuresplit/FeatureSetMetadata.kt
@@ -13,155 +13,117 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.android.build.gradle.internal.tasks.featuresplit
 
-package com.android.build.gradle.internal.tasks.featuresplit;
+import com.android.sdklib.AndroidVersion
+import com.google.common.annotations.VisibleForTesting
+import com.google.common.base.Charsets
+import com.google.common.collect.ImmutableMap
+import com.google.common.collect.ImmutableSet
+import com.google.common.io.Files
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import java.io.File
+import java.io.FileReader
+import java.io.IOException
 
+/** Container for all the feature split metadata.  */
+class FeatureSetMetadata private constructor(
+        val sourceFile: File?,
+        private val featureSplits: MutableSet<FeatureInfo>,
+        private val maxNumberOfSplitsBeforeO: Int,
+) {
 
-import static com.google.common.collect.ImmutableMap.toImmutableMap;
+    constructor(maxNumberOfSplitsBeforeO: Int) : this(
+            sourceFile = null,
+            featureSplits = HashSet(),
+            maxNumberOfSplitsBeforeO = maxNumberOfSplitsBeforeO,
+    )
 
-import com.android.annotations.NonNull;
-import com.android.annotations.Nullable;
-import com.android.sdklib.AndroidVersion;
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Charsets;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.io.Files;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.lang.reflect.Type;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
+    private constructor(featureSplits: Set<FeatureInfo>, sourceFile: File) : this(
+            maxNumberOfSplitsBeforeO = Integer.max(MAX_NUMBER_OF_SPLITS_BEFORE_O,
+                    featureSplits.size),
+            featureSplits = ImmutableSet.copyOf(featureSplits),
+            sourceFile = sourceFile,
+    )
 
-/** Container for all the feature split metadata. */
-public class FeatureSetMetadata {
-
-    public static final Integer MAX_NUMBER_OF_SPLITS_BEFORE_O = 50;
-    public static final Integer MAX_NUMBER_OF_SPLITS_STARTING_IN_O = 127;
-
-    @VisibleForTesting static final String OUTPUT_FILE_NAME = "feature-metadata.json";
-    /** Base module or application module resource ID */
-    @VisibleForTesting public static final int BASE_ID = 0x7F;
-
-    private final File sourceFile;
-    private final Set<FeatureInfo> featureSplits;
-    private final Integer maxNumberOfSplitsBeforeO;
-
-    public FeatureSetMetadata(Integer maxNumberOfSplitsBeforeO) {
-        this.maxNumberOfSplitsBeforeO = maxNumberOfSplitsBeforeO;
-        featureSplits = new HashSet<>();
-        sourceFile = null;
-    }
-
-    private FeatureSetMetadata(@NonNull Set<FeatureInfo> featureSplits, @NonNull File sourceFile) {
-        this.maxNumberOfSplitsBeforeO =
-                Integer.max(MAX_NUMBER_OF_SPLITS_BEFORE_O, featureSplits.size());
-        this.featureSplits = ImmutableSet.copyOf(featureSplits);
-        this.sourceFile = sourceFile;
-    }
-
-    public File getSourceFile() {
-        return sourceFile;
-    }
-
-    public void addFeatureSplit(
-            int minSdkVersion,
-            @NonNull String modulePath,
-            @NonNull String featureName,
-            @NonNull String packageName) {
-
-        int id;
-        if (minSdkVersion < AndroidVersion.VersionCodes.O) {
-            if (featureSplits.size() >= maxNumberOfSplitsBeforeO) {
-                throw new RuntimeException(
-                        "You have reached the maximum number of feature splits : "
-                                + maxNumberOfSplitsBeforeO);
+    fun addFeatureSplit(
+            minSdkVersion: Int,
+            modulePath: String,
+            featureName: String,
+            packageName: String) {
+        val id: Int = if (minSdkVersion < AndroidVersion.VersionCodes.O) {
+            if (featureSplits.size >= maxNumberOfSplitsBeforeO) {
+                throw RuntimeException("You have reached the maximum number of feature splits : "
+                        + maxNumberOfSplitsBeforeO)
             }
             // allocate split ID backwards excluding BASE_ID.
-            id = BASE_ID - 1 - featureSplits.size();
+            BASE_ID - 1 - featureSplits.size
         } else {
-            if (featureSplits.size() >= MAX_NUMBER_OF_SPLITS_STARTING_IN_O) {
-                throw new RuntimeException(
-                        "You have reached the maximum number of feature splits : "
-                                + MAX_NUMBER_OF_SPLITS_STARTING_IN_O);
+            if (featureSplits.size >= MAX_NUMBER_OF_SPLITS_STARTING_IN_O) {
+                throw RuntimeException("You have reached the maximum number of feature splits : "
+                        + MAX_NUMBER_OF_SPLITS_STARTING_IN_O)
             }
             // allocated forward excluding BASE_ID
-            id = BASE_ID + 1 + featureSplits.size();
+            BASE_ID + 1 + featureSplits.size
         }
-
-        featureSplits.add(new FeatureInfo(modulePath, featureName, id, packageName));
+        featureSplits.add(FeatureInfo(modulePath, featureName, id, packageName))
     }
 
-    @Nullable
-    public Integer getResOffsetFor(@NonNull String modulePath) {
-        Optional<FeatureInfo> featureInfo =
-                featureSplits
-                        .stream()
-                        .filter(metadata -> metadata.modulePath.equals(modulePath))
-                        .findFirst();
-        return featureInfo.isPresent() ? featureInfo.get().resOffset : null;
+    fun getResOffsetFor(modulePath: String): Int? {
+        return featureSplits.firstOrNull { it.modulePath == modulePath }?.resOffset
     }
 
-    @Nullable
-    public String getFeatureNameFor(@NonNull String modulePath) {
-        Optional<FeatureInfo> featureInfo =
-                featureSplits
-                        .stream()
-                        .filter(metadata -> metadata.modulePath.equals(modulePath))
-                        .findFirst();
-        return featureInfo.isPresent() ? featureInfo.get().featureName : null;
+    fun getFeatureNameFor(modulePath: String): String? {
+        return featureSplits.firstOrNull { it.modulePath == modulePath }?.featureName
     }
 
-    @NonNull
-    public Map<String, String> getFeatureNameToNamespaceMap() {
-        return featureSplits.stream()
-                .collect(toImmutableMap(info -> info.featureName, info -> info.namespace));
+    val featureNameToNamespaceMap: Map<String, String>
+        get() = ImmutableMap.copyOf(featureSplits.associateBy({ it.featureName }) { it.namespace })
+
+    @Throws(IOException::class)
+    fun save(outputFile: File) {
+        val gsonBuilder = GsonBuilder()
+        val gson = gsonBuilder.create()
+        Files.asCharSink(outputFile, Charsets.UTF_8).write(gson.toJson(featureSplits))
     }
 
-    public void save(@NonNull File outputFile) throws IOException {
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        Gson gson = gsonBuilder.create();
-        Files.asCharSink(outputFile, Charsets.UTF_8).write(gson.toJson(featureSplits));
-    }
+    private class FeatureInfo(
+            val modulePath: String,
+            val featureName: String,
+            val resOffset: Int,
+            val namespace: String,
+    )
 
-    /**
-     * Loads the feature set metadata file
-     *
-     * @param input the location of the file, or the folder that contains it.
-     * @return the FeatureSetMetadata instance that contains all the data from the file
-     * @throws IOException if the loading failed.
-     */
-    @NonNull
-    public static FeatureSetMetadata load(@NonNull File input) throws IOException {
-        if (input.isDirectory()) {
-            input = new File(input, OUTPUT_FILE_NAME);
-        }
+    companion object {
 
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        Gson gson = gsonBuilder.create();
-        Type typeToken = new TypeToken<HashSet<FeatureInfo>>() {}.getType();
-        try (FileReader fileReader = new FileReader(input)) {
-            Set<FeatureInfo> featureIds = gson.fromJson(fileReader, typeToken);
-            return new FeatureSetMetadata(featureIds, input);
-        }
-    }
+        const val MAX_NUMBER_OF_SPLITS_BEFORE_O = 50
+        const val MAX_NUMBER_OF_SPLITS_STARTING_IN_O = 127
 
-    private static class FeatureInfo {
-        final String modulePath;
-        final String featureName;
-        final int resOffset;
-        final String namespace;
+        @VisibleForTesting
+        const val OUTPUT_FILE_NAME = "feature-metadata.json"
 
-        FeatureInfo(String modulePath, String featureName, int resOffset, String namespace) {
-            this.modulePath = modulePath;
-            this.featureName = featureName;
-            this.resOffset = resOffset;
-            this.namespace = namespace;
+        /** Base module or application module resource ID  */
+        @VisibleForTesting
+        const val BASE_ID = 0x7F
+
+        /**
+         * Loads the feature set metadata file
+         *
+         * @param input the location of the file, or the folder that contains it.
+         * @return the FeatureSetMetadata instance that contains all the data from the file
+         * @throws IOException if the loading failed.
+         */
+        @JvmStatic
+        @Throws(IOException::class)
+        fun load(input: File): FeatureSetMetadata {
+            val inputFile = if (input.isDirectory) File(input, OUTPUT_FILE_NAME) else input
+            val gson = GsonBuilder().create()
+            val typeToken = object : TypeToken<HashSet<FeatureInfo?>?>() {}.type
+            FileReader(inputFile).use { fileReader ->
+                val featureIds = gson.fromJson<Set<FeatureInfo>>(fileReader, typeToken)
+                return FeatureSetMetadata(featureIds, inputFile)
+            }
         }
     }
 }
