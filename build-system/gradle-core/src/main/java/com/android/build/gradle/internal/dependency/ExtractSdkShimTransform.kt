d1a57bb83e12e5b7d326e7cf889b8b7c6d69f20e
Luke Edgar <lukeedgar@google.com>
2022-09-20 12:39:46 +0100

    Add java compilation for apigenerator ouputs
    Some sources produced by the sandbox apigenerator are Java sources, as
    they are produced by the aidl compiler, therefore Java compilation must
    also be supported in the ExtractSdkShimTransform.
    
    Bug: 246314848
    Test: Tested manually
    Change-Id: I2208df88a339ee7ff1b17153ee93ff4828eb904f

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/dependency/ExtractSdkShimTransform.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/dependency/ExtractSdkShimTransform.kt
@@ -16,39 +16,48 @@
 
 package com.android.build.gradle.internal.dependency
 
+import com.android.SdkConstants
 import com.android.build.gradle.internal.BuildToolsExecutableInput
-import com.android.ide.common.process.BaseProcessOutputHandler
-import com.android.ide.common.process.CachedProcessOutputHandler
-import com.android.ide.common.process.DefaultProcessExecutor
-import com.android.ide.common.process.ProcessInfoBuilder
 import com.android.utils.FileUtils
-import com.android.utils.LineCollector
-import com.android.utils.StdLogger
+import com.google.common.collect.ImmutableList
 import org.gradle.api.artifacts.transform.InputArtifact
 import org.gradle.api.artifacts.transform.TransformAction
 import org.gradle.api.artifacts.transform.TransformOutputs
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.FileSystemLocation
+import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.Nested
 import org.gradle.api.tasks.PathSensitive
 import org.gradle.api.tasks.PathSensitivity
 import org.gradle.process.ExecOperations
 import org.gradle.work.DisableCachingByDefault
+import java.io.BufferedOutputStream
 import java.io.File
 import java.io.IOException
 import java.net.URL
 import java.net.URLClassLoader
 import java.nio.file.Files
 import java.nio.file.Path
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
 import javax.inject.Inject
+import javax.tools.ToolProvider
 import kotlin.io.path.extension
+import kotlin.io.path.inputStream
+import kotlin.io.path.isDirectory
 import kotlin.io.path.pathString
 import kotlin.streams.toList
 
+/*
+* Invokes the specified library intended to generate client SDK shim code for interacting with a
+* privacy-sandbox-sdk. The SDK shim code generator library will receive the classes from the
+* sdk-interface-descriptors.jar from an ASAR as input.
+*/
 @DisableCachingByDefault
-abstract class ExtractSdkShimTransform: TransformAction<ExtractSdkShimTransform.Parameters> {
+abstract class ExtractSdkShimTransform : TransformAction<ExtractSdkShimTransform.Parameters> {
 
     interface Parameters: GenericTransformParameters {
 
@@ -82,58 +91,88 @@ abstract class ExtractSdkShimTransform: TransformAction<ExtractSdkShimTransform.
 
     override fun transform(transformOutputs: TransformOutputs) {
         val sdkInterfaceDescriptorJar = inputArtifact.get().asFile
-        val output =
-                transformOutputs.file("${sdkInterfaceDescriptorJar.nameWithoutExtension}-generated.jar")
+        val output = transformOutputs.file("sdk-shim-generated.jar")
+        val aidlExecutable = parameters.buildTools.aidlExecutableProvider().get().absoluteFile
+        val totalClasspath =
+                parameters.bootstrapClasspath.files + parameters.runtimeDependencies.files
+        val totalClasspathStr = totalClasspath.joinToString(File.pathSeparator) { it.path }
+        val apiGeneratorJarsFiles = parameters.apiGeneratorAndRuntimeDependenciesJars.files
+                ?: error("No library has been specified for client SDK shim generation.")
+        val apiGeneratorUrls: Array<URL> =
+                apiGeneratorJarsFiles.mapNotNull { it.toURI().toURL() }.toTypedArray()
         val tempDirForApiGeneratorOutputs = Files.createTempDirectory("extract-shim-transform")
+        if (!sdkInterfaceDescriptorJar.isFile) {
+            throw IOException("${sdkInterfaceDescriptorJar.absolutePath} must be a file.")
+        }
         try {
-            if (!sdkInterfaceDescriptorJar.isFile) {
-                throw IOException("${sdkInterfaceDescriptorJar.absolutePath} must be a file.")
-            }
+            val kotlinOutDir = File(tempDirForApiGeneratorOutputs.toFile(), "kotlin-compiled")
+            val javaOutDir = File(tempDirForApiGeneratorOutputs.toFile(), "java-compiled")
 
-            val aidlExecutable = parameters.buildTools.aidlExecutableProvider().get().absoluteFile
-
-            val apiGeneratorJarsFiles = parameters.apiGeneratorAndRuntimeDependenciesJars.files
-                    ?: error("No jar files founds for dependencies of apigenerator")
-            val apiGeneratorUrls: Array<URL> =
-                    apiGeneratorJarsFiles.mapNotNull { it.toURI().toURL() }.toTypedArray()
             URLClassLoader(apiGeneratorUrls).use {
+                // sandbox-apigenerator is invoked by reflection to avoid handling versioning.
                 Generator(it).generate(
                         sdkInterfaceDescriptors = sdkInterfaceDescriptorJar.toPath(),
                         aidlCompiler = aidlExecutable.toPath(),
                         outputDirectory = tempDirForApiGeneratorOutputs)
             }
 
-            val totalClasspath =
-                    parameters.bootstrapClasspath.files + parameters.runtimeDependencies.files
-
             val generatedFiles: List<Path>
             Files.walk(tempDirForApiGeneratorOutputs).use { stream ->
                 generatedFiles = stream.filter { Files.isRegularFile(it) }.toList()
             }
-            execOperations.javaexec { spec ->
-                spec.mainClass.set("org.jetbrains.kotlin.cli.jvm.K2JVMCompilerKt")
-                spec.classpath(parameters.kotlinCompiler)
-                spec.args = listOf(
-                        "-no-jdk",
-                        "-no-reflect") + generatedFiles.map { it.pathString } + listOf(
-                        "-classpath", totalClasspath.joinToString(File.pathSeparator) { it.path },
-                        "-d=${output.path}")
+
+            // Java sources are produced by the apigenerator invoking the aidl compiler, therefore
+            // java sources do not reference Kotlin sources.
+            compileKotlin(generatedFiles, totalClasspathStr, kotlinOutDir)
+            compileJava(generatedFiles, totalClasspathStr, javaOutDir)
+
+            ZipOutputStream(BufferedOutputStream(output.outputStream())).use { outJar ->
+                writeCompiledClassesToZip(kotlinOutDir, outJar)
+                writeCompiledClassesToZip(javaOutDir, outJar)
             }
         } finally {
             FileUtils.cleanOutputDir(tempDirForApiGeneratorOutputs.toFile())
         }
     }
 
+    private fun compileJava(generatedFiles: List<Path>, totalClasspathStr: String, javaOutDir: File) {
+        val javac = ToolProvider.getSystemJavaCompiler()
+        val manager = javac.getStandardFileManager(null, null, null)
+        val javaSourceClasspaths =
+                generatedFiles.filter { it.extension == SdkConstants.EXT_JAVA }
+        val source =
+                manager.getJavaFileObjectsFromFiles(javaSourceClasspaths.map { it.toFile() })
+        javac.getTask(null,
+                manager, null,
+                ImmutableList.of(
+                        "-classpath", totalClasspathStr,
+                        "-d", javaOutDir.absolutePath), null,
+                source
+        ).call()
+    }
+
+    private fun compileKotlin(generatedFiles: List<Path>, totalClasspathStr: String, kotlinOutDir: File) {
+        execOperations.javaexec { spec ->
+            spec.mainClass.set("org.jetbrains.kotlin.cli.jvm.K2JVMCompilerKt")
+            spec.classpath(parameters.kotlinCompiler)
+            spec.args = listOf(
+                    "-no-jdk",
+                    "-no-reflect") + generatedFiles.map { it.pathString } + listOf(
+                    "-classpath", totalClasspathStr,
+                    "-d", kotlinOutDir.path)
+        }
+    }
+
+    /* For invoking the sandbox-apigenerator via reflection. */
     class Generator(val classLoader: URLClassLoader) {
         private val apiGeneratorPackage = "androidx.privacysandbox.tools.apigenerator"
         private val privacySandboxApiGeneratorClass =
-                loadClass("$apiGeneratorPackage.PrivacySandboxApiGenerator")
+                classLoader.loadClass("$apiGeneratorPackage.PrivacySandboxApiGenerator")
         private val privacySandboxSdkGenerator = privacySandboxApiGeneratorClass
                 .getConstructor()
                 .newInstance()
         private val generateMethod = privacySandboxApiGeneratorClass
                 .getMethod("generate", Path::class.java, Path::class.java, Path::class.java)
-
         fun generate(
                 sdkInterfaceDescriptors: Path,
                 aidlCompiler: Path,
@@ -141,7 +180,17 @@ abstract class ExtractSdkShimTransform: TransformAction<ExtractSdkShimTransform.
             generateMethod.invoke(privacySandboxSdkGenerator,
                             sdkInterfaceDescriptors, aidlCompiler, outputDirectory)
         }
-        private fun loadClass(classToLoad: String) = classLoader.loadClass(classToLoad)
+    }
+
+    private fun writeCompiledClassesToZip(classesDir: File, outJar: ZipOutputStream) {
+        Files.walk(classesDir.toPath()).filter { Files.isRegularFile(it) }.forEach {
+            val entry = ZipEntry(it.toFile().relativeTo(classesDir).invariantSeparatorsPath)
+            outJar.putNextEntry(entry)
+            it.inputStream().use { inputStream ->
+                outJar.write(inputStream.readBytes())
+            }
+            outJar.closeEntry()
+        }
     }
 }
 
